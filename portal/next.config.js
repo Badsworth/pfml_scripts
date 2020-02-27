5e2376c64017b5476a33e41a9574a82fbf2fe583
Minghua Sun <clairesunstudio@gmail.com>
2020-02-27 18:52:32 -0500

    Mayflower tokens integration (#50)
    * wip
    
    * use import
    
    * use mayflower tokens via import
    
    * format vars
    
    * wip
    
    * update to new package
    
    * update path
    
    * link fonts from mayflower package
    
    * use includedPath
    
    * remove unused
    
    * upgrade to new package to access new vars
    
    * set vivid colors to false
    
    * set assets-path from public folder
    
    * link scss
    
    * upgrade packge
    
    * Revert "link scss"
    
    This reverts commit 5f2f28b1abd5207c0a4d684ac31ec5fae72e0570.
    
    * config for static fonts
    
    * remove public fonts
    
    * add doc
    
    * address review feedback
    
    * address review feedback
    
    * update inline comment

--- portal/next.config.js
+++ portal/next.config.js
@@ -5,6 +5,7 @@ const resolve = require("resolve");
 const withCSS = require("@zeit/next-css");
 // Portal uses Sass (and we import the U.S. Web Design System's Sass)
 const withSass = require("@zeit/next-sass");
+const withFonts = require("next-fonts");
 
 const buildEnv = process.env.BUILD_ENV || "dev";
 const envVariables = require("./../infra/portal_config/" + buildEnv);
@@ -12,49 +13,54 @@ const envVariables = require("./../infra/portal_config/" + buildEnv);
 global.navigator = () => null;
 
 module.exports = withSass(
-  withCSS({
-    cssLoaderOptions: {
-      url: false // Disable url() resolving and let it do what CSS normally does. Fixes USWDS usage.
-    },
-    env: {
-      ...envVariables
-    },
-    exportTrailingSlash: true,
-    exportPathMap: async function() {
-      const paths = {
-        "/": { page: "/" }
-      };
-      return paths;
-    },
-    sassLoaderOptions: {
-      includePaths: [path.resolve(__dirname, "node_modules/uswds/dist/scss")]
-    },
-    webpack(config, options) {
-      const { dir, isServer } = options;
-      config.externals = [];
-      // This modifies what is excluded by Webpack in output bundles to resolve conflicts with @aws-amplify CSS files.
-      if (isServer) {
-        config.externals.push((context, request, callback) => {
-          resolve(
-            request,
-            { basedir: dir, preserveSymlinks: true },
-            (err, res) => {
-              if (err) {
-                return callback();
+  withCSS(
+    // imported fonts are copied into .next/static/chunks/fonts
+    // See https://github.com/rohanray/next-fonts/blob/master/index.js
+    withFonts({
+      enableSvg: true,
+      env: {
+        ...envVariables
+      },
+      exportTrailingSlash: true,
+      exportPathMap: async function() {
+        const paths = {
+          "/": { page: "/" }
+        };
+        return paths;
+      },
+      sassLoaderOptions: {
+        includePaths: [
+          path.resolve(__dirname, "node_modules/uswds/dist/scss"),
+          path.resolve(__dirname, "node_modules")
+        ]
+      },
+      webpack(config, options) {
+        const { dir, isServer } = options;
+        config.externals = [];
+        // This modifies what is excluded by Webpack in output bundles to resolve conflicts with @aws-amplify CSS files.
+        if (isServer) {
+          config.externals.push((context, request, callback) => {
+            resolve(
+              request,
+              { basedir: dir, preserveSymlinks: true },
+              (err, res) => {
+                if (err) {
+                  return callback();
+                }
+                if (
+                  res.match(/node_modules[/\\].*\.css/) &&
+                  !res.match(/node_modules[/\\]webpack/) &&
+                  !res.match(/node_modules[/\\]@aws-amplify/)
+                ) {
+                  return callback(null, `commonjs ${request}`);
+                }
+                callback();
               }
-              if (
-                res.match(/node_modules[/\\].*\.css/) &&
-                !res.match(/node_modules[/\\]webpack/) &&
-                !res.match(/node_modules[/\\]@aws-amplify/)
-              ) {
-                return callback(null, `commonjs ${request}`);
-              }
-              callback();
-            }
-          );
-        });
+            );
+          });
+        }
+        return config;
       }
-      return config;
-    }
-  })
+    })
+  )
 );
