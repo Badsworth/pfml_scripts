b4b501579951ff1ecc8a25243080a013e91a389d
Sawyer Hollenshead <git@sawyerh.com>
2020-03-02 10:51:54 -0500

    Update U.S. Web Design System to 2.5.0 (#53)
    - Updated USWDS to latest release
    - Remove `postcss-sort-media-queries` dependency, which USWDS no longer requires
    - Enabled themed state colors (error, warning, success, etc), which is now supported in the USWDS [v2.5.0](https://github.com/uswds/uswds/releases/tag/v2.5.0)

--- portal/postcss.config.js
+++ portal/postcss.config.js
@@ -4,9 +4,7 @@
  */
 // Apply vendor prefixes for modern CSS syntax. Depends on browserslist file to determine target browsers.
 const autoprefixer = require("autoprefixer");
-// postcss-sort-media-queries is required for USWDS utility classes to have the correct specificity (as of v2.4.0)
-const sortMediaQueries = require("postcss-sort-media-queries");
 
 module.exports = {
-  plugins: [sortMediaQueries({ sort: "mobile-first" }), autoprefixer]
+  plugins: [autoprefixer]
 };
