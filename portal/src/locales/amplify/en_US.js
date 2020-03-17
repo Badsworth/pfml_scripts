c4415afc1c7c2ed6e6eab68d49a8d19c31216cbe
A Lmx <9326892+a-lmx@users.noreply.github.com>
2020-03-17 10:58:49 -0700

    Update more auth screen strings (#92)
    ## 🎫 Ticket
    
    [CP-134](https://lwd.atlassian.net/browse/CP-134)
    
    ## 🛠 Changes
    
    This updates strings on the Forgot Password, Create Account, and Create New Password pages.
    
    ## ℹ️ Context for reviewers
    
    This change is not comprehensive, more strings may need to be updated. Pushing this change will trigger an update, which will push new config changes needed to update the user pool settings.

--- portal/src/locales/amplify/en_US.js
+++ portal/src/locales/amplify/en_US.js
@@ -7,14 +7,23 @@
 
 const en_US = {
   // Note: to override a string, replace it with white space -- an empty string does not override the original.
+  "Back to Sign In": "Back to Log In",
+  "Confirm Sign Up": "Verify your email address",
+  "Create Account": "Create account",
+  "Create a new account": "Create an account",
   "Create account": "create an account",
+  Email: "Email address",
   "Enter your password": " ",
   "Enter your username": " ",
   "Forget your password? ": " ",
+  "Have an account? ": "Or ",
   "No account? ": "Or ",
   Password: "Password",
   "Reset password": "Forgot your password?",
+  "Reset your password": "Forgot your password?",
+  "Send Code": "Submit",
   "Sign In": "Log in",
+  "Sign in": "log in",
   "Sign in to your account": "Log in to get started",
   Username: "Email address",
 };
