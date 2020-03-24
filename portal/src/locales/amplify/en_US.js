5fdbfa6a6f24220b2f480a15ca2194abfe1a4e97
A Lmx <9326892+a-lmx@users.noreply.github.com>
2020-03-24 13:57:58 -0700

    Further refine messages on default auth screens (#108)
    ## 🎫 Ticket
    
    [CP-134](https://lwd.atlassian.net/browse/CP-134)
    
    ## 🛠 Changes
    
    - Updated messages per discussion w/ design.
    - Lowercased non-initial words.
    - Updated button text

--- portal/src/locales/amplify/en_US.js
+++ portal/src/locales/amplify/en_US.js
@@ -7,8 +7,10 @@
 
 const en_US = {
   // Note: to override a string, replace it with white space -- an empty string does not override the original.
-  "Back to Sign In": "Back to Log In",
+  "Back to Sign In": "Back to log in",
+  Confirm: "Submit",
   "Confirm Sign Up": "Verify your email address",
+  "Confirmation Code": "Confirmation code",
   "Create Account": "Create account",
   "Create a new account": "Create an account",
   "Create account": "create an account",
@@ -17,8 +19,10 @@ const en_US = {
   "Enter your username": " ",
   "Forget your password? ": " ",
   "Have an account? ": "Or ",
+  "New Password": "New password",
   "No account? ": "Or ",
   Password: "Password",
+  "Resend Code": "Resend code",
   "Reset password": "Forgot your password?",
   "Reset your password": "Forgot your password?",
   "Send Code": "Submit",
