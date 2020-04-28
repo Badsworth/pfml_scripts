bce3bfb5e3447ec80f37a832050d582811911853
James Bursa <james@navapbc.com>
2020-04-28 10:25:28 -0400

    API-101: add security group for RDS instances (#261)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/API-101
    
    ## 🛠 Changes
    
    - Add rds_postgresql security group that allows incoming connections to the PostgreSQL port.
    
    ## ℹ️ Context for reviewers
    
    The default VPC group allows no ingress so the connection to the database currently fails. This will add a dedicated security group for the database.
    
    This group is created using the standalone "aws_security_group_rule" approach as using inline groups will cause a cyclic dependency error:
    
    ```
    Error: Cycle: module.api.aws_security_group.app, module.api.aws_security_group.rds_postgresql
    ```
    
    Follow ups:
    - #267 use the security group created here.
    - #270 allow database connections from the app group (matches this change but from the app's point of view).
    
    ## 👀 Demo
    
    ```terraform
      # module.api.aws_security_group.rds_postgresql will be created
      + resource "aws_security_group" "rds_postgresql" {
          + arn                    = (known after apply)
          + description            = "Access for Pfml-Api RDS database"
          + egress                 = (known after apply)
          + id                     = (known after apply)
          + ingress                = (known after apply)
          + name                   = "pfml-api-rds-test"
          + owner_id               = (known after apply)
          + revoke_rules_on_delete = false
          + vpc_id                 = "vpc-097793bd468be1c65"
        }
    
      # module.api.aws_security_group_rule.rds_postgresql_egress_none will be created
      + resource "aws_security_group_rule" "rds_postgresql_egress_none" {
          + cidr_blocks              = [
              + "127.0.0.1/32",
            ]
          + description              = "Deny all egress"
          + from_port                = 0
          + id                       = (known after apply)
          + protocol                 = "tcp"
          + security_group_id        = (known after apply)
          + self                     = false
          + source_security_group_id = (known after apply)
          + to_port                  = 0
          + type                     = "egress"
        }
    
      # module.api.aws_security_group_rule.rds_postgresql_ingress_app will be created
      + resource "aws_security_group_rule" "rds_postgresql_ingress_app" {
          + description              = "PostgreSQL from ECS applications"
          + from_port                = 5432
          + id                       = (known after apply)
          + protocol                 = "tcp"
          + security_group_id        = (known after apply)
          + self                     = false
          + source_security_group_id = "sg-019e1a09af8693432"
          + to_port                  = 5432
          + type                     = "ingress"
        }
    ```
    
    Reviewers: @kyeah

--- infra/api/template/security_groups.tf
+++ infra/api/template/security_groups.tf
@@ -45,3 +45,41 @@ resource "aws_security_group" "app" {
     protocol    = "udp"
   }
 }
+
+# Security group for the RDS instance.
+#
+# Note: this group uses the standalone aws_security_group_rule format instead of the inline format, to avoid a
+# cyclic dependency between this group and aws_security_group.app.
+resource "aws_security_group" "rds_postgresql" {
+  name        = "${local.app_name}-rds-${var.environment_name}"
+  description = "Access for ${title(local.app_name)} RDS database"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Disallow outbound network connections.
+# We can't explicitly deny outbound access, but can override the default allow-all egress rule by making a more
+# limited one.
+resource "aws_security_group_rule" "rds_postgresql_egress_none" {
+  type              = "egress"
+  description       = "Deny all egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "tcp"
+  cidr_blocks       = ["127.0.0.1/32"]
+  security_group_id = aws_security_group.rds_postgresql.id
+}
+
+# Allow access from the ECS applications.
+resource "aws_security_group_rule" "rds_postgresql_ingress_app" {
+  type                     = "ingress"
+  description              = "PostgreSQL from ECS applications"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.app.id
+  security_group_id        = aws_security_group.rds_postgresql.id
+}
