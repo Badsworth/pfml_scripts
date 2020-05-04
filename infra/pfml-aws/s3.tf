f1702160cc2e0c6fe8df5e3ffc247e53097d605f
Kevin Yeh <kevinyeah@utexas.edu>
2020-05-04 13:30:24 -0400

    API-9: Create IAM users and S3 buckets for DOR (#232)
    * Add DOR user and S3 bucket for testing
    
    * block public acls
    
    * things i changed manually for testing
    
    * cleanup
    
    * Rename file
    
    * Create global agency transfer buckets
    
    * Fix typo

--- infra/pfml-aws/s3.tf
+++ infra/pfml-aws/s3.tf
@@ -47,3 +47,54 @@ resource "aws_s3_bucket_public_access_block" "terraform_block_public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# Create S3 buckets to receive agency data.
+#
+# Note that we're creating more buckets than agencies will be interacting with.
+# Here, we're creating one for each environment, as well as one "nonprod" bucket
+# for data will be sent ad-hoc for initial verification and whenever we need to
+# verify format changes.
+#
+# Since agencies like DOR do not have recurring non-PII test data, we cannot use
+# them as our main data sources for lower environments. Instead, they sent real
+# data to prod and ad-hoc data to nonprod, which we can feed other lower environments.
+#
+# In day-to-day operations, we'll likely be generating our own mock data to feed into
+# lower environments.
+#
+resource "aws_s3_bucket" "agency_transfer" {
+  for_each = toset(concat(local.environments, local.vpcs))
+  bucket   = "massgov-pfml-${each.key}-agency-transfer"
+  acl      = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    agency        = "eol"
+    application   = "coreinf"
+    businessowner = "kevin.yeh@mass.gov"
+    createdby     = "kevin.yeh@mass.gov"
+    environment   = each.key == "nonprod" ? "stage" : each.key
+    itowner       = "kevin.yeh@mass.gov"
+    public        = "no"
+    secretariat   = "eolwd"
+    Name          = "massgov-pfml-agency-transfer-dor-${each.key}"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "agency_transfer_block_public_access" {
+  for_each = toset(concat(local.environments, local.vpcs))
+  bucket   = aws_s3_bucket.agency_transfer[each.key].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
