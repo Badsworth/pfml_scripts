77da94c32cfadd34d57b7ecc4f0629f0d6ad07db
Oscar A. Ramirez <35975958+oscarAtNava@users.noreply.github.com>
2020-05-05 13:46:17 -0400

    API-150 : Mock endpoints for application creation (#294)
    Added specs in the openapi file for the Applications endpoints. Also created a shell applications Python class to test the mock endpoints. Spec definitions were based from Tech Spec: https://lwd.atlassian.net/wiki/spaces/API/pages/272728098/Submitting+an+Application

---
+++ api/massgov/pfml/api/applications.py
@@ -0,0 +1,71 @@
+import uuid
+
+import connexion
+from werkzeug.exceptions import NotFound
+
+import massgov.pfml.api.generate_fake_data as fake
+
+#
+#   Shell class to be implemented at a later date.
+#
+
+
+def applications_get_all_fake():
+    return fake.applications
+
+
+def application_get(application_id):
+    for application in fake.applications.values():
+        if application.get("application_id") == application_id:
+            return application
+
+    raise NotFound()
+
+
+def applications_get():
+    applications = []
+    for application in fake.applications.values():
+        short_application = {}
+        short_application["application_id"] = application["application_id"]
+        short_application["application_nickname"] = application["application_nickname"]
+        applications.append(short_application)
+
+    return applications
+
+
+def applications_start():
+    return {"application_id": str(uuid.uuid4())}, 201
+
+
+def applications_update(application_id):
+    body = connexion.request.json
+
+    # This is just a contrived error to test the different responses.
+    if body.get("application_id") == application_id:
+        return {"code": "200", "message": "Application updated without errors."}, 200
+    else:
+        return (
+            {
+                "code": "400",
+                "message": "Application has errors",
+                "errors": [
+                    {
+                        "message": "Application ID in body does not match header value.",
+                        "attribute": "application_id",
+                    }
+                ],
+            },
+            400,
+        )
+
+
+def applications_submit(application_id):
+    application_get(application_id)
+    success_response = {
+        "code": "201",
+        "message": "Application completed without errors",
+        "warnings": [],
+        "errors": [],
+    }
+
+    return success_response
--- api/massgov/pfml/api/generate_fake_data.py
+++ api/massgov/pfml/api/generate_fake_data.py
@@ -1,5 +1,6 @@
 import random
 import uuid
+from datetime import timedelta
 
 from faker import Faker
 
@@ -12,6 +13,7 @@ fake = Faker()
 wages = {}
 employees = {}
 employers = {}
+applications = {}
 
 # Period ID possibilities
 q1 = "2019-03-31"
@@ -74,6 +76,171 @@ def create_wages(employee_id, employer_id, period_id=None):
     return wages_object
 
 
+def create_application(employee):
+    application = {}
+    leave_types = ["bonding", "medical", "accident"]
+    occupations = ["Sales Clerk", "Administrative", "Engineer", "Doctor"]
+
+    wage_employers = wages.get(employee.get("employee_id"))
+    employer_id = wage_employers[0]["employer_id"]
+
+    leave_type = random.choice(leave_types)
+
+    application["application_nickname"] = "My " + leave_type + " leave application"
+    application["application_id"] = str(uuid.uuid4())
+    application["employee_id"] = employee["employee_id"]
+    application["employer_id"] = employer_id
+    application["first_name"] = fake.first_name()
+    application["last_name"] = fake.last_name()
+    application["occupation"] = random.choice(occupations)
+
+    application["leave_details"] = create_leave_details(leave_type)
+    application["payment_preferences"] = create_payment_preferences(
+        application.get("first_name"), application.get("last_name")
+    )
+
+    return application
+
+
+def create_leave_details(leave_type):
+    leave_details = {}
+    leave_periods = []
+    leave_period = {}
+    status = ["Known", "Estimated"]
+    employer_notification = ["In Writing", "In Person", "By Telephone", "Other"]
+
+    start_date = fake.date_this_month()
+    end_date = start_date + timedelta(fake.random_int(min=1, max=12, step=1))
+
+    leave_period["start_date"] = start_date
+    leave_period["end_date"] = end_date
+
+    if leave_type.find("bonding") > 0:
+        leave_details["reason"] = "Child Bonding"
+        leave_details["reason_qualifier1"] = "Newborn"
+        leave_period["status"] = random.choice(status)
+        continuous_leave_period = create_continuous_leave_period(leave_period)
+        leave_periods.append(continuous_leave_period)
+        leave_details["continuous_leave_periods"] = leave_periods
+        leave_details["primary_relationship"] = "Parent"
+        leave_details["primary_rel_qualifier1"] = "Biological"
+    elif leave_type.find("medical") > 0:
+        leave_details["reason"] = "Care For A Family Member"
+        leave_details["reason_qualifier1"] = "Serious Health Condition"
+        leave_period["status"] = random.choice(status)
+        reduced_leave_period = create_reduced_leave_period(leave_period)
+        leave_periods.append(reduced_leave_period)
+        leave_details["reduced_schedule_leave_periods"] = leave_periods
+    else:
+        leave_details["reason"] = "Serious Health Condition - Employee"
+        leave_details["reason_qualifier1"] = "Work Related"
+        leave_details["reason_qualifier2"] = "Accident/Injury"
+        intermittent_leave_period = create_intermittent_leave_period(leave_period)
+        leave_periods.append(intermittent_leave_period)
+        leave_details["intermittent_leave_periods"] = leave_periods
+
+    leave_details["employer_notified"] = True
+    leave_details["employer_notification_date"] = start_date - timedelta(
+        fake.random_int(min=1, max=120, step=1)
+    )
+    leave_details["employer_notification_method"] = random.choice(employer_notification)
+
+    return leave_details
+
+
+def create_payment_preferences(first_name, last_name):
+    payment_preferences = []
+    payment_preference = {}
+    payment_types = ["Elec Funds Transfer", "Check"]
+    account_type = ["Checking", "Savings"]
+
+    payment_type = random.choice(payment_types)
+    payment_preference["payment_method"] = payment_type
+    payment_preference["is_default"] = True
+
+    if payment_type == "Elec Funds Transfer":
+        account_details = {
+            "account_name": fake.sentence(),
+            "account_no": str(fake.random_number(digits=12)),
+            "routing_number": str(fake.random_number(digits=9)),
+            "account_type": random.choice(account_type),
+        }
+        payment_preference["account_details"] = account_details
+    else:
+        full_name = first_name + " " + last_name
+        check_details = {"name_to_print_on_check": full_name}
+        payment_preference["cheque_details"] = check_details
+
+    payment_preferences.append(payment_preference)
+
+    return payment_preferences
+
+
+def create_continuous_leave_period(leave_period):
+    # No attempt to determine if previous or next dates are business days.
+    leave_period["last_day_worked"] = leave_period["start_date"] - timedelta(1)
+    leave_period["expected_return_to_work_date"] = leave_period["end_date"] + timedelta(1)
+    leave_period["start_date_full_day"] = True
+    leave_period["start_date_off_hours"] = 0
+    leave_period["start_date_off_minutes"] = 0
+    leave_period["end_date_off_hours"] = 0
+    leave_period["end_date_off_minutes"] = 0
+    leave_period["end_date_full_day"] = True
+
+    return leave_period
+
+
+def create_reduced_leave_period(leave_period):
+    off_hours = fake.random_int(min=1, max=7, step=1)
+    off_minutes = fake.random_int(min=5, max=55, step=5)
+
+    leave_period["thursday_off_hours"] = off_hours
+    leave_period["thursday_off_minutes"] = off_minutes
+    leave_period["friday_off_hours"] = off_hours
+    leave_period["friday_off_minutes"] = off_minutes
+    leave_period["saturday_off_hours"] = off_hours
+    leave_period["saturday_off_minutes"] = off_minutes
+    leave_period["sunday_off_hours"] = off_hours
+    leave_period["sunday_off_minutes"] = off_minutes
+    leave_period["monday_off_hours"] = off_hours
+    leave_period["monday_off_minutes"] = off_minutes
+    leave_period["tuesday_off_hours"] = off_hours
+    leave_period["tuesday_off_minutes"] = off_minutes
+    leave_period["wednesday_off_hours"] = off_hours
+    leave_period["wednesday_off_minutes"] = off_minutes
+
+    return leave_period
+
+
+def create_intermittent_leave_period(leave_period):
+    interval_basis = ["Days", "Weeks", "Months"]
+    duration_choices = ["Minutes", "Hours", "Days"]
+
+    # Random combinations may not yield logical results. There is
+    # an opportunity to fine tune.
+
+    # Occurs <frequency> times
+    leave_period["frequency"] = fake.random_int(min=1, max=10, step=1)
+    # Every <frequency_interval> <frequency_interval_basis
+    leave_period["frequency_interval"] = fake.random_int(min=1, max=5, step=1)
+    leave_period["frequency_interval_basis"] = random.choice(interval_basis)
+
+    # For <duration> <duration_basis>
+    # e.g. Occurs 4 times every 2 weeks for 3 days
+    duration_basis = random.choice(duration_choices)
+
+    if duration_basis == "Minutes":
+        leave_period["duration"] = fake.random_int(min=1, max=60, step=1)
+    elif duration_basis == "Hours":
+        leave_period["duration"] = fake.random_int(min=1, max=24, step=1)
+    else:
+        leave_period["duration"] = fake.random_int(min=1, max=30, step=1)
+
+    leave_period["duration_basis"] = duration_basis
+
+    return leave_period
+
+
 def build_fake_data_dictionaries():
     employer_id_list = []
     # create fake employers
@@ -134,6 +301,11 @@ def build_fake_data_dictionaries():
         fake_wages4 = create_wages(employee_id, employer_id, q4)
 
         wages[employee_id] = [fake_wages, fake_wages2, fake_wages3, fake_wages4]
+
+    for app_employee in employees.values():
+        # Create one application per employee
+        application = create_application(app_employee)
+        applications[application["application_id"]] = application
 
     logger.info(
         "generated employers %i, employees %i, wages %i", len(employers), len(employees), len(wages)
