65331681d7807147ad3092b6e2a25644a8e8e319
A Lmx <9326892+a-lmx@users.noreply.github.com>
2020-06-05 13:38:37 -0700

    Delete unrestrictedClaimFlow flag (#457)
    ## 🎫 Ticket
    
    No ticket. At request of Nicole Budzius, PM for Claimant Portal.
    
    ## 🛠 Changes
    
    This removes the `unrestrictedClaimFlow` flag from the list of feature flags, as it is no longer in use.
    
    ## ℹ️ Context for reviewers
    
    This is the third of three steps:
    1. Flip flag to true in all environments (deployed to prod)
    2. Remove use of flag in code (deployed to prod)
    3. Delete flag from config entirely ◀️ 
    
    Each step can proceed after the previous step has been fully deployed to production.

--- portal/config/featureFlags.js
+++ portal/config/featureFlags.js
@@ -14,10 +14,6 @@ const flagsConfig = {
     // update the reference to it in _app.js), but try to keep it prefixed with pfml.
     // https://lwd.atlassian.net/browse/CP-459
     pfmlTerriyay: false,
-    // When unrestrictedClaimFlow is enabled, a user can proceed through the
-    // full claim flow. This is in place to prevent pilot users from going to
-    // screens that we're not yet ready to pilot.
-    unrestrictedClaimFlow: true,
   },
   // Environments can optionally override a default feature flag below.
   // The environment keys should use the same envName defined in
