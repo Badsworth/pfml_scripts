b0b4fe0a6d17b10944744bc59411bd400c4cfd38
Loren Yu <yuloren@gmail.com>
2020-07-16 19:22:58 -0400

    Add jsDoc to useCollectionState (#672)
    Add JsDoc annotations to useCollectionState for better development

--- portal/src/hooks/useCollectionState.js
+++ portal/src/hooks/useCollectionState.js
@@ -1,23 +1,41 @@
 import { useState } from "react";
 
-/** @typedef {import('../models/BaseCollection')} BaseCollection */
+/** @typedef {import('../models/BaseCollection').default} BaseCollection */
 
 /**
  * React hook for creating a state for a Collection of objects
  * @param {BaseCollection} [initialCollection] - initial collection state
- * @returns {Array} - [collection, setCollection, addItem, updateItem, removeItem];
+ * @returns {{addItem: addItemFunction, collection: BaseCollection, removeItem: removeItemFunction, setCollection: Function, updateItem: updateItemFunction}}
  */
 const useCollectionState = (initialCollection) => {
+  /**
+   * @type {[BaseCollection, Function]}
+   */
   const [collection, setCollection] = useState(initialCollection);
 
+  /**
+   * Function that adds an item to the collection
+   * @callback addItemFunction
+   * @param {BaseModel} item The item to add to the collection
+   */
   const addItem = (item) => {
     setCollection((prevCollection) => prevCollection.addItem(item));
   };
 
+  /**
+   * Function that updates an item within the collection
+   * @callback updateItemFunction
+   * @param {BaseModel} item The item to update
+   */
   const updateItem = (item) => {
     setCollection((prevCollection) => prevCollection.updateItem(item));
   };
 
+  /**
+   * Function that removes an item within the collection
+   * @callback removeItemFunction
+   * @param {string} itemId The id of the item to remove
+   */
   const removeItem = (itemId) => {
     setCollection((prevCollection) => prevCollection.removeItem(itemId));
   };
