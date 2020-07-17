Badsworth/pfml_scripts

commit 246a138172f5c84a87515c3e716cd0fda448bfd8

Initialize appErrors to empty collection (#678)
- Initialize appErrors to empty collection instead of null
- Clear appErrors by setting to empty collection instead of null
- Remove unnecessary null checks
- Allow multiple calls to catchError to not overwrite previous errors

portal/src/components/ErrorsSummary.js
@@ -18,7 +18,7 @@ function ErrorsSummary(props) {
    * Make sure the user sees the errors summary anytime the list of errors changes
    */
   useEffect(() => {
-    if (errors) {
+    if (errors && errors.items.length > 0) {
       window.scrollTo(0, 0);
       // Move focus to the alert so screen readers immediately announce that there are errors
       alertRef.current.focus();

portal/__tests__/components/ErrorsSummary.test.js
@@ -99,6 +99,15 @@ describe("ErrorsSummary", () => {
 
       expect(global.scrollTo).toHaveBeenCalledWith(0, 0);
     });
+
+    it("does not scroll if there are no errors", () => {
+      // Mount the component so useEffect is called
+      const errors = new AppErrorInfoCollection();
+      const mountComponent = true;
+      render({ errors }, mountComponent);
+
+      expect(global.scrollTo).not.toHaveBeenCalled();
+    });
   });
 
   describe("when the errors change", () => {

portal/__tests__/hooks/useAppErrorsLogic.test.js
@@ -21,13 +21,13 @@ describe("useAppErrorsLogic", () => {
     const { setAppErrors, appErrors } = appErrorsLogic;
 
     expect(setAppErrors).toBeInstanceOf(Function);
-    expect(appErrors).toBeNull();
+    expect(appErrors.items).toHaveLength(0);
   });
 
   describe("catchError", () => {
     beforeEach(() => {
       // We expect console.error to be called in this scenario
-      jest.spyOn(console, "error").mockImplementationOnce(jest.fn());
+      jest.spyOn(console, "error").mockImplementation(jest.fn());
     });
 
     it("sets app error with error info and tracks errors", () => {
@@ -37,6 +37,7 @@ describe("useAppErrorsLogic", () => {
 
       expect(appErrorsLogic.appErrors.items[0].type).toEqual("Error");
       expect(tracker.noticeError).toHaveBeenCalledTimes(1);
+      expect(console.error).toHaveBeenCalledTimes(1);
     });
 
     describe("when generic Error is thrown", () => {
@@ -74,6 +75,19 @@ describe("useAppErrorsLogic", () => {
         );
       });
     });
+
+    describe("when multiple errors are caught", () => {
+      it("adds adds all errors to the app errors list", () => {
+        act(() => {
+          appErrorsLogic.catchError(new Error("error 1"));
+          appErrorsLogic.catchError(new Error("error 2"));
+        });
+
+        expect(appErrorsLogic.appErrors.items).toHaveLength(2);
+        expect(tracker.noticeError).toHaveBeenCalledTimes(2);
+        expect(console.error).toHaveBeenCalledTimes(2);
+      });
+    });
   });
 
   describe("clearErrors", () => {
@@ -88,7 +102,7 @@ describe("useAppErrorsLogic", () => {
         appErrorsLogic.clearErrors();
       });
 
-      expect(appErrorsLogic.appErrors).toBeNull();
+      expect(appErrorsLogic.appErrors.items).toHaveLength(0);
     });
   });
 });

portal/__tests__/hooks/useAppLogic.test.js
@@ -1,3 +1,4 @@
+import AppErrorInfoCollection from "../../src/models/AppErrorInfoCollection";
 import { testHook } from "../test-utils";
 import useAppLogic from "../../src/hooks/useAppLogic";
 
@@ -39,7 +40,8 @@ describe("useAppLogic", () => {
       } = useAppLogic());
     });
 
-    expect(appErrors).toBeNull();
+    expect(appErrors).toBeInstanceOf(AppErrorInfoCollection);
+    expect(appErrors.items).toHaveLength(0);
     expect(auth).toEqual(expect.anything());
     expect(claims).toBeNull();
     expect(clearErrors).toBeInstanceOf(Function);

portal/__tests__/hooks/useAuthLogic.test.js
@@ -137,7 +137,7 @@ describe("useAuthLogic", () => {
         setAppErrors([{ message: "Pre-existing error" }]);
         await forgotPassword(username);
       });
-      expect(appErrors).toBeNull();
+      expect(appErrors.items).toHaveLength(0);
     });
   });
 
@@ -252,7 +252,7 @@ describe("useAuthLogic", () => {
         setAppErrors([{ message: "Pre-existing error" }]);
         login(username, password);
       });
-      expect(appErrors).toBeNull();
+      expect(appErrors.items).toHaveLength(0);
     });
   });
 
@@ -406,7 +406,7 @@ describe("useAuthLogic", () => {
         setAppErrors([{ message: "Pre-existing error" }]);
         await createAccount(username, password);
       });
-      expect(appErrors).toEqual(null);
+      expect(appErrors.items).toHaveLength(0);
     });
   });
 
@@ -496,7 +496,7 @@ describe("useAuthLogic", () => {
         setAppErrors([{ message: "Pre-existing error" }]);
         resendVerifyAccountCode(username);
       });
-      expect(appErrors).toBeNull();
+      expect(appErrors.items).toHaveLength(0);
     });
   });
 
@@ -675,8 +675,7 @@ describe("useAuthLogic", () => {
         setAppErrors([{ message: "Pre-existing error" }]);
         resetPassword(username, verificationCode, password);
       });
-
-      expect(appErrors).toEqual(null);
+      expect(appErrors.items).toHaveLength(0);
     });
   });
 
@@ -834,7 +833,7 @@ describe("useAuthLogic", () => {
         setAppErrors([{ message: "Pre-existing error" }]);
         verifyAccount(username, verificationCode);
       });
-      expect(appErrors).toBeNull();
+      expect(appErrors.items).toHaveLength(0);
     });
   });
 });

portal/__tests__/hooks/useClaimsLogic.test.js
@@ -147,7 +147,7 @@ describe("useClaimsLogic", () => {
       });
 
       it("clears errors", () => {
-        expect(appErrorsLogic.appErrors).toBeNull();
+        expect(appErrorsLogic.appErrors.items).toHaveLength(0);
       });
 
       it("stores the new claim", () => {

portal/__tests__/hooks/useUsersLogic.test.js
@@ -66,7 +66,7 @@ describe("useUsersLogic", () => {
       });
 
       it("clears errors", () => {
-        expect(appErrorsLogic.appErrors).toBeNull();
+        expect(appErrorsLogic.appErrors.items).toHaveLength(0);
       });
     });
 

portal/__tests__/models/AppErrorInfoCollection.test.js
@@ -3,14 +3,14 @@ import AppErrorInfoCollection from "../../src/models/AppErrorInfoCollection";
 
 describe("AppErrorInfoCollection", () => {
   describe("fieldErrorMessage", () => {
-    it("returns undefined result if there are no errors", () => {
+    it("returns null result if there are no errors", () => {
       const collection = new AppErrorInfoCollection();
       const result = collection.fieldErrorMessage("first_name");
 
-      expect(result).toBeUndefined();
+      expect(result).toBeNull();
     });
 
-    it("returns undefined if no errors match the given field's path", () => {
+    it("returns null if no errors match the given field's path", () => {
       const collection = new AppErrorInfoCollection([
         new AppErrorInfo({
           field: "foo",
@@ -21,7 +21,7 @@ describe("AppErrorInfoCollection", () => {
 
       const result = collection.fieldErrorMessage(field);
 
-      expect(result).toBeUndefined();
+      expect(result).toBeNull();
     });
 
     it("returns merged string if multiple errors match the given field's path", () => {

commit e78a93920d7cf0d760d9ddae4d5d8e492d35ee16

Don't render error summary when there are no errors (#683)
Fixes a regression introduced in #678

portal/src/components/ErrorsSummary.js
@@ -26,7 +26,7 @@ function ErrorsSummary(props) {
   }, [errors]);
 
   // Don't render anything if there are no errors present
-  if (!errors) {
+  if (!errors || errors.items.length <= 0) {
     return null;
   }
 

portal/__tests__/components/ErrorsSummary.test.js
@@ -24,9 +24,17 @@ function render(customProps = {}, mountComponent = false) {
 }
 
 describe("ErrorsSummary", () => {
-  describe("when no errors exist", () => {
+  describe("when errors is null", () => {
     it("does not render an alert", () => {
       const { wrapper } = render({ errors: null });
+
+      expect(wrapper.isEmptyRender()).toBeTruthy();
+    });
+  });
+
+  describe("when no errors exist", () => {
+    it("does not render an alert", () => {
+      const { wrapper } = render({ errors: new AppErrorInfoCollection() });
 
       expect(wrapper.isEmptyRender()).toBeTruthy();
     });