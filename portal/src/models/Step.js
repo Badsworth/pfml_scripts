905095486cbe1455b8e42f9b0aff75a56be25a68
Lawrence Kang <lawrencekang@gmail.com>
2020-08-11 10:37:44 -0700

    allow user to complete application using bonding leave reason (#808)
    🎫 Ticket
    https://lwd.atlassian.net/browse/CP-741
    
    🛠 Changes
    Added a field to ignoredField in models/Step.js that allows the user to continue to Step 3 using the bonding leave reason in Step 2.

--- portal/src/models/Step.js
+++ portal/src/models/Step.js
@@ -110,6 +110,7 @@ export default class Step extends BaseModel {
           "claim.leave_details.intermittent_leave_periods[0]",
           "claim.middle_name",
           "claim.other_incomes",
+          "claim.pregnant_or_recent_birth",
           "claim.previous_leaves",
           "claim.temp.leave_details.avg_weekly_work_hours",
           "claim.temp.leave_details.continuous_leave_periods[0]",
