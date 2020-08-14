63088f0158bca4f5d916aa6b0e85b087b02c1c40
Rocket <rocketnova@users.noreply.github.com>
2020-08-14 09:46:10 -0700

    API: Fix multiple heads in PR 828. (#840)

---
+++ api/massgov/pfml/db/migrations/versions/2020_08_14_16_19_09_eba20f9af443_merge_multiple_heads.py
@@ -0,0 +1,21 @@
+"""Merge multiple heads
+
+Revision ID: eba20f9af443
+Revises: 4535bd46fe78, 791abdb2ba16
+Create Date: 2020-08-14 16:19:09.957458
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "eba20f9af443"
+down_revision = ("4535bd46fe78", "791abdb2ba16")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
