16b6312b9fb68bf8415be644ebba16011ccf03ae
Kevin Yeh <kevinyeah@utexas.edu>
2020-09-16 11:02:54 -0400

    API-92: Run API application behind Gunicorn (#997)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/API-92
    
    ## 🛠 Changes
    
    Updates the API to run behind Gunicorn. This removes the following warning:
    
    ```
    mass-pfml-api    |    WARNING: This is a development server. Do not use it in a production deployment.
    mass-pfml-api    |    Use a production WSGI server instead.
    ```
    
    ## ℹ️ Context for reviewers
    
    Thank you to @apopma and @aplybeah for taking an initial look at implementing Gunicorn with me a while back. It was expected to be a quick change, but ended up needing some workarounds and rummaging in documentation.
    
    ----
    
    Gunicorn is a production WSGI server that runs multiple workers in front of your API for handling requests.

---
+++ api/massgov/pfml/api/gunicorn_wrapper.py
@@ -0,0 +1,51 @@
+import multiprocessing
+
+import connexion
+import gunicorn.app.base
+
+
+class GunicornAppWrapper(gunicorn.app.base.BaseApplication):
+    """
+    Wrapper inspired by the Gunicorn Custom Application documentation.
+    See: https://docs.gunicorn.org/en/stable/custom.html
+
+    This wraps the core flask application configured by Connexion and
+    runs it with Gunicorn.
+    """
+
+    def __init__(self, app: connexion.FlaskApp, port: int):
+        # Run Gunicorn with the recommended default of (2 * num_cores) + 1.
+        #
+        # Also provide twice the number of threads, so each worker could
+        # potentially handle multiple requests using its CPU if it is waiting
+        # for a database (I/O) transaction to complete.
+        workers = (multiprocessing.cpu_count() * 2) + 1
+        threads = 2 * workers
+
+        self.options = {
+            # Bind the API to the provided port on 0.0.0.0 instead of 127.0.0.1.
+            # This allows connections to originate from outside the container's network.
+            "bind": "%s:%s" % ("0.0.0.0", port),
+            "workers": workers,
+            "threads": threads,
+            # Use the gthread class to enable multi-threading.
+            "worker-class": "gthread",
+            "log-level": "info",
+            # Instead of logging to a file, log directly to stdout/stderr.
+            "log-file": "-",
+        }
+
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self) -> connexion.FlaskApp:
+        return self.application
