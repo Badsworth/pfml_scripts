Badsworth/pfml_scripts

commit fd1383fdecc766489e12191baac29f52d57972af

CP-883:  Add mailing address section (#1047)
* add mailing address related questions

* add tests

* remove address input in payment page

* refactor

* reset mailingaddress in claim model

* fix test

* store 'has_mailing_address` in `Claim` model

* fix lint

* refactor

* address comments

* swap the radio value

* set default mailing_address to null

* address comments

portal/src/models/Step.js
@@ -133,6 +133,7 @@ export default class Step extends BaseModel {
           "claim.leave_details.child_birth_date",
           "claim.leave_details.child_placement_date",
           "claim.leave_details.pregnant_or_recent_birth",
+          "claim.mailing_address",
           "claim.previous_leaves",
           "claim.temp.leave_details.avg_weekly_work_hours",
           "claim.temp.leave_details.continuous_leave_periods[0]",

portal/src/locales/app/en-US.js
@@ -207,8 +207,14 @@ const pages = {
     usernameLabel: "$t(shared.usernameLabel)",
   },
   claimsAddress: {
+    choiceNo: "$t(shared.choiceNo)",
+    choiceYes: "$t(shared.choiceYes)",
+    hasMailingAddressHint:
+      "We may send notices to this address. If you choose to get your payments through a debit card, we will mail the card to this address.",
+    hasMailingAddressLabel: "Do you get your mail at this address?",
     hint:
       "If you are part of an Address Confidentiality Program, please provide your substitute address.",
+    mailingAddressLabel: "What is your mailing address?",
     sectionLabel: "What is your current residential address?",
     title: "$t(shared.claimsVerifyIdTitle)",
   },
@@ -507,11 +513,6 @@ const pages = {
     achLead:
       "We need this information so you can receive your weekly benefit through Direct Deposit. Contact your bank if you are having trouble finding this information.",
     achSectionLabel: "Enter your bank account information",
-    addressCityLabel: "City",
-    addressLine1Label: "Street address 1",
-    addressLine2Label: "Street address 2",
-    addressStateLabel: "State",
-    addressZipLabel: "ZIP Code",
     choiceAch: "$t(shared.paymentMethodAch)",
     choiceDebit: "$t(shared.paymentMethodDebit)",
     choiceHintAch: "Requires a bank account",

portal/src/models/Claim.js
@@ -38,6 +38,8 @@ class Claim extends BaseModel {
         reason_qualifier: null,
         reduced_schedule_leave_periods: null,
       },
+      // address object. See Address model
+      mailing_address: null,
       mass_id: null,
       middle_name: null,
       // array of OtherIncome objects. See the OtherIncome model
@@ -53,6 +55,8 @@ class Claim extends BaseModel {
        * this temporary space.
        */
       temp: {
+        // TODO (CP-1019): Connect address preference to the API
+        has_mailing_address: null,
         leave_details: {
           // TODO (CP-719): Connect intermittent leave fields to the API
           avg_weekly_work_hours: null,

portal/src/pages/claims/address.js
@@ -1,5 +1,8 @@
+import AddressModel from "../../models/Address";
 import Claim from "../../models/Claim";
+import ConditionalContent from "../../components/ConditionalContent";
 import FieldsetAddress from "../../components/FieldsetAddress";
+import InputChoiceGroup from "../../components/InputChoiceGroup";
 import PropTypes from "prop-types";
 import QuestionPage from "../../components/QuestionPage";
 import React from "react";
@@ -9,12 +12,21 @@ import useFunctionalInputProps from "../../hooks/useFunctionalInputProps";
 import { useTranslation } from "../../locales/i18n";
 import withClaim from "../../hoc/withClaim";
 
-export const fields = ["claim.residential_address"];
+export const fields = [
+  "claim.residential_address",
+  "claim.mailing_address",
+  "claim.temp.has_mailing_address",
+];
 
 export const Address = (props) => {
   const { appLogic, claim } = props;
   const { t } = useTranslation();
-  const { formState, updateFields } = useFormState(pick(props, fields).claim);
+  const { formState, getField, updateFields, removeField } = useFormState(
+    pick(props, fields).claim
+  );
+
+  const { temp } = formState;
+  const { has_mailing_address } = temp;
 
   const handleSave = () =>
     appLogic.claims.update(claim.application_id, formState);
@@ -25,16 +37,57 @@ export const Address = (props) => {
     updateFields,
   });
 
+  const residentialAddressProps = getFunctionalInputProps(
+    "residential_address"
+  );
+  if (!residentialAddressProps.value) {
+    residentialAddressProps.value = new AddressModel();
+  }
+
+  const mailingAddressProps = getFunctionalInputProps("mailing_address");
+  if (!mailingAddressProps.value) {
+    mailingAddressProps.value = new AddressModel();
+  }
+
   return (
     <QuestionPage title={t("pages.claimsAddress.title")} onSave={handleSave}>
       <FieldsetAddress
         appErrors={appLogic.appErrors}
         label={t("pages.claimsAddress.sectionLabel")}
         hint={t("pages.claimsAddress.hint")}
-        {...getFunctionalInputProps("residential_address")}
+        {...residentialAddressProps}
       />
-      {/* TODO (CP-883): Collect the user's mailing address if it's different
-      from their residential address */}
+      <InputChoiceGroup
+        {...getFunctionalInputProps("temp.has_mailing_address")}
+        choices={[
+          {
+            checked: has_mailing_address === false,
+            label: t("pages.claimsAddress.choiceYes"),
+            value: "false",
+          },
+          {
+            checked: has_mailing_address === true,
+            label: t("pages.claimsAddress.choiceNo"),
+            value: "true",
+          },
+        ]}
+        label={t("pages.claimsAddress.hasMailingAddressLabel")}
+        hint={t("pages.claimsAddress.hasMailingAddressHint")}
+        type="radio"
+      />
+      <ConditionalContent
+        fieldNamesClearedWhenHidden={["mailing_address"]}
+        getField={getField}
+        removeField={removeField}
+        updateFields={updateFields}
+        visible={has_mailing_address}
+      >
+        <FieldsetAddress
+          appErrors={appLogic.appErrors}
+          label={t("pages.claimsAddress.mailingAddressLabel")}
+          {...mailingAddressProps}
+        />
+      </ConditionalContent>
     </QuestionPage>
   );
 };

portal/tests/pages/claims/__snapshots__/address.test.js.snap
@@ -1,6 +1,6 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
-exports[`Address renders the page 1`] = `
+exports[`Address renders the page without any address info 1`] = `
 <QuestionPage
   onSave={[Function]}
   title="Your identity"
@@ -18,13 +18,67 @@ exports[`Address renders the page 1`] = `
     onChange={[Function]}
     value={
       Address {
-        "city": "Boston",
-        "line_1": "19 Staniford St",
-        "line_2": "Suite 505",
-        "state": "MA",
-        "zip": "02214",
+        "city": null,
+        "line_1": null,
+        "line_2": null,
+        "state": null,
+        "zip": null,
       }
     }
   />
+  <InputChoiceGroup
+    choices={
+      Array [
+        Object {
+          "checked": false,
+          "label": "Yes",
+          "value": "false",
+        },
+        Object {
+          "checked": false,
+          "label": "No",
+          "value": "true",
+        },
+      ]
+    }
+    hint="We may send notices to this address. If you choose to get your payments through a debit card, we will mail the card to this address."
+    label="Do you get your mail at this address?"
+    name="temp.has_mailing_address"
+    onChange={[Function]}
+    type="radio"
+    value=""
+  />
+  <ConditionalContent
+    fieldNamesClearedWhenHidden={
+      Array [
+        "mailing_address",
+      ]
+    }
+    getField={[Function]}
+    removeField={[Function]}
+    updateFields={[Function]}
+    visible={null}
+  >
+    <FieldsetAddress
+      appErrors={
+        AppErrorInfoCollection {
+          "items": Array [],
+          "itemsById": Object {},
+        }
+      }
+      label="What is your mailing address?"
+      name="mailing_address"
+      onChange={[Function]}
+      value={
+        Address {
+          "city": null,
+          "line_1": null,
+          "line_2": null,
+          "state": null,
+          "zip": null,
+        }
+      }
+    />
+  </ConditionalContent>
 </QuestionPage>
 `;

portal/tests/pages/claims/address.test.js
@@ -1,41 +1,77 @@
+import { renderWithAppLogic, simulateEvents } from "../../test-utils";
 import Address from "../../../src/pages/claims/address";
 import AddressModel from "../../../src/models/Address";
-import pick from "lodash/pick";
-import { renderWithAppLogic } from "../../test-utils";
 
 jest.mock("../../../src/hooks/useAppLogic");
 
 describe("Address", () => {
-  let appLogic, claim, residentialAddress, wrapper;
+  let appLogic, wrapper;
+  const testAddress = new AddressModel({
+    line_1: "19 Staniford St",
+    line_2: "Suite 505",
+    city: "Boston",
+    state: "MA",
+    zip: "02214",
+  });
 
   beforeEach(() => {
-    residentialAddress = new AddressModel({
-      line_1: "19 Staniford St",
-      line_2: "Suite 505",
-      city: "Boston",
-      state: "MA",
-      zip: "02214",
-    });
-
-    ({ appLogic, claim, wrapper } = renderWithAppLogic(Address, {
+    ({ appLogic, wrapper } = renderWithAppLogic(Address, {
       claimAttrs: {
-        residential_address: residentialAddress,
+        residential_address: null,
+        mailing_address: null,
       },
     }));
   });
 
-  it("renders the page", () => {
+  it("renders the page without any address info", () => {
     expect(wrapper).toMatchSnapshot();
   });
 
-  describe("when the form is successfully submitted", () => {
-    it("calls claims.update", () => {
-      wrapper.find("QuestionPage").simulate("save");
+  describe("when residential address is available", () => {
+    let changeField, changeRadioGroup;
 
-      expect(appLogic.claims.update).toHaveBeenCalledWith(
-        expect.any(String),
-        pick(claim, ["residential_address"])
-      );
+    beforeEach(() => {
+      ({ changeField, changeRadioGroup } = simulateEvents(wrapper));
+      changeField("residential_address", testAddress);
+      changeRadioGroup("temp.has_mailing_address", "false");
+    });
+
+    it("hides conditional content", () => {
+      expect(wrapper.find("ConditionalContent").props().visible).toBeFalsy();
+    });
+
+    it("submits residential address", () => {
+      wrapper.find("QuestionPage").simulate("save");
+      expect(appLogic.claims.update).toHaveBeenCalledWith(expect.any(String), {
+        residential_address: testAddress,
+        temp: { has_mailing_address: false },
+        mailing_address: null,
+      });
+    });
+
+    describe("When user has a different mailing address", () => {
+      beforeEach(() => {
+        changeRadioGroup("temp.has_mailing_address", "true");
+      });
+
+      it("shows mailing address fields", () => {
+        expect(wrapper.find("ConditionalContent").props().visible).toBe(true);
+      });
+
+      it("submits both address info", () => {
+        const { changeField } = simulateEvents(wrapper);
+        changeField("mailing_address", testAddress);
+        wrapper.find("QuestionPage").simulate("save");
+
+        expect(appLogic.claims.update).toHaveBeenCalledWith(
+          expect.any(String),
+          {
+            residential_address: testAddress,
+            mailing_address: testAddress,
+            temp: { has_mailing_address: true },
+          }
+        );
+      });
     });
   });
 });

portal/tests/test-utils.js
@@ -302,6 +302,7 @@ export class MockClaimBuilder {
             zip: "00000",
           })
     );
+    set(this.claimAttrs, "temp.has_mailing_address", false);
     return this;
   }
 

commit 2b0e9a6bcd9dd5902e7eae266d83a2eb33d74168

CP-883: Ignore has_mailing_address from Checklist logic (#1117)
## 🎫 Ticket

https://lwd.atlassian.net/browse/CP-883

## 🛠 Changes

- Fixes regression introduced in fd1383fdecc766489e12191baac29f52d57972af that resulted in Step 1 of the Checklist never showing as "Completed" when the page was refreshed since the `has_mailing_address` field is only stored in memory for now.

portal/src/models/Step.js
@@ -135,6 +135,7 @@ export default class Step extends BaseModel {
           "claim.leave_details.pregnant_or_recent_birth",
           "claim.mailing_address",
           "claim.previous_leaves",
+          "claim.temp.has_mailing_address",
           "claim.temp.leave_details.avg_weekly_work_hours",
           "claim.temp.leave_details.continuous_leave_periods[0]",
           "claim.temp.leave_details.bonding.date_of_child",