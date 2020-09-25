4269e703a90b8f5664a22475157f300ad2673248
James Bursa <james@navapbc.com>
2020-09-25 14:08:00 -0400

    API: filter out more audit logging noise (#1163)
    ## 🎫 Ticket
    
    None
    
    ## 🛠 Changes
    
    - Don't audit log `os.chmod` with an int fd argument.
    - Don't audit log `code.__new__` with a path starting with `"/tmp/pip-unpacked-wheel"`
    - Add class `massgov.pfml.util.collections.dict.LeastRecentlyUsedDict` which is a dict with a maximum size, evicting the least recently written key when full.
    - Use LeastRecentlyUsedDict to reduce logging of repeated events.
    
    ## ℹ️ Context for reviewers
    
    The audit logging logs various events that may have security implications for visibility.
    
    There's currently a lot of excess logging. This change removes some normal events entirely, and reduces logging of repeated events. When the same event happens frequently, it is logged the first 10 times. After that it is logged only 10% of the time until it occurs 100 times, and then it is only logged 1% of the time.
    
    ## 👀 Demo
    
    In AWS most of the log lines of this nature are related to New Relic monitoring and don't appear locally. For example this is logged every few seconds due to status checks:
    
    ```
    18:36:10.153  massgov.pfml.util.logging.audit      audit_hook                   INFO     socket.getaddrinfo ('collector-009.newrelic.com', 443, 2, 1, 0)                  
    18:36:10.168  massgov.pfml.util.logging.audit      audit_hook                   INFO     socket.connect (<socket.socket fd=5, family=AddressFamily.AF_INET, type=SocketKind.SOCK_STREAM, proto=6, laddr=('0.0.0.0', 0)>, ('162.247.243.149', 443)) 
    ```
    
    To test this locally I temporarily remove the suppression of `os.chmod` logging and used Gunicorn locally. Here are some lines showing that messages are logged at 9, 10, and then skipped until count=20:
    
    ```
    15:52:24.210  massgov.pfml.util.logging.audit      audit_log                    INFO     os.chmod (8, 0, -1)                                                              count=9
    15:52:24.237  massgov.pfml.util.logging.audit      audit_log                    INFO     os.chmod (9, 0, -1)                                                              count=9
    ...
    15:52:26.213  massgov.pfml.util.logging.audit      audit_log                    INFO     os.chmod (8, 0, -1)                                                              count=10
    15:52:26.240  massgov.pfml.util.logging.audit      audit_log                    INFO     os.chmod (9, 0, -1)                                                              count=10
    ...
    15:52:46.237  massgov.pfml.util.logging.audit      audit_log                    INFO     os.chmod (8, 0, -1)                                                              count=20
    15:52:46.264  massgov.pfml.util.logging.audit      audit_log                    INFO     os.chmod (9, 0, -1)                                                              count=20
    ```
    
    Reviewers: @mhuneke

--- api/massgov/pfml/util/logging/audit.py
+++ api/massgov/pfml/util/logging/audit.py
@@ -1,13 +1,17 @@
 #
-# Application-level audit logging
+# Application-level audit logging.
+#
+# See https://docs.python.org/3/library/audit_events.html
+# https://docs.python.org/3/library/sys.html#sys.addaudithook
+# https://www.python.org/dev/peps/pep-0578/
 #
 
-import logging  # noqa: B1
 import sys
 
-from massgov.pfml.util.logging import get_logger
+import massgov.pfml.util.collections.dict
+import massgov.pfml.util.logging
 
-logger = get_logger(__name__)
+logger = massgov.pfml.util.logging.get_logger(__name__)
 
 
 def init_security_logging():
@@ -30,15 +34,26 @@ IGNORE_AUDIT_EVENTS = {
 }
 
 
-AUDIT_EVENT_TO_LEVEL = {"open": logging.INFO}  # noqa: B1
-
-
 def audit_hook(event_name, args):
     if event_name in IGNORE_AUDIT_EVENTS:
         return
     if event_name == "open" and isinstance(args[0], str) and "/__pycache__/" in args[0]:
         return
+    if event_name == "os.chmod" and type(args[0]) is int:
+        # Gunicorn generates a high volume of these events in normal operation (see workertmp.py)
+        return
+    if event_name == "code.__new__" and args[1].startswith("/tmp/pip-unpacked-wheel"):
+        # Normal startup behaviour
+        return
 
-    logger.log(
-        AUDIT_EVENT_TO_LEVEL.get(event_name, logging.INFO), "%s %r", event_name, args,  # noqa: B1
-    )
+    audit_log("%s %r" % (event_name, args))
+
+
+def audit_log(message):
+    """Log a message but only log recently repeated messages at intervals."""
+    count = audit_message_count[message] = audit_message_count[message] + 1
+    if count <= 10 or (count <= 100 and (count % 10) == 0) or (count % 100) == 0:
+        logger.info(message, extra={"count": count})  # noqa: B1
+
+
+audit_message_count = massgov.pfml.util.collections.dict.LeastRecentlyUsedDict()
