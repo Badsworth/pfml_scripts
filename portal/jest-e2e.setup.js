17b21af96584f6dcb7724c15bcef7d60148a79cf
Sawyer Hollenshead <git@sawyerh.com>
2020-09-25 15:41:47 -0400

    CP: Increase e2e test timeout to 15 seconds (#1165)
    ## ℹ️ Context for reviewers
    
    End-to-end test suite has been flakey, due to the test timing out. When I ran the tests locally, the test was clocking in right around the 9-10 second range, so I've bumped it up to 15 seconds.

--- portal/jest-e2e.setup.js
+++ portal/jest-e2e.setup.js
@@ -1,9 +1,9 @@
 // Expose custom puppeteer matchers (i.e `expect(page).toMatch(...)`)
 import "expect-puppeteer";
 
-// Adjust the Jest timeout to account for slow-mo
+// Adjust the Jest timeout to account for sometimes slow Network requests (and slow-mo mode)
 const timeout = process.env.JEST_PUPPETEER_SLOW_MO
   ? 30000 // 30 seconds to account for intentionally slowed tests
-  : 10000; // 10 seconds otherwise
+  : 15000; // 15 seconds otherwise
 
 jest.setTimeout(timeout);
