fba0fbebeb132ae6edf670a99a05c596285a6881
teshd <17507503+teshd@users.noreply.github.com>
2020-10-07 20:55:17 +0000

    API-647 - removing dor lambda related cloudwatch trigger resources (#1305)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/API-647
    
    ## 🛠 Changes
    
    Removing Cloudwatch event trigger targets related to the DOR import lambda.
    
    ## ℹ️ Context for reviewers
    
    The DOR import lambda is getting migrated to run as an ECS task. This PR contains the Terraform changes to remove schedule trigger that executes the DOR lambda.
    
    ## 👀 Demo
    
    `tf plan` @ test ev:
    ```
    # module.api.aws_cloudwatch_event_target.trigger_dor_import_lambda_daily_at_11_pm will be destroyed
      - resource "aws_cloudwatch_event_target" "trigger_dor_import_lambda_daily_at_11_pm" {
          - arn       = "arn:aws:lambda:us-east-1:***:function:massgov-pfml-test-dor-import" -> null
          - id        = "daily-at-11-pm-dor_import_test_lambda_event_target" -> null
          - rule      = "daily-at-11-pm" -> null
          - target_id = "dor_import_test_lambda_event_target" -> null
        }
    
     # module.api.aws_lambda_permission.allow_cloudwatch_to_call_dor_import will be destroyed
      - resource "aws_lambda_permission" "allow_cloudwatch_to_call_dor_import" {
          - action        = "lambda:InvokeFunction" -> null
          - function_name = "arn:aws:lambda:us-east-1:***:function:massgov-pfml-test-dor-import" -> null
          - id            = "AllowExecutionFromCloudWatch" -> null
          - principal     = "events.amazonaws.com" -> null
          - source_arn    = "arn:aws:events:us-east-1:***:rule/daily-at-11-pm" -> null
          - statement_id  = "AllowExecutionFromCloudWatch" -> null
        }
    
    ```

--- infra/api/template/cloudwatch.tf
+++ infra/api/template/cloudwatch.tf
@@ -13,11 +13,6 @@ resource "aws_cloudwatch_log_group" "service_logs" {
   name = "service/${local.app_name}-${var.environment_name}"
 }
 
-resource "aws_cloudwatch_event_target" "trigger_dor_import_lambda_daily_at_11_pm" {
-  rule      = aws_cloudwatch_event_rule.daily_11pm_et.name
-  arn       = aws_lambda_function.dor_import.arn
-  target_id = "dor_import_${var.environment_name}_lambda_event_target"
-}
 
 resource "aws_cloudwatch_event_target" "trigger_formstack_import_lambda_daily_at_11_pm" {
   rule      = aws_cloudwatch_event_rule.formstack_lambda_daily_11pm_et.name
@@ -47,14 +42,6 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_formstack_import" {
   function_name = aws_lambda_function.formstack_import.arn
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.formstack_lambda_daily_11pm_et.arn
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_to_call_dor_import" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.dor_import.arn
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.daily_11pm_et.arn
 }
 
 # ----------------------------------------------------------------------------------------------------------------------
