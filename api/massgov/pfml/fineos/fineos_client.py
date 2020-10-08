Badsworth/pfml_scripts

commit 7fec198025542c8bf40430e5feabf603fc5d81b1

CP-1022: Add update_week_based_work_pattern to fineos_client (#1177)
## 🎫 Ticket

https://lwd.atlassian.net/browse/CP-1022

## 🛠 Changes

- Adds `FINEOSClient#get_absence_occupations`
- Adds `FINEOSClient#get_week_based_work_pattern`
- Adds `FINEOSClient#add_week_based_work_pattern`
- Adds `FINEOSClient#update_week_based_work_pattern`
- Adds `FINEOSActions#build_week_based_work_pattern` that splits hours_work_per_week across 7 `WorkDays`
- Adds `FINEOSActions#upsert_week_based_work_pattern`
- Adds `set_empty_dates_to_none` to `json_to_obj` utilities

## ℹ️ Context for Reviewers

The program is currently deciding whether splitting hours_work_per_week across 7 will satisfy business requirements and we may end up storing full work patterns. In either case, claimants with a `variable` work pattern will probably need to have to their hours split across 7 days, so this code may be reused. See: https://lwd.atlassian.net/wiki/spaces/CP/pages/683442506/Work+Patterns for more details

## 🧐 

@jamesbursa

api/massgov/pfml/fineos/fineos_client.py
@@ -6,7 +6,7 @@ import datetime
 import os.path
 import urllib.parse
 import xml.etree.ElementTree
-from typing import Any, Callable, Dict, List, Optional
+from typing import Any, Callable, Dict, List, Optional, Union
 
 import defusedxml.ElementTree
 import oauthlib.oauth2
@@ -16,6 +16,7 @@ import requests_oauthlib
 import xmlschema
 
 import massgov.pfml.util.logging
+from massgov.pfml.util.converters.json_to_obj import set_empty_dates_to_none
 
 from . import client, exception, models
 
@@ -218,10 +219,11 @@ class FINEOSClient(client.AbstractFINEOSClient):
         )
         json = response.json()
         logger.debug("json %r", json)
-        # Workaround empty strings in response instead of null. These cause parse_obj to fail.
-        for prop in ("accidentDate", "expectedDeliveryDate", "actualDeliveryDate"):
-            if json[0][prop] == "":
-                json[0][prop] = None
+
+        set_empty_dates_to_none(
+            json, ["accidentDate", "expectedDeliveryDate", "actualDeliveryDate"]
+        )
+
         # Doesn't match OpenAPI file - API returns a single-item list instead of the object.
         return models.customer_api.NotificationCaseSummary.parse_obj(json[0])
 
@@ -231,14 +233,24 @@ class FINEOSClient(client.AbstractFINEOSClient):
         logger.debug("json %r", json)
         # Workaround empty strings in response instead of null. These cause parse_obj to fail.
         for item in json:
-            for prop in ("startDate", "endDate"):
-                if item[prop] == "":
-                    item[prop] = None
+            set_empty_dates_to_none(item, ["startDate", "endDate"])
+
         return pydantic.parse_obj_as(List[models.customer_api.AbsenceCaseSummary], json)
 
     def get_absence(self, user_id: str, absence_id: str) -> models.customer_api.AbsenceDetails:
         response = self._customer_api("GET", f"customer/absence/absences/{absence_id}", user_id)
         return models.customer_api.AbsenceDetails.parse_obj(response.json())
+
+    def get_absence_occupations(
+        self, user_id: str, absence_id: str
+    ) -> List[models.customer_api.ReadCustomerOccupation]:
+        response = self._customer_api("GET", f"customer/cases/{absence_id}/occupations", user_id,)
+
+        json = response.json()
+        for item in json:
+            set_empty_dates_to_none(item, ["dateJobBegan", "dateJobEnded"])
+
+        return pydantic.parse_obj_as(List[models.customer_api.ReadCustomerOccupation], json)
 
     def add_payment_preference(
         self, user_id: str, payment_preference: models.customer_api.NewPaymentPreference
@@ -294,3 +306,53 @@ class FINEOSClient(client.AbstractFINEOSClient):
         documents = response.json()
 
         return [client_response_json_to_document(doc) for doc in documents]
+
+    def get_week_based_work_pattern(
+        self, user_id: str, occupation_id: Union[str, int],
+    ) -> models.customer_api.WeekBasedWorkPattern:
+
+        response = self._customer_api(
+            "GET", f"customer/occupations/{occupation_id}/week-based-work-pattern", user_id,
+        )
+
+        json = response.json()
+        set_empty_dates_to_none(json, ["patternStartDate"])
+
+        return models.customer_api.WeekBasedWorkPattern.parse_obj(json)
+
+    def add_week_based_work_pattern(
+        self,
+        user_id: str,
+        occupation_id: Union[str, int],
+        week_based_work_pattern: models.customer_api.WeekBasedWorkPattern,
+    ) -> models.customer_api.WeekBasedWorkPattern:
+
+        response = self._customer_api(
+            "POST",
+            f"customer/occupations/{occupation_id}/week-based-work-pattern",
+            user_id,
+            data=week_based_work_pattern.json(exclude_none=True),
+        )
+
+        json = response.json()
+        set_empty_dates_to_none(json, ["patternStartDate"])
+
+        return models.customer_api.WeekBasedWorkPattern.parse_obj(json)
+
+    def update_week_based_work_pattern(
+        self,
+        user_id: str,
+        occupation_id: Union[str, int],
+        week_based_work_pattern: models.customer_api.WeekBasedWorkPattern,
+    ) -> models.customer_api.WeekBasedWorkPattern:
+
+        response = self._customer_api(
+            "POST",
+            f"customer/occupations/{occupation_id}/week-based-work-pattern/replace",
+            user_id,
+            data=week_based_work_pattern.json(exclude_none=True),
+        )
+
+        json = response.json()
+
+        return models.customer_api.WeekBasedWorkPattern.parse_obj(json)

api/massgov/pfml/api/services/fineos_actions.py
@@ -13,6 +13,7 @@
 ###
 
 import datetime
+import math
 import mimetypes
 import uuid
 from typing import List, Optional
@@ -113,6 +114,8 @@ def send_to_fineos(application: Application, db_session: massgov.pfml.db.Session
         application.fineos_notification_case_id = new_case.notificationCaseId
         db_session.commit()
 
+        upsert_week_based_work_pattern(fineos, fineos_user_id, application)
+
     except massgov.pfml.fineos.FINEOSClientError:
         logger.exception("FINEOS API error")
         return False
@@ -201,6 +204,62 @@ def build_absence_case(
         employerNotificationDate=application.employer_notification_date,
     )
     return absence_case
+
+
+def upsert_week_based_work_pattern(fineos_client, user_id, application):
+    """Add or update work pattern on an in progress absence case"""
+
+    week_based_work_pattern = build_week_based_work_pattern(application)
+    absence_id = application.fineos_absence_id
+    occupation = fineos_client.get_absence_occupations(user_id, absence_id)[0]
+    occupation_id = occupation.occupationId
+
+    if occupation_id is None:
+        raise ValueError
+
+    try:
+        fineos_client.add_week_based_work_pattern(user_id, occupation_id, week_based_work_pattern)
+    except massgov.pfml.fineos.exception.FINEOSClientBadResponse as error:
+        # FINEOS returns a 403 forbidden when attempting to an add a work pattern
+        # for an occupation when one already exists
+        if error.response_status == 403:
+            fineos_client.update_week_based_work_pattern(
+                user_id, occupation_id, week_based_work_pattern
+            )
+        else:
+            raise error
+
+
+def build_week_based_work_pattern(application):
+    """Split hours_worked_per_week across 7 week days"""
+
+    hours_worked_per_week = getattr(application, "hours_worked_per_week", 40)
+    week_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    hours_per_day = math.floor(hours_worked_per_week / 7)
+    remainder = hours_worked_per_week % 7
+    minutes_remainder = round(60 * (remainder % 1))
+    work_pattern_days = []
+
+    for i, week_day in enumerate(week_days):
+        hours = hours_per_day + 1 if remainder >= 1 else hours_per_day
+        minutes = minutes_remainder if i == 0 else 0
+
+        work_pattern_days.append(
+            massgov.pfml.fineos.models.customer_api.WorkPatternDay(
+                dayOfWeek=week_day, weekNumber=1, hours=hours, minutes=minutes
+            )
+        )
+
+        remainder -= 1
+
+    return massgov.pfml.fineos.models.customer_api.WeekBasedWorkPattern(
+        workPatternType="Fixed",
+        workWeekStarts=week_days[0],
+        patternStartDate=None,
+        patternStatus=None,
+        workPatternDays=work_pattern_days,
+    )
 
 
 def upload_document(

api/massgov/pfml/db/models/factories.py
@@ -208,6 +208,7 @@ class ApplicationFactory(BaseFactory):
     user_id = factory.LazyAttribute(lambda a: a.user.user_id)
 
     employer = factory.SubFactory(EmployerFactory)
+    hours_worked_per_week = 70
     employer_id = factory.LazyAttribute(lambda a: a.employer.employer_id)
 
     tax_identifier = factory.SubFactory(TaxIdentifierFactory)

api/massgov/pfml/fineos/client.py
@@ -57,6 +57,12 @@ class AbstractFINEOSClient(abc.ABC, metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
+    def get_absence_occupations(
+        self, user_id: str, absence_id: str
+    ) -> typing.List[models.customer_api.ReadCustomerOccupation]:
+        pass
+
+    @abc.abstractmethod
     def add_payment_preference(
         self, user_id: str, payment_preference: models.customer_api.NewPaymentPreference
     ) -> models.customer_api.PaymentPreferenceResponse:
@@ -79,4 +85,28 @@ class AbstractFINEOSClient(abc.ABC, metaclass=abc.ABCMeta):
     def get_documents(
         self, user_id: str, absence_id: str
     ) -> typing.List[models.customer_api.Document]:
+        pass
+
+    @abc.abstractmethod
+    def get_week_based_work_pattern(
+        self, user_id: str, occupation_id: typing.Union[str, int],
+    ) -> models.customer_api.WeekBasedWorkPattern:
+        pass
+
+    @abc.abstractmethod
+    def add_week_based_work_pattern(
+        self,
+        user_id: str,
+        occupation_id: typing.Union[str, int],
+        week_based_work_pattern: models.customer_api.WeekBasedWorkPattern,
+    ) -> models.customer_api.WeekBasedWorkPattern:
+        pass
+
+    @abc.abstractmethod
+    def update_week_based_work_pattern(
+        self,
+        user_id: str,
+        occupation_id: typing.Union[str, int],
+        week_based_work_pattern: models.customer_api.WeekBasedWorkPattern,
+    ) -> models.customer_api.WeekBasedWorkPattern:
         pass

api/massgov/pfml/fineos/mock_client.py
@@ -8,7 +8,7 @@
 import datetime
 import pathlib
 import typing
-from typing import List
+from typing import List, Union
 
 import faker
 import requests
@@ -130,6 +130,12 @@ class MockFINEOSClient(client.AbstractFINEOSClient):
     def get_absence(self, user_id: str, absence_id: str) -> models.customer_api.AbsenceDetails:
         return models.customer_api.AbsenceDetails()
 
+    def get_absence_occupations(
+        self, user_id: str, absence_id: str
+    ) -> List[models.customer_api.ReadCustomerOccupation]:
+        _capture_call("get_absence_occupations", user_id, absence_id=absence_id)
+        return [models.customer_api.ReadCustomerOccupation(occupationId=12345)]
+
     def add_payment_preference(
         self, user_id: str, payment_preference: models.customer_api.NewPaymentPreference
     ) -> models.customer_api.PaymentPreferenceResponse:
@@ -153,6 +159,40 @@ class MockFINEOSClient(client.AbstractFINEOSClient):
     def get_documents(self, user_id: str, absence_id: str) -> List[models.customer_api.Document]:
         document = mock_document(absence_id)
         return [models.customer_api.Document.parse_obj(document)]
+
+    def get_week_based_work_pattern(
+        self, user_id: str, occupation_id: Union[str, int],
+    ) -> models.customer_api.WeekBasedWorkPattern:
+        _capture_call("get_week_based_work_pattern", user_id, occupation_id=occupation_id)
+        return models.customer_api.WeekBasedWorkPattern(workPatternType="Fixed", workPatternDays=[])
+
+    def add_week_based_work_pattern(
+        self,
+        user_id: str,
+        occupation_id: Union[str, int],
+        week_based_work_pattern: models.customer_api.WeekBasedWorkPattern,
+    ) -> models.customer_api.WeekBasedWorkPattern:
+        _capture_call(
+            "add_week_based_work_pattern", user_id, week_based_work_pattern=week_based_work_pattern
+        )
+
+        if user_id == "USER_WITH_EXISTING_WORK_PATTERN":
+            raise exception.FINEOSClientBadResponse(200, 403)
+        else:
+            return week_based_work_pattern
+
+    def update_week_based_work_pattern(
+        self,
+        user_id: str,
+        occupation_id: Union[str, int],
+        week_based_work_pattern: models.customer_api.WeekBasedWorkPattern,
+    ) -> models.customer_api.WeekBasedWorkPattern:
+        _capture_call(
+            "update_week_based_work_pattern",
+            user_id,
+            week_based_work_pattern=week_based_work_pattern,
+        )
+        return week_based_work_pattern
 
 
 def start_capture():

api/massgov/pfml/util/converters/json_to_obj.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Mapping, Optional, TypeVar
+from typing import Any, Dict, List, Mapping, Optional, TypeVar
 
 _T = TypeVar("_T")
 
@@ -35,3 +35,17 @@ def get_json_from_object(object_instance: Optional[Any]) -> Optional[Dict[str, A
             json_str[key] = object_dict[key]
 
     return json_str
+
+
+def set_empty_dates_to_none(
+    object_instance: Optional[Any], date_strings: List[str]
+) -> Optional[Dict[str, Any]]:
+    """Workaround empty strings in response instead of null. These cause parse_obj to fail."""
+    if object_instance is None:
+        return None
+
+    for prop in date_strings:
+        if object_instance[prop] == "":
+            object_instance[prop] = None
+
+    return object_instance

api/openapi.yaml
@@ -930,6 +930,10 @@ components:
             employer_fein:
               $ref: "#/components/schemas/Fein"
               nullable: true
+            hours_worked_per_week:
+              type: number
+              nullable: true
+              example: 40.5
             first_name:
               type: string
               nullable: true

api/tests/api/services/test_fineos_actions.py
@@ -133,3 +133,35 @@ def test_document_upload(user, test_db_session):
     assert fineos_document["fileExtension"] == ".png"
     assert fineos_document["originalFilename"] == file_name
     assert fineos_document["description"] == description
+
+
+def test_build_week_based_work_pattern(user):
+    application = ApplicationFactory.create(user=user)
+
+    application.hours_worked_per_week = 70
+    evenly_split_pattern = fineos_actions.build_week_based_work_pattern(application)
+
+    for day in evenly_split_pattern.workPatternDays:
+        assert day.hours == 10
+
+    application.hours_worked_per_week = 72
+    pattern_with_2_hours_remainder = fineos_actions.build_week_based_work_pattern(application)
+    # it adds an hour to each day until there is no remainder
+    assert pattern_with_2_hours_remainder.workPatternDays[0].hours == 11
+    assert pattern_with_2_hours_remainder.workPatternDays[1].hours == 11
+
+    for i, workPatternDay in enumerate(pattern_with_2_hours_remainder.workPatternDays):
+        if i < 2:
+            continue
+
+        assert workPatternDay.hours == 10
+
+    application.hours_worked_per_week = 70.55
+    pattern_with_minutes_remainder = fineos_actions.build_week_based_work_pattern(application)
+    # it adds minutes to first day
+    assert pattern_with_minutes_remainder.workPatternDays[0].minutes == 33
+
+    application.hours_worked_per_week = 70.56
+    pattern_with_seconds_remainder = fineos_actions.build_week_based_work_pattern(application)
+    # it rounds to the nearest minute
+    assert pattern_with_seconds_remainder.workPatternDays[0].minutes == 34

commit 38662ad323f1db81ef50f9f7b82f0216c64d6b76

get 0 index in JSON response (#1307)
Fixing regression introduced in #1177 . See [diff](https://github.com/EOLWD/pfml/pull/1177/files#diff-2e8aecfefd780129a63654360d2ce4a7L221-R225).

🧐 
@doshitan @sawyerh

api/massgov/pfml/fineos/fineos_client.py
@@ -221,7 +221,7 @@ class FINEOSClient(client.AbstractFINEOSClient):
         logger.debug("json %r", json)
 
         set_empty_dates_to_none(
-            json, ["accidentDate", "expectedDeliveryDate", "actualDeliveryDate"]
+            json[0], ["accidentDate", "expectedDeliveryDate", "actualDeliveryDate"]
         )
 
         # Doesn't match OpenAPI file - API returns a single-item list instead of the object.