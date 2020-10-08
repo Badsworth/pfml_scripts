712a1727edb34d6b4cfb2dd484cdc1ad15cd5c27
Melquisedeque Pereira <hire@melquip.com>
2020-10-08 18:16:16 +0100

    END-314: Add Portal Registration LST scenario (#1313)
    Adds a PortalRegistration LST scenario to test user registration as part of load and stress testing

--- e2e/src/flood/helpers.ts
+++ e2e/src/flood/helpers.ts
@@ -1,4 +1,5 @@
 import { Locator, Browser, By, ElementHandle, Until } from "@flood/element";
+import { config } from "./config";
 
 export const formatDate = (d: string | null | undefined): string => {
   const notificationDate = new Date(d || "");
@@ -40,3 +41,109 @@ export const waitForElement = async (
   await browser.focus(locator);
   return browser.findElement(locator);
 };
+
+export type TestMailVerificationFetcher = {
+  getVerificationCodeForUser: (address: string) => Promise<string>;
+  getCodeFromMessage: (message: { html: string }) => string;
+  getTagFromAddress: (address: string) => string;
+  getCredentials: () => { username: string; password: string };
+};
+
+export async function getMailVerifier(
+  browser: Browser
+): Promise<TestMailVerificationFetcher> {
+  const apiKey = await config("E2E_TESTMAIL_APIKEY");
+  const namespace = await config("E2E_TESTMAIL_NAMESPACE");
+  const endpoint = "https://api.testmail.app/api/json";
+  if (!apiKey || !namespace) {
+    throw new Error(
+      "Unable to create Test Mail API client due to missing environment variables."
+    );
+  }
+
+  async function getVerificationCodeForUser(address: string): Promise<string> {
+    const params = {
+      apikey: apiKey,
+      namespace: namespace,
+      tag: getTagFromAddress(address),
+    };
+
+    const paramsString = Object.entries(params)
+      .map(([key, val]) => `${key}=${val}`)
+      .join("&");
+
+    const body = await browser.evaluate(
+      (baseUrl, query) => {
+        return new Promise((resolve, reject) => {
+          fetch(`${baseUrl}?${query}`)
+            .then((r) => {
+              resolve(r.json());
+            })
+            .catch(reject);
+        });
+      },
+      endpoint,
+      paramsString
+    );
+
+    if (body.result !== "success") {
+      throw new Error(
+        `There was an error fetching the verification code: ${body.message}`
+      );
+    }
+    if (!Array.isArray(body.emails) || !(body.emails.length > 0)) {
+      throw new Error(`No emails found for this user.`);
+    }
+
+    return getCodeFromMessage(body.emails[0]);
+  }
+
+  function getCodeFromMessage(message: { html: string }): string {
+    const match = message.html.match(/(\d{6})<\/strong>/);
+    if (!match) {
+      throw new Error(`Unable to parse verification code from message.`);
+    }
+    return match[1];
+  }
+
+  function getTagFromAddress(address: string): string {
+    const re = new RegExp(`^${namespace}\.(.*)@inbox\.testmail\.app$`);
+    const match = address.match(re);
+    if (!match || !(match[1].length > 0)) {
+      throw new Error(
+        `Oops, this doesn't look like a testmail address: ${address}`
+      );
+    }
+    return match[1];
+  }
+
+  function rStr(length: number) {
+    return Array(length)
+      .fill("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+      .map((x) => x[Math.floor(Math.random() * x.length)])
+      .join("");
+  }
+
+  function rStrWithNums(length: number) {
+    return Math.random().toString(36).substr(2, length);
+  }
+
+  function rNum(max: number) {
+    return Math.floor(Math.random() * max) + 1;
+  }
+
+  function getCredentials() {
+    const tag = rStrWithNums(8);
+    return {
+      username: `${namespace}.${tag}@inbox.testmail.app`,
+      password: rStr(10) + rNum(999),
+    };
+  }
+
+  return {
+    getVerificationCodeForUser,
+    getCodeFromMessage,
+    getTagFromAddress,
+    getCredentials,
+  };
+}
---
+++ e2e/src/flood/tests/PortalRegistration.perf.ts
@@ -0,0 +1,80 @@
+import { TestData, Browser, step, By } from "@flood/element";
+import { SimulationClaim } from "../../simulation/types";
+import { globalElementSettings as settings, PortalBaseUrl } from "../config";
+import {
+  getMailVerifier,
+  TestMailVerificationFetcher,
+  labelled,
+  waitForElement,
+} from "../helpers";
+
+let emailVerifier: TestMailVerificationFetcher;
+let username: string;
+let password: string;
+
+export { settings };
+export const scenario = "PortalRegistration";
+export const steps = [
+  {
+    name: "Visit Portal homepage",
+    test: async (browser: Browser): Promise<void> => {
+      await browser.page.setCookie({
+        name: "_ff",
+        value: JSON.stringify({
+          pfmlTerriyay: true,
+        }),
+        url: PortalBaseUrl,
+      });
+      await browser.visit(PortalBaseUrl);
+    },
+  },
+  {
+    name: "Register new user in Portal",
+    test: async (browser: Browser): Promise<void> => {
+      // create email verifier and user credentials
+      emailVerifier = await getMailVerifier(browser);
+      ({ username, password } = emailVerifier.getCredentials());
+      // go to registration page
+      await (
+        await waitForElement(browser, By.visibleText("create an account"))
+      ).click();
+      // fill out the form
+      await (await labelled(browser, "Email address")).type(username);
+      await (await labelled(browser, "Password")).type(password);
+      // submit new user
+      await (
+        await waitForElement(browser, By.css("button[type='submit']"))
+      ).click();
+    },
+  },
+  {
+    name: "Verify new user's email",
+    test: async (browser: Browser): Promise<void> => {
+      // TODO: how much do we need to wait
+      // for verification email to arrive
+      await browser.wait(20000);
+      // get the email code
+      const code = await emailVerifier.getVerificationCodeForUser(username);
+      // type code
+      await (await labelled(browser, "6-digit code")).type(code);
+      // submit code
+      await (
+        await waitForElement(browser, By.css("button[type='submit']"))
+      ).click();
+      // wait for successful registration
+      await waitForElement(
+        browser,
+        By.visibleText("Email successfully verified")
+      );
+    },
+  },
+];
+
+export default (): void => {
+  TestData.fromJSON<SimulationClaim>("../data/claims.json").filter(
+    (line) => line.scenario === scenario
+  );
+  steps.forEach((action) => {
+    step(action.name, action.test);
+  });
+};
