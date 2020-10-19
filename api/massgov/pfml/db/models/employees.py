01a6c74b31f16b85a843ed8bd47188609b908b9a
Anteneh Addis <antxaddis@gmail.com>
2020-10-19 17:00:34 -0400

    API 539: Revert PR merge
    This reverts commit 0f898ff2100cb802cab680d0aea23f959a4e3d31.
    
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/API-539
    
    ## 🛠 Changes
    
    Revert PR merge until this is done: https://lwd.atlassian.net/browse/API-719
    
    ## ℹ️ Context for reviewers
    
    We were having issues running migrations in the reverted PR merge because existing data in the test environment conflicted with the new unique constraint.
    
    ## 👀 Demo

--- api/massgov/pfml/db/models/employees.py
+++ api/massgov/pfml/db/models/employees.py
@@ -328,7 +328,7 @@ class HealthCareProviderAddress(Base):
 class User(Base):
     __tablename__ = "user"
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid_gen)
-    active_directory_id = Column(Text, unique=True)
+    active_directory_id = Column(Text)
     email_address = Column(Text)
     consented_to_data_sharing = Column(Boolean, default=False, nullable=False)
 
