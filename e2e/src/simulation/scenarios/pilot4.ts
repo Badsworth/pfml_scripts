23a73b7b81b6a03e4ba6ec1d4ed4912e2193343a
Rob Bayliss <rob@lastcallmedia.com>
2020-10-20 12:18:18 -0400

    END-279: Implement pilot 4 GBM and UNH claims (#1439)
    https://lwd.atlassian.net/browse/END-279
    
    This PR stubs in the GBM and UNH claims from END-279. These are actually mostly implemented, aside from the @todo notes that are left.
    
    * END-279: Stub full suite of bonding cases for Pilot 4
    
    * Remove prebirth certification for HAP3
    
    * Adjust todos
    
    * Fix lint error

--- e2e/src/simulation/scenarios/pilot4.ts
+++ e2e/src/simulation/scenarios/pilot4.ts
@@ -4,6 +4,10 @@ import formatISO from "date-fns/formatISO";
 
 // Cases drawn from: https://massgov.sharepoint.com/:x:/r/sites/EOL-PFMLProject/_layouts/15/Doc.aspx?sourcedoc=%7B7541FAF6-B95A-48F0-B7EA-24CD06E52CF0%7D&file=Cases%20to%20Create%20v1.xlsx&action=default&mobileredirect=true
 
+// @todo: Check and clarify distinction betwen prebirth, postbirth, and way post birth.
+// Then implement that distinction.
+
+// Happy Path Foster
 export const BHAP1 = scenario("BHAP1", {
   reason: "Child Bonding",
   reason_qualifier: "Foster Care",
@@ -14,6 +18,7 @@ export const BHAP1 = scenario("BHAP1", {
   },
 });
 
+// Happy Path Adoptive
 export const BHAP2 = scenario("BHAP2", {
   reason: "Child Bonding",
   reason_qualifier: "Adoption",
@@ -24,13 +29,13 @@ export const BHAP2 = scenario("BHAP2", {
   },
 });
 
+// Happy Path Gestational/non-gestational
 export const BHAP3 = scenario("BHAP3", {
   reason: "Child Bonding",
   reason_qualifier: "Newborn",
   residence: "MA-proofed",
   docs: {
     MASSID: {},
-    PREBIRTH: {},
   },
 });
 
@@ -40,23 +45,45 @@ export const BHAP4 = scenario("BHAP4", {
   reason_qualifier: "Newborn",
   residence: "OOS",
   financiallyIneligible: true,
-  child_birth_date: formatISO(subMonths(new Date(), 13)),
   docs: {
     OOSID: {},
     PREBIRTH: {},
   },
 });
 
-// Happy path denial due to adoption > 12 months ago.
+// Happy Path Denial for Failed Certification (> 12 months)
 export const BHAP5 = scenario("BHAP5", {
   reason: "Child Bonding",
   reason_qualifier: "Adoption",
-  residence: "OOS",
+  residence: "MA-proofed",
   child_birth_date: formatISO(subMonths(new Date(), 13)),
   docs: {
-    OOSID: {},
-    // @todo: Implement document type.
+    MASSID: {},
     ADOPTIONCERT: {},
+  },
+});
+
+// Happy Path Denial for Plan Availability
+// @todo: Implement plan availability?
+export const BHAP6 = scenario("BHAP6", {
+  reason: "Child Bonding",
+  reason_qualifier: "Adoption",
+  residence: "MA-proofed",
+  child_birth_date: formatISO(subMonths(new Date(), 13)),
+  docs: {
+    MASSID: {},
+    ADOPTIONCERT: {},
+  },
+});
+
+// Happy Path post-birth Gestational/non-gestational
+export const BHAP7 = scenario("BHAP7", {
+  reason: "Child Bonding",
+  reason_qualifier: "Newborn",
+  residence: "MA-proofed",
+  docs: {
+    MASSID: {},
+    BIRTHCERTIFICATE: {},
   },
 });
 
@@ -66,6 +93,99 @@ export const BHAP = chance([
   [1, BHAP3],
   [1, BHAP4],
   [1, BHAP5],
+  [1, BHAP6],
+  [1, BHAP7],
 ]);
 
-export default chance([[1, BHAP]]);
+// Missing Foster Documentation
+export const BGBM1 = scenario("BGBM1", {
+  reason: "Child Bonding",
+  reason_qualifier: "Foster Care",
+  residence: "MA-proofed",
+  docs: {
+    MASSID: {},
+  },
+});
+
+export const BGBM2 = scenario("BGBM2", {
+  reason: "Child Bonding",
+  reason_qualifier: "Adoption",
+  residence: "OOS",
+  docs: {
+    OOSID: {},
+  },
+});
+
+export const BGBM = chance([
+  [1, BGBM1],
+  [1, BGBM2],
+]);
+
+// Birth Document Mismatch
+export const BUNH1 = scenario("BUNH1", {
+  reason: "Child Bonding",
+  reason_qualifier: "Newborn",
+  residence: "OOS",
+  docs: {
+    OOSID: {},
+    BIRTHCERTIFICATE: {
+      // @todo: Implement mismatched name birth cert.
+      mismatchedName: true,
+    },
+  },
+});
+
+// Invalid birth document
+export const BUNH2 = scenario("BUNH2", {
+  reason: "Child Bonding",
+  reason_qualifier: "Newborn",
+  residence: "MA-proofed",
+  docs: {
+    MASSID: {},
+    // @todo: Implement personal letter.
+    // PERSONALLETTER: {},
+  },
+});
+
+// Invalid Cat Documentation
+export const BUNH3 = scenario("BUNH3", {
+  reason: "Child Bonding",
+  reason_qualifier: "Newborn",
+  residence: "OOS",
+  docs: {
+    OOSID: {},
+    // @todo: Implement cat pic.
+    // CATPIC: {},
+  },
+});
+
+// Birth Certificate does not match template
+// export const BUNH4 = scenario("BUNH4", {
+//   reason: "Child Bonding",
+//   reason_qualifier: "Newborn",
+//   residence: "MA-proofed",
+//   docs: {
+//     MASSID: {},
+//     // @todo: Implement birth cert that doesn't match template. Question out in spreadsheet to see how this should be
+//     //  implemented.
+//     // BIRTHCERTIFICATE: {},
+//   },
+// });
+
+// @todo: Spot check and review documents for each claim type.
+// @todo: Spot check that scenarios match spreadsheet.
+// @todo: Attempt submission of each scenario type.
+
+export const BUNH = chance([
+  [1, BUNH1],
+  [1, BUNH2],
+  [1, BUNH3],
+  // [1, BUNH4],
+]);
+
+export default chance([
+  // @todo: Doublecheck probabilities here.
+  [1, BHAP],
+  [1, BGBM],
+  [1, BUNH],
+]);
