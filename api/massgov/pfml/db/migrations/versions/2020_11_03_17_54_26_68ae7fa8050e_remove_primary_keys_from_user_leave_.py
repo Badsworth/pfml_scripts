5f1b74886d6eeddc99e06f7f6232fd730c440691
Sasha Reid <sashareid@navapbc.com>
2020-11-04 17:11:43 -0800

    Patching migration from user leave administrator changes (#1664)
    * Patching migration from user leave administrator changes
    
    * [EMPLOYER-523] Fixing lint / formatting

--- api/massgov/pfml/db/migrations/versions/2020_11_03_17_54_26_68ae7fa8050e_remove_primary_keys_from_user_leave_.py
+++ api/massgov/pfml/db/migrations/versions/2020_11_03_17_54_26_68ae7fa8050e_remove_primary_keys_from_user_leave_.py
@@ -5,6 +5,8 @@ Revises: ac77dfa6613a
 Create Date: 2020-11-03 17:54:26.647158
 
 """
+import uuid
+
 import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects import postgresql
@@ -23,8 +25,35 @@ def upgrade():
     )
     op.add_column(
         "link_user_leave_administrator",
-        sa.Column("user_leave_administrator_id", postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column("user_leave_administrator_id", postgresql.UUID(as_uuid=True), nullable=True),
     )
+    connection = op.get_bind()
+    t_lula = sa.Table(
+        "link_user_leave_administrator",
+        sa.MetaData(),
+        sa.Column("user_leave_administrator_id", postgresql.UUID(as_uuid=True), nullable=True),
+        sa.Column("user_id", postgresql.UUID(as_uuid=True)),
+        sa.Column("employer_id", postgresql.UUID(as_uuid=True)),
+        sa.Column("fineos_web_id", sa.Text()),
+    )
+    results = connection.execute(
+        sa.select([t_lula.c.user_id, t_lula.c.employer_id, t_lula.c.fineos_web_id])
+    ).fetchall()
+    # Iterate over all selected data tuples.
+    for user_id, employer_id, fineos_web_id in results:
+        connection.execute(
+            t_lula.update()
+            .where(
+                sa.and_(
+                    t_lula.c.user_id == user_id,
+                    t_lula.c.employer_id == employer_id,
+                    t_lula.c.fineos_web_id == fineos_web_id,
+                )
+            )
+            .values(user_leave_administrator_id=uuid.uuid4())
+        )
+
+    op.alter_column("link_user_leave_administrator", "user_leave_administrator_id", nullable=False)
     # ### end Alembic commands ###
 
 
