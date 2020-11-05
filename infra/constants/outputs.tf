b38913be8c82e07b069e50d9e90484da24cb0058
apopma <adapopma@navapbc.com>
2020-11-05 09:16:57 -0600

    Using new tag assignments from EOTSS (#1661)

--- infra/constants/outputs.tf
+++ infra/constants/outputs.tf
@@ -1,9 +1,7 @@
 output "common_tags" {
   value = {
-    # TODO: Change to DFML once a value exists in the standards
-    agency = "eol"
-    # TODO: Change to PFML application once a value exists in the standards
-    application   = "coreinf"
+    agency        = "dfml"
+    application   = "pfml"
     businessowner = "ma-pfml-alerts@mass.gov"
     createdby     = "ma-pfml-alerts@mass.gov"
     itowner       = "ma-pfml-alerts@mass.gov"
