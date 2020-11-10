6086c9b1cc86ed1038a4d7ce6bf23d06f95d223b
Loren Yu <yuloren@gmail.com>
2020-11-09 16:22:43 -0800

    Point portal stage/dev to API stage (#1730)
    * Point Portal to API stage by default
    * Point Portal test to API test temporarily

--- portal/config/test.js
+++ portal/config/test.js
@@ -9,10 +9,14 @@ module.exports = {
   envName: "test",
   domain: "paidleave-test.mass.gov",
   newRelicAppId: "847038274",
-  // awsConfig: {
-  //   cognitoRegion: "us-east-1",
-  //   // Cognito test environment (must match whatever API environment we're targeting)
-  //   cognitoUserPoolId: "us-east-1_HhQSLYSIe",
-  //   cognitoUserPoolWebClientId: "7sjb96tvg8251lrq5vdk7de9",
-  // },
+  // TODO (CP-1362): Revert temporary switch to Test environment
+  apiUrl: "https://paidleave-api-test.mass.gov/api/v1",
+  awsConfig: {
+    cognitoRegion: "us-east-1",
+    // Cognito test environment (must match whatever API environment we're targeting)
+    // TODO (CP-1362): Revert temporary switch to Test environment
+    cognitoUserPoolId: "us-east-1_HhQSLYSIe",
+    // TODO (CP-1362): Revert temporary switch to Test environment
+    cognitoUserPoolWebClientId: "7sjb96tvg8251lrq5vdk7de9",
+  },
 };
