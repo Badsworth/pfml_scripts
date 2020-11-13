7c8fe2166ffe41020f3c6c02c12c20b25944dc46
Alsia Plybeah <alsia@navapbc.com>
2020-11-13 13:16:26 -0500

    API-788 Send ECS logs to New Relic (#1779)
    🎫 Ticket
    https://lwd.atlassian.net/browse/API-788
    
    🛠 Changes
    added clooudwatch subscription filters and lambda permissions for ECS logs
    ℹ️ Context for reviewers
    all ECS task logs needed to be sent to the newrelic-log-ingestion lambda

--- infra/api/template/cloudwatch.tf
+++ infra/api/template/cloudwatch.tf
@@ -13,6 +13,22 @@ resource "aws_cloudwatch_log_group" "service_logs" {
   name = "service/${local.app_name}-${var.environment_name}"
 }
 
+resource "aws_cloudwatch_log_subscription_filter" "ecs_service_logging" {
+  name            = "ecs_service_logs"
+  log_group_name  = aws_cloudwatch_log_group.service_logs.name
+  destination_arn = local.newrelic_log_ingestion_lambda
+  # matches all log events
+  filter_pattern = ""
+}
+
+resource "aws_lambda_permission" "ecs_permission_service_logging" {
+  statement_id  = "NRLambdaPermission_ECSLogging_${var.environment_name}"
+  action        = "lambda:InvokeFunction"
+  function_name = local.newrelic_log_ingestion_lambda
+  principal     = "logs.us-east-1.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_log_group.service_logs.arn}:*"
+}
+
 
 resource "aws_cloudwatch_event_target" "trigger_formstack_import_lambda_daily_at_11_pm" {
   rule      = aws_cloudwatch_event_rule.formstack_lambda_daily_11pm_et.name
--- infra/ecs-tasks/template/cloudwatch.tf
+++ infra/ecs-tasks/template/cloudwatch.tf
@@ -2,3 +2,26 @@
 resource "aws_cloudwatch_log_group" "ecs_tasks" {
   name = "service/${local.app_name}-${var.environment_name}/ecs-tasks"
 }
+
+locals {
+  # This ARN describes a 3rd-party lambda installed outside of Terraform thru the AWS Serverless Application Repository.
+  # This lambda ingests CloudWatch logs from several sources, and packages them for transmission to New Relic's servers.
+  # This lambda was modified post-installation to fix an apparent bug in the processing/packaging of its telemetry data.
+  newrelic_log_ingestion_lambda = module.constants.newrelic_log_ingestion_arn
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "ecs_task_logging" {
+  name            = "ecs_task_logs"
+  log_group_name  = aws_cloudwatch_log_group.ecs_tasks.name
+  destination_arn = local.newrelic_log_ingestion_lambda
+  # matches all log events
+  filter_pattern = ""
+}
+
+resource "aws_lambda_permission" "ecs_permission_tasks_logging" {
+  statement_id  = "NRLambdaPermission_ECSTasksLogging_${var.environment_name}"
+  action        = "lambda:InvokeFunction"
+  function_name = local.newrelic_log_ingestion_lambda
+  principal     = "logs.us-east-1.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_log_group.ecs_tasks.arn}:*"
+}
