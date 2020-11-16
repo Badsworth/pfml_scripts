02a26cec0edc228bfa5b2ae11da666e280653c59
Alsia Plybeah <alsia@navapbc.com>
2020-11-16 15:48:05 -0500

    remove cloudtrail and associated buckets (#1834)
    🎫 Ticket
    https://lwd.atlassian.net/browse/API-820
    
    🛠 Changes
    removed the massgov-pfml-{env-name}-logging cloudtrails and their associated buckets.
    
    ℹ️ Context for reviewers
    Delete the S3 object log cloudtrails, since Smartronix is going to manage them instead
    
    in addition, there is a 5 trail limit for Cloudtrail. Exceeding this limit causes some deploys to error out. Example
    Essentially, the goal of this pr is to undo what was added here

--- infra/api/template/iam.tf
+++ infra/api/template/iam.tf
@@ -515,39 +515,3 @@ data "aws_iam_policy_document" "document_upload" {
   #  }
 }
 
-data "aws_iam_policy_document" "cloudtrail_logging" {
-  statement {
-    sid = "WriteCloudtrailLogs"
-
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions = [
-      "s3:PutObject"
-    ]
-    resources = [
-      "arn:aws:s3:::massgov-pfml-${var.environment_name}-cloudtrail-s3-logging/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
-    ]
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
-    }
-  }
-
-  statement {
-    sid = "CloudtrailAclCheck"
-
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions = [
-      "s3:GetBucketAcl"
-    ]
-    resources = ["arn:aws:s3:::massgov-pfml-${var.environment_name}-cloudtrail-s3-logging"]
-  }
-}
