0aedfe6004aa5450ea28a051cffe019804da2314
Tanner Doshier <git@doshitan.com>
2020-11-16 14:13:11 -0500

    API: Configure `stage` for the dev FINEOS IAM role (#1818)
    It was originally thought that the IDT buckets would be in the pre-live
    FINEOS environment, but they exist in the dev environment. The bucket
    path was already updated[1] for this, but the IAM role was still
    configured for the UAT/pre-live environment. Update that to use the dev
    environment role.
    
    [1] c95483aee61026fbf7919040b3ea2ca4bc3426b4

--- infra/ecs-tasks/environments/stage/main.tf
+++ infra/ecs-tasks/environments/stage/main.tf
@@ -27,8 +27,8 @@ module "tasks" {
   fineos_client_oauth2_url                   = "https://idt-api.masspfml.fineos.com/oauth2/token"
   fineos_client_oauth2_client_id             = "1fa281uto9tjuqtm21jle7loam"
 
-  fineos_aws_iam_role_arn         = "arn:aws:iam::016390658835:role/sompre-IAMRoles-CustomerAccountAccessRole-S0EP9ABIA02Z"
-  fineos_aws_iam_role_external_id = "8jFBtjr4UA@"
+  fineos_aws_iam_role_arn         = "arn:aws:iam::666444232783:role/somdev-IAMRoles-CustomerAccountAccessRole-BF05IBJSG74B"
+  fineos_aws_iam_role_external_id = "12345"
 
   fineos_eligibility_feed_output_directory_path = "s3://fin-somdev-data-import/IDT/absence-eligibility/upload"
 }
