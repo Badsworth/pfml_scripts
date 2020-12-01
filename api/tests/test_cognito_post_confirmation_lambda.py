0df58b88173940cb2d51c15b9e7be5d8311b7478
Sasha Reid <sashareid@navapbc.com>
2020-12-01 10:17:30 -0800

    [EMPLOYER-631] Commit new test and code changes to remove FINEOS reg … (#2085)
    * [EMPLOYER-631] Commit new test and code changes to remove FINEOS reg from post-confirmation hook
    
    * [EMPLOYER-631] Updating deps to pass deps-check
    
    * [EMPLOYER-631] Making changes to test to ensure the record is updated
    
    * [EMPLOYER-631] Making smaller changes to poetry.lock and removing explicit lxml from pyproject.toml

--- api/tests/test_cognito_post_confirmation_lambda.py
+++ api/tests/test_cognito_post_confirmation_lambda.py
@@ -4,6 +4,7 @@ import os
 import pytest
 
 import massgov.pfml.cognito_post_confirmation_lambda.lib as lib
+from massgov.pfml.api.services.administrator_fineos_actions import register_leave_admin_with_fineos
 from massgov.pfml.db.models.employees import Employer, Role, User, UserLeaveAdministrator
 
 
@@ -188,8 +189,8 @@ def test_leave_admin_handler(test_db_session, leave_admin_event_dict, logging_fi
         .filter(UserLeaveAdministrator.user_id == created_user.user_id)
         .one()
     )
-
-    assert created_leave_admin.fineos_web_id is not None
+    assert created_leave_admin
+    assert created_leave_admin.fineos_web_id is None
     assert created_leave_admin.employer_id == employer.employer_id
 
 
@@ -215,8 +216,63 @@ def test_leave_admin_create(test_db_session):
         .one_or_none()
     )
 
-    assert created_leave_admin.fineos_web_id is not None
+    # This post-confirmation hook no longer registers the user with FINEOS
+    assert created_leave_admin.fineos_web_id is None
     assert created_leave_admin.employer_id == employer.employer_id
+
+
+def test_register_fineos_updates_ula_record(test_db_session):
+    employer = Employer(
+        employer_fein="133701337", employer_dba="Acme Corp", fineos_employer_id="93"
+    )
+
+    test_db_session.add(employer)
+    test_db_session.commit()
+
+    lib.leave_admin_create(test_db_session, "UUID", "fake@fake.com", employer.employer_fein, {})
+
+    created_user = test_db_session.query(User).filter(User.active_directory_id == "UUID").one()
+
+    assert created_user.active_directory_id == "UUID"
+    assert created_user.email_address == "fake@fake.com"
+    assert created_user.roles[0].role_id == Role.EMPLOYER.role_id
+
+    created_leave_admin = (
+        test_db_session.query(UserLeaveAdministrator)
+        .filter(UserLeaveAdministrator.user_id == created_user.user_id)
+        .one_or_none()
+    )
+    assert created_leave_admin.fineos_web_id is None
+    assert created_leave_admin.employer_id == employer.employer_id
+
+    ula_updated = register_leave_admin_with_fineos(
+        admin_full_name="Bogus McUser",
+        admin_email="fake@fake.com",
+        admin_area_code=None,
+        admin_phone_number=None,
+        employer=employer,
+        user=created_user,
+        db_session=test_db_session,
+        fineos_client=None,
+    )
+
+    assert (
+        ula_updated.user_leave_administrator_id == created_leave_admin.user_leave_administrator_id
+    )
+    assert ula_updated.fineos_web_id is not None
+
+    test_db_session.commit()
+
+    updated_leave_admin = (
+        test_db_session.query(UserLeaveAdministrator)
+        .filter(UserLeaveAdministrator.user_id == created_user.user_id)
+        .one_or_none()
+    )
+
+    assert updated_leave_admin.fineos_web_id == ula_updated.fineos_web_id
+    assert (
+        updated_leave_admin.user_leave_administrator_id == ula_updated.user_leave_administrator_id
+    )
 
 
 def test_forgot_password_handler(test_db_session, forgot_password_event_dict):
