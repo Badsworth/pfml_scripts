e03de85fcbcc9f74a2cdac86f61d2f5e47fad5bb
Melquisedeque Pereira <hire@melquip.com>
2020-12-03 23:35:04 +0000

    END-492: Create script to filter claims.json based on spreadsheet of imported employees (#2114)
    https://lwd.atlassian.net/browse/END-492
    
    Added filter user data script that takes a generated claims.json and outputs a claimsFiltered.json that only contains employees which tax_identifier numbers are in the ssn.csv file
    Added split user data script that takes a claims.json and outputs 2 json files, one with "clean" employees (defaults to 10.000), and another with the remaining employees we're actually going to use during LST.

---
+++ e2e/src/simulation/commands/splitUserData.ts
@@ -0,0 +1,85 @@
+import fs from "fs";
+import { promisify } from "util";
+import { CommandModule } from "yargs";
+import stream, { pipeline } from "stream";
+import JSONStream from "JSONStream";
+import SimulationStorage from "../SimulationStorage";
+import { SimulationClaim } from "../types";
+import { SystemWideArgs } from "../../cli";
+
+type ImportUserDataArgs = { directory: string } & SystemWideArgs;
+
+// Create a promised version of the pipeline function.
+const pipelineP = promisify(pipeline);
+
+const cmd: CommandModule<SystemWideArgs, ImportUserDataArgs> = {
+  command: "splitUserData",
+  describe: "Splits claims.json into clean and non clean employees .json files",
+  builder: {
+    directory: {
+      type: "string",
+      description: "The directory with existing claim and user data",
+      demandOption: true,
+      requiresArg: true,
+      normalize: true,
+      alias: "d",
+    },
+    split: {
+      type: "string",
+      description: "The number of clean employees needed",
+      demandOption: false,
+      requiresArg: false,
+      default: 10000,
+      normalize: true,
+      alias: "n",
+    },
+  },
+  async handler(args) {
+    args.logger.profile("splitUserData");
+    const storage = new SimulationStorage(args.directory);
+    const cleanEmployeesTotal = args.split as number;
+    let totalCleanClaims = 0;
+    let totalNonCleanClaims = 0;
+
+    const cleanOutput = `${args.directory}/claimsCleanEmployees.json`;
+    const nonCleanOutput = `${args.directory}/claimsNonCleanEmployees.json`;
+    await fs.promises.unlink(cleanOutput);
+    await fs.promises.unlink(nonCleanOutput);
+
+    const cleanEmployeesOutputStream = fs.createWriteStream(cleanOutput);
+    const nonCleanEmployeesOutputStream = fs.createWriteStream(nonCleanOutput);
+    // pipeline to output
+    nonCleanEmployeesOutputStream.write("[\n");
+    await pipelineP(
+      fs.createReadStream(storage.claimFile, { encoding: "utf8" }),
+      JSONStream.parse("*"),
+      // output only clean employers
+      new stream.Transform({
+        objectMode: true,
+        transform(chunk: SimulationClaim, encoding, next) {
+          if (totalCleanClaims < cleanEmployeesTotal) {
+            // write clean employee
+            this.push(chunk);
+            totalCleanClaims++;
+          } else {
+            // write NON clean employee
+            nonCleanEmployeesOutputStream.write(
+              (totalNonCleanClaims ? ",\n" : "") + JSON.stringify(chunk)
+            );
+            totalNonCleanClaims++;
+          }
+          next();
+        },
+      }),
+      JSONStream.stringify(),
+      cleanEmployeesOutputStream
+    );
+    nonCleanEmployeesOutputStream.write("\n]");
+    args.logger.info(`Extracted ${cleanEmployeesTotal} clean employees.`);
+    args.logger.info(`${totalNonCleanClaims} claims remaining.`);
+  },
+};
+
+const { command, describe, builder, handler } = cmd;
+
+export { command, describe, builder, handler };
