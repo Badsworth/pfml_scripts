Badsworth/pfml_scripts

commit af271a4a2733892e484f1e0577ccb1404eaeae0c

API-860: modify DOR generator to create various financial eligibility scenarios (#2124)
## 🎫 Ticket

https://lwd.atlassian.net/browse/API-860

## 🛠 Changes

- Modify DOR generator to create various financial eligibility scenarios.
- Also generate more realistic company sizes (number of employees per employer).
- Add JSON payloads for the scenarios.
- Fix bug that prevented create-user.py from working with a role argument.

## ℹ️ Context for reviewers

This adds fake data for various financial eligibility scenarios. The goal is to add this data into the test or stage environment so that the endpoint can be tested end-to-end (including from FINEOS).

This is done via the DOR generator. The DOR generator is written to be deterministic and generate the same data each time it is run.

Also changes the way employees are allocated to employers. Previously an employee was randomly allocated to 1 to 4 employers. Now each employer has a pre-computed size and a random sample of employees of that size is allocated to it. This gives a more realistic distribution (including some very large employers).

Will be deployed as part of #1715.

## 👀 Demo

```python
$ make dor-generate 
poetry run dor-generate --folder dor_mock --count 1000 2>&1 | python3 -u massgov/pfml/util/logging/decodelog.py
...
INFO     chunk 0: 100 employers, 991 employees   
INFO     chunk 100: 100 employers, 500 employees 
INFO     chunk 200: 100 employers, 288 employees 
INFO     chunk 300: 100 employers, 245 employees 
INFO     chunk 400: 100 employers, 412 employees 
INFO     chunk 500: 100 employers, 405 employees 
INFO     chunk 600: 100 employers, 323 employees 
INFO     chunk 700: 100 employers, 4420 employees
INFO     chunk 800: 100 employers, 323 employees 
INFO     chunk 900: 100 employers, 611 employees 
```

```
$ make dor-import
...
```

```python
pfml/api/massgov/pfml/api/eligibility/samples$ ./run_all.sh 
=============== eligible.json ===============

api/massgov/pfml/dor/mock/generate.py
@@ -8,10 +8,12 @@
 import argparse
 import datetime as dt
 import decimal
+import math
 import os
 import random
 import string
 from datetime import datetime, timedelta
+from typing import Any, Dict, List, Tuple
 
 import faker
 
@@ -37,7 +39,6 @@ parser.add_argument(
     "--folder", type=str, default="generated_files", help="Output folder for generated files"
 )
 
-EMPLOYER_COUNT_RANDOM_POOL = (1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 4)
 WAGE_CHANGE_RANDOM_POOL = (0, 0, 0, 0, 1000, 2500, 4400, 7800, -1200, -3500)
 
 
@@ -108,10 +109,7 @@ def main():
 
 
 def generate(
-    employer_count,
-    employer_file,
-    employee_file,
-    employer_count_random_pool=EMPLOYER_COUNT_RANDOM_POOL,
+    employer_count, employer_file, employee_file,
 ):
     if employer_count <= 0 or employer_count % 100 != 0:
         raise RuntimeError("employer_count must be a multiple of 100")
@@ -122,34 +120,52 @@ def generate(
     #
     # (If we generated all employers then all employees, the possible employer_account_keys for an
     # employee would change depending on the employer_count.)
+    employee_generate_id = 1
     for chunk in range(0, employer_count, 100):
-        employer_account_keys = generate_employer_file(chunk + 1, 100, employer_file, employee_file)
-        generate_employee_file(
-            chunk * EMPLOYER_TO_EMPLOYEE_RATIO + 1,
-            100 * EMPLOYER_TO_EMPLOYEE_RATIO,
-            employer_account_keys,
-            employee_file,
-            employer_count_random_pool,
+        # Generate employers
+        employers = tuple(generate_employers(chunk + 1, 100))
+
+        total_employees = sum(employer["number_of_employees"] for employer in employers)
+
+        # Assign employees to employers according to number of employees
+        employee_ids = tuple(range(employee_generate_id, employee_generate_id + total_employees))
+        random.seed(chunk)
+        employee_employers: Dict[int, List[Dict[str, Any]]] = {}
+        for employer in employers:
+            employer_employee_ids = random.sample(employee_ids, employer["number_of_employees"])
+            for employee_id in employer_employee_ids:
+                if employee_id not in employee_employers:
+                    employee_employers[employee_id] = []
+                employee_employers[employee_id].append(employer)
+
+        # Generate employee rows
+        employee_wage_rows: List[Dict[str, Any]] = []
+        for employee_id, employer_ids in employee_employers.items():
+            employee_wage_rows += tuple(generate_single_employee(employee_id, employer_ids))
+
+        logger.info(
+            "chunk %i: %i employers, %i employees", chunk, len(employers), len(employee_employers)
         )
 
+        # Write files
+        write_employer_file(employers, employer_file)
 
-# == Employer ==
+        for employer in employers:
+            for employer_wage_row in generate_employer_wage_rows(employer):
+                write_employer_wage_row(employer_wage_row, employee_file)
+        for row in employee_wage_rows:
+            write_employee_line(row, employee_file)
 
-
-def generate_employer_file(base_id, employer_count, employers_file, employees_file):
-    """Generate employers, print rows to file"""
-    employer_account_keys = []
-
-    for employer, employer_wage_rows in generate_employers(base_id, employer_count):
-        write_employee_row(employer, employers_file)
-        for employer_wage_row in employer_wage_rows:
-            write_employer_wage_row(employer_wage_row, employees_file)
-        employer_account_keys.append(employer["account_key"])
-
-    return employer_account_keys
+        employee_generate_id += total_employees
 
 
-def write_employee_row(row, employers_file):
+def write_employer_file(employers, employers_file):
+    """Write employer rows to file"""
+    for employer in employers:
+        write_employer_row(employer, employers_file)
+
+
+def write_employer_row(row, employers_file):
     line = "{}{:255}{:14}{:255}{:30}{}{}{}{:255}{}{}{}{}{}\n".format(
         row["account_key"],
         row["employer_name"],
@@ -187,20 +203,8 @@ def write_employer_wage_row(employer_wage_row, employer_wage_row_file):
 
 def generate_employers(base_id, employer_count):
     """Generate employer rows"""
-    # TODO use better id generators to match DOR format when available
-    count = 0
-
-    logger.info("Generating employer information - count: %i", employer_count)
-
     for index in range(employer_count):
-        if count > 0 and (count % 1000) == 0:
-            logger.info("Generating employers, current count: %i", count)
-
         yield generate_single_employer(base_id + index)
-
-        count += 1
-
-    logger.info("Generated employers total: %i", count)
 
 
 def generate_single_employer(employer_generate_id):
@@ -215,7 +219,11 @@ def generate_single_employer(employer_generate_id):
     account_key = str(employer_generate_id).rjust(11, "0")
     fein = str(100000000 + employer_generate_id)
 
+    # occasionally generates a duplicate, but that's realistic as it happens in the real data
     employer_name = fake.company()
+
+    if employer_generate_id % 500 == 0:
+        employer_name += " “Company”"  # add some Unicode characters
 
     (
         employer_address_country,
@@ -241,8 +249,8 @@ def generate_single_employer(employer_generate_id):
 
     updated_date = get_date_days_before(SIMULATED_TODAY, random.randrange(1, 90))
 
-    # Generate an employer row for each quarter
-    # TODO randomize subset of quarters
+    number_of_employees = math.floor(random.paretovariate(1))
+
     employer = {
         "account_key": account_key,
         "employer_name": employer_name,
@@ -258,8 +266,13 @@ def generate_single_employer(employer_generate_id):
         "exemption_commence_date": exemption_commence_date,
         "exemption_cease_date": exemption_cease_date,
         "updated_date": updated_date,
+        "number_of_employees": number_of_employees,
     }
 
+    return employer
+
+
+def generate_employer_wage_rows(employer):
     employer_wage_rows = []
     for quarter in QUARTERS:
         # is the quarter information amended
@@ -282,7 +295,7 @@ def generate_single_employer(employer_generate_id):
         }
         employer_wage_rows.append(employer_row)
 
-    return employer, employer_wage_rows
+    return employer_wage_rows
 
 
 def generate_fake_address():
@@ -300,23 +313,6 @@ def generate_fake_address():
         postal_code = fake.zipcode_plus4().replace("-", "")
 
     return country, state, city, street, postal_code
-
-
-# == Employee ==
-
-
-def generate_employee_file(
-    base_id,
-    employee_count,
-    employer_account_keys,
-    employees_file,
-    employer_count_random_pool=EMPLOYER_COUNT_RANDOM_POOL,
-):
-    """Generate employees rows, print rows to file"""
-    for employee in generate_employee_employer_quarterly_wage_rows(
-        base_id, employee_count, employer_account_keys, employer_count_random_pool
-    ):
-        write_employee_line(employee, employees_file)
 
 
 def write_employee_line(employee_wage_info, employees_file):
@@ -339,34 +335,7 @@ def write_employee_line(employee_wage_info, employees_file):
     employees_file.write(line)
 
 
-def generate_employee_employer_quarterly_wage_rows(
-    base_id,
-    employee_count,
-    employer_account_keys,
-    employer_count_random_pool=EMPLOYER_COUNT_RANDOM_POOL,
-):
-    """Generate employee and employer quarterly wage information rows"""
-
-    logger.info("Generating employee rows ...")
-
-    count = 0
-
-    # for the number of employees we want to generate
-    for index in range(employee_count):
-        yield from generate_single_employee(
-            base_id + index, employer_account_keys, employer_count_random_pool
-        )
-
-        count += 1
-        if count > 0 and (count % 1000) == 0:
-            logger.info("Generating employee rows, current employee count: %i", count)
-
-    logger.info("Generated employees info - Employee count: %i", count)
-
-
-def generate_single_employee(
-    employee_generate_id, employer_account_keys, employer_count_random_pool
-):
+def generate_single_employee(employee_generate_id, employers):
     """Generate a single employee.
 
     This is intended to always generate the same fake values for a given employee_generate_id.
@@ -379,23 +348,60 @@ def generate_single_employee(
     first_name = fake.first_name()
     last_name = fake.last_name()
 
-    # randomly pick employers by random count
-    employer_count = random.choice(employer_count_random_pool)
-    employer_account_keys_for_employee = random.sample(employer_account_keys, employer_count)
-
-    # for each employer account key randomly chosen for an employee
-    for account_key in employer_account_keys_for_employee:
+    # for each employer randomly chosen for this employee
+    for employer in employers:
+        account_key = employer["account_key"]
 
         # information about the employees classification with this employer
         independent_contractor = fake.boolean()
         opt_in = fake.boolean()
 
-        # initial quarterly wage
-        qtr_wages = decimal.Decimal(random.randrange(6000000)) / 100
+        wage_change_random_pool: Tuple[int, ...] = WAGE_CHANGE_RANDOM_POOL
+        quarters: Tuple[Quarter, ...] = ()
 
-        # which quarters to generate?
-        start_quarter = random.choice(QUARTERS)
-        quarters = start_quarter.series(random.randint(1, 8))
+        if (employee_generate_id % 100) <= 5:
+            # Special scenario 1: one quarter, wage $4000 to $5000
+            # Financial eligibility: when 2 employers, fails 30x rule
+            qtr_wages = decimal.Decimal(4000 + random.randrange(1000))  # initial quarterly wage
+            quarters = (Quarter(2020, 4),)
+
+        elif (employee_generate_id % 100) <= 10:
+            # Special scenario 2: two quarters, wages $5000 and $10000
+            # Financial eligibility: fails 30x rule
+            qtr_wages = decimal.Decimal(5000)  # initial quarterly wage
+            quarters = (
+                Quarter(2020, 4),
+                Quarter(2021, 1),
+            )
+            wage_change_random_pool = (10000 - 5000,)
+
+        elif (employee_generate_id % 100) <= 13:
+            # Special scenario 3: two quarters, wages $2000 and $2000
+            # Financial eligibility: when 1 employer, not eligible / when 2 employers, eligible
+            qtr_wages = decimal.Decimal(2000)  # initial quarterly wage
+            quarters = (
+                Quarter(2020, 4),
+                Quarter(2021, 1),
+            )
+            wage_change_random_pool = (0,)
+
+        elif (employee_generate_id % 100) <= 15:
+            # Special scenario 4: two quarters, wages $5000 and $21000
+            # Financial eligibility: eligible in 2021, but not in 2022 (due to 2021 max benefit)
+            qtr_wages = decimal.Decimal(5000)  # initial quarterly wage
+            quarters = (
+                Quarter(2021, 3),
+                Quarter(2021, 4),
+            )
+            wage_change_random_pool = (21000 - 5000,)
+
+        else:
+            # General random scenario
+            qtr_wages = decimal.Decimal(random.randrange(6000000)) / 100  # initial quarterly wage
+
+            # which quarters to generate?
+            start_quarter = random.choice(QUARTERS)
+            quarters = start_quarter.series(random.randint(1, 8))
 
         # generate information for the selected quarters:
         ytd_wages = decimal.Decimal(0)
@@ -424,7 +430,7 @@ def generate_single_employee(
 
             yield employee
 
-            qtr_wages += random.choice(WAGE_CHANGE_RANDOM_POOL)
+            qtr_wages += random.choice(wage_change_random_pool)
             if qtr_wages <= 0:
                 qtr_wages = decimal.Decimal(1)
 

api/tests/dor/importer/test_import_dor.py
@@ -571,7 +571,6 @@ def test_parse_employer_file(test_fs_path):
 def test_e2e_parse_and_persist(test_db_session, dor_employer_lookups):
     # generate files for import
     employer_count = 100
-    employee_count = employer_count * generator.EMPLOYER_TO_EMPLOYEE_RATIO
 
     employer_file_path = get_temp_file_path()
     employee_file_path = get_temp_file_path()
@@ -579,7 +578,7 @@ def test_e2e_parse_and_persist(test_db_session, dor_employer_lookups):
     employer_file = open(employer_file_path, "w")
     employee_file = open(employee_file_path, "w")
 
-    generator.generate(employer_count, employer_file, employee_file, [1])
+    generator.generate(employer_count, employer_file, employee_file)
     employer_file.close()
     employee_file.close()
 
@@ -592,7 +591,7 @@ def test_e2e_parse_and_persist(test_db_session, dor_employer_lookups):
 
     assert len(employee_a_lines) == employer_count * 4
     wages_contributions_count = len(employee_b_lines)
-    assert wages_contributions_count >= employee_count * 4
+    assert wages_contributions_count >= employer_count
 
     # import
     import_batches = [
@@ -612,7 +611,7 @@ def test_e2e_parse_and_persist(test_db_session, dor_employer_lookups):
     assert report_one.created_employers_count == employer_count
 
     report_two = reports[1]
-    assert report_two.created_employees_count == employee_count
+    assert report_two.created_employees_count >= employer_count
     assert report_two.created_wages_and_contributions_count == wages_contributions_count
 
     assert report_two.created_employer_quarters_count == len(employee_a_lines)

api/bin/create-user.py
@@ -2,8 +2,11 @@
 import pprint
 import sys
 
+import massgov.pfml.db
 import massgov.pfml.db.models.employees as employee_models
 from massgov.pfml.db.models.factories import UserFactory
+
+massgov.pfml.db.init(sync_lookups=True)
 
 roles = []
 

api/massgov/pfml/api/eligibility/samples/README
@@ -1,0 +1,20 @@
+This directory contains sample payloads for the financial eligibility API.
+
+The payloads assume that data generated using the DOR generator is used.
+
+To use, generate and import fake DOR data:
+
+  make dor-generate
+  make dor-import
+
+Create a user with the FINEOS role:
+
+  make create-user args=fineos
+
+Get a JWT for that user:
+
+  make jwt auth_id=<UUID from active_directory_id attribute>
+
+Run the API calls:
+
+  TOKEN=<JWT token from above> ./run_all.sh

api/massgov/pfml/api/eligibility/samples/run_all.sh
@@ -1,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Run various financial eligibility scenarios.
+#
+
+API=http://localhost:1550/v1/financial-eligibility
+
+for f in *.json
+do
+  echo =============== $f ===============
+  cat "$f"
+  echo "==>"
+  curl --silent \
+       --header "Authorization: Bearer $TOKEN" \
+       --header "Accept: application/json" \
+       --header "Content-Type: application/json" \
+       --data @$f \
+       "$API" \
+    | json_pp
+  echo
+done

api/tests/dor/mock/test_generate.py
@@ -6,104 +6,38 @@ from decimal import Decimal
 import pytest
 
 import massgov.pfml.dor.mock.generate as generate
-from massgov.pfml.dor.importer.dor_file_formats import (
-    EMPLOYEE_FORMAT,
-    EMPLOYER_FILE_FORMAT,
-    EMPLOYER_QUARTER_INFO_FORMAT,
-)
+from massgov.pfml.dor.importer.dor_file_formats import EMPLOYEE_FORMAT
 from massgov.pfml.util.datetime.quarter import Quarter
 
 QUARTERS = tuple(Quarter(2019, 2).series(4))
 
 
 def test_generate_employers():
-    employer_info_list = []
-    employer_wage_info_list = []
+    count = 20
 
-    count = 200
+    employers = tuple(generate.generate_employers(1, count))
 
-    for employer_info, employer_wage_data in generate.generate_employers(1, count):
-        employer_info_list.append(employer_info)
-        employer_wage_info_list.extend(employer_wage_data)
+    wage_rows = generate.generate_employer_wage_rows(employers[0])
 
-    assert len(employer_info_list) == count
-    assert len(employer_wage_info_list) == count * 4  # one line for each quarter
+    assert len(employers) == count
+    assert len(wage_rows) == 4  # one line for each quarter
 
-    validate_employer_object(employer_info_list[0])
-    validate_employer_wage(employer_wage_info_list[0], employer_info_list[0], QUARTERS[0])
-
-
-def test_employer_file_populate():
-    count = 200
-    employers_file = io.StringIO()
-    employer_wage_file = io.StringIO()
-    employer_account_keys = generate.generate_employer_file(
-        1, count, employers_file, employer_wage_file
-    )
-
-    assert len(employer_account_keys) == count
-
-    # employee info
-    employers_file.seek(0)
-    employer_lines = employers_file.readlines()
-    assert len(employer_lines) == count
-
-    employer_line = employer_lines[0].rstrip()
-    assert len(employer_line) == EMPLOYER_FILE_FORMAT.get_line_length()
-
-    parsed_employer_obj = EMPLOYER_FILE_FORMAT.parse_line(employer_line)
-    validate_employer_object(parsed_employer_obj)
-
-    # employee wage info
-    employer_wage_file.seek(0)
-    employer_wage_lines = employer_wage_file.readlines()
-    assert len(employer_wage_lines) == count * 4  # one line for each quarter
-
-    employer_wage_line = employer_wage_lines[0].rstrip()
-    assert len(employer_wage_line) == EMPLOYER_QUARTER_INFO_FORMAT.get_line_length()
-
-    parsed_employer_wage_obj = EMPLOYER_QUARTER_INFO_FORMAT.parse_line(employer_wage_line)
-    validate_employer_wage(parsed_employer_wage_obj, parsed_employer_obj, QUARTERS[0].as_date())
-
-
-def test_generate_employee_employer_quarterly_wage_rows():
-    account_keys = ["%011i" % i for i in range(1, 21)]
-
-    count = 200
-    employees_wage_infos = list(
-        generate.generate_employee_employer_quarterly_wage_rows(1, count, account_keys, [1])
-    )
-    assert len(employees_wage_infos) >= count  # At least 1 line per employee
-
-    ytd = Decimal(0)
-    current_employer = None
-    current_employee = None
-    for employee_wage in employees_wage_infos:
-        if (
-            employee_wage["account_key"] != current_employer
-            or employee_wage["employee_ssn"] != current_employee
-            or employee_wage["filing_period"].quarter == 1
-        ):
-            ytd = Decimal(0)
-            current_employer = employee_wage["account_key"]
-            current_employee = employee_wage["employee_ssn"]
-        ytd += employee_wage["employee_qtr_wages"]
-        assert employee_wage["employee_ytd_wages"] == ytd
-        validate_employee_wage(employee_wage)
+    validate_employer_object(employers[0])
+    validate_employer_wage(wage_rows[0], employers[0], QUARTERS[0])
 
 
 def test_employee_file_populate():
-    count = 200
     employee_wage_file = io.StringIO()
 
-    account_key = "00000000001"
-    employer_account_keys = [account_key]
-    generate.generate_employee_file(1, count, employer_account_keys, employee_wage_file, [1])
+    employers = tuple(generate.generate_employers(1, 3))
+    employee_wage_rows = tuple(generate.generate_single_employee(1, employers))
+    for row in employee_wage_rows:
+        generate.write_employee_line(row, employee_wage_file)
 
     # employee info
     employee_wage_file.seek(0)
     employee_wage_lines = employee_wage_file.readlines()
-    assert len(employee_wage_lines) >= count  # At least 1 line per employee
+    assert len(employee_wage_lines) >= 3  # At least 1 line per employee
 
     employee_wage_line = employee_wage_lines[0].rstrip()
     assert len(employee_wage_line) == EMPLOYEE_FORMAT.get_line_length()
@@ -120,7 +54,7 @@ def test_full_generate():
     employer_file = io.StringIO()
     employer_employee_wage_file = io.StringIO()
 
-    generate.generate(employer_count, employer_file, employer_employee_wage_file, [1])
+    generate.generate(employer_count, employer_file, employer_employee_wage_file)
 
     employer_file.seek(0)
     employer_file_lines = employer_file.readlines()

commit be5e90a2ea3cdd93f616a14ba3c29d75c2b5d96a

API: Fix DOR import test breakage (#2297)
Broken by af271a4a2733892e484f1e0577ccb1404eaeae0c.

api/massgov/pfml/dor/mock/generate.py
@@ -140,8 +140,10 @@ def generate(
 
         # Generate employee rows
         employee_wage_rows: List[Dict[str, Any]] = []
-        for employee_id, employer_ids in employee_employers.items():
-            employee_wage_rows += tuple(generate_single_employee(employee_id, employer_ids))
+        for employee_id, employers_for_employee in employee_employers.items():
+            employee_wage_rows += tuple(
+                generate_single_employee(employee_id, employers_for_employee)
+            )
 
         logger.info(
             "chunk %i: %i employers, %i employees", chunk, len(employers), len(employee_employers)

api/tests/dor/importer/test_import_dor.py
@@ -275,14 +275,14 @@ def test_log_employees_with_new_employers(test_db_session):
 
     # Employee generate helper
     def generate_employee_and_wage_item(id, employer):
-        employee = next(generator.generate_single_employee(id, [employer["account_key"]], [1]))
+        employee = next(generator.generate_single_employee(id, [employer]))
         # convert quarter to date
         employee["filing_period"] = employee["filing_period"].as_date()
         return employee
 
     # Create two employers
-    employer1, employer_wage_rows = generator.generate_single_employer(1)
-    employer2, employer_wage_rows = generator.generate_single_employer(2)
+    employer1 = generator.generate_single_employer(1)
+    employer2 = generator.generate_single_employer(2)
     employers = [employer1, employer2]
 
     report, report_log_entry = get_new_import_report(test_db_session)