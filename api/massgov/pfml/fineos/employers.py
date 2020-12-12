d08e4442c719fa7c4a0981b9f78c735d8e2f17a7
Tanner Doshier <git@doshitan.com>
2020-12-12 00:40:11 -0500

    API-729: Complete support for sending Employer updates to FINEOS  (#2196)
    - Update `load-employers-to-fineos` executable to support an "updates" mode and
      accept a `--process-id` argument
    - Utilize `process_id` on `EmployerLog` to grab batches of `Employer` records to
      update, without overlapping Employers with concurrent processes
    
    Ticket: https://lwd.atlassian.net/browse/API-729

--- api/massgov/pfml/fineos/employers.py
+++ api/massgov/pfml/fineos/employers.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Iterable, MutableSet, Optional
 
 import massgov.pfml.api.services.fineos_actions as fineos_actions
 import massgov.pfml.db as db
@@ -66,54 +66,136 @@ def load_all(db_session: db.Session, fineos: AbstractFINEOSClient) -> LoadEmploy
     return report
 
 
-def load_updates(db_session: db.Session, fineos: AbstractFINEOSClient) -> LoadEmployersReport:
+def load_updates(
+    db_session: db.Session, fineos: AbstractFINEOSClient, process_id: int = 1
+) -> LoadEmployersReport:
     start_time = utcnow()
     report = LoadEmployersReport(start=start_time.isoformat())
 
-    updated_employer_ids_query = (
-        db_session.query(EmployerLog.employer_id)
-        .filter(EmployerLog.action.in_(["INSERT", "UPDATE"]))
-        .group_by(EmployerLog.employer_id)
+    logger.info("Starting Employer updates load to FINEOS", extra={"process_id": process_id})
+
+    batch_size = 100
+    errored_employer_ids: MutableSet[str] = set()
+    employer_batches = get_update_batches(db_session, batch_size, process_id, errored_employer_ids)
+    employer_batches_with_logging = massgov.pfml.util.logging.log_every(
+        logger,
+        employer_batches,
+        count=1,
+        start_time=start_time,
+        item_name="Employer Batch",
+        extra={"process_id": process_id},
     )
 
-    updated_employers_query = db_session.query(Employer).filter(
-        Employer.employer_id.in_(updated_employer_ids_query)
-    )
+    for employers in employer_batches_with_logging:
+        for employer in employers:
+            report.total_employers_count += 1
 
-    employers = db.skip_locked_query(updated_employers_query, Employer.employer_id)
+            # we must commit or rollback the transaction for each item to ensure the
+            # row lock put in place by `skip_locked_query` is released
+            try:
+                is_create = employer.fineos_employer_id is None
 
-    employers_with_logging = massgov.pfml.util.logging.log_every(
-        logger, employers, count=1000, start_time=start_time
-    )
+                fineos_actions.create_or_update_employer(fineos, employer)
+                fineos_actions.create_service_agreement_for_employer(fineos, employer)
 
-    for employer in employers_with_logging:
-        report.total_employers_count += 1
+                db_session.commit()
 
-        # we must commit or rollback the transaction for each item to ensure the
-        # row lock put in place by `skip_locked_query` is released
-        try:
-            fineos_actions.create_or_update_employer(fineos, employer)
-            fineos_actions.create_service_agreement_for_employer(fineos, employer)
+                report.loaded_employers_count += 1
 
-            db_session.commit()
+                # Delete the entries for this Employer that triggered this
+                # particular update process...
+                db_session.query(EmployerLog).filter(
+                    EmployerLog.process_id == process_id,
+                    EmployerLog.employer_id == employer.employer_id,
+                ).delete(synchronize_session=False)
 
-            report.loaded_employers_count += 1
-        except Exception:
-            logger.exception(
-                "Error creating/updating employer in FINEOS",
-                extra={
-                    "internal_employer_id": employer.employer_id,
-                    "fineos_employer_id": employer.fineos_employer_id,
-                },
-            )
+                # ...and clean up any entries we created in the process.
+                #
+                # Scoping the above delete to `process_id` will miss the
+                # "UPDATE" entry that could be added from updating the
+                # `employer.fineos_employer_id` column. So if we think we are
+                # creating the FINEOS employer and will therefore have updated
+                # the row in `employer`, delete the latest "UPDATE" event for
+                # that employer from EmployerLog.
+                if is_create:
+                    db_session.query(EmployerLog).filter(
+                        EmployerLog.employer_log_id
+                        == (
+                            db_session.query(EmployerLog.employer_log_id)
+                            .filter(
+                                EmployerLog.employer_id == employer.employer_id,
+                                EmployerLog.action == "UPDATE",
+                            )
+                            .order_by(EmployerLog.modified_at)
+                            .limit(1)
+                        )
+                    ).delete(synchronize_session=False)
+            except Exception:
+                logger.exception(
+                    "Error creating/updating employer in FINEOS",
+                    extra={
+                        "internal_employer_id": employer.employer_id,
+                        "fineos_employer_id": employer.fineos_employer_id,
+                        "process_id": process_id,
+                    },
+                )
 
-            db_session.rollback()
+                db_session.rollback()
 
-            report.errored_employers_count += 1
-            continue
+                errored_employer_ids.add(employer.employer_id)
+                report.errored_employers_count += 1
+                continue
 
     end_time = utcnow()
     report.end = end_time.isoformat()
     report.process_duration_in_seconds = (end_time - start_time).total_seconds()
 
     return report
+
+
+def get_update_batches(
+    db_session: db.Session, batch_size: int, process_id: int, employer_ids_to_skip: MutableSet[str]
+) -> Iterable[Iterable[Employer]]:
+    """Yields batches of Employers that have not been marked as being processed by another process_id, until none are left."""
+
+    while True:
+        with db_session.begin_nested():
+            db_session.execute(f"LOCK TABLE {EmployerLog.__table__} IN ACCESS EXCLUSIVE MODE;")
+
+            employer_ids_that_are_already_tagged_with_different_process_id_query = db_session.query(
+                EmployerLog.employer_id
+            ).filter(EmployerLog.process_id.isnot(None), EmployerLog.process_id != process_id)
+
+            updated_employer_ids_for_process_query = (
+                db_session.query(EmployerLog.employer_id)
+                .filter(
+                    EmployerLog.action.in_(["INSERT", "UPDATE"]),
+                    EmployerLog.employer_id.notin_(
+                        employer_ids_that_are_already_tagged_with_different_process_id_query
+                    ),
+                    EmployerLog.employer_id.notin_(employer_ids_to_skip),
+                )
+                .group_by(EmployerLog.employer_id)
+                .limit(batch_size)
+            )
+
+            db_session.query(EmployerLog).filter(
+                EmployerLog.employer_id.in_(updated_employer_ids_for_process_query)
+            ).update({EmployerLog.process_id: process_id}, synchronize_session=False)
+
+        updated_employer_ids_query = (
+            db_session.query(EmployerLog.employer_id)
+            .filter(EmployerLog.process_id == process_id)
+            .group_by(EmployerLog.employer_id)
+        )
+
+        updated_employers_query = db_session.query(Employer).filter(
+            Employer.employer_id.in_(updated_employer_ids_query)
+        )
+
+        employer_count = updated_employers_query.count()
+
+        if employer_count == 0:
+            break
+
+        yield db.skip_locked_query(updated_employers_query, Employer.employer_id)
--- api/tests/fineos/test_employers.py
+++ api/tests/fineos/test_employers.py
@@ -93,18 +93,25 @@ def test_load_updates_simple(test_db_session, initialize_factories_session, crea
 
     assert employer.fineos_employer_id is None
 
-    employer_log_entries = (
+    employer_log_entries_before = (
         test_db_session.query(EmployerLog)
         .filter(EmployerLog.employer_id == employer.employer_id)
         .all()
     )
-    assert len(employer_log_entries) == 1
+    assert len(employer_log_entries_before) == 1
 
     result = fineos_employers.load_updates(test_db_session, fineos_client)
 
     assert result.total_employers_count == 1
     assert result.loaded_employers_count == 1
     assert result.errored_employers_count == 0
+
+    employer_log_entries_after = (
+        test_db_session.query(EmployerLog)
+        .filter(EmployerLog.employer_id == employer.employer_id)
+        .all()
+    )
+    assert len(employer_log_entries_after) == 0
 
     test_db_session.refresh(employer)
 
@@ -125,12 +132,12 @@ def test_load_updates_multiple_log_entries_only_run_once(
     test_db_session.commit()
 
     # and ensure it was recorded in the log table
-    employer_log_entries = (
+    employer_log_entries_before = (
         test_db_session.query(EmployerLog)
         .filter(EmployerLog.employer_id == employer.employer_id)
         .all()
     )
-    assert len(employer_log_entries) == 2
+    assert len(employer_log_entries_before) == 2
 
     # then test
     result = fineos_employers.load_updates(test_db_session, fineos_client)
@@ -138,6 +145,13 @@ def test_load_updates_multiple_log_entries_only_run_once(
     assert result.total_employers_count == 1
     assert result.loaded_employers_count == 1
     assert result.errored_employers_count == 0
+
+    employer_log_entries_after = (
+        test_db_session.query(EmployerLog)
+        .filter(EmployerLog.employer_id == employer.employer_id)
+        .all()
+    )
+    assert len(employer_log_entries_after) == 0
 
     test_db_session.refresh(employer)
 
@@ -149,6 +163,9 @@ def test_load_updates_multiple(test_db_session, initialize_factories_session, cr
 
     employers = EmployerOnlyDORDataFactory.create_batch(size=10)
 
+    employer_log_entries_before = test_db_session.query(EmployerLog).all()
+    assert len(employer_log_entries_before) == 10
+
     result = fineos_employers.load_updates(test_db_session, fineos_client)
 
     assert result.total_employers_count == 10
@@ -158,3 +175,94 @@ def test_load_updates_multiple(test_db_session, initialize_factories_session, cr
     for employer in employers:
         test_db_session.refresh(employer)
         assert employer.fineos_employer_id is not None
+
+    employer_log_entries_after = test_db_session.query(EmployerLog).all()
+    assert len(employer_log_entries_after) == 0
+
+
+class SpecialTestException(Exception):
+    """Exception only defined here for ensure mocked exception is bubbled up"""
+
+    pass
+
+
+def test_load_updates_does_not_get_stuck_in_loop_with_failing_employer(
+    test_db_session, initialize_factories_session, create_triggers, monkeypatch
+):
+    fineos_client = massgov.pfml.fineos.MockFINEOSClient()
+
+    employer = EmployerOnlyDORDataFactory.create()
+
+    assert employer.fineos_employer_id is None
+
+    employer_log_entries_before = (
+        test_db_session.query(EmployerLog)
+        .filter(EmployerLog.employer_id == employer.employer_id)
+        .all()
+    )
+    assert len(employer_log_entries_before) == 1
+
+    # have the call to FINEOS fail
+    def mock(*args, **kwargs):
+        raise SpecialTestException
+
+    monkeypatch.setattr(fineos_client, "create_or_update_employer", mock)
+
+    result = fineos_employers.load_updates(test_db_session, fineos_client)
+
+    assert result.total_employers_count == 1
+    assert result.loaded_employers_count == 0
+    assert result.errored_employers_count == 1
+
+    employer_log_entries_after = (
+        test_db_session.query(EmployerLog)
+        .filter(EmployerLog.employer_id == employer.employer_id)
+        .all()
+    )
+    assert len(employer_log_entries_after) == 1
+
+    test_db_session.refresh(employer)
+
+    assert employer.fineos_employer_id is None
+
+
+def test_load_updates_picks_up_left_behind_work(
+    test_db_session, initialize_factories_session, create_triggers, monkeypatch
+):
+    fineos_client = massgov.pfml.fineos.MockFINEOSClient()
+
+    employer = EmployerOnlyDORDataFactory.create()
+
+    assert employer.fineos_employer_id is None
+
+    employer_log_entries = (
+        test_db_session.query(EmployerLog)
+        .filter(EmployerLog.employer_id == employer.employer_id)
+        .all()
+    )
+    assert len(employer_log_entries) == 1
+
+    process_id = 1
+
+    # update the log row to indicate it had already been attempted processing by
+    # this process id, simulating a failed run in the past
+    test_db_session.query(EmployerLog).filter(
+        EmployerLog.employer_id == employer.employer_id
+    ).update({EmployerLog.process_id: process_id})
+
+    result = fineos_employers.load_updates(test_db_session, fineos_client, process_id)
+
+    assert result.total_employers_count == 1
+    assert result.loaded_employers_count == 1
+    assert result.errored_employers_count == 0
+
+    employer_log_entries_after = (
+        test_db_session.query(EmployerLog)
+        .filter(EmployerLog.employer_id == employer.employer_id)
+        .all()
+    )
+    assert len(employer_log_entries_after) == 0
+
+    test_db_session.refresh(employer)
+
+    assert employer.fineos_employer_id is not None
