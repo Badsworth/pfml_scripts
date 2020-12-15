84df41e2d086090c231fb93c76b369f51177c2bb
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2020-12-15 15:28:04 -0500

    API: Alter reference_file table (#2345)

---
+++ api/massgov/pfml/db/migrations/versions/2020_12_15_19_22_35_a1855a6442ed_merge_multiple_heads.py
@@ -0,0 +1,21 @@
+"""Merge multiple heads
+
+Revision ID: a1855a6442ed
+Revises: 8e69b1f33fbb, 59343011b60c
+Create Date: 2020-12-15 19:22:35.118500
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "a1855a6442ed"
+down_revision = ("8e69b1f33fbb", "59343011b60c")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
