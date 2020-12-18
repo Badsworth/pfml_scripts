03eac86c29e6a176af1850cf0ec6a9dbd719e68f
Sawyer Hollenshead <git@sawyerh.com>
2020-12-18 16:45:52 -0500

    CP [infra]: Increase threshold of baseline error rate alert (#2447)
    ## 🛠 Changes
    
    - Increase threshold and aggregation window of our error rate alarms to hopefully reduce number of false alarms.
    
    ## ℹ️ Context for reviewers
    
    These alerts have been unhelpfully triggering a handful of times each day.

--- infra/modules/newrelic_baseline_error_rate/main.tf
+++ infra/modules/newrelic_baseline_error_rate/main.tf
@@ -3,10 +3,14 @@
 # we can identify when an endpoint begins to consistently fail.
 resource "newrelic_nrql_alert_condition" "newrelic_baseline_error_rate" {
   # WARN: Query value above 90% for at least 5 minutes
-  # CRITICAL: Query value above 95% for at least 5 minutes
+  # CRITICAL: Query value equal to 100% for at least 5 minutes
   enabled   = true
   name      = var.name
   policy_id = var.policy_id
+
+  # Collect data points for *10* minutes before running the function in the NRQL query.
+  # A longer window can smooth out choppy data.
+  aggregation_window = 600
 
   type                 = "static"
   value_function       = "single_value"
@@ -25,9 +29,13 @@ resource "newrelic_nrql_alert_condition" "newrelic_baseline_error_rate" {
   }
 
   critical {
-    threshold_duration    = 300
-    threshold             = 95
-    operator              = "above"
+    threshold_duration = 300
+    # This is as high as it goes since 95% was still triggering false alarms for
+    # the Employer Sign Up endpoint. We may want to further tune this for other
+    # endpoints in the future, but this should hopefully cover worst case
+    # scenario (endpoint being down entirely).
+    threshold             = 100
+    operator              = "equals"
     threshold_occurrences = "ALL"
   }
 }
