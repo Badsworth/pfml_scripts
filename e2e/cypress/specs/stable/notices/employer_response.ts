61d56c401210e560109cc2866410da5f479e91c5
Rob Bayliss <rob@lastcallmedia.com>
2020-12-18 15:15:20 -0500

    END-526: Check for employer response e-mail as part of employer_response test (#2438)
    https://lwd.atlassian.net/browse/END-526
    
    Adds assertions for the presence and content of the employer response e-mail to the employer_response test. I'm not deeply checking the employer response e-mail because I feel that doing so is a concern of whatever component manages the templates. Our concern here is:
    
    Was the e-mail delivered
    Does it contain the link we need to act on it.
    
    * END-526: Check for employer response e-mail as part of ...

--- e2e/cypress/specs/stable/notices/employer_response.ts
+++ e2e/cypress/specs/stable/notices/employer_response.ts
@@ -13,8 +13,11 @@ describe("Employer Responses", () => {
       if (!(typeof employer_fein === "string")) {
         throw new Error("No employer_fein property was added to this claim.");
       }
+      cy.stash("claim", claim);
+      cy.stash("timestamp_from", Date.now());
       cy.task("submitClaimToAPI", claim).then((response) => {
         cy.stash("fineos_absence_id", response.fineos_absence_id);
+        cy.stash("applicationID", response.application_id);
         portal.login(getLeaveAdminCredentials(employer_fein));
         portal.respondToLeaveAdminRequest(
           response.fineos_absence_id,
@@ -38,4 +41,30 @@ describe("Employer Responses", () => {
       });
     }
   );
+
+  it("As an employer, I should receive a notification about my response being required", () => {
+    cy.unstash<SimulationClaim>("claim").then((claim) => {
+      if (
+        !claim.claim.employer_fein ||
+        !claim.claim.first_name ||
+        !claim.claim.last_name
+      ) {
+        throw new Error("This employer has no FEIN");
+      }
+      cy.unstash<string>("fineos_absence_id").then((caseNumber) => {
+        cy.unstash<number>("timestamp_from").then((timestamp_from) => {
+          cy.task("getEmails", {
+            address: "gqzap.notifications@inbox.testmail.app",
+            subject: `Action required: Respond to ${claim.claim.first_name} ${claim.claim.last_name}'s paid leave application`,
+            timestamp_from,
+          }).then((emails) => {
+            expect(emails.length).to.be.greaterThan(0);
+            expect(emails[0].html).to.contain(
+              `/employers/applications/new-application/?absence_id=${caseNumber}`
+            );
+          });
+        });
+      });
+    });
+  });
 });
