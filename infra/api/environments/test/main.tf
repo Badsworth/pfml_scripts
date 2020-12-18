ebc519f5a7dc5065c77f74634914b4769cc71685
Tanner Doshier <git@doshitan.com>
2020-12-18 09:37:09 -0500

    API-1059: Fix production RMV URL (#2426)
    The production RMV service does not have the `/vs` part in it's
    structure.
    
    Ticket: https://lwd.atlassian.net/browse/API-1059

--- infra/api/environments/test/main.tf
+++ infra/api/environments/test/main.tf
@@ -71,7 +71,7 @@ module "api" {
   cognito_enable_provisioned_concurrency              = false
   logging_level                                       = "massgov.pfml.fineos.fineos_client=DEBUG"
   formstack_import_lambda_build_s3_key                = local.formstack_lambda_artifact_s3_key
-  rmv_client_base_url                                 = "https://atlas-staging-gateway.massdot.state.ma.us"
+  rmv_client_base_url                                 = "https://atlas-staging-gateway.massdot.state.ma.us/vs"
   rmv_client_certificate_binary_arn                   = "arn:aws:secretsmanager:us-east-1:498823821309:secret:/service/pfml-api-test/rmv_client_certificate-zWimpc"
   rmv_check_behavior                                  = "fully_mocked"
   rmv_check_mock_success                              = "1"
