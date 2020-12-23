9d86d96755bd08fa58a8f1b073b7b17349f205a0
Alsia Plybeah <alsia@navapbc.com>
2020-12-23 17:20:05 -0500

    [API-938][API-695] Send Security Scans to Pagerduty (#2468)
    🎫 Ticket
    https://lwd.atlassian.net/browse/API-938
    https://lwd.atlassian.net/browse/API-695
    
    🛠 Changes
    Generic integration added for PD alerts
    Additional step added to the API-Scheduled job to send alerts to PD
    ℹ️ Context for reviewers
    This ticket is to configure a final step to send alerts to PD, and to configure the bandit/safety checks in this workflow to only fail on high-severity vulnerabilities. LOW or MED severity issues should be picked up automatically by PR checks and released to stage/prod within ~7 days.

--- infra/pagerduty/outputs.tf
+++ infra/pagerduty/outputs.tf
@@ -27,3 +27,7 @@ output "high_priority_cloudwatch_integration_key" {
   value       = pagerduty_service_integration.cloudwatch_high_priority_notification.integration_key
   description = "this key is used to redirect high priority events to the AWS CloudWatch integration"
 }
+output "security_scan_notification_channel_integration_key" {
+  value       = pagerduty_service_integration.security_scan_notification_channel.integration_key
+  description = "this key is used to redirect Security and Bandit events to the Events Api integration"
+}
