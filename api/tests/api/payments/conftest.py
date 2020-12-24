a36566795da095f16b61d6ee5e3f44779922248f
Jiayun Fang <jnfang@users.noreply.github.com>
2020-12-24 10:36:39 -0800

    API-682/API-780: Build PEI Writeback file, upload to S3, and store reference files (#2393)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/API-682
    https://lwd.atlassian.net/browse/API-780
    
    ## 🛠 Changes
    
    - Created fineos_pei_writeback.py for generating PEI writeback file, uploading to S3 buckets, and updating metadata
    - Wrote tests
    
    ## ℹ️ Context for reviewers
    
    - Ran unit tests
    - Successfully tested saving the CSV to S3
    
    ## 👀 Demo
    Gist covering how I tested uploading to S3 and confirmed that the correct CSV is generated. It's calling an old version of the code but a similar test can be done by calling `fineos_pei_writeback.write_to_s3_and_save_reference_files` which will also write the reference files to the db in addition to writing to s3. 
    
    https://gist.github.com/jnfang/7002a8b83b5723ba9777752cde1948bb 
    
    Gist in Python of inputs to REPL to test creation of ReferenceFile by using `uuid_gen`: https://gist.github.com/jnfang/5ace31cfd0a7964d528ae4a476466721
    
    CSV produced:
    ```
    pei_C_value,pei_I_value,status,statusEffectiveDate,statusReason,stockNo,extractionDate,transactionNo,transactionStatus,transStatusDate
    123,456,Active,,Pending,,06/25/1972,,,
    123,456,Active,,Pending,,04/18/2013,,,
    
    ```
    
    
    ## 🌡 End to End testing
    N/A

---
+++ api/tests/api/payments/conftest.py
@@ -0,0 +1,20 @@
+import boto3
+import pytest
+
+
+@pytest.fixture
+def mock_fineos_s3_bucket(mock_s3_bucket):
+    # This relies on the context manager setup in the mock_s3_bucket fixture
+    # which remains open because it uses yield rather than return
+    s3 = boto3.resource("s3")
+    bucket_name = "fineos_bucket"
+    s3.create_bucket(Bucket=bucket_name)
+    yield bucket_name
+
+
+@pytest.fixture
+def set_exporter_env_vars(mock_s3_bucket, mock_fineos_s3_bucket, monkeypatch):
+    monkeypatch.setenv("FINEOS_DATA_EXPORT_PATH", f"s3://{mock_fineos_s3_bucket}/DT2/dataexports/")
+    monkeypatch.setenv("PFML_FINEOS_INBOUND_PATH", f"s3://{mock_s3_bucket}/cps/inbound/received/")
+    monkeypatch.setenv("FINEOS_DATA_IMPORT_PATH", f"s3://{mock_fineos_s3_bucket}/DT2/dataimports/")
+    monkeypatch.setenv("PFML_FINEOS_OUTBOUND_PATH", f"s3://{mock_s3_bucket}/cps/outbound/")
--- api/tests/api/payments/test_fineos_payment_export.py
+++ api/tests/api/payments/test_fineos_payment_export.py
@@ -30,24 +30,6 @@ from massgov.pfml.payments.payments_util import ValidationIssue, ValidationReaso
 ### UTILITY METHODS
 
 
-@pytest.fixture
-def mock_fineos_s3_bucket(mock_s3_bucket):
-    # This relies on the context manager setup in the mock_s3_bucket fixture
-    # which remains open because it uses yield rather than return
-    s3 = boto3.resource("s3")
-    bucket_name = "fineos_bucket"
-    s3.create_bucket(Bucket=bucket_name)
-    yield bucket_name
-
-
-@pytest.fixture
-def set_exporter_env_vars(mock_s3_bucket, mock_fineos_s3_bucket, monkeypatch):
-    monkeypatch.setenv("FINEOS_DATA_EXPORT_PATH", f"s3://{mock_fineos_s3_bucket}/DT2/dataexports/")
-    monkeypatch.setenv("PFML_FINEOS_INBOUND_PATH", f"s3://{mock_s3_bucket}/cps/inbound/received/")
-    monkeypatch.setenv("FINEOS_DATA_IMPORT_PATH", "n/a")
-    monkeypatch.setenv("PFML_FINEOS_OUTBOUND_PATH", "n/a")
-
-
 def make_s3_zip_file(s3_bucket, key, test_file_name):
     # Utility method to convert one of our test files to a zipped
     # file in a temporary test directory and upload it to the mocked S3.
