Badsworth/pfml_scripts

commit 729b510d69b8da4cc2b887ee8b5705dfbeb86b6f

API-1165: enhance email functionality (#2723)
consolidate two email methods into one.

infra/ecs-tasks/template/tasks.tf
@@ -164,6 +164,7 @@ locals {
 
         pfml_email_address                     = var.pfml_email_address
         bounce_forwarding_email_address        = var.bounce_forwarding_email_address
+        bounce_forwarding_email_address_arn    = var.bounce_forwarding_email_address_arn
         ctr_gax_bievnt_email_address           = var.ctr_gax_bievnt_email_address
         ctr_vcc_bievnt_email_address           = var.ctr_vcc_bievnt_email_address
         dfml_business_operations_email_address = var.dfml_business_operations_email_address

api/massgov/pfml/payments/config.py
@@ -148,6 +148,7 @@ class PaymentsEmailConfig:
     # Bounce forwarding email address
     # Ex: noreplypfml@mass.gov
     bounce_forwarding_email_address: str
+    bounce_forwarding_email_address_arn: str
     # Recipient email address for GAX BIEVNT info
     # Ex: Accounts.Payable@detma.org
     ctr_gax_bievnt_email_address: str
@@ -165,6 +166,7 @@ def get_email_config() -> PaymentsEmailConfig:
         bounce_forwarding_email_address=os.getenv(
             "BOUNCE_FORWARDING_EMAIL_ADDRESS", "noreplypfml@mass.gov"
         ),
+        bounce_forwarding_email_address_arn=os.getenv("BOUNCE_FORWARDING_EMAIL_ADDRESS_ARN", ""),
         ctr_gax_bievnt_email_address=os.getenv("CTR_GAX_BIEVNT_EMAIL_ADDRESS", ""),
         ctr_vcc_bievnt_email_address=os.getenv("CTR_VCC_BIEVNT_EMAIL_ADDRESS", ""),
         dfml_business_operations_email_address=os.getenv(
@@ -177,6 +179,7 @@ def get_email_config() -> PaymentsEmailConfig:
         extra={
             "pfml_email_address": payments_email_config.pfml_email_address,
             "bounce_forwarding_email_address": payments_email_config.bounce_forwarding_email_address,
+            "bounce_forwarding_email_address_arn": payments_email_config.bounce_forwarding_email_address_arn,
             "ctr_gax_bievnt_email_address": payments_email_config.ctr_gax_bievnt_email_address,
             "ctr_vcc_bievnt_email_address": payments_email_config.ctr_vcc_bievnt_email_address,
             "dfml_business_operations_email_address": payments_email_config.dfml_business_operations_email_address,

api/massgov/pfml/payments/error_reports.py
@@ -25,6 +25,7 @@ from massgov.pfml.db.models.employees import (
 
 # from massgov.pfml.util.aws.ses import EmailRecipient, send_email_with_attachment
 
+
 logger = logging.get_logger(__name__)
 
 ####
@@ -431,8 +432,14 @@ def _send_errors_email(subject: str, body: str, error_files: List[pathlib.Path])
     email_config = payments_config.get_email_config()
     sender = email_config.pfml_email_address
     recipient = EmailRecipient(to_addresses=[email_config.dfml_business_operations_email_address])
-    send_email_with_attachment(
-        recipient=recipient, subject=subject, body_text=body, sender=sender, attachments=error_files
+    send_email(
+        recipient=recipient,
+        subject=subject,
+        body_text=body,
+        sender=sender,
+        bounce_forwarding_email_address_arn=email_config.bounce_forwarding_email_address_arn,
+        attachments=error_files
+
     )
     """
     s3_prefix = payments_config.get_s3_config().pfml_error_reports_path

api/massgov/pfml/payments/payments_util.py
@@ -32,7 +32,7 @@ from massgov.pfml.db.models.employees import (
     ReferenceFileType,
     State,
 )
-from massgov.pfml.util.aws.ses import EmailRecipient, send_email, send_email_with_attachment
+from massgov.pfml.util.aws.ses import EmailRecipient, send_email
 
 logger = logging.get_logger(__package__)
 
@@ -652,7 +652,7 @@ def get_inf_data_as_plain_text(inf_data: Dict) -> str:
 
 
 def email_inf_data(
-    ref_file: ReferenceFile, db_session: db.Session, email: EmailRecipient, subject: str
+    ref_file: ReferenceFile, db_session: db.Session, recipient: EmailRecipient, subject: str
 ) -> None:
 
     inf_data = get_inf_data_from_reference_file(ref_file, db_session)
@@ -665,13 +665,15 @@ def email_inf_data(
         return
 
     payment_config = payments_config.get_email_config()
+    sender = payment_config.pfml_email_address
     data = get_inf_data_as_plain_text(inf_data)
+    bounce_forwarding_email_address_arn = payment_config.bounce_forwarding_email_address_arn
     send_email(
-        email,
-        subject,
-        data,
-        payment_config.pfml_email_address,
-        payment_config.bounce_forwarding_email_address,
+        recipient=recipient,
+        subject=subject,
+        body_text=data,
+        sender=sender,
+        bounce_forwarding_email_address_arn=bounce_forwarding_email_address_arn,
     )
 
 
@@ -697,8 +699,13 @@ def email_fineos_vendor_customer_numbers(
     fieldnames = ["fineos_customer_number", "ctr_vendor_customer_code"]
     csv_data_path = [create_csv_from_list(fineos_vendor_customer_numbers, fieldnames)]
 
-    send_email_with_attachment(
-        email, subject, body_text, payment_config.pfml_email_address, csv_data_path
+    send_email(
+        email,
+        subject,
+        body_text,
+        payment_config.pfml_email_address,
+        payment_config.bounce_forwarding_email_address_arn,
+        csv_data_path,
     )
 
 

api/massgov/pfml/util/aws/ses.py
@@ -62,38 +62,8 @@ def send_email(
     subject: str,
     body_text: str,
     sender: str,
-    bounce_forwarding_email: str,
-) -> Dict:
-    aws_ses = boto3.client("ses")
-
-    try:
-        response = aws_ses.send_email(
-            Destination={
-                "BccAddresses": recipient.bcc_addresses,
-                "CcAddresses": recipient.cc_addresses,
-                "ToAddresses": recipient.to_addresses,
-            },
-            Message={
-                "Body": {"Text": {"Charset": CHARSET, "Data": body_text,},},
-                "Subject": {"Charset": CHARSET, "Data": subject,},
-            },
-            Source=sender,
-            ReturnPath=bounce_forwarding_email,
-        )
-        logger.info(f"Email sent! Message ID: {response['MessageId']}")
-        return response
-    except ClientError as e:
-        error_message = e.response["Error"]["Message"]
-        logger.exception("Error sending email: %s", error_message)
-        raise RuntimeError("Error sending email: %s", error_message)
-
-
-def send_email_with_attachment(
-    recipient: EmailRecipient,
-    subject: str,
-    body_text: str,
-    sender: str,
-    attachments: List[pathlib.Path],
+    bounce_forwarding_email_address_arn: str,
+    attachments: Optional[List[pathlib.Path]] = None,
 ) -> Dict:
     """
     attachments is a list containing the full-paths to the file that will be attached to the email.
@@ -105,11 +75,9 @@ def send_email_with_attachment(
 
     msg["Subject"] = subject
     msg["From"] = sender
-    msg["To"] = ", ".join(
-        recipient.to_addresses + (recipient.cc_addresses if recipient.cc_addresses else [])
-    )
-    if recipient.bcc_addresses:
-        msg["Bcc"] = ", ".join(recipient.bcc_addresses if recipient.bcc_addresses else [])
+    msg["To"] = ", ".join(recipient.to_addresses)
+    msg["CC"] = ", ".join(recipient.cc_addresses if recipient.cc_addresses else [])
+    msg["Bcc"] = ", ".join(recipient.bcc_addresses if recipient.bcc_addresses else [])
 
     msg_body = MIMEMultipart("alternative")
     msg_text = MIMEText(str(body_text.encode(CHARSET)), "plain", CHARSET)
@@ -118,11 +86,15 @@ def send_email_with_attachment(
     msg.attach(msg_body)
 
     # Add the attachment to the parent container.
-    create_email_attachments(msg, attachments)
+    if attachments is not None:
+        create_email_attachments(msg, attachments)
 
     try:
         response = aws_ses.send_raw_email(
-            Source=msg["From"], Destinations=[msg["To"]], RawMessage={"Data": msg.as_string(),},
+            Source=msg["From"],
+            Destinations=[msg["To"], msg["CC"], msg["Bcc"]],
+            RawMessage={"Data": msg.as_string(),},
+            ReturnPathArn=bounce_forwarding_email_address_arn,
         )
 
         logger.info(f"Email sent! Message ID: {response['MessageId']}")

api/tests/api/payments/test_config.py
@@ -4,6 +4,10 @@ import massgov.pfml.payments.config as payments_config
 def test_get_config_fully_specified(monkeypatch):
     monkeypatch.setenv("PFML_EMAIL_ADDRESS", "noreplypfml@mass.gov")
     monkeypatch.setenv("BOUNCE_FORWARDING_EMAIL_ADDRESS", "noreplypfml@mass.gov")
+    monkeypatch.setenv(
+        "BOUNCE_FORWARDING_EMAIL_ADDRESS_ARN",
+        "arn:aws:ses:us-east-1:498823821309:identity/noreplypfml@mass.gov",
+    )
     monkeypatch.setenv("CTR_GAX_BIEVNT_EMAIL_ADDRESS", "test1@test.com")
     monkeypatch.setenv("CTR_VCC_BIEVNT_EMAIL_ADDRESS", "test2@test.com")
     monkeypatch.setenv("DFML_BUSINESS_OPERATIONS_EMAIL_ADDRESS", "test3@test.com")
@@ -13,6 +17,7 @@ def test_get_config_fully_specified(monkeypatch):
     assert payments_email_config == payments_config.PaymentsEmailConfig(
         pfml_email_address="noreplypfml@mass.gov",
         bounce_forwarding_email_address="noreplypfml@mass.gov",
+        bounce_forwarding_email_address_arn="arn:aws:ses:us-east-1:498823821309:identity/noreplypfml@mass.gov",
         ctr_gax_bievnt_email_address="test1@test.com",
         ctr_vcc_bievnt_email_address="test2@test.com",
         dfml_business_operations_email_address="test3@test.com",

api/tests/conftest.py
@@ -207,6 +207,10 @@ def mock_ses(monkeypatch, reset_aws_env_vars):
 
     monkeypatch.setenv("PFML_EMAIL_ADDRESS", "noreplypfml@mass.gov")
     monkeypatch.setenv("BOUNCE_FORWARDING_EMAIL", "noreplypfml@mass.gov")
+    monkeypatch.setenv(
+        "BOUNCE_FORWARDING_EMAIL_ADDRESS_ARN",
+        "arn:aws:ses:us-east-1:498823821309:identity/noreplypfml@mass.gov",
+    )
     monkeypatch.setenv("CTR_GAX_BIEVNT_EMAIL_ADDRESS", "test1@example.com")
     monkeypatch.setenv("CTR_VCC_BIEVNT_EMAIL_ADDRESS", "test2@example.com")
     monkeypatch.setenv("DFML_BUSINESS_OPERATIONS_EMAIL_ADDRESS", "test3@example.com")
@@ -215,7 +219,7 @@ def mock_ses(monkeypatch, reset_aws_env_vars):
     with moto.mock_ses():
         ses = boto3.client("ses")
         ses.verify_email_identity(EmailAddress=os.getenv("PFML_EMAIL_ADDRESS"))
-        yield
+        yield ses
 
 
 @pytest.fixture

api/tests/util/aws/test_ses.py
@@ -1,49 +1,81 @@
 import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 import pytest
 from pydantic import ValidationError
 
 import massgov.pfml.payments.payments_util as payments_util
-import massgov.pfml.util.aws.ses as ses
+import massgov.pfml.util.aws.ses as conn
 from massgov.pfml.api.validation.exceptions import ValidationException
 
 
+def get_raw_email_msg_object():
+    message = MIMEMultipart("mixed")
+    message["Subject"] = "Test"
+    message["To"] = "to@example.com"
+    message["CC"] = "cc@example.com"
+    message["Bcc"] = "bcc@example.com"
+
+    # Message body
+    msg_body = MIMEMultipart("alternative")
+    msg_txt = MIMEText("test file attached")
+    msg_body.attach(msg_txt)
+    message.attach(msg_body)
+
+    # Attachment
+    csv_file_path = [payments_util.create_csv_from_list([{"name": "test"}], ["name"])]
+    conn.create_email_attachments(message, csv_file_path)
+    return message
+
+
 def test_send_email(mock_ses):
-    recipient = ses.EmailRecipient(to_addresses=["test@example.com"])
-    sender = os.getenv("PFML_EMAIL_ADDRESS")
-    subject = "INF data"
-    body_text = "Test Message"
-    response = ses.send_email(recipient, subject, body_text, sender, sender)
-    assert response is not None
-    assert response["MessageId"] is not None
-    assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
-
-
-def test_send_email_with_attachments(mock_ses):
-    recipient = ses.EmailRecipient(to_addresses=["test@example.com"])
+    recipient = conn.EmailRecipient(
+        to_addresses=["test@example.com"],
+        cc_addresses=["test@example.com"],
+        bcc_addresses=["test@example.com"],
+    )
     sender = os.getenv("PFML_EMAIL_ADDRESS")
     subject = "INF data"
     body_text = "Test Message"
 
+    bounce_forwarding_email_address_arn = os.getenv("BOUNCE_FORWARDING_EMAIL_ADDRESS_ARN", "")
     fieldnames = ["fineos_customer_number", "ctr_vendor_customer_code"]
     fineos_vendor_customer_numbers = [
         {"fineos_customer_number": 1, "ctr_vendor_customer_code": 1},
         {"fineos_customer_number": 2, "ctr_vendor_customer_code": 2},
     ]
     attachments = [payments_util.create_csv_from_list(fineos_vendor_customer_numbers, fieldnames)]
-    response = ses.send_email_with_attachment(recipient, subject, body_text, sender, attachments)
+    response = conn.send_email(
+        recipient, subject, body_text, sender, bounce_forwarding_email_address_arn, attachments
+    )
 
     assert response is not None
     assert response["MessageId"] is not None
     assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
 
 
+def test_send_email_attachments(mock_ses):
+    message = get_raw_email_msg_object()
+    sender = os.getenv("PFML_EMAIL_ADDRESS")
+    kwargs = dict(Source=sender, RawMessage={"Data": message.as_string()})
+
+    mock_ses.send_raw_email(**kwargs)
+
+    send_quota = mock_ses.get_send_quota()
+    sent_count = int(send_quota["SentLast24Hours"])
+    assert sent_count == len(message["To"].split(",")) + len(message["CC"].split(",")) + len(
+        message["Bcc"].split(",")
+    )
+    assert mock_ses.list_identities()["Identities"] == [sender]
+
+
 def test_email_format():
 
     # valid email addresses
-    ses.EmailRecipient(to_addresses=["test@example.com"])
-    ses.EmailRecipient(to_addresses=["test1@example.com", "test2@example.com"])
-    ses.EmailRecipient(
+    conn.EmailRecipient(to_addresses=["test@example.com"])
+    conn.EmailRecipient(to_addresses=["test1@example.com", "test2@example.com"])
+    conn.EmailRecipient(
         to_addresses=["test@example.com"],
         cc_addresses=["test@example.com"],
         bcc_addresses=["test@example.com"],
@@ -51,20 +83,20 @@ def test_email_format():
 
     # invalid email addresses
     with pytest.raises(ValidationException):
-        ses.EmailRecipient(to_addresses=[])
+        conn.EmailRecipient(to_addresses=[])
     with pytest.raises(ValidationException):
-        ses.EmailRecipient(to_addresses=[""])
+        conn.EmailRecipient(to_addresses=[""])
     with pytest.raises(ValidationError):
-        ses.EmailRecipient(cc_addresses=["test@test.com"])
+        conn.EmailRecipient(cc_addresses=["test@test.com"])
     with pytest.raises(ValidationException):
-        ses.EmailRecipient(to_addresses=["test@test.com", ""])
+        conn.EmailRecipient(to_addresses=["test@test.com", ""])
     with pytest.raises(ValidationException):
-        ses.EmailRecipient(to_addresses=[""], bcc_addresses=["test@test.com"])
+        conn.EmailRecipient(to_addresses=[""], bcc_addresses=["test@test.com"])
     with pytest.raises(ValidationException):
-        ses.EmailRecipient(to_addresses=["test@test.com"], cc_addresses=[""])
+        conn.EmailRecipient(to_addresses=["test@test.com"], cc_addresses=[""])
     with pytest.raises(ValidationException):
-        ses.EmailRecipient(to_addresses=["test@test.com"], bcc_addresses=["test@test.com", ""])
+        conn.EmailRecipient(to_addresses=["test@test.com"], bcc_addresses=["test@test.com", ""])
     with pytest.raises(ValidationException):
-        ses.EmailRecipient(
+        conn.EmailRecipient(
             to_addresses=["test@test.com"], cc_addresses=[""], bcc_addresses=["test@test.com"]
         )

commit d0cbf9cdfc6248eec8a4f4fe18e5dea961e90b6e

API-1165: Fix terraform config (#2772)
Set `bounce_forwarding_email_address_arn` variable for all ECS tasks
alongside the existing `bounce_forwarding_email_address`.

It's only used by the two payments tasks, but since
`bounce_forwarding_email_address` is set globally already, just moved it there.

Fixes breakage introduced in 729b510d69b8da4cc2b887ee8b5705dfbeb86b6f

Ticket: https://lwd.atlassian.net/browse/API-1165

infra/ecs-tasks/template/tasks.tf
@@ -164,8 +164,6 @@ locals {
 
         dfml_project_manager_email_address     = var.dfml_project_manager_email_address
         pfml_email_address                     = var.pfml_email_address
-        bounce_forwarding_email_address        = var.bounce_forwarding_email_address
-        bounce_forwarding_email_address_arn    = var.bounce_forwarding_email_address_arn
         ctr_gax_bievnt_email_address           = var.ctr_gax_bievnt_email_address
         ctr_vcc_bievnt_email_address           = var.ctr_vcc_bievnt_email_address
         dfml_business_operations_email_address = var.dfml_business_operations_email_address
@@ -277,6 +275,7 @@ data "template_file" "task_container_definitions" {
 
     pfml_email_address                     = var.pfml_email_address
     bounce_forwarding_email_address        = var.bounce_forwarding_email_address
+    bounce_forwarding_email_address_arn    = var.bounce_forwarding_email_address_arn
     ctr_gax_bievnt_email_address           = var.ctr_gax_bievnt_email_address
     ctr_vcc_bievnt_email_address           = var.ctr_vcc_bievnt_email_address
     dfml_business_operations_email_address = var.dfml_business_operations_email_address