ee9f427d59e68efc584b94fc5ecb71affc2ba6c7
Kevin Yeh <kevin@navapbc.com>
2021-02-09 12:02:17 -0500

    INFRA-231: Add new eol.mass.gov email domain identity (#3024)
    https://lwd.atlassian.net/browse/INFRA-231
    
        Add a new domain identity verification in SES for eol.mass.gov
        Add a new email identity verification in SES for PFML_DoNotReply@eol.mass.gov
        Add a new domain identity verification in SES for eol.state.ma.us
        Add a new email identity verification in SES for PFML_DoNotReply@eol.state.ma.us
    
    ℹ️ Context for reviewers
    
    Our email identity is being transitioned from noreplypfml@mass.gov to PFML_DoNotReply@eol.mass.gov in order to support SPF and DKIM verification and prevent superfluous security-related bounces from email servers.
    
    Ron will follow up after this to ensure:
    
        the domain + email address resources are verified
        the proper records are added for SPF/DKIM
        the SNS topics are hooked up

--- infra/pfml-aws/ses.tf
+++ infra/pfml-aws/ses.tf
@@ -1,4 +1,31 @@
-# Add the email we want to send from	
+# Deprecated email identity
 resource "aws_ses_email_identity" "noreply" {
   email = "noreplypfml@mass.gov"
+}
+
+# New email domain and identity -- see https://lwd.atlassian.net/browse/PFMLPB-640.
+#
+resource "aws_ses_domain_identity" "eol" {
+  domain = "eol.mass.gov"
+}
+
+resource "aws_ses_domain_dkim" "eol" {
+  domain = aws_ses_domain_identity.eol.domain
+}
+
+resource "aws_ses_email_identity" "pfml_donotreply" {
+  email = "PFML_DoNotReply@eol.mass.gov"
+}
+
+# Alias
+resource "aws_ses_domain_identity" "eol_state" {
+  domain = "eol.state.ma.us"
+}
+
+resource "aws_ses_domain_dkim" "eol_state" {
+  domain = aws_ses_domain_identity.eol_state.domain
+}
+
+resource "aws_ses_email_identity" "pfml_donotreply_state" {
+  email = "PFML_DoNotReply@eol.state.ma.us"
 }
