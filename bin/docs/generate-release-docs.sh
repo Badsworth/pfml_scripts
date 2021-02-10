65ca287d7aa3ec996bdfe05b3da492198b51d581
Kevin Yeh <kevin@navapbc.com>
2021-02-10 11:57:27 -0500

    INFRA-248: Generate release page (#3042)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/INFRA-248
    
    ## 🛠 Changes
    
    - Add API and Portal release pages, which include the last (attempted) version deployment for each environment + the list of tickets slated for the next release.
    - Trigger an update to Github Pages on every merged PR, or whenever a deployment happens to a non-test environment.
    
    ## ℹ️ Context for reviewers
    
    This is me trying to make this information more accessible to anyone with Github access. Things you can do now:
    
    - Find all the versions for each environment on one page.
    - See the list of tickets, every time a change is merged.
    - Copy the list of tickets directly from the page if you don't want to update your local branches and run `make release-notes` locally.
    - Find a link to deployments activity for each environment (if you go from the main Deployments page, you can only select the 10 most recently deployed environments because clearly GitHub doesn't care very much for this feature.)
    
    Note that the `test` version may not be accurate if someone deploys a feature branch, it didn't seem very important to try and fix that right now.
    
    ## 👀 Demo
    
    http://eolwd.github.io/pfml/releases/api
    http://eolwd.github.io/pfml/releases/portal

---
+++ bin/docs/generate-release-docs.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+#
+# Generates release pages. These document the current release on each environment, and what changes are next up from the main branch.
+#
+
+regex_escape_all () {
+    while read data; do
+        echo $data | sed -E 's/([/. ?()])/\\\1/g'
+    done
+}
+
+regex_escape_pipes () {
+    while read data; do
+        echo $data | sed -E 's/([| ])/\\\1/g'
+    done
+}
+
+
+mkdir -p docs/releases
+
+for COMPONENT in api portal; do
+    # Grab the version tag, if it exists, and create a link to the Github release page around it.
+    #
+    # e.g.
+    # old: api/v1.1.0
+    # new: <a href="...">api/v1.1.0</a>
+    #
+    VERSION_CAPTURE="($COMPONENT\/v.*)"
+    GITHUB_RELEASE_LINK_FORMAT=$(echo '<a href="https://github.com/EOLWD/pfml/releases/tag/\\\1">\\\1</a>' | regex_escape_all)
+
+    # Grab the first two columns, i.e. the environment name and release version.
+    # Insert them back in as-is, then add a link to the GH Deployments page for the environment.
+    #
+    # e.g.
+    # old: |test|api/v1.1.0
+    # new: |test|api/v1.1.0|<a href="..."GitHub</a>
+    #
+    ENV_CAPTURE=$(echo '| *(.*)|(.*)$' | regex_escape_pipes)
+    ENV_REPLACE=$(echo '|\\\1|\\\2|' | regex_escape_pipes)
+    GITHUB_DEPLOYMENTS_FORMAT=$(echo '<a href="https://github.com/EOLWD/pfml/deployments/activity_log?environment='$COMPONENT'+(\\\1)">GitHub</a>' | regex_escape_all)
+
+    released=$(make -s -C $COMPONENT whats-released-short |
+                   sed -E "s/origin\/(deploy\/$COMPONENT\/)?//g" |
+                   sed -E 's/master/test/g' |
+                   sed -E 's/(\*|:)/\|/g' |
+                   sed -E "s/$VERSION_CAPTURE/$GITHUB_RELEASE_LINK_FORMAT/g" |
+                   sed -E "s/$ENV_CAPTURE/$ENV_REPLACE$GITHUB_DEPLOYMENTS_FORMAT/g")
+
+    tickets=$(make -s -C $COMPONENT release-list)
+
+    if [[ $COMPONENT == 'api' ]]; then
+        CONFLUENCE_NOTES='https://lwd.atlassian.net/wiki/spaces/DD/pages/1086226543/2021+Releases'
+    else
+        CONFLUENCE_NOTES='https://lwd.atlassian.net/wiki/spaces/DD/pages/819036412/Portal+Release+Notes'
+    fi
+
+    cat > docs/releases/$COMPONENT.md <<EOF
+|environment|release|deployments|
+|----|----|----|
+$released
+
+Full release notes are available in [Confluence]($CONFLUENCE_NOTES).
+
+Next Release ([Jira List](https://lwd.atlassian.net/issues/?jql=id in ($tickets))):
+
+$(make -s -C $COMPONENT release-notes | sort -r)
+
+<details>
+<summary>Raw</summary>
+$(make -s -C $COMPONENT release-notes | sort -r | sed -E 's/\*/<br\>\*/g')
+</details>
+EOF
+done
