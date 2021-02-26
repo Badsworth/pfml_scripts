1d7306ad6b292b3b8b20c5b255e00e81bc2b8e27
Lawrence Kang <lawrencekang@gmail.com>
2021-02-25 17:48:38 -0800

    CP-1817 refactor dob test (#3197)
    🎫 Ticket
    https://lwd.atlassian.net/browse/CP-1814
    Subtask: https://lwd.atlassian.net/browse/CP-1817
    
    🛠 Changes
    Refactor test to include user action.

--- portal/tests/pages/applications/date-of-birth.test.js
+++ portal/tests/pages/applications/date-of-birth.test.js
@@ -1,32 +1,43 @@
+import { renderWithAppLogic, simulateEvents } from "../../test-utils";
 import DateOfBirth from "../../../src/pages/applications/date-of-birth";
 import { pick } from "lodash";
-import { renderWithAppLogic } from "../../test-utils";
 
 jest.mock("../../../src/hooks/useAppLogic");
+
+const DATE_OF_BIRTH = "2019-02-28";
 
 describe("DateOfBirth", () => {
   let appLogic, claim, wrapper;
 
-  beforeEach(() => {
+  const render = (claimAttrs = {}) => {
     ({ appLogic, claim, wrapper } = renderWithAppLogic(DateOfBirth, {
-      claimAttrs: {
-        date_of_birth: "2019-02-28",
-      },
+      claimAttrs,
     }));
-  });
+  };
 
   it("renders the page", () => {
+    render({ date_of_birth: DATE_OF_BIRTH });
     expect(wrapper).toMatchSnapshot();
   });
 
-  describe("when the form is successfully submitted", () => {
-    it("calls claims.update", () => {
-      wrapper.find("QuestionPage").simulate("save");
+  it("calls claims.update when the form is successfully submitted with newly-entered data", () => {
+    render();
+    const { changeField } = simulateEvents(wrapper);
+    changeField("date_of_birth", DATE_OF_BIRTH);
+    wrapper.find("QuestionPage").simulate("save");
 
-      expect(appLogic.claims.update).toHaveBeenCalledWith(
-        expect.any(String),
-        pick(claim, ["date_of_birth"])
-      );
+    expect(appLogic.claims.update).toHaveBeenCalledWith(expect.any(String), {
+      date_of_birth: DATE_OF_BIRTH,
     });
+  });
+
+  it("calls claims.update when the form is successfully submitted with pre-filled data", () => {
+    render({ date_of_birth: DATE_OF_BIRTH });
+    wrapper.find("QuestionPage").simulate("save");
+
+    expect(appLogic.claims.update).toHaveBeenCalledWith(
+      expect.any(String),
+      pick(claim, ["date_of_birth"])
+    );
   });
 });
