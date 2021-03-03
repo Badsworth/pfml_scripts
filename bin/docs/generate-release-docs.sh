6edefeb3c61fd8dd300da682894abfb4c416cbdf
Kevin Yeh <kevin@navapbc.com>
2021-03-03 17:14:20 -0500

    INFRA-295: provide better release info for untagged heads (#3251)
    https://lwd.atlassian.net/browse/INFRA-295
    
        Print and parse untagged commit heads so more information can be displayed.

--- bin/docs/generate-release-docs.sh
+++ bin/docs/generate-release-docs.sh
@@ -25,24 +25,34 @@ for COMPONENT in api portal; do
     # old: api/v1.1.0
     # new: <a href="...">api/v1.1.0</a>
     #
-    VERSION_CAPTURE="($COMPONENT\/v.*)"
-    GITHUB_RELEASE_LINK_FORMAT=$(echo '<a href="https://github.com/EOLWD/pfml/releases/tag/\\\1">\\\1</a>' | regex_escape_all)
+    VERSION_CAPTURE="([^/])($COMPONENT\/v[^ |)]*)"
+    GITHUB_RELEASE_LINK_FORMAT=$(echo '\\\1<a href="https://github.com/EOLWD/pfml/releases/tag/\\\2">\\\2</a>' | regex_escape_all)
+
+    # Grab "dirty" version tags and parse them, linking to the latest commit and the diff from the latest tag.
+    #
+    # e.g.
+    # old: api/v1.1.0-2-gabcd
+    # new: api/v1.1.0 (2 commits ahead on abcd)
+    #
+    DIRTY_CAPTURE="($COMPONENT\/v.*)-([0-9]+)-g(.*)"
+    DIRTY_FORMAT=$(echo '<a href="https://github.com/EOLWD/pfml/commits/\\\3">\\\3</a> \(<a href="https://github.com/EOLWD/pfml/compare/\\\1...\\\3">\\\2 commits ahead</a> of \\\1)' | regex_escape_all)
 
     # Grab the first two columns, i.e. the environment name and release version.
     # Insert them back in as-is, then add a link to the GH Deployments page for the environment.
     #
     # e.g.
     # old: |test|api/v1.1.0
-    # new: |test|api/v1.1.0|<a href="..."GitHub</a>
+    # new: |test|api/v1.1.0|<a href="...">GitHub</a>
     #
-    ENV_CAPTURE=$(echo '| *(.*)|(.*)$' | regex_escape_pipes)
-    ENV_REPLACE=$(echo '|\\\1|\\\2|' | regex_escape_pipes)
+    ENV_CAPTURE=$(echo ' | *(.*)|(.*)$' | regex_escape_pipes)
+    ENV_REPLACE=$(echo ' |\\\1|\\\2|' | regex_escape_pipes)
     GITHUB_DEPLOYMENTS_FORMAT=$(echo '<a href="https://github.com/EOLWD/pfml/deployments/activity_log?environment='$COMPONENT'+(\\\1)">GitHub</a>' | regex_escape_all)
 
     released=$(make -s -C $COMPONENT whats-released-short |
                    sed -E "s/origin\/(deploy\/$COMPONENT\/)?//g" |
                    sed -E 's/main/test/g' |
                    sed -E 's/(\*|:)/\|/g' |
+                   sed -E "s/$DIRTY_CAPTURE/$DIRTY_FORMAT/g" |
                    sed -E "s/$VERSION_CAPTURE/$GITHUB_RELEASE_LINK_FORMAT/g" |
                    sed -E "s/$ENV_CAPTURE/$ENV_REPLACE$GITHUB_DEPLOYMENTS_FORMAT/g")
 
