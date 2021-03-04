5c6b9d786f60133a3835a9b03d4f66a34757c64c
Lawrence Kang <lawrencekang@gmail.com>
2021-03-04 11:50:43 -0800

    refactor test (#3267)
    🎫 Ticket
    https://lwd.atlassian.net/browse/CP-1814
    Subtask: https://lwd.atlassian.net/browse/CP-1823
    
    🛠 Changes
     Refactor test to add user interaction test

--- portal/tests/pages/applications/leave-period-continuous.test.js
+++ portal/tests/pages/applications/leave-period-continuous.test.js
@@ -4,7 +4,6 @@ import {
   simulateEvents,
 } from "../../test-utils";
 import LeavePeriodContinuous from "../../../src/pages/applications/leave-period-continuous";
-import { act } from "react-dom/test-utils";
 
 jest.mock("../../../src/hooks/useAppLogic");
 
@@ -56,15 +55,13 @@ describe("LeavePeriodContinuous", () => {
         render: "mount", // support useEffect
       }
     );
-    const { changeRadioGroup } = simulateEvents(wrapper);
+    const { changeRadioGroup, submitForm } = simulateEvents(wrapper);
 
     // Trigger the effect
     changeRadioGroup("has_continuous_leave_periods", "true");
 
     // Submit the form and assert against what's submitted
-    await act(async () => {
-      await wrapper.find("form").simulate("submit");
-    });
+    await submitForm();
 
     expect(appLogic.claims.update).toHaveBeenCalledWith(claim.application_id, {
       has_continuous_leave_periods: true,
@@ -74,7 +71,7 @@ describe("LeavePeriodContinuous", () => {
     });
   });
 
-  it("sends continuous leave dates and ID to the api", () => {
+  it("sends continuous leave dates and ID to the api when the claim has leave data", async () => {
     const claim = new MockClaimBuilder().continuous().create();
     const {
       end_date,
@@ -86,12 +83,45 @@ describe("LeavePeriodContinuous", () => {
       claimAttrs: claim,
     });
 
-    wrapper.find("QuestionPage").simulate("save");
+    const { submitForm } = simulateEvents(wrapper);
+    await submitForm();
 
     expect(appLogic.claims.update).toHaveBeenCalledWith(claim.application_id, {
       has_continuous_leave_periods: true,
       leave_details: {
         continuous_leave_periods: [{ end_date, start_date, leave_period_id }],
+      },
+    });
+  });
+
+  it("sends continuous leave dates and ID to the api when the user enters leave data", async () => {
+    const startDate = "2021-01-01";
+    const endDate = "2021-03-01";
+    const claim = new MockClaimBuilder().create();
+
+    const { appLogic, wrapper } = renderWithAppLogic(LeavePeriodContinuous, {
+      claimAttrs: claim,
+    });
+
+    const { changeField, changeRadioGroup, submitForm } = simulateEvents(
+      wrapper
+    );
+
+    changeRadioGroup("has_continuous_leave_periods", "true");
+    changeField(
+      "leave_details.continuous_leave_periods[0].start_date",
+      startDate
+    );
+    changeField("leave_details.continuous_leave_periods[0].end_date", endDate);
+
+    await submitForm();
+
+    expect(appLogic.claims.update).toHaveBeenCalledWith(claim.application_id, {
+      has_continuous_leave_periods: true,
+      leave_details: {
+        continuous_leave_periods: [
+          { end_date: endDate, start_date: startDate },
+        ],
       },
     });
   });
