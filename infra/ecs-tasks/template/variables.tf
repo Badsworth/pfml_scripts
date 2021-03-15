18efaae319de58bfb7a2add98fc77b645b1fe664
teshd <17507503+teshd@users.noreply.github.com>
2021-03-15 16:42:21 +0000

    API-1324 - enabling reductions tasks (#3335)

--- infra/ecs-tasks/template/variables.tf
+++ infra/ecs-tasks/template/variables.tf
@@ -265,13 +265,13 @@ variable "enable_sentry" {
 variable "enable_reductions_send_claimant_lists_to_agencies_schedule" {
   description = "Enable scheduling for 'reductions-send-claimant-lists-to-agencies' ECS task"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "enable_reductions_retrieve_payment_lists_from_agencies_schedule" {
   description = "Enable scheduling for 'reductions-retrieve-payment-lists-from-agencies' ECS task"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "enable_reductions_send_wage_replacement_payments_to_dfml_schedule" {
