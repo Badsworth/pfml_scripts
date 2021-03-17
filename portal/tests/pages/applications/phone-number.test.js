1b688a8b6947ee6a8d1d0a44243d2ad292b8769e
Lawrence Kang <lawrencekang@gmail.com>
2021-03-17 16:54:46 -0700

    refactor test to use setup and add user action test (#3399)
    🎫 Ticket
    https://lwd.atlassian.net/browse/CP-1814
    Subtask: https://lwd.atlassian.net/browse/CP-1832
    
    🛠 Changes
     refactor test to use setup
     add test for user action

--- portal/tests/pages/applications/phone-number.test.js
+++ portal/tests/pages/applications/phone-number.test.js
@@ -1,40 +1,71 @@
+import { renderWithAppLogic, simulateEvents } from "../../test-utils";
 import PhoneNumber from "../../../src/pages/applications/phone-number";
-import { renderWithAppLogic } from "../../test-utils";
+import { PhoneType } from "../../../src/models/Claim";
 
 jest.mock("../../../src/hooks/useAppLogic");
 
-describe("PhoneNumber", () => {
-  let appLogic, claim, wrapper;
+const phone_number = "123-456-7890";
+const phone_type = PhoneType.cell;
 
-  beforeEach(() => {
-    ({ appLogic, claim, wrapper } = renderWithAppLogic(PhoneNumber, {
-      claimAttrs: {
-        phone: {
-          phone_number: "123-456-7890",
-          phone_type: "cell",
-        },
-      },
-    }));
+const setup = (phone = {}) => {
+  const { appLogic, claim, wrapper } = renderWithAppLogic(PhoneNumber, {
+    claimAttrs: {
+      phone,
+    },
   });
 
+  const { changeField, changeRadioGroup, submitForm } = simulateEvents(wrapper);
+
+  return {
+    appLogic,
+    changeField,
+    changeRadioGroup,
+    claim,
+    submitForm,
+    wrapper,
+  };
+};
+
+describe("PhoneNumber", () => {
   it("renders the page", () => {
+    const { wrapper } = setup({ phone_number, phone_type });
     expect(wrapper).toMatchSnapshot();
   });
 
-  describe("when the form is successfully submitted", () => {
-    it("calls claims.update", () => {
-      wrapper.find("QuestionPage").simulate("save");
+  it("calls claims.update when the form is successfully submitted with existing data", () => {
+    const { appLogic, claim, submitForm } = setup({ phone_number, phone_type });
 
-      expect(appLogic.claims.update).toHaveBeenCalledWith(
-        claim.application_id,
-        {
-          phone: {
-            int_code: "1",
-            phone_number: "123-456-7890",
-            phone_type: "cell",
-          },
-        }
-      );
+    submitForm();
+
+    expect(appLogic.claims.update).toHaveBeenCalledWith(claim.application_id, {
+      phone: {
+        int_code: "1",
+        phone_number,
+        phone_type,
+      },
+    });
+  });
+
+  it("calls claims.update when the form is successfully submitted with newly entered data", () => {
+    const {
+      appLogic,
+      changeField,
+      changeRadioGroup,
+      claim,
+      submitForm,
+    } = setup();
+
+    changeRadioGroup("phone.phone_type", phone_type);
+    changeField("phone.phone_number", phone_number);
+
+    submitForm();
+
+    expect(appLogic.claims.update).toHaveBeenCalledWith(claim.application_id, {
+      phone: {
+        int_code: "1",
+        phone_number,
+        phone_type,
+      },
     });
   });
 });
