712a1f014cf3ed249c6afab73e67cdad23990d6e
Lawrence Kang <lawrencekang@gmail.com>
2021-03-17 16:37:02 -0700

    refactor to use helper methods (#3361)
    🎫 Ticket
    https://lwd.atlassian.net/browse/CP-1814
    Subtask: https://lwd.atlassian.net/browse/CP-1827
    
    🛠 Changes
     refactor to use helper methods
     Add test for user actions

--- portal/tests/pages/applications/name.test.js
+++ portal/tests/pages/applications/name.test.js
@@ -1,34 +1,60 @@
+import { renderWithAppLogic, simulateEvents } from "../../test-utils";
 import Name from "../../../src/pages/applications/name";
 import { pick } from "lodash";
-import { renderWithAppLogic } from "../../test-utils";
 
 jest.mock("../../../src/hooks/useAppLogic");
 
-describe("Name", () => {
-  let appLogic, claim, wrapper;
-
-  beforeEach(() => {
-    ({ appLogic, claim, wrapper } = renderWithAppLogic(Name, {
-      claimAttrs: {
-        first_name: "Aquib",
-        middle_name: "cricketer",
-        last_name: "Khan",
-      },
-    }));
+const defaultName = {
+  first_name: "Aquib",
+  middle_name: "cricketer",
+  last_name: "Khan",
+};
+const setup = (name) => {
+  const { appLogic, wrapper } = renderWithAppLogic(Name, {
+    claimAttrs: {
+      ...name,
+    },
   });
 
+  const { changeField, submitForm } = simulateEvents(wrapper);
+
+  return {
+    appLogic,
+    changeField,
+    submitForm,
+    wrapper,
+  };
+};
+
+describe("Name", () => {
   it("renders the page", () => {
+    const { wrapper } = setup(defaultName);
     expect(wrapper).toMatchSnapshot();
   });
 
-  describe("when the form is successfully submitted", () => {
-    it("calls claims.update", () => {
-      wrapper.find("QuestionPage").simulate("save");
+  it("calls claims.update when the form is successfully submitted with pre-filled data", async () => {
+    const { appLogic, submitForm } = setup(defaultName);
 
-      expect(appLogic.claims.update).toHaveBeenCalledWith(
-        expect.any(String),
-        pick(claim, ["first_name", "last_name", "middle_name"])
-      );
-    });
+    await submitForm();
+
+    expect(appLogic.claims.update).toHaveBeenCalledWith(
+      expect.any(String),
+      pick(defaultName, ["first_name", "last_name", "middle_name"])
+    );
+  });
+
+  it("calls claims.update when the form is successfully submitted with new data", async () => {
+    const { appLogic, changeField, submitForm } = setup({});
+
+    changeField("first_name", defaultName.first_name);
+    changeField("middle_name", defaultName.middle_name);
+    changeField("last_name", defaultName.last_name);
+
+    await submitForm();
+
+    expect(appLogic.claims.update).toHaveBeenCalledWith(
+      expect.any(String),
+      pick(defaultName, ["first_name", "last_name", "middle_name"])
+    );
   });
 });
