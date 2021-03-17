0638660e5085a0c32409728afecdf35967769d7e
Jacky Alciné <jackyalcine@navapbc.com>
2021-03-17 10:20:55 -0700

    Add similar basis for importing DIA payments from S3/reference files.

--- api/massgov/pfml/reductions/dia.py
+++ api/massgov/pfml/reductions/dia.py
@@ -7,6 +7,7 @@ from sqlalchemy import func
 
 import massgov.pfml.api.util.state_log_util as state_log_util
 import massgov.pfml.db as db
+import massgov.pfml.util.files as file_util
 import massgov.pfml.util.logging as logging
 from massgov.pfml.db.models.employees import Claim, ReferenceFile, ReferenceFileType, State
 from massgov.pfml.payments.payments_util import get_now
@@ -173,7 +174,7 @@ def _payment_list_has_been_downloaded_today(db_session: db.Session) -> bool:
         .filter(
             ReferenceFile.created_at >= midnight_today,
             ReferenceFile.reference_file_type_id
-            == ReferenceFileType.DUA_PAYMENT_LIST.reference_file_type_id,
+            == ReferenceFileType.DIA_PAYMENT_LIST.reference_file_type_id,
         )
         .scalar()
     )
@@ -222,3 +223,61 @@ def download_payment_list_if_none_today(db_session: db.Session) -> None:
     # so we only incur the overhead of that connection if we haven't retrieved a payment list today.
     if _payment_list_has_been_downloaded_today(db_session) is False:
         download_payment_list_from_moveit(db_session)
+
+
+def _get_pending_dia_payment_reference_files(
+    pending_directory: str, db_session: db.Session
+) -> List[ReferenceFile]:
+    # Add a trailing % so that we match anything within the directory.
+    return (
+        db_session.query(ReferenceFile)
+        .filter(
+            ReferenceFile.reference_file_type_id
+            == ReferenceFileType.DIA_PAYMENT_LIST.reference_file_type_id,
+            ReferenceFile.file_location.like(pending_directory + "%"),
+        )
+        .all()
+    )
+
+
+def _load_dia_payment_from_reference_file(
+    ref_file: ReferenceFile, archive_directory: str, db_session: db.Session
+) -> None:
+    # Load to database.
+    with file_util.open_stream(ref_file.file_location) as f:
+        _load_new_rows_from_file(f, db_session)
+
+    # Move to archive directory and update ReferenceFile.
+    filename = os.path.basename(ref_file.file_location)
+    dest_path = os.path.join(archive_directory, filename)
+    file_util.rename_file(ref_file.file_location, dest_path)
+    ref_file.file_location = dest_path
+    db_session.commit()
+
+    # Create StateLog entry.
+    state_log_util.create_finished_state_log(
+        associated_model=ref_file,
+        end_state=State.DUA_PAYMENT_LIST_SAVED_TO_DB,
+        outcome=state_log_util.build_outcome("Loaded DUA payment file into database"),
+        db_session=db_session,
+    )
+    db_session.commit()
+
+
+def load_new_dia_payments(db_session: db.Session) -> None:
+    s3_config = get_s3_config()
+    pending_dir = os.path.join(s3_config.s3_bucket_uri, s3_config.s3_dia_pending_directory_path)
+    archive_dir = os.path.join(s3_config.s3_bucket_uri, s3_config.s3_dia_archive_directory_path)
+
+    for ref_file in _get_pending_dia_payment_reference_files(pending_dir, db_session):
+        try:
+            _load_dia_payment_from_reference_file(ref_file, archive_dir, db_session)
+        except Exception:
+            # Log exceptions but continue attempting to load other payment files into the database.
+            logger.exception(
+                "Failed to load new DIA payments to database from file",
+                extra={
+                    "file_location": ref_file.file_location,
+                    "reference_file_id": ref_file.reference_file_id,
+                },
+            )
