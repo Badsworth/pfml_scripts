ae49eaefa275cc1a173e8554ef37d82c3d6b6774
Sawyer Hollenshead <git@sawyerh.com>
2021-03-26 16:19:14 -0400

    CP-1927: Configure 3/27 start time in Stage for testing

--- portal/config/stage.js
+++ portal/config/stage.js
@@ -7,5 +7,9 @@ module.exports = {
   // Not finding what you're looking for? Check default.js
   envName: "stage",
   domain: "paidleave-stage.mass.gov",
+  // See docs/portal/maintenance-pages.md
+  maintenancePageRoutes: ["/*"], // required
+  maintenanceStart: "2021-03-27T00:00:00-04:00",
+  // maintenanceEnd: null, // optional ISO 8601 datetime
   newRelicAppId: "847043861",
 };
