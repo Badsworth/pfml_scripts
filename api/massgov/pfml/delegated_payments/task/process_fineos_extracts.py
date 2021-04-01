f118b7c1a2229b7f0f1ea6143092c7572dc92887
Lowell Wood <32683958+lowellrex@users.noreply.github.com>
2021-04-01 13:37:14 -0400

    PUB-112: Call Experian address API from PUB payments process (#3573)

--- api/massgov/pfml/delegated_payments/task/process_fineos_extracts.py
+++ api/massgov/pfml/delegated_payments/task/process_fineos_extracts.py
@@ -4,6 +4,7 @@ from typing import List
 
 import massgov.pfml.db as db
 import massgov.pfml.util.logging as logging
+from massgov.pfml.delegated_payments.address_validation import AddressValidationStep
 from massgov.pfml.delegated_payments.audit.delegated_payment_audit_report import (
     PaymentAuditReportStep,
 )
@@ -19,6 +20,7 @@ ALL = "ALL"
 RUN_AUDIT_CLEANUP = "audit-cleanup"
 CLAIMANT_EXTRACT = "claimant-extract"
 PAYMENT_EXTRACT = "payment-extract"
+VALIDATE_ADDRESSES = "validate-addresses"
 CREATE_AUDIT_REPORT = "audit-report"
 ERROR_REPORT = "error-report"
 ALLOWED_VALUES = [
@@ -26,6 +28,7 @@ ALLOWED_VALUES = [
     RUN_AUDIT_CLEANUP,
     CLAIMANT_EXTRACT,
     PAYMENT_EXTRACT,
+    VALIDATE_ADDRESSES,
     CREATE_AUDIT_REPORT,
     ERROR_REPORT,
 ]
@@ -35,6 +38,7 @@ class Configuration:
     do_audit_cleanup: bool
     do_claimant_extract: bool
     do_payment_extract: bool
+    validate_addresses: bool
     make_audit_report: bool
     make_error_report: bool
 
@@ -57,12 +61,14 @@ class Configuration:
             self.do_audit_cleanup = True
             self.do_claimant_extract = True
             self.do_payment_extract = True
+            self.validate_addresses = True
             self.make_audit_report = True
             self.make_error_report = True
         else:
             self.do_audit_cleanup = RUN_AUDIT_CLEANUP in steps
             self.do_claimant_extract = CLAIMANT_EXTRACT in steps
             self.do_payment_extract = PAYMENT_EXTRACT in steps
+            self.validate_addresses = VALIDATE_ADDRESSES in steps
             self.make_audit_report = CREATE_AUDIT_REPORT in steps
             self.make_error_report = CREATE_AUDIT_REPORT in steps
 
@@ -98,6 +104,11 @@ def _process_fineos_extracts(
 
     if config.do_payment_extract:
         PaymentExtractStep(db_session=db_session, log_entry_db_session=log_entry_db_session).run()
+
+    if config.validate_addresses:
+        AddressValidationStep(
+            db_session=db_session, log_entry_db_session=log_entry_db_session
+        ).run()
 
     if config.make_audit_report:
         PaymentAuditReportStep(
---
+++ api/tests/delegated_payments/test_address_validation.py
@@ -0,0 +1,304 @@
+from datetime import timedelta
+from decimal import Decimal
+from typing import List, Optional
+from unittest import mock
+
+import faker
+import pytest
+import sqlalchemy
+
+import massgov.pfml.api.util.state_log_util as state_log_util
+import massgov.pfml.db as db
+from massgov.pfml.db.models.employees import (
+    Employee,
+    LkState,
+    Payment,
+    PaymentMethod,
+    State,
+    StateLog,
+)
+from massgov.pfml.db.models.factories import (
+    AddressFactory,
+    ClaimFactory,
+    EmployeeFactory,
+    ExperianAddressPairFactory,
+    PaymentFactory,
+)
+from massgov.pfml.delegated_payments.address_validation import AddressValidationStep
+from massgov.pfml.experian.physical_address.client.mock import MockClient
+from massgov.pfml.experian.physical_address.client.models import Confidence
+
+fake = faker.Faker()
+
+
+def _random_valid_employee_with_state_log(db_session: db.Session) -> Employee:
+    address_pair = ExperianAddressPairFactory(experian_address=AddressFactory())
+    employee = EmployeeFactory(experian_address_pair=address_pair)
+
+    state_log_util.create_finished_state_log(
+        end_state=State.CLAIMANT_READY_FOR_ADDRESS_VALIDATION,
+        outcome=state_log_util.build_outcome("Claimant ready for address validation"),
+        associated_model=employee,
+        db_session=db_session,
+    )
+    db_session.commit()
+
+    return employee
+
+
+def _set_up_employees(
+    client: MockClient,
+    db_session: db.Session,
+    payment_count: int,
+    confidence: Optional[Confidence] = None,
+) -> List[Employee]:
+    employees = []
+    for _i in range(payment_count):
+        employee = _random_valid_employee_with_state_log(db_session)
+
+        if confidence is not None:
+            # Unset the experian_address_pair.experian_address so _address_has_been_validated()
+            # returns False and we make a request to the Experian API.
+            employee.experian_address_pair.experian_address = None
+
+            # Add experian_address_pair.fineos_address to mock client.
+            address = employee.experian_address_pair.fineos_address
+            client.add_mock_address_response(address, confidence)
+
+        employees.append(employee)
+
+    return employees
+
+
+def _assert_employee_state(
+    db_session: db.Session, state: LkState, employees: List[Employee]
+) -> None:
+    employee_ids = [employee.employee_id for employee in employees]
+    assert db_session.query(sqlalchemy.func.count(StateLog.state_log_id)).filter(
+        StateLog.end_state_id == state.state_id
+    ).filter(StateLog.employee_id.in_(employee_ids)).scalar() == len(employees)
+
+
+def _random_valid_check_payment_with_state_log(db_session: db.Session) -> Payment:
+    # Create the employee and claim ourselves so the payment has an associated address.
+    address_pair = ExperianAddressPairFactory(experian_address=AddressFactory())
+    employee = EmployeeFactory(experian_address_pair=address_pair)
+    claim = ClaimFactory(employee=employee)
+
+    # Set the dates to some reasonably recent dates in the past.
+    start_date = fake.date_between("-10w", "-2w")
+    end_date = start_date + timedelta(days=6)
+    payment_date = end_date + timedelta(days=1)
+
+    payment = PaymentFactory(
+        claim=claim,
+        period_start_date=start_date,
+        period_end_date=end_date,
+        payment_date=payment_date,
+        amount=Decimal(fake.random_int(min=10, max=9_999)),
+        disb_method_id=PaymentMethod.CHECK.payment_method_id,
+    )
+
+    state_log_util.create_finished_state_log(
+        end_state=State.PAYMENT_READY_FOR_ADDRESS_VALIDATION,
+        outcome=state_log_util.build_outcome("Payment ready for address validation"),
+        associated_model=payment,
+        db_session=db_session,
+    )
+    db_session.commit()
+
+    return payment
+
+
+def _set_up_payments(
+    client: MockClient,
+    db_session: db.Session,
+    payment_count: int,
+    confidence: Optional[Confidence] = None,
+) -> List[Payment]:
+    payments = []
+    for _i in range(payment_count):
+        payment = _random_valid_check_payment_with_state_log(db_session)
+
+        if confidence is not None:
+            # Unset the experian_address_pair.experian_address so _address_has_been_validated()
+            # returns False and we make a request to the Experian API.
+            payment.claim.employee.experian_address_pair.experian_address = None
+
+            # Add experian_address_pair.fineos_address to mock client.
+            address = payment.claim.employee.experian_address_pair.fineos_address
+            client.add_mock_address_response(address, confidence)
+
+        payments.append(payment)
+
+    return payments
+
+
+def _assert_payment_state(db_session: db.Session, state: LkState, payments: List[Payment]) -> None:
+    payment_ids = [payment.payment_id for payment in payments]
+    assert db_session.query(sqlalchemy.func.count(StateLog.state_log_id)).filter(
+        StateLog.end_state_id == state.state_id
+    ).filter(StateLog.payment_id.in_(payment_ids)).scalar() == len(payments)
+
+
+def test_run_step_state_transitions(
+    initialize_factories_session, test_db_session, test_db_other_session
+):
+    client = MockClient()
+
+    employees_with_validated_addresses = _set_up_employees(
+        client, test_db_session, fake.random_int(min=1, max=7)
+    )
+    employees_with_single_verified_matching_addresses = _set_up_employees(
+        client, test_db_session, fake.random_int(min=3, max=6), Confidence.VERIFIED_MATCH
+    )
+    employees_with_non_matching_addresses = _set_up_employees(
+        client, test_db_session, fake.random_int(min=4, max=9), Confidence.NO_MATCHES
+    )
+    employees_with_multiple_matching_addresses = _set_up_employees(
+        client, test_db_session, fake.random_int(min=8, max=12), Confidence.MULTIPLE_MATCHES
+    )
+
+    check_payments_with_validated_addresses = _set_up_payments(
+        client, test_db_session, fake.random_int(min=5, max=8)
+    )
+    check_payments_with_single_verified_matching_addresses = _set_up_payments(
+        client, test_db_session, fake.random_int(min=7, max=11), Confidence.VERIFIED_MATCH
+    )
+    check_payments_with_non_matching_addresses = _set_up_payments(
+        client, test_db_session, fake.random_int(min=1, max=6), Confidence.NO_MATCHES
+    )
+    check_payments_with_multiple_matching_addresses = _set_up_payments(
+        client, test_db_session, fake.random_int(min=4, max=7), Confidence.MULTIPLE_MATCHES
+    )
+
+    # Commit the various experian_address_pair.experian_address = None changes to the database.
+    test_db_session.commit()
+
+    with mock.patch(
+        "massgov.pfml.delegated_payments.address_validation._get_experian_client",
+        return_value=client,
+    ):
+        AddressValidationStep(
+            db_session=test_db_session, log_entry_db_session=test_db_other_session
+        ).run()
+
+    for employee in (
+        employees_with_validated_addresses + employees_with_single_verified_matching_addresses
+    ):
+        assert employee.experian_address_pair.experian_address is not None
+
+    # Expect employees with already valid addresses to transition into the
+    # DELEGATED_CLAIMANT_EXTRACTED_FROM_FINEOS state.
+    _assert_employee_state(
+        test_db_other_session,
+        State.DELEGATED_CLAIMANT_EXTRACTED_FROM_FINEOS,
+        employees_with_validated_addresses,
+    )
+
+    # Expect employees with verified matching addresses according to Experian to transition into the
+    # DELEGATED_CLAIMANT_EXTRACTED_FROM_FINEOS state.
+    _assert_employee_state(
+        test_db_other_session,
+        State.DELEGATED_CLAIMANT_EXTRACTED_FROM_FINEOS,
+        employees_with_single_verified_matching_addresses,
+    )
+
+    # Expect employees with no matching addresses according to Experian to transition into the
+    # CLAIMANT_FAILED_ADDRESS_VALIDATION state.
+    _assert_employee_state(
+        test_db_other_session,
+        State.CLAIMANT_FAILED_ADDRESS_VALIDATION,
+        employees_with_non_matching_addresses,
+    )
+
+    # Expect employees with multiple matching addresses according to Experian to transition into the
+    # CLAIMANT_FAILED_ADDRESS_VALIDATION state.
+    _assert_employee_state(
+        test_db_other_session,
+        State.CLAIMANT_FAILED_ADDRESS_VALIDATION,
+        employees_with_multiple_matching_addresses,
+    )
+
+    for payment in (
+        check_payments_with_validated_addresses
+        + check_payments_with_single_verified_matching_addresses
+    ):
+        assert payment.claim.employee.experian_address_pair.experian_address is not None
+
+    # Expect payments with already valid addresses to transition into the
+    # DELEGATED_PAYMENT_STAGED_FOR_PAYMENT_AUDIT_REPORT_SAMPLING state.
+    _assert_payment_state(
+        test_db_other_session,
+        State.DELEGATED_PAYMENT_STAGED_FOR_PAYMENT_AUDIT_REPORT_SAMPLING,
+        check_payments_with_validated_addresses,
+    )
+
+    # Expect payments with verified matching addresses according to Experian to transition into the
+    # DELEGATED_PAYMENT_STAGED_FOR_PAYMENT_AUDIT_REPORT_SAMPLING state.
+    _assert_payment_state(
+        test_db_other_session,
+        State.DELEGATED_PAYMENT_STAGED_FOR_PAYMENT_AUDIT_REPORT_SAMPLING,
+        check_payments_with_single_verified_matching_addresses,
+    )
+
+    # Expect payments with no matching addresses according to Experian to transition into the
+    # PAYMENT_FAILED_ADDRESS_VALIDATION state.
+    _assert_payment_state(
+        test_db_other_session,
+        State.PAYMENT_FAILED_ADDRESS_VALIDATION,
+        check_payments_with_non_matching_addresses,
+    )
+
+    # Expect payments with multiple matching addresses according to Experian to transition into the
+    # PAYMENT_FAILED_ADDRESS_VALIDATION state.
+    _assert_payment_state(
+        test_db_other_session,
+        State.PAYMENT_FAILED_ADDRESS_VALIDATION,
+        check_payments_with_multiple_matching_addresses,
+    )
+
+
+def test_run_step_no_database_changes_on_exception(
+    initialize_factories_session, test_db_session, test_db_other_session
+):
+    client = MockClient()
+
+    _set_up_employees(client, test_db_session, fake.random_int(min=1, max=7))
+    _set_up_employees(
+        client, test_db_session, fake.random_int(min=3, max=6), Confidence.VERIFIED_MATCH
+    )
+    _set_up_employees(client, test_db_session, fake.random_int(min=4, max=9), Confidence.NO_MATCHES)
+    _set_up_employees(
+        client, test_db_session, fake.random_int(min=8, max=12), Confidence.MULTIPLE_MATCHES
+    )
+
+    # Commit the various experian_address_pair.experian_address = None changes to the database.
+    test_db_session.commit()
+
+    # Mock _get_payments_awaiting_address_validation() to raise an error so we can test rolling back
+    # the changes we would have made previously to the claimant addresses.
+    with mock.patch(
+        "massgov.pfml.delegated_payments.address_validation._get_payments_awaiting_address_validation",
+        side_effect=Exception("Raising error to test rollback"),
+    ), mock.patch(
+        "massgov.pfml.delegated_payments.address_validation._get_experian_client",
+        return_value=client,
+    ), pytest.raises(
+        Exception, match="Raising error to test rollback"
+    ):
+        AddressValidationStep(
+            db_session=test_db_session, log_entry_db_session=test_db_other_session
+        ).run()
+
+    # We expect to find no employees in either of the post-address validation states.
+    post_address_validation_states = [
+        State.DELEGATED_CLAIMANT_EXTRACTED_FROM_FINEOS.state_id,
+        State.CLAIMANT_FAILED_ADDRESS_VALIDATION.state_id,
+    ]
+    assert (
+        test_db_other_session.query(sqlalchemy.func.count(StateLog.state_log_id))
+        .filter(StateLog.end_state_id.in_(post_address_validation_states))
+        .scalar()
+        == 0
+    )
