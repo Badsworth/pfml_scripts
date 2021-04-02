e8edec4d287dbdff494eb2c3337286d86939f310
Anteneh Addis <antxaddis@gmail.com>
2021-04-02 12:26:14 -0400

    PUB-125: Add sql files for PUB reports (#3623)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/PUB-125
    
    ## 🛠 Changes
    
    - Add sql command files for various pub reports
    
    ## ℹ️ Context for reviewers
    
    SQL files were generated by Doug.
    
    https://lwd.atlassian.net/wiki/spaces/API/pages/1418854458/PUB+Reports
    
    ## 👀 Demo
    
    `make test args=tests/delegated_payments/reporting/test_delegated_payment_sql_reports.py`

--- api/massgov/pfml/delegated_payments/reporting/delegated_payment_sql_reports.py
+++ api/massgov/pfml/delegated_payments/reporting/delegated_payment_sql_reports.py
@@ -1,3 +1,4 @@
+import os
 from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, List, Optional
@@ -9,17 +10,18 @@ class FolderName(str, Enum):
 
 
 class ReportName(str, Enum):
-    CLAIMANT_EXTRACT_ERROR_REPORT = "claimant-extract-error-report"
+    CLAIMANT_EXTRACT_ERROR_REPORT = "claimant-extract-error-report"  # TODO needs code change first
     PAYMENT_EXTRACT_ERROR_REPORT = "payment-extract-error-report"
-    ADDRESS_ERROR_REPORT = "address-error-report"
+    ADDRESS_ERROR_REPORT = "address-error-report"  # TODO wait for PUB-111
     OVERPAYMENT_REPORT = "overpayment-report"
     ZERO_DOLLAR_PAYMENT_REPORT = "zero-dollar-payment_report"
     CANCELLATION_REPORT = "cancellation-report"
     EMPLOYER_REIMBURSEMENT_REPORT = "employer-reimbursement-report"
-    PUB_PAYMENT_REPORT = "pub-payment-report"
+    ACH_PAYMENT_REPORT = "ach-payment-report"
+    CHECK_PAYMENT_REPORT = "check-payment-report"
     DAILY_CASH_REPORT = "daily-cash-report"
-    PUB_ERROR_REPORT = "pub-error-report"
-    EFT_ERROR_REPORT = "eft-error-report"
+    PUB_ERROR_REPORT = "pub-error-report"  # TODO wait for PUB-75
+    EFT_ERROR_REPORT = "eft-error-report"  # TODO wait for PUB-75
     PAYMENT_RECONCILIATION_SUMMARY_REPORT = "payment-reconciliation-summary-report"
 
 
@@ -35,7 +37,8 @@ CREATE_PUB_FILES_REPORTS: List[ReportName] = [
     ReportName.ZERO_DOLLAR_PAYMENT_REPORT,
     ReportName.CANCELLATION_REPORT,
     ReportName.EMPLOYER_REIMBURSEMENT_REPORT,
-    ReportName.PUB_PAYMENT_REPORT,
+    ReportName.ACH_PAYMENT_REPORT,
+    ReportName.CHECK_PAYMENT_REPORT,
     ReportName.DAILY_CASH_REPORT,
 ]
 PROCESS_PUB_RESPONSES_REPORTS: List[ReportName] = [
@@ -51,6 +54,13 @@ class Report:
     folder_name: FolderName
 
 
+def _get_report_sql_command_from_file(sql_file_name: str) -> str:
+    expected_output = open(
+        os.path.join(os.path.dirname(__file__), "sql", f"{sql_file_name}.sql"), "r"
+    ).read()
+    return expected_output
+
+
 REPORTS: List[Report] = [
     Report(
         sql_command="select * from lk_state",
@@ -58,7 +68,7 @@ REPORTS: List[Report] = [
         folder_name=FolderName.PROGRAM_INTEGRITY,
     ),
     Report(
-        sql_command="select * from lk_state",
+        sql_command=_get_report_sql_command_from_file(ReportName.PAYMENT_EXTRACT_ERROR_REPORT),
         report_name=ReportName.PAYMENT_EXTRACT_ERROR_REPORT,
         folder_name=FolderName.PROGRAM_INTEGRITY,
     ),
@@ -68,32 +78,37 @@ REPORTS: List[Report] = [
         folder_name=FolderName.PROGRAM_INTEGRITY,
     ),
     Report(
-        sql_command="select * from lk_state",
+        sql_command=_get_report_sql_command_from_file(ReportName.OVERPAYMENT_REPORT),
         report_name=ReportName.OVERPAYMENT_REPORT,
         folder_name=FolderName.FINANCE,
     ),
     Report(
-        sql_command="select * from lk_state",
+        sql_command=_get_report_sql_command_from_file(ReportName.ZERO_DOLLAR_PAYMENT_REPORT),
         report_name=ReportName.ZERO_DOLLAR_PAYMENT_REPORT,
         folder_name=FolderName.FINANCE,
     ),
     Report(
-        sql_command="select * from lk_state",
+        sql_command=_get_report_sql_command_from_file(ReportName.CANCELLATION_REPORT),
         report_name=ReportName.CANCELLATION_REPORT,
         folder_name=FolderName.FINANCE,
     ),
     Report(
-        sql_command="select * from lk_state",
+        sql_command=_get_report_sql_command_from_file(ReportName.EMPLOYER_REIMBURSEMENT_REPORT),
         report_name=ReportName.EMPLOYER_REIMBURSEMENT_REPORT,
         folder_name=FolderName.FINANCE,
     ),
     Report(
-        sql_command="select * from lk_state",
-        report_name=ReportName.PUB_PAYMENT_REPORT,
+        sql_command=_get_report_sql_command_from_file(ReportName.ACH_PAYMENT_REPORT),
+        report_name=ReportName.ACH_PAYMENT_REPORT,
         folder_name=FolderName.FINANCE,
     ),
     Report(
-        sql_command="select * from lk_state",
+        sql_command=_get_report_sql_command_from_file(ReportName.CHECK_PAYMENT_REPORT),
+        report_name=ReportName.CHECK_PAYMENT_REPORT,
+        folder_name=FolderName.FINANCE,
+    ),
+    Report(
+        sql_command=_get_report_sql_command_from_file(ReportName.DAILY_CASH_REPORT),
         report_name=ReportName.DAILY_CASH_REPORT,
         folder_name=FolderName.FINANCE,
     ),
@@ -108,7 +123,9 @@ REPORTS: List[Report] = [
         folder_name=FolderName.PROGRAM_INTEGRITY,
     ),
     Report(
-        sql_command="select * from lk_state",
+        sql_command=_get_report_sql_command_from_file(
+            ReportName.PAYMENT_RECONCILIATION_SUMMARY_REPORT
+        ),
         report_name=ReportName.PAYMENT_RECONCILIATION_SUMMARY_REPORT,
         folder_name=FolderName.FINANCE,
     ),
---
+++ api/massgov/pfml/delegated_payments/reporting/sql/payment-extract-error-report.sql
@@ -0,0 +1,23 @@
+-- PFML Payment Error Report
+SELECT P.PAYMENT_ID "Payment ID", 
+       C.FINEOS_ABSENCE_ID "NTN Number", 
+       C.ABSENCE_PERIOD_START_DATE "Absence Period Start Date", 
+       C.ABSENCE_PERIOD_END_DATE "Absence Period End Date",
+       P.PERIOD_START_DATE "Payment Period Start Date", 
+       P.PERIOD_END_DATE "Payment Period End Date", 
+       P.PAYMENT_DATE "Payment Date", 
+       P.AMOUNT "Payment Amount", 
+       P.FINEOS_PEI_C_VALUE "C", 
+       P.FINEOS_PEI_I_VALUE "I", 
+       P.FINEOS_EXTRACTION_DATE "Fineos Extraction Date", 
+       SL.OUTCOME->>'message',-- "Error Message",
+       json_array_elements(SL.OUTCOME->'validation_container'->'validation_issues')->>'reason',-- "Reason",
+       json_array_elements(SL.OUTCOME->'validation_container'->'validation_issues')->>'details'-- "Details"
+FROM PAYMENT P
+LEFT OUTER JOIN CLAIM C ON P.CLAIM_ID = C.CLAIM_ID
+INNER JOIN STATE_LOG SL ON P.PAYMENT_ID = SL.PAYMENT_ID
+INNER JOIN LK_STATE ST1 ON SL.END_STATE_ID = ST1.STATE_ID
+WHERE P.FINEOS_EXTRACT_IMPORT_LOG_ID = (SELECT MAX(IMPORT_LOG_ID)
+                                        FROM IMPORT_LOG
+                                        WHERE SOURCE = 'PaymentExtractStep')
+  AND ST1.STATE_ID = 120 
