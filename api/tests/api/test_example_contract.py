c7260c62f7bdcbf2c08bfa5ea4d3dc5fa17858f8
Sheldon Bachstein <sheldon@navahq.com>
2021-04-08 16:59:44 -0400

    Add blocking test. Just starts a web server

--- api/tests/api/test_example_contract.py
+++ api/tests/api/test_example_contract.py
@@ -2,7 +2,9 @@
 
 import logging
 import os
+import requests
 
+import massgov.pfml.api.app as api_app
 from pact import Verifier
 from massgov.pfml.db.models.factories import ApplicationFactory
 from datetime import date, datetime
@@ -18,6 +20,22 @@ PACT_MOCK_HOST = 'localhost'
 PACT_MOCK_PORT = 1550
 PACT_URL = "http://{}:{}/v1".format(PACT_MOCK_HOST, PACT_MOCK_PORT)
 PACT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+def test_setup(app):
+    app_config = api_app.get_app_config(app)
+    openapi_files = list(
+        map(lambda f: os.path.join(api_app.get_project_root_dir(), f), api_app.openapi_filenames())
+    )
+    # this next call is blocking. I don't know how to run it in the background
+    app.run(port=app_config.port, use_reloader=True, extra_files=openapi_files)
+
+    # below this line doesn't get run because the above line is blocking
+    print("setup run!")
+    r = requests.get('http://localhost:1550/v1/status')
+    # r = requests.get('https://xkcd.com/1906/')
+    print(r)
+    assert False
+
 
 def test_example():
     verifier = Verifier(provider='API service', provider_base_url=PACT_URL)
