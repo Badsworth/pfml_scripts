ef1d095846958b9df6015dc523702f7a346b0a30
Asa Needle <asa@asaneedle.com>
2021-04-09 14:19:25 -0400

    expand jest test

--- portal/tests/pages/convert-to-employer.test.js
+++ portal/tests/pages/convert-to-employer.test.js
@@ -1,20 +1,19 @@
 import User, { RoleDescription } from "../../src/models/User";
-import { renderWithAppLogic, simulateEvents, testHook } from "../test-utils";
+import { MockEmployerClaimBuilder, renderWithAppLogic, simulateEvents, testHook } from "../test-utils";
 import ClaimCollection from "../../src/models/ClaimCollection";
 import ConvertToEmployer from "../../src/pages/convert-to-employer";
 import { mockRouter } from "next/router";
 import routes from "../../src/routes";
 import useAppLogic from "../../src/hooks/useAppLogic";
+import { before } from "lodash";
 
 jest.mock("../../src/hooks/useAppLogic");
 
 describe("ConvertToEmployer", () => {
+
   function render(customProps = {}, options = { claims: [] }) {
     let appLogic;
     mockRouter.pathname = routes.auth.convert;
-    process.env.featureFlags = {
-      claimantConvertToEmployer: true,
-    };
 
     testHook(() => {
       appLogic = useAppLogic();
@@ -32,24 +31,54 @@ describe("ConvertToEmployer", () => {
 
     return { appLogic, wrapper };
   }
-  it("renders form", () => {
-    const { wrapper } = render();
-    expect(wrapper).toMatchSnapshot();
+
+  describe("when convertToEmployer is set to true", () => {
+    beforeEach(() => {
+      process.env.featureFlags = {
+        claimantConvertToEmployer: true,
+      };
+    })
+    describe("when user has no claims", () => {
+      it("renders form", () => {
+        const { wrapper } = render();
+        expect(wrapper.find("InputText").length).toEqual(1)
+        expect(wrapper).toMatchSnapshot();
+      });
+
+      it("can convert to employer account", async () => {
+        const fein = "123456789";
+        const { appLogic, wrapper } = render();
+        const { changeField, submitForm } = simulateEvents(wrapper);
+        changeField("employer_fein", fein);
+        await submitForm();
+        expect(appLogic.users.updateUser).toHaveBeenCalledWith(
+          expect.any(String), {
+          role: {
+            role_description: RoleDescription.employer,
+          },
+          user_leave_administrator: {
+            employer_fein: fein,
+          },
+        });
+      })
+    })
+    describe("when user has at least one claim", () => {
+      it("it does not render page", () => {
+        const claims = [new MockEmployerClaimBuilder().completed().create()];
+        const { wrapper } = render({}, {claims});
+        expect(wrapper.find("InputText").length).toEqual(0);
+      })
+    })
   });
-  it("can convert to employer account", async () => {
-    const fein = "123456789";
-    const { appLogic, wrapper } = render();
-    const { changeField, submitForm } = simulateEvents(wrapper);
-    changeField("employer_fein", fein);
-    await submitForm();
-    expect(appLogic.users.updateUser).toHaveBeenCalledWith(
-      expect.any(String), {
-      role: {
-        role_description: RoleDescription.employer,
-      },
-      user_leave_administrator: {
-        employer_fein: fein,
-      },
+  describe("when convertToEmployer is set to false", () => {
+    beforeEach(() => {
+      process.env.featureFlags = {
+        claimantConvertToEmployer: false,
+      };
+    })
+    it("it does not render page", () => {
+      const { wrapper } = render();
+      expect(wrapper.find("InputText").length).toEqual(0)
     });
   });
 });
