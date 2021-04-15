afab9506a0ba12a9332d50b8d32df3a92f5e977a
apopma <adapopma@navapbc.com>
2021-04-15 12:27:08 -0500

    tf fmt

--- infra/pfml-aws/alarms-aws.tf
+++ infra/pfml-aws/alarms-aws.tf
@@ -6,20 +6,20 @@ locals {
 
 # TODO: a "burn rate" metric: given a send rate and remaining send quota, how long until the entire quota is used up?
 resource "aws_cloudwatch_metric_alarm" "ses_send_rate" {
-  alarm_name = "massgov_pfml_allenvs-ses_send_rate"
+  alarm_name        = "massgov_pfml_allenvs-ses_send_rate"
   alarm_description = "(SES CRIT) Abnormally high email send rate through SES"
-  namespace = "AWS/SES"
-  metric_name = "Send"
+  namespace         = "AWS/SES"
+  metric_name       = "Send"
 
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold = 500  # if 500 or more emails are sent in a single 'period'
-  statistic = "Sum"
-  evaluation_periods = "12" # look back at the last hour
-  datapoints_to_alarm = "9" # any nine five-minute periods, e.g. 45 minutes in the last hour
-  period = "300" # five-minute aggregation window
+  threshold           = 500 # if 500 or more emails are sent in a single 'period'
+  statistic           = "Sum"
+  evaluation_periods  = "12"  # look back at the last hour
+  datapoints_to_alarm = "9"   # any nine five-minute periods, e.g. 45 minutes in the last hour
+  period              = "300" # five-minute aggregation window
 
   actions_enabled = true
-  alarm_actions = [local.api_high_priority_alerts]
+  alarm_actions   = [local.api_high_priority_alerts]
 
   tags = merge(module.constants.common_tags, {
     environment = module.constants.environment_tags["prod"]
