e8a70d44b7570faea84e4d429daf48d67333e800
apopma <adapopma@navapbc.com>
2021-04-15 12:05:32 -0500

    tuning thresholds with reference to historic send rates

--- infra/pfml-aws/alarms-aws.tf
+++ infra/pfml-aws/alarms-aws.tf
@@ -12,20 +12,14 @@ resource "aws_cloudwatch_metric_alarm" "ses_send_rate" {
   metric_name = "Send"
 
   comparison_operator = "GreaterThanOrEqualToThreshold"
-  threshold = 300  # if 300 or more emails are sent in a single 'period'
+  threshold = 500  # if 500 or more emails are sent in a single 'period'
   statistic = "Sum"
-  evaluation_periods = "6" # look back at the last 30 minutes
-  datapoints_to_alarm = "2" # any two five-minute periods
+  evaluation_periods = "12" # look back at the last hour
+  datapoints_to_alarm = "9" # any nine five-minute periods, e.g. 45 minutes in the last hour
   period = "300" # five-minute aggregation window
 
   actions_enabled = true
   alarm_actions = [local.api_high_priority_alerts]
-
-  # There should always be a send rate, even if it's zero.
-  # If there's NO send rate, we've hit our send quota and NO environment can send any more emails from our AWS account.
-  # Someone must get paged if this happens, because user sign-up is broken in prod if we can't send any emails.
-  insufficient_data_actions = [local.api_high_priority_alerts]
-  treat_missing_data = "breaching"
 
   tags = merge(module.constants.common_tags, {
     environment = module.constants.environment_tags["prod"]
