3e7d3b6d36984fcbdaef7e0f1a6fabb63901e3dc
apopma <adapopma@navapbc.com>
2021-04-15 11:24:42 -0500

    WIP: write an early warning alarm for email spam through SES

--- infra/modules/alarms_api/alarms-aws.tf
+++ infra/modules/alarms_api/alarms-aws.tf
@@ -142,3 +142,32 @@ resource "aws_cloudwatch_metric_alarm" "failed_invocations" {
     environment = module.constants.environment_tags[var.environment_name]
   })
 }
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# TODO: a "burn rate" metric: given a send rate and remaining send quota, how long until the entire quota is used up?
+# TODO: this alarm must not be unique by environment; SES send rate metrics and quotas are account-wide
+resource "aws_cloudwatch_metric_alarm" "ses_send_rate" {
+  alarm_name = "ses_send_rate"
+  alarm_description = "(SES CRIT) Abnormally high email send rate through SES"
+  namespace = "AWS/SES"
+  metric_name = "Send"
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  threshold = 300  # if 300 or more emails are sent in a single 'period'
+  statistic = "Sum"
+  evaluation_periods = "6" # look back at the last 30 minutes
+  datapoints_to_alarm = "2" # any two five-minute periods
+  period = "300" # five-minute aggregation window
+
+  actions_enabled = true
+  alarm_actions = [var.critical_alert_sns_topic_arn]
+
+  # There should always be a send rate, even if it's zero.
+  # If there's NO send rate, we've hit our send quota and NO environment can send any more emails from our AWS account.
+  # Someone must get paged if this happens, because user sign-up is broken in prod if we can't send any emails.
+  insufficient_data_actions = [var.critical_alert_sns_topic_arn]
+  treat_missing_data = "breaching"
+
+  tags = module.constants.common_tags
+}
