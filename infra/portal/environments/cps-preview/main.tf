fbd1c2ba9c74f10921abe6cfebec4edfda7b4b82
paz-focus <76061418+paz-focus@users.noreply.github.com>
2021-04-21 10:41:35 -0400

    INFRA-346: portal environment for cps preview (#3758)
    * initial
    
    * INFRA-346: add new entry to index.js
    
    * INFRA-346: added env.js file with cognito values
    
    * INFRA-346: added thew new env domain to new-relic js
    
    * INFRA-346: added build env in package json
    
    * INFRA-346: added new env to workflow
    
    * INFRA-346: fix the lint issue
    
    * INFRA-346: conflict fix
    
    * INFRA-346: update the new relic account
    
    * INFRA-346: pull result
    
    * INFRA-346: lockversion change

--- infra/portal/environments/cps-preview/main.tf
+++ infra/portal/environments/cps-preview/main.tf
@@ -45,7 +45,6 @@ module "massgov_pfml" {
   # 'false' will set rule to 'COUNT' (counts traffic that meets rule(s) instead of blocking)
   enforce_cloudfront_rate_limit     = true
   enforce_cloudfront_fortinet_rules = true
-
   # You probably don't need to change the variables below:
   source                 = "../../template"
   environment_name       = "cps-preview"
