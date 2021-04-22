20408ae84dbe35011d54f4a738d15978e0da51a8
Jamal Pace <jkpace@users.noreply.github.com>
2021-04-22 14:56:04 -0400

    Update verification success copy (#3857)

--- portal/src/locales/app/en-US.js
+++ portal/src/locales/app/en-US.js
@@ -1763,7 +1763,7 @@ const pages = {
     continueButton: "Continue",
     employerIdNumberLabel: "<strong>Employer ID number (EIN):</strong> {{ein}}",
     instructions:
-      "Your account has been verified. In 15 minutes you will be able to log in and review applications. If anyone else on your team needs to review applications, they'll also need to complete the <learn-more-link>verification process</learn-more-link>.",
+      "Your account has been verified. It may take up to 15 minutes for our systems to update so that you can log in and review applications. If anyone else on your team needs to review applications, they’ll also need to complete the <learn-more-link>verification process</learn-more-link>.",
     title: "Thanks for verifying your paid leave contributions",
   },
   employersOrganizationsVerifyContributions: {
