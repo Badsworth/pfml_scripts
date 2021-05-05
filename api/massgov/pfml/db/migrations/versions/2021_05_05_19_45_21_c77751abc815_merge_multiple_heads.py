f0cc745a728d5334b94af0f8c21831639c86c6c2
Phil Opaola <phil.opaola@focusconsulting.io>
2021-05-05 15:46:05 -0400

    Merge multiple alembic heads

---
+++ api/massgov/pfml/db/migrations/versions/2021_05_05_19_45_21_c77751abc815_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads
+
+Revision ID: c77751abc815
+Revises: 89b58b9c6d17, f11d9353b6f6
+Create Date: 2021-05-05 19:45:21.577982
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c77751abc815"
+down_revision = ("89b58b9c6d17", "f11d9353b6f6")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
