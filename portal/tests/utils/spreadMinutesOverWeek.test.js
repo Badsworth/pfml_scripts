d33ed4d54a3cdd822da1b333f5e610a8cc862f8d
Mike L <myclues@gmail.com>
2021-05-05 09:56:53 -0400

    API-1611: new logic for dividing variable input for leave minutes (#4010)
    API-1611: Updating logic to split manually-entered leave time into 15-minute increments with leftovers added to next valid day.
    
    https://lwd.atlassian.net/browse/API-1611
    
    Updates logic and tests to split the total minutes before sending payload to backend.
    
    Demo: Affected unit tests in file changelist.

--- portal/tests/utils/spreadMinutesOverWeek.test.js
+++ portal/tests/utils/spreadMinutesOverWeek.test.js
@@ -1,14 +1,34 @@
 import spreadMinutesOverWeek from "../../src/utils/spreadMinutesOverWeek";
 
 describe("spreadMinutesOverWeek", () => {
-  it("splits the minutes evenly across 7 days", () => {
-    const dailyMinutes = spreadMinutesOverWeek(630);
-    expect(dailyMinutes).toEqual([90, 90, 90, 90, 90, 90, 90]);
+  it("splits the minutes evenly across 7 days in 15-minute increments", () => {
+    const dailyMinutes = spreadMinutesOverWeek(15 * 7);
+    expect(dailyMinutes).toEqual([15, 15, 15, 15, 15, 15, 15]);
   });
 
   it("distributes remainder of minutes when they can't be evenly split across 7 days", () => {
     const dailyMinutes = spreadMinutesOverWeek(480);
-    expect(dailyMinutes).toEqual([69, 69, 69, 69, 68, 68, 68]);
+    expect(dailyMinutes).toEqual([75, 75, 75, 75, 60, 60, 60]);
+  });
+
+  it("distributes remainder of minutes in minimum 15-minute increments", () => {
+    const dailyMinutes = spreadMinutesOverWeek(60 * 39 + 8);
+    expect(dailyMinutes).toEqual([345, 345, 338, 330, 330, 330, 330]);
+  });
+
+  it("distributes minutes correctly when there are only full hours and less than 15 min remaining", () => {
+    const dailyMinutes = spreadMinutesOverWeek(60 * 70 + 3);
+    expect(dailyMinutes).toEqual([603, 600, 600, 600, 600, 600, 600]);
+  });
+
+  it("distributes minutes correctly under 15 minutes per day", () => {
+    const dailyMinutes = spreadMinutesOverWeek(15 * 4 + 3);
+    expect(dailyMinutes).toEqual([15, 15, 15, 15, 3, 0, 0]);
+  });
+
+  it("handles zero minutes correctly", () => {
+    const dailyMinutes = spreadMinutesOverWeek(0);
+    expect(dailyMinutes).toEqual([0, 0, 0, 0, 0, 0, 0]);
   });
 
   it("throws error if minutes null or undefined", () => {
