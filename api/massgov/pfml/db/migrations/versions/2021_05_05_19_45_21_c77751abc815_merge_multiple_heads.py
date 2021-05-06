5efe468660dea96ee2ca180866c18103e6c07dbf
Phil Opaola <phil.opaola@focusconsulting.io>
2021-05-06 11:55:52 -0400

    Run make format

--- api/massgov/pfml/db/migrations/versions/2021_05_05_19_45_21_c77751abc815_merge_multiple_heads.py
+++ api/massgov/pfml/db/migrations/versions/2021_05_05_19_45_21_c77751abc815_merge_multiple_heads.py
@@ -5,9 +5,8 @@ Revises: 89b58b9c6d17, f11d9353b6f6
 Create Date: 2021-05-05 19:45:21.577982
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "c77751abc815"
