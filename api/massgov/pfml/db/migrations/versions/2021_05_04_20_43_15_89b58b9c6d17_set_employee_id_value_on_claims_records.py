f3fdf6c9cb2e1bc0c16c3a38fa648f50686a03f6
Phil Opaola <phil.opaola@focusconsulting.io>
2021-05-07 11:39:21 -0400

    Make migration script more readable

--- api/massgov/pfml/db/migrations/versions/2021_05_04_20_43_15_89b58b9c6d17_set_employee_id_value_on_claims_records.py
+++ api/massgov/pfml/db/migrations/versions/2021_05_04_20_43_15_89b58b9c6d17_set_employee_id_value_on_claims_records.py
@@ -42,12 +42,14 @@ def upgrade():
         sa.Column("tax_identifier_id", postgresql.UUID(as_uuid=True), nullable=False),
     )
 
-    null_employee_claims = connection.execute(
+    # First get all claims that have a null employee_id
+    null_employee_claims_query = connection.execute(
         sa.select([claims.c.claim_id]).where(claims.c.employee_id == None)  # noqa: E711
-    ).fetchall()
-    null_employee_claim_ids = set([row.claim_id for row in null_employee_claims])
+    )
+    null_employee_claim_ids = set([row.claim_id for row in null_employee_claims_query.fetchall()])
 
-    applications_claims_and_tax_identifier_ids = connection.execute(
+    # Then lookup all applications for those claims and the corresponding tax identifier ids
+    applications_claims_and_tax_identifier_query = connection.execute(
         sa.select(
             [
                 applications.c.application_id,
@@ -55,28 +57,31 @@ def upgrade():
                 applications.c.tax_identifier_id,
             ]
         ).where(applications.c.claim_id.in_(null_employee_claim_ids))
-    ).fetchall()
-
-    tax_identifier_ids_to_claim_ids = {
-        row.tax_identifier_id: row.claim_id for row in applications_claims_and_tax_identifier_ids
-    }
-
-    tax_identifier_ids = set(
-        [row.tax_identifier_id for row in applications_claims_and_tax_identifier_ids]
     )
 
-    tax_identifier_ids_to_employees_results = connection.execute(
+    # Build a mapping of all tax identifier ids to claim ids
+    tax_identifier_ids_to_claim_ids = {
+        row.tax_identifier_id: row.claim_id
+        for row in applications_claims_and_tax_identifier_query.fetchall()
+    }
+    tax_identifier_ids = set(
+        [tax_identifier_id for tax_identifier_id in tax_identifier_ids_to_claim_ids]
+    )
+
+    # Build a mapping of all tax identifier ids to employee ids
+    tax_identifier_ids_to_employees_query = connection.execute(
         sa.select([employees.c.tax_identifier_id, employees.c.employee_id]).where(
             employees.c.tax_identifier_id.in_(tax_identifier_ids)
         )
-    ).fetchall()
-
+    )
     tax_identifier_ids_to_employees = {
-        row.tax_identifier_id: row.employee_id for row in tax_identifier_ids_to_employees_results
+        row.tax_identifier_id: row.employee_id
+        for row in tax_identifier_ids_to_employees_query.fetchall()
     }
 
+    # Use the tax identifier ids -> employees and tax identifier ids -> claims mappings to update
+    # the claims tables with correct employee_ids
     bulk_update_params = []
-
     for tax_identifier_id in tax_identifier_ids_to_employees:
         claim_id = tax_identifier_ids_to_claim_ids[tax_identifier_id]
         employee_id = tax_identifier_ids_to_employees[tax_identifier_id]
@@ -86,15 +91,16 @@ def upgrade():
     total_batches = math.ceil(len(bulk_update_params) / batch_size)
     current_batch = 0
 
-    stmt = (
+    bulk_update_statement = (
         claims.update()
         .where(claims.c.claim_id == bindparam("_claim_id"))
-        .values({"employee_id": bindparam("employee_id"),})
+        .values({"employee_id": bindparam("employee_id")})
     )
 
     while current_batch < total_batches:
         connection.execute(
-            stmt, bulk_update_params[current_batch * batch_size : (current_batch + 1) * batch_size]
+            bulk_update_statement,
+            bulk_update_params[current_batch * batch_size : (current_batch + 1) * batch_size],
         )
         current_batch += 1
 
