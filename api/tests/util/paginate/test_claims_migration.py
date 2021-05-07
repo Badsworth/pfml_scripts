6dea8d6806d634b169336b581ca46dd26f889291
Phil Opaola <phil.opaola@focusconsulting.io>
2021-05-07 12:04:05 -0400

    fix migration

--- api/tests/util/paginate/test_claims_migration.py
+++ api/tests/util/paginate/test_claims_migration.py
@@ -35,7 +35,7 @@ def upgrade(context):
         "claim",
         sa.MetaData(),
         sa.Column("claim_id", postgresql.UUID(as_uuid=True), nullable=False),
-        sa.Column("employee_id", postgresql.UUID(as_uuid=True), nullable=True)
+        sa.Column("employee_id", postgresql.UUID(as_uuid=True), nullable=True),
     )
     employees = sa.Table(
         "employee",
@@ -44,68 +44,67 @@ def upgrade(context):
         sa.Column("tax_identifier_id", postgresql.UUID(as_uuid=True), nullable=False),
     )
 
-    null_employee_claims = connection.execute(sa.select([claims.c.claim_id]).where(claims.c.employee_id==None)).fetchall()
-    null_employee_claim_ids = set([row.claim_id for row in null_employee_claims])
+    # First get all claims that have a null employee_id
+    null_employee_claims_query = connection.execute(
+        sa.select([claims.c.claim_id]).where(claims.c.employee_id == None)  # noqa: E711
+    )
+    null_employee_claim_ids = set([row.claim_id for row in null_employee_claims_query.fetchall()])
 
-    applications_claims_and_tax_identifier_ids = connection.execute(
-        sa.select([
-            applications.c.application_id,
-            applications.c.claim_id,
-            applications.c.tax_identifier_id,
-        ]).where(applications.c.claim_id.in_(null_employee_claim_ids))
-    ).fetchall()
-
-    tax_identifier_ids_to_claim_ids = {
-        row.tax_identifier_id: row.claim_id
-        for row in applications_claims_and_tax_identifier_ids
-    }
-
-    tax_identifier_ids = set(
-        [row.tax_identifier_id for row in applications_claims_and_tax_identifier_ids]
+    # Then lookup all applications for those claims and the corresponding tax identifier ids
+    applications_claims_and_tax_identifier_query = connection.execute(
+        sa.select(
+            [
+                applications.c.application_id,
+                applications.c.claim_id,
+                applications.c.tax_identifier_id,
+            ]
+        ).where(applications.c.claim_id.in_(null_employee_claim_ids))
     )
 
-    tax_identifier_ids_to_employees_results = connection.execute(
-        sa.select([
-            employees.c.tax_identifier_id, employees.c.employee_id
-        ]).where(
+    # Build a mapping of all tax identifier ids to claim ids
+    tax_identifier_ids_to_claim_ids = {
+        row.tax_identifier_id: row.claim_id
+        for row in applications_claims_and_tax_identifier_query.fetchall()
+    }
+    tax_identifier_ids = set(
+        [tax_identifier_id for tax_identifier_id in tax_identifier_ids_to_claim_ids]
+    )
+
+    # Build a mapping of all tax identifier ids to employee ids
+    tax_identifier_ids_to_employees_query = connection.execute(
+        sa.select([employees.c.tax_identifier_id, employees.c.employee_id]).where(
             employees.c.tax_identifier_id.in_(tax_identifier_ids)
         )
-    ).fetchall()
-
+    )
     tax_identifier_ids_to_employees = {
         row.tax_identifier_id: row.employee_id
-        for row in tax_identifier_ids_to_employees_results
+        for row in tax_identifier_ids_to_employees_query.fetchall()
     }
 
+    # Use the tax identifier ids -> employees and tax identifier ids -> claims mappings to update
+    # the claims tables with correct employee_ids
     bulk_update_params = []
     for tax_identifier_id in tax_identifier_ids_to_employees:
         claim_id = tax_identifier_ids_to_claim_ids[tax_identifier_id]
         employee_id = tax_identifier_ids_to_employees[tax_identifier_id]
-        bulk_update_params.append({'_claim_id': claim_id, 'employee_id': employee_id})
+        bulk_update_params.append({"_claim_id": claim_id, "_employee_id": employee_id})
 
     batch_size = 5
     total_batches = math.ceil(len(bulk_update_params) / batch_size)
     current_batch = 0
 
-    stmt = claims.update(). \
-        where(claims.c.claim_id == bindparam('_claim_id')).values(
-        {
-            'employee_id': bindparam('employee_id'),
-        }
+    bulk_update_statement = (
+        claims.update()
+        .where(claims.c.claim_id == bindparam("_claim_id"))
+        .values({"employee_id": bindparam("_employee_id")})
     )
 
     while current_batch < total_batches:
         connection.execute(
-            stmt,
-            bulk_update_params[current_batch * batch_size: (current_batch + 1) * batch_size]
+            bulk_update_statement,
+            bulk_update_params[current_batch * batch_size : (current_batch + 1) * batch_size],
         )
         current_batch += 1
-        """
-        try:
-        except:
-            pass
-        finally:"""
-    # ### end Alembic commands ###
 
 
 @pytest.mark.integration
