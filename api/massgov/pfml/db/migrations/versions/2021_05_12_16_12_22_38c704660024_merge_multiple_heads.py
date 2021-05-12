deec563bae51b46a32858ec402529b2d342fe626
Phil Opaola <78548006+phil-focus@users.noreply.github.com>
2021-05-12 13:41:17 -0400

    EMPLOYER-1193: claims employee id migration (#4025)
    * Add migration tables for applications, claims, and employees
    
    * General structure of migration script
    
    * Use test_claims_migration.py to setup test migration environment
    
    * Migration script actually calls bulk_update_mappings
    
    * Use sqlalchemy core bulk update pattern
    
    * Revert ApplicationFactory changes
    
    * Merge multiple alembic heads
    
    * Run make format
    
    * Fix linter errors
    
    * Make migration script more readable
    
    * Log total count of claims before && after migrtion
    
    * Fix logger name in migration
    
    * Fix logger complaint
    
    * Merge multiple alembic heads
    
    * Generate completely new alembic migration from latest head
    
    * Simplify upgade migration with update_employee_ids_query
    
    * Autoformat API files
    
    * Fix multiple migration heads conflict
    
    Co-authored-by: PFMLDevOps <68565627+PFMLDevOps@users.noreply.github.com>

---
+++ api/massgov/pfml/db/migrations/versions/2021_05_12_16_12_22_38c704660024_merge_multiple_heads.py
@@ -0,0 +1,21 @@
+"""Merge multiple heads
+
+Revision ID: 38c704660024
+Revises: 74c8e9780024, 115d906a631f
+Create Date: 2021-05-12 16:12:22.149472
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "38c704660024"
+down_revision = ("74c8e9780024", "115d906a631f")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
