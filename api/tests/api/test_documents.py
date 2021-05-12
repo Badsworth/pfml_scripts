Badsworth/pfml_scripts

commit a259a338756f7acc09065897888cd419ea436a83

API-1590: Continue Using Old Doc Type As Needed (#4108)
For a period of time, the API needs to support both the old document type, State managed Paid Leave Confirmation, and the new types introduced by the new plan proofs. This PR implements code that allows the API to determine which document type to use. It also ensures that documents with the new document type are marked as evidence received when uploaded.

api/massgov/pfml/api/applications.py
@@ -484,7 +484,26 @@ def document_upload(application_id, body, file):
         file_description = ""
         if document_details.description:
             file_description = document_details.description
-        document_type = document_details.document_type.value
+
+        # For now, if there are documents previously submitted for the application with the
+        # STATE_MANAGED_PAID_LEAVE_CONFIRMATION document type, that document type must also
+        # be used for subsequent documents uploaded to the application. If not, the document type
+        # from the request should be used instead.
+        existing_documents_with_old_doc_type = (
+            db_session.query(Document)
+            .filter(Document.application_id == existing_application.application_id)
+            .filter(
+                Document.document_type_id
+                == DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_id
+            )
+        ).all()
+
+        if len(existing_documents_with_old_doc_type) > 0:
+            document_type = (
+                DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_description
+            )
+        else:
+            document_type = document_details.document_type.value
 
         log_attributes = {
             **get_application_log_attributes(existing_application),
@@ -526,7 +545,7 @@ def document_upload(application_id, body, file):
         now = datetime_util.utcnow()
         document.created_at = now
         document.updated_at = now
-        document.document_type_id = DocumentType.get_id(document_details.document_type.value)
+        document.document_type_id = DocumentType.get_id(document_type)
         document.content_type_id = ContentType.get_id(content_type)
         document.size_bytes = file_size
         document.fineos_id = fineos_document["documentId"]

api/tests/api/test_documents.py
@@ -6,7 +6,7 @@ import pytest
 import massgov.pfml.fineos.mock_client
 from massgov.pfml.api.models.applications.common import ContentType as AllowedContentTypes
 from massgov.pfml.db.models.applications import DocumentType
-from massgov.pfml.db.models.factories import ApplicationFactory, ClaimFactory
+from massgov.pfml.db.models.factories import ApplicationFactory, ClaimFactory, DocumentFactory
 from massgov.pfml.fineos import fineos_client, models
 
 # every test in here requires real resources
@@ -28,6 +28,42 @@ MISSING_DOCUMENT_TYPE_FORM_DATA = {
 }
 
 FILE_WITH_NO_EXTENSION = (io.BytesIO(b"abcdef"), "test")
+
+STATE_MANAGED_PAID_LEAVE_CONFIRMATION_DATA = {
+    "document_type": "State managed Paid Leave Confirmation",
+    "name": "state_managed_pl_conf_test.png",
+    "description": "State managed Paid Leave Confirmation",
+}
+
+CARE_FOR_A_FAMILY_MEMBER_FORM_DATA = {
+    "document_type": "Care for a family member form",
+    "name": "care_test.png",
+    "description": "Care for a family member form",
+}
+
+OWN_SERIOUS_HEALTH_CONDITION_FORM_DATA = {
+    "document_type": "Own serious health condition form",
+    "name": "own_test.png",
+    "description": "Own serious health condition form",
+}
+
+PREGNANCY_MATERNITY_FORM_DATA = {
+    "document_type": "Pregnancy/Maternity form",
+    "name": "pregnancy_test.png",
+    "description": "Pregnancy/Maternity form",
+}
+
+CHILD_BONDING_EVIDENCE_FORM_DATA = {
+    "document_type": "Child bonding evidence form",
+    "name": "bonding_test.png",
+    "description": "Child bonding evidence form",
+}
+
+MILITARY_EXIGENCY_FORM_DATA = {
+    "document_type": "Military exigency form",
+    "name": "military_test.png",
+    "description": "Military exigency form",
+}
 
 
 def valid_file():
@@ -175,6 +211,149 @@ def test_document_upload_defaults_for_name(
     assert response["data"]["name"] == "test.png"
 
 
+def test_caring_leave_doc_upload(client, consented_user, consented_user_token, test_db_session):
+    response = document_upload_helper(
+        client=client,
+        user=consented_user,
+        auth_token=consented_user_token,
+        form_data=document_upload_payload_helper(CARE_FOR_A_FAMILY_MEMBER_FORM_DATA, valid_file()),
+    )
+
+    assert response["status_code"] == 200
+
+    response_data = response["data"]
+    assert response_data["content_type"] == "image/png"
+    assert response_data["description"] == "Care for a family member form"
+    assert response_data["document_type"] == "Care for a family member form"
+    assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
+    assert response_data["name"] == "care_test.png"
+    assert response_data["user_id"] == str(consented_user.user_id)
+    assert response_data["created_at"] is not None
+
+
+def test_own_serious_health_doc_upload(
+    client, consented_user, consented_user_token, test_db_session
+):
+    response = document_upload_helper(
+        client=client,
+        user=consented_user,
+        auth_token=consented_user_token,
+        form_data=document_upload_payload_helper(
+            OWN_SERIOUS_HEALTH_CONDITION_FORM_DATA, valid_file()
+        ),
+    )
+
+    assert response["status_code"] == 200
+
+    response_data = response["data"]
+    assert response_data["content_type"] == "image/png"
+    assert response_data["description"] == "Own serious health condition form"
+    assert response_data["document_type"] == "Own serious health condition form"
+    assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
+    assert response_data["name"] == "own_test.png"
+    assert response_data["user_id"] == str(consented_user.user_id)
+    assert response_data["created_at"] is not None
+
+
+def test_pregnancy_maternity_doc_upload(
+    client, consented_user, consented_user_token, test_db_session
+):
+    response = document_upload_helper(
+        client=client,
+        user=consented_user,
+        auth_token=consented_user_token,
+        form_data=document_upload_payload_helper(PREGNANCY_MATERNITY_FORM_DATA, valid_file()),
+    )
+
+    assert response["status_code"] == 200
+
+    response_data = response["data"]
+    assert response_data["content_type"] == "image/png"
+    assert response_data["description"] == "Pregnancy/Maternity form"
+    assert response_data["document_type"] == "Pregnancy/Maternity form"
+    assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
+    assert response_data["name"] == "pregnancy_test.png"
+    assert response_data["user_id"] == str(consented_user.user_id)
+    assert response_data["created_at"] is not None
+
+
+def test_child_bonding_doc_upload(client, consented_user, consented_user_token, test_db_session):
+    response = document_upload_helper(
+        client=client,
+        user=consented_user,
+        auth_token=consented_user_token,
+        form_data=document_upload_payload_helper(CHILD_BONDING_EVIDENCE_FORM_DATA, valid_file()),
+    )
+
+    assert response["status_code"] == 200
+
+    response_data = response["data"]
+    assert response_data["content_type"] == "image/png"
+    assert response_data["description"] == "Child bonding evidence form"
+    assert response_data["document_type"] == "Child bonding evidence form"
+    assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
+    assert response_data["name"] == "bonding_test.png"
+    assert response_data["user_id"] == str(consented_user.user_id)
+    assert response_data["created_at"] is not None
+
+
+def test_military_exigency_doc_upload(
+    client, consented_user, consented_user_token, test_db_session
+):
+    response = document_upload_helper(
+        client=client,
+        user=consented_user,
+        auth_token=consented_user_token,
+        form_data=document_upload_payload_helper(MILITARY_EXIGENCY_FORM_DATA, valid_file()),
+    )
+
+    assert response["status_code"] == 200
+
+    response_data = response["data"]
+    assert response_data["content_type"] == "image/png"
+    assert response_data["description"] == "Military exigency form"
+    assert response_data["document_type"] == "Military exigency form"
+    assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
+    assert response_data["name"] == "military_test.png"
+    assert response_data["user_id"] == str(consented_user.user_id)
+    assert response_data["created_at"] is not None
+
+
+def test_old_document_type_saved(client, consented_user, consented_user_token, test_db_session):
+    claim = ClaimFactory.create(
+        fineos_notification_id="NTN-111", fineos_absence_id="NTN-111-ABS-01"
+    )
+
+    application = ApplicationFactory.create(user=consented_user, claim=claim)
+
+    # Create a document with the STATE_MANAGED_PAID_LEAVE_CONFIRMATION document type
+    DocumentFactory.create(
+        user_id=consented_user.user_id,
+        application_id=application.application_id,
+        document_type_id=DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_id,
+    )
+
+    # POST a document with one of the new document types
+    response = client.post(
+        "/v1/applications/{}/documents".format(application.application_id),
+        headers={"Authorization": f"Bearer {consented_user_token}"},
+        content_type="multipart/form-data",
+        data=document_upload_payload_helper(CARE_FOR_A_FAMILY_MEMBER_FORM_DATA, valid_file()),
+    ).get_json()
+
+    assert response["status_code"] == 200
+
+    response_data = response["data"]
+    # Assert that the document has the old, rather than the new, document type
+    assert response_data["document_type"] == "State managed Paid Leave Confirmation"
+    assert response_data["content_type"] == "image/png"
+    assert response_data["description"] == "Care for a family member form"
+    assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
+    assert response_data["name"] == "care_test.png"
+    assert response_data["user_id"] == str(consented_user.user_id)
+    assert response_data["created_at"] is not None
+
+
 @pytest.mark.parametrize(
     "mark_evidence_received_flag,document_type,expected_client_function_calls",
     (
@@ -225,7 +404,66 @@ def test_document_upload_and_mark_evidence_received(
     assert len(capture) == len(expected_client_function_calls)
 
     for i in range(len(capture)):
-        print(capture[i][0])
+        assert capture[i][0] == expected_client_function_calls[i]
+
+
+@pytest.mark.parametrize(
+    "mark_evidence_received_flag,document_type,expected_client_function_calls",
+    (
+        (
+            True,
+            DocumentType.CARE_FOR_A_FAMILY_MEMBER_FORM.document_type_description,
+            ("find_employer", "register_api_user", "upload_document", "mark_document_as_received"),
+        ),
+        (
+            True,
+            DocumentType.OWN_SERIOUS_HEALTH_CONDITION_FORM.document_type_description,
+            ("find_employer", "register_api_user", "upload_document", "mark_document_as_received"),
+        ),
+        (
+            True,
+            DocumentType.PREGNANCY_MATERNITY_FORM.document_type_description,
+            ("find_employer", "register_api_user", "upload_document", "mark_document_as_received"),
+        ),
+        (
+            True,
+            DocumentType.CHILD_BONDING_EVIDENCE_FORM.document_type_description,
+            ("find_employer", "register_api_user", "upload_document", "mark_document_as_received"),
+        ),
+        (
+            True,
+            DocumentType.MILITARY_EXIGENCY_FORM.document_type_description,
+            ("find_employer", "register_api_user", "upload_document", "mark_document_as_received"),
+        ),
+    ),
+)
+def test_new_document_types_upload_and_mark_evidence_received(
+    client,
+    consented_user,
+    consented_user_token,
+    test_db_session,
+    mark_evidence_received_flag,
+    document_type,
+    expected_client_function_calls,
+):
+    form_data = VALID_FORM_DATA
+    form_data["mark_evidence_received"] = mark_evidence_received_flag
+    form_data["document_type"] = document_type
+
+    massgov.pfml.fineos.mock_client.start_capture()
+    response = document_upload_helper(
+        client=client,
+        user=consented_user,
+        auth_token=consented_user_token,
+        form_data=document_upload_payload_helper(form_data, valid_file()),
+    )
+    assert response["status_code"] == 200
+
+    capture = massgov.pfml.fineos.mock_client.get_capture()
+
+    assert len(capture) == len(expected_client_function_calls)
+
+    for i in range(len(capture)):
         assert capture[i][0] == expected_client_function_calls[i]
 
 

api/massgov/pfml/api/services/fineos_actions.py
@@ -270,6 +270,11 @@ def complete_intake(application: Application, db_session: massgov.pfml.db.Sessio
 DOCUMENT_TYPES_ASSOCIATED_WITH_EVIDENCE = (
     DocumentType.IDENTIFICATION_PROOF.document_type_id,
     DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_id,
+    DocumentType.OWN_SERIOUS_HEALTH_CONDITION_FORM.document_type_id,
+    DocumentType.PREGNANCY_MATERNITY_FORM.document_type_id,
+    DocumentType.CHILD_BONDING_EVIDENCE_FORM.document_type_id,
+    DocumentType.CARE_FOR_A_FAMILY_MEMBER_FORM.document_type_id,
+    DocumentType.MILITARY_EXIGENCY_FORM.document_type_id,
 )
 
 

commit 0131a8899f1537b09e90ba550befff297290330b

Add fix to account for order of uploads and test. (#4143)
Fixes bug that tags ID docs as cert docs, introduced in #4108.

api/massgov/pfml/api/applications.py
@@ -54,6 +54,13 @@ from massgov.pfml.util.sqlalchemy import get_or_404
 
 logger = massgov.pfml.util.logging.get_logger(__name__)
 
+ID_DOCS = [
+    DocumentType.PASSPORT.document_type_description,
+    DocumentType.DRIVERS_LICENSE_MASS.document_type_description,
+    DocumentType.DRIVERS_LICENSE_OTHER_STATE.document_type_description,
+    DocumentType.IDENTIFICATION_PROOF.document_type_description,
+]
+
 
 def application_get(application_id):
     with app.db_session() as db_session:
@@ -485,25 +492,27 @@ def document_upload(application_id, body, file):
         if document_details.description:
             file_description = document_details.description
 
-        # For now, if there are documents previously submitted for the application with the
-        # STATE_MANAGED_PAID_LEAVE_CONFIRMATION document type, that document type must also
-        # be used for subsequent documents uploaded to the application. If not, the document type
-        # from the request should be used instead.
-        existing_documents_with_old_doc_type = (
-            db_session.query(Document)
-            .filter(Document.application_id == existing_application.application_id)
-            .filter(
-                Document.document_type_id
-                == DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_id
-            )
-        ).all()
+        document_type = document_details.document_type.value
 
-        if len(existing_documents_with_old_doc_type) > 0:
-            document_type = (
-                DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_description
-            )
-        else:
-            document_type = document_details.document_type.value
+        # TODO: (API-1647) Remove this logic once State manage Paid Leave Confirmation is obsolete
+        if document_type not in ID_DOCS:
+            # For now, if there are documents previously submitted for the application with the
+            # STATE_MANAGED_PAID_LEAVE_CONFIRMATION document type, that document type must also
+            # be used for subsequent documents uploaded to the application. If not, the document type
+            # from the request should be used instead. This logic is only relevant for non ID docs.
+            existing_documents_with_old_doc_type = (
+                db_session.query(Document)
+                .filter(Document.application_id == existing_application.application_id)
+                .filter(
+                    Document.document_type_id
+                    == DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_id
+                )
+            ).all()
+
+            if len(existing_documents_with_old_doc_type) > 0:
+                document_type = (
+                    DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_description
+                )
 
         log_attributes = {
             **get_application_log_attributes(existing_application),

api/tests/api/test_documents.py
@@ -319,6 +319,7 @@ def test_military_exigency_doc_upload(
     assert response_data["created_at"] is not None
 
 
+# TODO: (API-1647) This test should be removed once State manage Paid Leave Confirmation is obsolete
 def test_old_document_type_saved(client, consented_user, consented_user_token, test_db_session):
     claim = ClaimFactory.create(
         fineos_notification_id="NTN-111", fineos_absence_id="NTN-111-ABS-01"
@@ -350,6 +351,44 @@ def test_old_document_type_saved(client, consented_user, consented_user_token, t
     assert response_data["description"] == "Care for a family member form"
     assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
     assert response_data["name"] == "care_test.png"
+    assert response_data["user_id"] == str(consented_user.user_id)
+    assert response_data["created_at"] is not None
+
+
+# TODO: (API-1647) This test should also be removed once State manage Paid Leave Confirmation is obsolete
+def test_document_type_with_id_doc(client, consented_user, consented_user_token, test_db_session):
+    # Regression test to to verify that we do not switch ID doc types to the State managed
+    # Paid Leave Confirmation type
+
+    claim = ClaimFactory.create(
+        fineos_notification_id="NTN-111", fineos_absence_id="NTN-111-ABS-01"
+    )
+
+    application = ApplicationFactory.create(user=consented_user, claim=claim)
+
+    # Create a document with the STATE_MANAGED_PAID_LEAVE_CONFIRMATION document type
+    DocumentFactory.create(
+        user_id=consented_user.user_id,
+        application_id=application.application_id,
+        document_type_id=DocumentType.STATE_MANAGED_PAID_LEAVE_CONFIRMATION.document_type_id,
+    )
+
+    # POST a document with one of the ID document types to make sure it isn't overwritten with cert doc type
+    response = client.post(
+        "/v1/applications/{}/documents".format(application.application_id),
+        headers={"Authorization": f"Bearer {consented_user_token}"},
+        content_type="multipart/form-data",
+        data=document_upload_payload_helper(VALID_FORM_DATA, valid_file()),
+    ).get_json()
+
+    assert response["status_code"] == 200
+
+    response_data = response["data"]
+    assert response_data["content_type"] == "image/png"
+    assert response_data["description"] == "Passport"
+    assert response_data["document_type"] == "Passport"
+    assert response_data["fineos_document_id"] == "3011"  # See massgov/pfml/fineos/mock_client.py
+    assert response_data["name"] == "passport.png"
     assert response_data["user_id"] == str(consented_user.user_id)
     assert response_data["created_at"] is not None
 