7568e0cbd76497a1672972c5cc8c01803e9c7661
Asa Needle <asa@asaneedle.com>
2021-05-13 10:23:26 -0400

    merge db heads

---
+++ api/massgov/pfml/db/migrations/versions/2021_05_13_14_22_11_c9950bde0041_merge_multiple_heads.py
@@ -0,0 +1,24 @@
+"""Merge multiple heads
+
+Revision ID: c9950bde0041
+Revises: 8e477b802405, 38c704660024
+Create Date: 2021-05-13 14:22:11.368216
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c9950bde0041"
+down_revision = ("8e477b802405", "38c704660024")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
