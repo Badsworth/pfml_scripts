6d2bdf05aa7a08098943e5a8ebe29f9dc3cd7a8d
Rob Bayliss <rob@lastcallmedia.com>
2021-06-23 05:15:54 -0400

    PFMLPB-1016: Generate other leave and benefit properties for ER (#4574)
    * PFMLPB-1016: Generate other leave and benefit properties for employer response
    
    * Update portalSubmitter

--- e2e/src/generation/PreviousLeaves.ts
+++ e2e/src/generation/PreviousLeaves.ts
@@ -1,27 +1,19 @@
 import { differenceInDays, formatISO, parseISO, subDays } from "date-fns";
 import subWeeks from "date-fns/subWeeks";
 import { PreviousLeave } from "../api";
-import { NonEmptyArray } from "../types";
 import { getLeavePeriod } from "../util/claims";
 import { ApplicationLeaveDetails } from "../_api";
-import { ClaimSpecification } from "./Claim";
 
 /**
  * Generates previous leaves for the claim.
  * Prefills start & end dates such that previous leave will end 2 weeks before the start of the current leave.
- * @param spec - Claim specification.
+ * @param spec - Previous leave specification.
  * @param leave_details - used to get the leave period for the current leave.
  */
 export function generatePreviousLeaves(
-  {
-    previous_leaves_same_reason,
-    previous_leaves_other_reason,
-  }: ClaimSpecification,
+  previous_leaves: PreviousLeave[] | undefined,
   leave_details: ApplicationLeaveDetails
-): Pick<
-  ClaimSpecification,
-  "previous_leaves_other_reason" | "previous_leaves_same_reason"
-> {
+): PreviousLeave[] | undefined {
   const [startDate, endDate] = getLeavePeriod(leave_details);
 
   const setLeaveDatesIfNotSpecified = (leave: PreviousLeave) => {
@@ -53,16 +45,7 @@ export function generatePreviousLeaves(
     };
   };
 
-  previous_leaves_same_reason = previous_leaves_same_reason
-    ? (previous_leaves_same_reason.map(
-        setLeaveDatesIfNotSpecified
-      ) as NonEmptyArray<PreviousLeave>)
+  return previous_leaves
+    ? previous_leaves.map(setLeaveDatesIfNotSpecified)
     : undefined;
-  previous_leaves_other_reason = previous_leaves_other_reason
-    ? (previous_leaves_other_reason.map(
-        setLeaveDatesIfNotSpecified
-      ) as NonEmptyArray<PreviousLeave>)
-    : undefined;
-
-  return { previous_leaves_other_reason, previous_leaves_same_reason };
 }
