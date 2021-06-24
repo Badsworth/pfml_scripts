b228808acfb59254ca90a1c3c4f921328f11670b
Tyler Stephens <roberttstephens@gmail.com>
2021-06-24 10:02:59 -0400

    Add our layout

--- admin/pages/_app.tsx
+++ admin/pages/_app.tsx
@@ -1,7 +1,29 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="page">
+      <Head>
+        <title>Admin Portal</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header class="page__header">
+          LOGO HERE        
+      </header>
+      <aside class="page__sidebar">
+        <nav>
+          <ul>
+            <li>Hey there!</li>
+          </ul>
+        </nav>
+      </aside>
+      <main class="page__main">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  )
 }
 export default MyApp
