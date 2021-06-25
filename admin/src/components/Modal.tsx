a0bd966618742bab35be5ee2a7e54e29f3263b49
Tyler Stephens <roberttstephens@gmail.com>
2021-06-25 17:35:53 -0400

    Make the modal functional (open and close)

--- admin/src/components/Modal.tsx
+++ admin/src/components/Modal.tsx
@@ -1,9 +1,23 @@
+import { MouseEvent } from "react";
+
 type Props = {
   title: string;
   body: string;
+  handleCancelCallback: Function;
+  handleContinueCallback: Function;
 };
 
-const Modal = ({ title, body }: Props) => {
+const Modal = ({ title, body, handleContinueCallback }: Props) => {
+  const handleCancel = (event: MouseEvent) => {
+    event.preventDefault();
+    handleContinueCallback(event);
+  };
+
+  const handleContinue = (event: MouseEvent) => {
+    event.preventDefault();
+    handleContinueCallback(event);
+  };
+
   return (
     <dialog open className="modal">
       <div className="modal__background"></div>
@@ -13,10 +27,10 @@ const Modal = ({ title, body }: Props) => {
           <p>{body}</p>
         </div>
         <div className="modal__action-buttons modal__action-buttons--stacked">
-          <button className="btn">
+          <button className="btn" onClick={handleContinue}>
             <span className="btn__text">Continue</span>
           </button>
-          <button className="btn">
+          <button className="btn" onClick={handleCancel}>
             <span className="btn__text">Cancel</span>
           </button>
         </div>
--- admin/src/pages/index.tsx
+++ admin/src/pages/index.tsx
@@ -1,16 +1,35 @@
-import Image from "next/image";
 import Modal from "../components/Modal";
-
 import { Helmet } from "react-helmet";
+import { useState } from "react";
 
 export default function Home() {
+  const [showModal, setShowModal] = useState(false);
+
+  const modalContinueCallback = () => {
+    setShowModal(false);
+  };
+
+  const modalCancelCallback = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <Helmet>
         <title>Dashboard</title>
       </Helmet>
       <h1>h1 HTML5 Kitchen Sink</h1>
-      <Modal title="Enable Caring Leave Type" body="Lorum ipsum" />
+      <button onClick={() => setShowModal(true)}>
+        <span>Click to open</span>
+      </button>
+      {showModal && (
+        <Modal
+          title="Enable Caring Leave Type"
+          body="Lorum ipsum"
+          handleCancelCallback={modalCancelCallback}
+          handleContinueCallback={modalContinueCallback}
+        />
+      )}
     </>
   );
 }
