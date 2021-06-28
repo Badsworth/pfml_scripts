f2119d90f35ba61adc989097663a94c67daeb77b
Umi Hui <umihui86@gmail.com>
2021-06-28 16:15:17 -0700

    CP-2341: 7/4 maintenance (#4630)

--- portal/config/stage.js
+++ portal/config/stage.js
@@ -10,6 +10,6 @@ module.exports = {
   newRelicAppId: "847043861",
   // See docs/portal/maintenance-pages.md
   maintenancePageRoutes: ["/*"],
-  maintenanceStart: "2021-06-25T19:59:00-04:00",
-  maintenanceEnd: null,
+  maintenanceStart: "2021-07-04T03:59:00-04:00",
+  maintenanceEnd: "2021-07-04T05:00:00-04:00",
 };
