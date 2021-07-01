caa1fdf232b49ce7f4d49b2be02f029fb3de4779
Robert Stephens <roberttstephens@gmail.com>
2021-07-01 15:21:29 -0400

    PFMLPB-942: Create the admin directory (#4672)
    * PFMLPB-942: Create the admin directory

---
+++ admin/src/pages/_app.tsx
@@ -0,0 +1,61 @@
+import "../../styles/index.scss";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <HelmetProvider>
+      <div className="page">
+        <Helmet htmlAttributes={{ lang: "en" }}>
+          <title>Admin Portal</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Helmet>
+
+        <header className="page__header">LOGO HERE</header>
+        <aside className="page__sidebar">
+          <nav className="menu">
+            <ul className="menu__list">
+              <li className="menu__list-item">
+                <a href="#" className="menu__link">
+                  Nav Item One
+                </a>
+              </li>
+              <li className="menu__list-item">
+                <a href="#" className="menu__link">
+                  Nav Item Two
+                </a>
+              </li>
+              <li className="menu__list-item">
+                <a href="#" className="menu__link">
+                  Nav Item Three
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="settings">
+            <ul className="settings__list">
+              <li className="settings__list-item">
+                <a href="#" className="settings__link">
+                  Settings
+                </a>
+              </li>
+              <li className="settings__list-item">
+                <a href="#" className="settings__link">
+                  Help
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="environment">
+            <div className="environment__label">Environment</div>
+            <div className="environment__flag">Production</div>
+          </div>
+        </aside>
+        <main className="page__main">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </HelmetProvider>
+  );
+}
+export default MyApp;
