a37bf9d15fb3e1dcbc9a5c6ccc0446bee625c0b4
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-07-07 23:06:00 +0500

    add er to fineos_military_reduced_submittal.ts

--- e2e/cypress/specs/stable/scenario/fineos_military_reduced_submittal.ts
+++ e2e/cypress/specs/stable/scenario/fineos_military_reduced_submittal.ts
@@ -1,10 +1,12 @@
-import { fineos } from "../../../actions";
+import { fineos, portal } from "../../../actions";
 import { extractLeavePeriod } from "../../../../src/util/claims";
-import { getFineosBaseUrl } from "../../../config";
+import { getFineosBaseUrl, getLeaveAdminCredentials } from "../../../config";
 import { assertValidClaim } from "../../../../src/util/typeUtils";
+import { ClaimPage } from "../../../actions/fineos.pages";
+import { Submission } from "../../../../src/types";
 
 describe("Create a new continuous leave, military caregiver claim in FINEOS", () => {
-  it(
+  const claimSubmission = it(
     "Should be able to create a claim",
     { baseUrl: getFineosBaseUrl() },
     () => {
@@ -12,6 +14,7 @@ describe("Create a new continuous leave, military caregiver claim in FINEOS", ()
       cy.visit("/");
       cy.task("generateClaim", "MIL_RED").then((claim) => {
         assertValidClaim(claim.claim);
+        cy.stash("claim", claim);
 
         fineos.searchClaimantSSN(claim.claim.tax_identifier);
         fineos.clickBottomWidgetButton("OK");
@@ -29,10 +32,33 @@ describe("Create a new continuous leave, military caregiver claim in FINEOS", ()
           .then((text) => {
             const fineos_absence_id = text.slice(24);
             cy.log(fineos_absence_id);
-            fineos.visitClaim(fineos_absence_id);
-            fineos.assertClaimStatus("Adjudication");
+            cy.stash("submission", { fineos_absence_id });
+
+            ClaimPage.visit(fineos_absence_id).adjudicate((adjudication) => {
+              adjudication.evidence((evidence) => {
+                claim.documents.forEach((doc) =>
+                  evidence.receive(doc.document_type)
+                );
+                evidence.receive("Covered Service Member Identification Proof");
+              });
+            });
           });
       });
     }
   );
+
+  it("LA can review and deny the claim", () => {
+    cy.dependsOnPreviousPass([claimSubmission]);
+    portal.before();
+    cy.unstash<DehydratedClaim>("claim").then(({ claim }) => {
+      cy.unstash<Submission>("submission").then((submission) => {
+        assertValidClaim(claim);
+        portal.login(getLeaveAdminCredentials(claim.employer_fein));
+        // Access the review page
+        portal.visitActionRequiredERFormPage(submission.fineos_absence_id);
+        // Deny the claim
+        portal.respondToLeaveAdminRequest(false, true, false, false);
+      });
+    });
+  });
 });
