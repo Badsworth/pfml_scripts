54fc0093e889c793efc378b1c1fa39c5f9378ce1
Joseph C.A. Garcia <48695203+kingmoc@users.noreply.github.com>
2021-07-15 15:38:12 -0500

    PFMLPB-1635: Resolve hanging unstable tests (#4828)
    Resolves some hanging tests we've been spotting recently in the unstable test suite. After extensive investigation, we determined the hanging was coming from Fineos displaying an error page (/outofdatedataerror.jsp). Cypress doesn't handle Fineos' error pages well, and anytime this page appeared, we were getting a total freezeup of the browser.
    
    The fix is to replace that particular error page with a static "This page was blocked" message. This will still cause test failures when we see the page, which should allow us to see with better accuracy where and when they're happening.
    
    Co-authored-by: Rob Bayliss <rob@lastcallmedia.com>
    Co-authored-by: Joseph Garcia <joe@lastcallmedia.com>

--- e2e/cypress/actions/fineos.ts
+++ e2e/cypress/actions/fineos.ts
@@ -39,7 +39,11 @@ export function before(): void {
     ) {
       return false;
     }
-    if (e.message.match(/Cannot set property 'status' of undefined/)) {
+    if (
+      e.message.match(
+        /Cannot (set|read) property ('status'|'range') of undefined/
+      )
+    ) {
       return false;
     }
     return true;
@@ -53,7 +57,7 @@ export function before(): void {
   // Fineos error pages have been found to cause test crashes when rendered. This is very hard to debug, as Cypress
   // crashes with no warning and removes the entire run history, so when a Fineos error page is detected, we instead
   // throw an error.
-  cy.intercept(/\/util\/errorpage.jsp/, (req) => {
+  cy.intercept(/\/(util\/errorpage\.jsp|outofdatedataerror\.jsp)/, (req) => {
     req.reply(
       "A fatal Fineos error was thrown at this point. We've blocked the rendering of this page to prevent test crashes"
     );
