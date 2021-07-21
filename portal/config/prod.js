a980ab3b277b8a53852f2bcc3bc1c135fd5247a9
Jesse <jesseshepherd@navapbc.com>
2021-07-21 12:55:43 -0700

    Add maintenance window for 8/1 (#4872)

--- portal/config/prod.js
+++ portal/config/prod.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   // See docs/portal/maintenance-pages.md
   maintenancePageRoutes: ["/*"],
-  maintenanceStart: "2021-07-25T03:59:00-04:00",
-  maintenanceEnd: "2021-07-25T05:00:00-04:00",
+  maintenanceStart: "2021-08-01T03:59:00-04:00",
+  maintenanceEnd: "2021-08-01T05:00:00-04:00",
   newRelicAppId: "847045300",
 };
--- portal/config/stage.js
+++ portal/config/stage.js
@@ -10,6 +10,6 @@ module.exports = {
   newRelicAppId: "847043861",
   // See docs/portal/maintenance-pages.md
   maintenancePageRoutes: ["/*"],
-  maintenanceStart: "2021-07-25T03:59:00-04:00",
-  maintenanceEnd: "2021-07-25T05:00:00-04:00",
+  maintenanceStart: "2021-08-01T03:59:00-04:00",
+  maintenanceEnd: "2021-08-01T05:00:00-04:00",
 };
