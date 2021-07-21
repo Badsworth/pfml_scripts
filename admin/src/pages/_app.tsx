a5345e764f3380afc393314f4901914fb4c2a9f6
Melquisedeque Pereira <melqui1@live.com.pt>
2021-07-21 15:46:24 +0100

    Finalized app-level login/logout requests

--- admin/src/pages/_app.tsx
+++ admin/src/pages/_app.tsx
@@ -1,83 +1,92 @@
 import "../../styles/index.scss";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import type { AppProps } from "next/app";
-import { useState, useEffect } from "react";
+import { useState, useEffect, MouseEvent } from "react";
 import * as api from "../api";
 
+export const SSO_AUTH_URI = "SSO_AUTH_URI";
+export const SSO_ACCESS_TOKENS = "SSO_ACCESS_TOKENS";
+const noop = () => {};
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<api.UserResponse>();
+
+  const retryLogin = (time: number) => {
+    return (e: Error) => {
+      console.error(e);
+      localStorage.removeItem(SSO_AUTH_URI);
+      localStorage.removeItem(SSO_ACCESS_TOKENS);
+      setTimeout(() => {
+        window.location.href = window.location.origin;
+      }, time * 1000);
+    };
+  };
+
+  const logout = (e: MouseEvent) => {
+    e.preventDefault();
+    api
+      .getAdminLogout()
+      .then(({ data }) => {
+        localStorage.removeItem(SSO_ACCESS_TOKENS);
+        console.log(data);
+        location.href = data as string;
+      })
+      .catch(retryLogin(10));
+  };
 
   useEffect(() => {
-    const localTokens = JSON.parse(
-      localStorage.getItem("SSOAuthenticationTokens") || "{}",
+    if (user) return noop;
+    // Handle the cases where user already has a token
+    const localTokens: api.AdminTokenResponse = JSON.parse(
+      localStorage.getItem(SSO_ACCESS_TOKENS) || "{}",
     );
     if ("access_token" in localTokens) {
-      const isValidToken = false;
-
-      console.log("Trying to login:", localTokens);
-      // @todo: send token and verify if is valid token
-      setIsLoggedIn(isValidToken);
-
-      let retryToken: NodeJS.Timeout;
-      if (!isValidToken) {
-        localStorage.removeItem("SSOAuthenticationTokens");
-        retryToken = setTimeout(() => {
-          window.location.href = window.location.origin;
-        }, 60000);
-        return () => clearInterval(retryToken);
-      }
-      return () => {};
+      api
+        .getAdminLogin(
+          /* localTokens, */ {
+            headers: {
+              Authorization: `Bearer ${localTokens.access_token}`,
+            },
+          },
+        )
+        .then(({ data }) => {
+          console.info("Logged in!", data);
+          setUser(data);
+        })
+        .catch(retryLogin(10));
+      return noop;
     }
-
-    let authCodeFlow: api.AuthCodeFlow;
-    authCodeFlow = JSON.parse(
-      localStorage.getItem("SSOAuthorizationRequest") || "{}",
-    );
-    if (!window.location.search) {
-      // get auth url
+    // If the user doesn't have a token, initiate auth code flow
+    let authURIRes: api.AuthURIResponse;
+    authURIRes = JSON.parse(localStorage.getItem(SSO_AUTH_URI) || "{}");
+    if (!location.search) {
       api
         .getAdminAuthorize()
         .then(({ data }) => {
-          localStorage.setItem("SSOAuthorizationRequest", JSON.stringify(data));
-          window.location.href = data.auth_uri as string;
+          // console.log("Authorization:", data);
+          localStorage.setItem(SSO_AUTH_URI, JSON.stringify(data));
+          location.href = data.auth_uri as string;
         })
-        .catch((e) => {
-          // @todo: error getting authorization url!
-          console.error("Error getting authorization url:", e);
-        });
-    } else if ("state" in authCodeFlow) {
-      // after it comes back from MS
+        .catch(retryLogin(10));
+    } else if ("state" in authURIRes) {
+      // when the user receives the code, we'll trade it for an access_token
       const authCodeRes: api.AuthCodeResponse = parseURLSearch(
         location.search.substring(1),
       );
-      // get access tokens
       api
-        .postAdminLogin({
+        .postAdminToken({
+          authURIRes,
           authCodeRes,
-          authCodeFlow,
         })
         .then(({ data }) => {
-          console.log("Logged in!", data);
-          localStorage.removeItem("SSOAuthorizationRequest");
-          localStorage.setItem("SSOAuthenticationTokens", JSON.stringify(data));
-          window.location.href = window.location.origin;
+          // console.log("Tokens:", data);
+          localStorage.removeItem(SSO_AUTH_URI);
+          localStorage.setItem(SSO_ACCESS_TOKENS, JSON.stringify(data));
+          location.href = location.origin;
         })
-        .catch((e) => {
-          // @todo: error logging in!
-          console.error("Error logging in:", e);
-        });
+        .catch(retryLogin(10));
     }
-    return () => {};
+    return noop;
   }, []);
-
-  if (!isLoggedIn) {
-    return (
-      <div className="login">
-        <i className="pfml-icon--pfml-logo login__logo" />
-        <h1 className="login__title">Logging in...</h1>
-      </div>
-    );
-  }
 
   return (
     <HelmetProvider>
@@ -95,72 +104,87 @@ function MyApp({ Component, pageProps }: AppProps) {
               className="page__logo-link"
             ></a>
           </div>
-          <div className="page__user-options">
-            <a
-              href="#"
-              role="button"
-              aria-label="FirstName LastName - User Options"
-              className="user-options"
-            >
-              <span className="user-options__avatar">
-                <img
-                  className="user-options__avatar-image"
-                  src="https://via.placeholder.com/32"
-                  alt="FirstName LastName"
-                />
-              </span>
-              <span className="user-options__name">FirstName LastName</span>
-              <span className="user-options__dropdown"></span>
-            </a>
-          </div>
+          {user && (
+            <div className="page__user-options">
+              <a
+                href="#"
+                role="button"
+                aria-label={`${user.email_address} - User Options`}
+                className="user-options"
+                onClick={logout}
+              >
+                <span className="user-options__avatar">
+                  <img
+                    className="user-options__avatar-image"
+                    src="https://via.placeholder.com/32"
+                    alt={user.email_address}
+                  />
+                </span>
+                <span className="user-options__name">{user.email_address}</span>
+                <span className="user-options__dropdown"></span>
+              </a>
+            </div>
+          )}
         </header>
-        <aside className="page__sidebar" tabIndex={0}>
-          <nav className="menu">
-            <ul className="menu__list">
-              <li className="menu__list-item">
-                <a href="#" className="menu__link menu__link--dashboard">
-                  Dashboard
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a href="#" className="menu__link menu__link--user-lookup">
-                  User Lookup
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a href="#" className="menu__link menu__link--maintenance">
-                  Maintenance
-                </a>
-              </li>
-              <li className="menu__list-item">
-                <a href="#" className="menu__link menu__link--features">
-                  Features
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <div className="settings">
-            <ul className="settings__list">
-              <li className="settings__list-item">
-                <a href="#" className="settings__link settings__link--settings">
-                  Settings
-                </a>
-              </li>
-              <li className="settings__list-item">
-                <a href="#" className="settings__link settings__link--help">
-                  Help
-                </a>
-              </li>
-            </ul>
+        {user && (
+          <>
+            <aside className="page__sidebar" tabIndex={0}>
+              <nav className="menu">
+                <ul className="menu__list">
+                  <li className="menu__list-item">
+                    <a href="#" className="menu__link menu__link--dashboard">
+                      Dashboard
+                    </a>
+                  </li>
+                  <li className="menu__list-item">
+                    <a href="#" className="menu__link menu__link--user-lookup">
+                      User Lookup
+                    </a>
+                  </li>
+                  <li className="menu__list-item">
+                    <a href="#" className="menu__link menu__link--maintenance">
+                      Maintenance
+                    </a>
+                  </li>
+                  <li className="menu__list-item">
+                    <a href="#" className="menu__link menu__link--features">
+                      Features
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="settings">
+                <ul className="settings__list">
+                  <li className="settings__list-item">
+                    <a
+                      href="#"
+                      className="settings__link settings__link--settings"
+                    >
+                      Settings
+                    </a>
+                  </li>
+                  <li className="settings__list-item">
+                    <a href="#" className="settings__link settings__link--help">
+                      Help
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="environment">
+                <div className="environment__label">Environment</div>
+                <div className="environment__flag">Production</div>
+              </div>
+            </aside>
+            <main className="page__main" tabIndex={0}>
+              <Component {...pageProps} />
+            </main>
+          </>
+        )}
+        {!user && (
+          <div className="login">
+            <div className="login__title">Logging in...</div>
           </div>
-          <div className="environment">
-            <div className="environment__label">Environment</div>
-            <div className="environment__flag">Production</div>
-          </div>
-        </aside>
-        <main className="page__main" tabIndex={0}>
-          <Component {...pageProps} />
-        </main>
+        )}
       </div>
     </HelmetProvider>
   );
