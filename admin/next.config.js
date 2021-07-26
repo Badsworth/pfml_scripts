ced66427e42a064cca55e88d070137edff02eb6a
Melquisedeque Pereira <melqui1@live.com.pt>
2021-07-26 17:12:27 +0100

    Added @svgr/webpack to import SVGs as React components

--- admin/next.config.js
+++ admin/next.config.js
@@ -1,5 +1,13 @@
 const buildEnv = process.env.BUILD_ENV || "development";
 
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
   reactStrictMode: true,
 };
