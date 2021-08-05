73887ab46f90ea8913fb91996fcad91efb6198ee
EyuelAbebe <eyueld@gmail.com>
2021-08-05 14:03:25 -0400

    API-1802: add additional data fields in absence period (#4946)
    * updated absence period table, added three new lktables
    
    * update relationship, cleanup.
    
    * create migration scripts
    
    * removed merge multiple migration heads script
    
    * update migration revision
    
    * fix ids
    
    * added fk_key relation
    
    * fix error
    
    * update migration revision
    
    * removed outdated migration script, created new one
    
    * update table names
    
    * fix migration mis-organization
    
    * fix migration mis-organization
    
    * fix spelling
    updated migration accordingly
    
    * migration accordingly
    
    * use numerals instead of digits
    
    * update migration to use numerals

--- api/massgov/pfml/db/models/employees.py
+++ api/massgov/pfml/db/models/employees.py
@@ -50,6 +50,46 @@ else:
     PostgreSQLUUID = UUID(as_uuid=True)
 
 
+class LkAbsencePeriodType(Base):
+    __tablename__ = "lk_absence_period_type"
+    absence_period_type_id = Column(Integer, primary_key=True, autoincrement=True)
+    absence_period_type_description = Column(Text)
+
+    def __init__(self, absence_period_type_id, absence_period_type_description):
+        self.absence_period_type_id = absence_period_type_id
+        self.absence_period_type_description = absence_period_type_description
+
+
+class LkAbsenceReason(Base):
+    __tablename__ = "lk_absence_reason"
+    absence_reason_id = Column(Integer, primary_key=True, autoincrement=True)
+    absence_reason_description = Column(Text)
+
+    def __init__(self, absence_reason_id, absence_reason_description):
+        self.absence_reason_id = absence_reason_id
+        self.absence_reason_description = absence_reason_description
+
+
+class LkAbsenceReasonQualifierOne(Base):
+    __tablename__ = "lk_absence_reason_qualifier_one"
+    absence_reason_qualifier_one_id = Column(Integer, primary_key=True, autoincrement=True)
+    absence_reason_qualifier_one_description = Column(Text)
+
+    def __init__(self, absence_reason_qualifier_one_id, absence_reason_qualifier_one_description):
+        self.absence_reason_qualifier_one_id = absence_reason_qualifier_one_id
+        self.absence_reason_qualifier_one_description = absence_reason_qualifier_one_description
+
+
+class LkAbsenceReasonQualifierTwo(Base):
+    __tablename__ = "lk_absence_reason_qualifier_two"
+    absence_reason_qualifier_two_id = Column(Integer, primary_key=True, autoincrement=True)
+    absence_reason_qualifier_two_description = Column(Text)
+
+    def __init__(self, absence_reason_qualifier_two_id, absence_reason_qualifier_two_description):
+        self.absence_reason_qualifier_two_id = absence_reason_qualifier_two_id
+        self.absence_reason_qualifier_two_description = absence_reason_qualifier_two_description
+
+
 class LkAbsenceStatus(Base):
     __tablename__ = "lk_absence_status"
     absence_status_id = Column(Integer, primary_key=True, autoincrement=True)
@@ -280,20 +320,34 @@ class AbsencePeriod(Base, TimestampMixin):
     )
 
     absence_period_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid_gen)
+    absence_period_start_date = Column(Date)
+    absence_period_end_date = Column(Date)
+    absence_period_type_id = Column(
+        Integer, ForeignKey("lk_absence_period_type.absence_period_type_id"), nullable=False
+    )
+    absence_reason_qualifier_one_id = Column(
+        Integer, ForeignKey("lk_absence_reason_qualifier_one.absence_reason_qualifier_one_id")
+    )
+    absence_reason_qualifier_two_id = Column(
+        Integer, ForeignKey("lk_absence_reason_qualifier_two.absence_reason_qualifier_two_id")
+    )
+    absence_reason_id = Column(
+        Integer, ForeignKey("lk_absence_reason.absence_reason_id"), nullable=False
+    )
     claim_id = Column(UUID(as_uuid=True), ForeignKey("claim.claim_id"), index=True, nullable=False)
     fineos_absence_period_class_id = Column(Integer, nullable=False, index=True)
     fineos_absence_period_index_id = Column(Integer, nullable=False, index=True)
     fineos_leave_request_id = Column(Integer)
-    absence_period_start_date = Column(Date)
-    absence_period_end_date = Column(Date)
     leave_request_decision_id = Column(
         Integer, ForeignKey("lk_leave_request_decision.leave_request_decision_id"), nullable=False
     )
     is_id_proofed = Column(Boolean)
-    claim_type_id = Column(Integer, ForeignKey("lk_claim_type.claim_type_id"), nullable=False)
 
     claim = relationship("Claim")
-    claim_type = relationship(LkClaimType)
+    absence_period_type = relationship(LkAbsencePeriodType)
+    absence_reason = relationship(LkAbsenceReason)
+    absence_reason_qualifier_one = relationship(LkAbsenceReasonQualifierOne)
+    absence_reason_qualifier_two = relationship(LkAbsenceReasonQualifierTwo)
     leave_request_decision = relationship(LkLeaveRequestDecision)
 
 
@@ -1221,6 +1275,94 @@ class AddressType(LookupTable):
     BUSINESS = LkAddressType(2, "Business")
     MAILING = LkAddressType(3, "Mailing")
     RESIDENTIAL = LkAddressType(4, "Residential")
+
+
+class AbsencePeriodType(LookupTable):
+    model = LkAbsencePeriodType
+    column_names = ("absence_period_type_id", "absence_period_type_description")
+
+    TIME_OFF_PERIOD = LkAbsencePeriodType(1, "Time off period")
+    REDUCED_SCHEDULE = LkAbsencePeriodType(2, "Reduced Schedule")
+    EPISODIC = LkAbsencePeriodType(3, "Episodic")
+    OFFICE_VISIT = LkAbsencePeriodType(4, "Office Visit")
+    INCAPACITY = LkAbsencePeriodType(5, "Incapacity")
+    OFFICE_VISIT_EPISODIC = LkAbsencePeriodType(6, "Office Visit Episodic")
+    INCAPACITY_EPISODIC = LkAbsencePeriodType(7, "Incapacity Episodic")
+    BLACKOUT_PERIOD = LkAbsencePeriodType(8, "Blackout Period")
+    UNSPECIFIED = LkAbsencePeriodType(9, "Unspecified")
+
+
+class AbsenceReason(LookupTable):
+    model = LkAbsenceReason
+    column_names = ("absence_reason_id", "absence_reason_description")
+
+    SERIOUS_HEALTH_CONDITION_EMPLOYEE = LkAbsenceReason(1, "Serious Health Condition - Employee")
+    MEDICAL_DONATION_EMPLOYEE = LkAbsenceReason(2, "Medical Donation - Employee")
+    PREVENTATIVE_CARE_EMPLOYEE = LkAbsenceReason(3, "Preventative Care - Employee")
+    SICKENESS_NON_SERIOUS_HEALTH_CONDITION_EMPLOYEE = LkAbsenceReason(
+        4, "Sickness - Non-Serious Health Condition - Employee"
+    )
+    PREGNANCY_MATERNITY = LkAbsenceReason(5, "Pregnancy/Maternity")
+    CHILD_BONDING = LkAbsenceReason(6, "Child Bonding")
+    CARE_OF_A_FAMILY_MEMBER = LkAbsenceReason(7, "Care for a Family Member")
+    BEREAVEMENT = LkAbsenceReason(8, "Bereavement")
+    EDUCATIONAL_ACTIVITY_FAMILY = LkAbsenceReason(9, "Educational Activity - Family")
+    MEDICAL_DONATION_FAMILY = LkAbsenceReason(10, "Medical Donation - Family")
+    MILITARY_EXIGENCY_FAMILY = LkAbsenceReason(11, "Military Caregiver")
+    MILITARY_EXIGENCY_FAMILY = LkAbsenceReason(12, "Military Exigency - Family")
+    PREVENTATIVE_CARE_FAMILY_MEMBER = LkAbsenceReason(13, "Preventative Care - Family Member")
+    PUBLIC_HEALTH_EMERGENCY_FAMILY = LkAbsenceReason(14, "Public Health Emergency - Family")
+
+
+class AbsenceReasonQualifierOne(LookupTable):
+    model = LkAbsenceReasonQualifierOne
+    column_names = ("absence_reason_qualifier_one_id", "absence_reason_qualifier_one_description")
+
+    NOT_WORK_RELATED = LkAbsenceReason(1, "Not Work Related")
+    WORK_RELATED = LkAbsenceReason(2, "Work Related")
+    BLOOD = LkAbsenceReason(3, "Blood")
+    BLOOD_STEM_CELL = LkAbsenceReason(4, "Blood Stem Cell")
+    BONE_MARROW = LkAbsenceReason(5, "Bone Marrow")
+    ORGAN = LkAbsenceReason(6, "Organ")
+    OTHER = LkAbsenceReason(7, "Other")
+    POSTNATAL_DISABILITY = LkAbsenceReason(8, "Postnatal Disability")
+    PRENATAL_CARE = LkAbsenceReason(9, "Prenatal Care")
+    PRENATAL_DISABILITY = LkAbsenceReason(10, "Prenatal Disability")
+    ADOPTION = LkAbsenceReason(11, "Adoption")
+    FOSTER_CARE = LkAbsenceReason(12, "Foster Care")
+    NEWBORN = LkAbsenceReason(13, "Newborn")
+    PREGNANCY_RELATED = LkAbsenceReason(14, "Pregnancy Related")
+    RIGHT_TO_LEAVE = LkAbsenceReason(15, "Right to Leave")
+    SERIOUS_HEALTH_CONDITION = LkAbsenceReason(16, "Serious Health Condition")
+    SICKNESS_NON_SERIOUS_HEALTH_CONDITION = LkAbsenceReason(
+        17, "Sickness - Non-Serious Health Condition"
+    )
+    CHILDCARE = LkAbsenceReason(18, "Childcare")
+    COUNSELING = LkAbsenceReason(19, "Counseling")
+    FINANCIAL_AND_LEGAL_ARRANGEMENTS = LkAbsenceReason(20, "Financial & Legal Arrangements")
+    MILITARY_EVENTS_AND_RELATED_ACTIVITIES = LkAbsenceReason(
+        21, "Military Events & Related Activities"
+    )
+    OTHER_ADDITIONAL_ACTIVITIES = LkAbsenceReason(22, "Other Additional Activities")
+    PRENATAL_CARE = LkAbsenceReason(23, "Prenatal Care")
+    POST_DEPLOYMENT_ACTIVITES_INCLUDING_BEREAVEMENT = LkAbsenceReason(
+        24, "Post Deployment Activities - Including Bereavement"
+    )
+    REST_AND_RECUPERATION = LkAbsenceReason(25, "Rest & Recuperation")
+    SHORT_NOTICE_DEPLOYMENT = LkAbsenceReason(26, "Short Notice Deployment")
+    CLOSURE_OF_SCHOOL_CHILDCARE = LkAbsenceReason(27, "Closure of School/Childcare")
+    QUARANTINE_ISOLATION_NON_SICK = LkAbsenceReason(28, "Quarantine/Isolation - Not Sick")
+    BIRTH_DISABILITY = LkAbsenceReason(29, "Birth Disability")
+    CHILDCARE_AND_SCHOOL_ACTIVITIES = LkAbsenceReason(30, "Childcare and School Activities")
+
+
+class AbsenceReasonQualifierTwo(LookupTable):
+    model = LkAbsenceReasonQualifierTwo
+    column_names = ("absence_reason_qualifier_two_id", "absence_reason_qualifier_two_description")
+
+    ACCIDENT_INJURY = LkAbsenceReason(1, "Accident / Injury")
+    MEDICAL_RELATED = LkAbsenceReason(2, "Medical Related")
+    NON_MEDICAL = LkAbsenceReason(3, "Non Medical")
 
 
 class GeoState(LookupTable):
