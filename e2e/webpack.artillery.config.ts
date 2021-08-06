b960b8292ce496d6aa602485b46284c702af95c2
Armando Roman <armandoroman1016@gmail.com>
2021-08-06 00:48:21 -0700

    PFMLPB-1744: manage artillery lst concurrency (#5003)
    Updates spec.yml to run more realistic traffic through our load and stress tests
    Adds a script to run a single iteration of claim submission for local development.

--- e2e/webpack.artillery.config.ts
+++ e2e/webpack.artillery.config.ts
@@ -3,55 +3,61 @@
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 import webpack from "webpack";
 import CopyPlugin from "copy-webpack-plugin";
-
-module.exports = {
-  name: "artillery",
-  mode: "development",
-  entry: {
-    processor: "./src/artillery/processor.ts",
-  },
-  output: {
-    libraryTarget: "commonjs2",
-  },
-  target: "node",
-  node: {
-    // @todo: Webpack 5 has an eval-only version of this. That would be much better, as this
-    // one operates on absolute paths.
-    __dirname: true,
-  },
-  externals: {
-    "playwright-chromium": "playwright-chromium",
-  },
-  resolve: {
-    // @see https://medium.com/better-programming/the-right-usage-of-aliases-in-webpack-typescript-4418327f47fa
-    plugins: [new TsconfigPathsPlugin()],
-    extensions: [".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]s$/,
-        exclude: [/node_modules/],
-        use: [
-          {
-            loader: "babel-loader",
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      "process.env.DOCUMENT_FORMS": JSON.stringify("forms"),
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "./.env", to: "./" },
-        { from: "./employees/*.employees.json", to: "./" },
-        { from: "./forms/*.pdf", to: "./" },
-        { from: "./src/artillery/package.json", to: "./" },
-        { from: "./src/artillery/spec.yml", to: "./" },
+import dotenv from "dotenv";
+module.exports = () => {
+  dotenv.config();
+  return {
+    name: "artillery",
+    mode: "development",
+    entry: {
+      processor: "./src/artillery/processor.ts",
+    },
+    output: {
+      libraryTarget: "commonjs2",
+    },
+    target: "node",
+    node: {
+      // @todo: Webpack 5 has an eval-only version of this. That would be much better, as this
+      // one operates on absolute paths.
+      __dirname: true,
+    },
+    externals: {
+      "playwright-chromium": "playwright-chromium",
+    },
+    resolve: {
+      // @see https://medium.com/better-programming/the-right-usage-of-aliases-in-webpack-typescript-4418327f47fa
+      plugins: [new TsconfigPathsPlugin()],
+      extensions: [".ts", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.[jt]s$/,
+          exclude: [/node_modules/],
+          use: [
+            {
+              loader: "babel-loader",
+            },
+          ],
+        },
       ],
-    }),
-  ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.DOCUMENT_FORMS": JSON.stringify("forms"),
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "./.env", to: "./" },
+          { from: "./employees/*.employees.json", to: "./" },
+          { from: "./forms/*.pdf", to: "./" },
+          { from: "./src/artillery/package.json", to: "./" },
+          { from: "./src/artillery/spec.yml", to: "./" },
+        ],
+      }),
+      new webpack.EnvironmentPlugin({
+        E2E_ENVIRONMENT: process.env.E2E_ENVIRONMENT,
+      }),
+    ],
+  };
 };
