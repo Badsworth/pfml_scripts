654fc3ca90253dc3a911949043ced01d92f90bd5
Robert Stephens <roberttstephens@gmail.com>
2021-08-06 15:25:04 -0400

    PFMLPB-1722: Prepare admin maintenance pages for API changes (#4955)
    Update the maintenance pages so the type matches what is expected from the API. Also update the VerticalMenu component to take links or buttons in enabled or disabled states.

--- admin/src/pages/maintenance.tsx
+++ admin/src/pages/maintenance.tsx
@@ -1,57 +1,87 @@
 import "react-datetime/css/react-datetime.css";
-//import { ApiResponse, FlagsResponse, getFlagsByNameLogs } from "../api";
+//import {
+//  ApiResponse,
+//  Flag,
+//  FlagsResponse,
+//  getFlagsByNameLogs,
+//  getFlagsByName,
+//} from "../api";
 import Alert from "../components/Alert";
+import ConfirmationDialog from "../components/ConfirmationDialog";
 import Link from "next/link";
 import { Helmet } from "react-helmet-async";
 import React from "react";
 import Table from "../components/Table";
 import Toggle from "../components/Toggle";
+import VerticalMenu from "../components/VerticalMenu";
 import moment from "moment";
 
 export default function Maintenance() {
-  const [maintenanceHistory, setMaintenanceHistory] = React.useState<
-    MaintenanceHistory[]
-  >([
-    {
-      enabled: true,
-      start: "2021-06-17 18:00:00-04",
-      end: "2021-06-17 17:00:00-04",
-      options: {
-        maintenanceName: "Fineos downtime two custom page routes",
-        page_routes: ["/applications/*", "/test/", "/another/"],
-      },
-    },
-    {
-      enabled: true,
-      start: "2021-06-17 18:00:00-04",
-      end: "2021-06-17 17:00:00-04",
-      options: {
-        maintenanceName: "Fineos downtime two checked page routes",
-        page_routes: ["/applications/*", "/employers/*", "/test/", "/another/"],
-      },
-    },
-  ]);
+  // Remove when Flag is in ../../api.
+  interface Flag {
+    start?: string | null;
+    end?: string | null;
+    name?: string;
+    options?: object;
+    enabled?: boolean;
+  }
+  type FlagsResponse = Flag[];
 
-  /*
+  const [showConfirmationDialog, setShowConfirmationDialog] =
+    React.useState(false);
+  const [maintenanceHistory, setMaintenanceHistory] =
+    React.useState<FlagsResponse>([
+      {
+        enabled: true,
+        end: "2021-07-30T04:00:00+00:00",
+        name: "maintenance",
+        options: {
+          name: "Two checked page routes, one custom",
+          page_routes: ["/*", "/applications/*", "/custom/*"],
+        },
+        start: "2021-07-29T04:00:00+00:00",
+      },
+    ]);
+  const [maintenance, setMaintenance] = React.useState<Flag | null>({
+    enabled: true,
+    end: "2021-08-03T04:00:00+00:00",
+    name: "maintenance",
+    options: {
+      name: "Current maintenance status",
+      page_routes: ["/*"],
+    },
+    start: "2021-08-02T04:00:00+00:00",
+  });
+
   React.useEffect(() => {
+    /*
+    getFlagsByName({ name: "maintenance" }).then(
+      (response: ApiResponse<Flag>) => {
+        setMaintenance(response.data);
+      },
+    );
     getFlagsByNameLogs({ name: "maintenance" }).then(
       (response: ApiResponse<FlagsResponse>) => {
         setMaintenanceHistory(response.data);
       },
     );
-  });
-  */
+    */
+  }, []);
 
-  type optionsObject = {
-    maintenanceName?: string;
-    page_routes?: string[];
+  const confirmationDialogCancelCallback = () => {
+    setShowConfirmationDialog(false);
   };
 
-  type MaintenanceHistory = {
-    enabled?: boolean;
-    start?: string | null;
-    end?: string | null;
-    options?: optionsObject | null;
+  const confirmationDialogContinueCallback = () => {
+    setMaintenance({ ...maintenance, ...{ enabled: false } });
+    // disable at API.
+    //patchFlagsByName({ name: "maintenance" }, flag).then().finally();
+    setShowConfirmationDialog(false);
+  };
+
+  type options = {
+    name?: string;
+    page_routes?: string[];
   };
 
   const checkedValues = [
@@ -62,37 +92,36 @@ export default function Maintenance() {
     "/login",
   ];
 
-  const currentMaintenance = maintenanceHistory[0];
-  const showCurrent = currentMaintenance.enabled ?? false;
-
   // Functions to format table.
   const formatDateTime = (datetime: string) => {
     return moment(datetime).format("MM-DD-YY hh:mmA");
   };
-  const getName = (m: MaintenanceHistory) => <>{m?.options?.maintenanceName}</>;
-  const getDuration = (m: MaintenanceHistory) => (
+  const getName = (m: Flag) => <>{(m?.options as options)?.name}</>;
+  const getDuration = (m: Flag) => (
     <>
       {(m.start ? formatDateTime(m.start) : "No start provided") +
         " - " +
         (m.end ? formatDateTime(m.end) : "No end provided")}
     </>
   );
-  const getPageRoutes = (m: MaintenanceHistory) => {
-    const routes = m?.options?.page_routes ?? [];
+  const getPageRoutes = (m: Flag) => {
+    const routes = (m?.options as options)?.page_routes ?? [];
 
     return routes.join(", ");
   };
-  const getCreatedBy = (m: MaintenanceHistory) => <>{"Admin"}</>;
-  const getOptions = (m: MaintenanceHistory) => {
+  const getCreatedBy = (m: Flag) => <>{"Admin"}</>;
+  const getOptions = (m: Flag) => {
     const linkValues: { [key: string]: string | string[] } = {};
-    linkValues.maintenance_name = m?.options?.maintenanceName ?? "";
-    const page_routes = m?.options?.page_routes ?? [];
+    linkValues.name = (m?.options as options)?.name ?? "";
+    const page_routes =
+      ((m?.options as options)?.page_routes as string[]) ?? [];
     linkValues.checked_page_routes = page_routes.filter((item) =>
       checkedValues.includes(item),
     );
     linkValues.custom_page_routes = page_routes.filter(
       (item) => !checkedValues.includes(item),
     );
+
     return (
       <>
         <Link
@@ -105,6 +134,10 @@ export default function Maintenance() {
         </Link>
       </>
     );
+  };
+
+  const getMaintenanceEnabled = (): boolean => {
+    return maintenance ? maintenance.enabled ?? false : false;
   };
 
   return (
@@ -128,39 +161,62 @@ export default function Maintenance() {
               to users instead of the normal page content.
             </p>
           </div>
-          <Toggle status={false} />
-          <Link
-            href={{
-              pathname: "/maintenance/add",
-            }}
-          >
-            <a className="maintenance-info__a-config"></a>
-          </Link>
+          <Toggle status={getMaintenanceEnabled()} />
+          <VerticalMenu
+            options={[
+              {
+                enabled: !getMaintenanceEnabled(),
+                href: "/maintenance/add",
+                text: "Configure",
+                type: "link",
+              },
+              {
+                enabled: getMaintenanceEnabled(),
+                href: "/maintenance/add",
+                text: "Edit",
+                type: "link",
+              },
+              {
+                enabled: getMaintenanceEnabled(),
+                onClick: () => setShowConfirmationDialog(true),
+                text: "Turn Off",
+                type: "button",
+              },
+            ]}
+          />
         </div>
-        {showCurrent && (
+        {showConfirmationDialog && (
+          <ConfirmationDialog
+            title="Turn off Maintenance"
+            body="If maintenance is turned off, the maintenance takeover component will no longer be shown in the portal. The change may take up to 5 minutes for some users due to browser cache."
+            handleCancelCallback={confirmationDialogCancelCallback}
+            handleContinueCallback={confirmationDialogContinueCallback}
+          />
+        )}
+        {maintenance && (maintenance.enabled ?? false) && (
           <div className="maintenance-status">
             <div className="maintenance-status__row">
               <div className="maintenance-status__label">Name</div>
               <div className="maintenance-status__value">
-                {getName(currentMaintenance)}
+                {getName(maintenance)}
               </div>
             </div>
             <div className="maintenance-status__row">
               <div className="maintenance-status__label">Created By</div>
               <div className="maintenance-status__value">
-                {getCreatedBy(currentMaintenance)}
+                {getCreatedBy(maintenance)}
               </div>
             </div>
             <div className="maintenance-status__row">
               <div className="maintenance-status__label">Duration</div>
               <div className="maintenance-status__value">
-                {getDuration(currentMaintenance)}
+                {getDuration(maintenance)}
               </div>
             </div>
             <div className="maintenance-status__row">
               <div className="maintenance-status__label">Page Routes</div>
               <div className="maintenance-status__value">
-                {getPageRoutes(currentMaintenance)}
+                {getPageRoutes(maintenance)}
               </div>
             </div>
           </div>
