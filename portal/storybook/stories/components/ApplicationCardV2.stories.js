e4e941b6bccee4cb360164a55933e5472e1ee5bd
Daniel W <wade.daniel.w@gmail.com>
2021-08-13 13:52:15 -0700

    CP-2438 implement ApplicationCardV2 (#5065)
    * added ApplicationCardV2 to components and Storybook
    
    * added current progress for early review
    
    * completed all current todos, ready for review
    
    * fixed ESLint errors
    
    * updated code per comments in PR
    
    * added basic unit test
    
    * removed unused prop type
    
    * fixed lint issues
    
    * updated component name per comments in PR
    
    * updated name of button section component
    
    * updated tests per PR comments

---
+++ portal/storybook/stories/components/ApplicationCardV2.stories.js
@@ -0,0 +1,43 @@
+import AppErrorInfoCollection from "src/models/AppErrorInfoCollection";
+import { ApplicationCardV2 } from "src/components/ApplicationCardV2";
+import BenefitsApplication from "src/models/BenefitsApplication";
+import { MockBenefitsApplicationBuilder } from "tests/test-utils";
+import React from "react";
+
+export default {
+  title: "Components/ApplicationCardV2",
+  component: ApplicationCardV2,
+  argTypes: {
+    claim: {
+      defaultValue: "Completed",
+      control: {
+        type: "radio",
+        options: ["Completed"],
+      },
+    },
+  },
+};
+
+export const Story = ({ claim, _documents, ...args }) => {
+  const attachedDocuments = [];
+  const errors = [];
+  const claimAttrs = {
+    Completed: new MockBenefitsApplicationBuilder().completed().create(),
+  }[claim];
+
+  const appLogic = {
+    appErrors: new AppErrorInfoCollection(errors),
+    documents: {
+      download: () => {},
+    },
+  };
+
+  return (
+    <ApplicationCardV2
+      appLogic={appLogic}
+      claim={new BenefitsApplication(claimAttrs)}
+      {...args}
+      documents={attachedDocuments}
+    />
+  );
+};
