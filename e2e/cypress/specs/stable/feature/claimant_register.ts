4ae9bd0802161278215bededf7766cf480a3e08b
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-08-18 17:21:38 +0500

    test

--- e2e/cypress/specs/stable/feature/claimant_register.ts
+++ e2e/cypress/specs/stable/feature/claimant_register.ts
@@ -1,4 +1,4 @@
-import { portal } from "../../../actions";
+import { portal, fineos } from "../../../actions";
 
 describe("Claimant Registration", () => {
   it("Should allow a new claimant to register", () => {
