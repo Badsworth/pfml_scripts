7341e89237d8e3dbab4c1d555c9b571e3bbb6ca5
Daniel Wade <wade.daniel.w@gmail.com>
2021-08-20 08:08:19 -0700

    updated application card v2 to include legal notices when available

--- portal/src/components/ApplicationCardV2.js
+++ portal/src/components/ApplicationCardV2.js
@@ -1,16 +1,20 @@
+import Document, { DocumentType } from "../models/Document";
 import routeWithParams, {
   createRouteWithQuery,
 } from "../utils/routeWithParams";
 
 import BenefitsApplication from "../models/BenefitsApplication";
 import ButtonLink from "../components/ButtonLink";
+import DownloadableDocument from "../components/DownloadableDocument";
 import Heading from "../components/Heading";
 import Icon from "../components/Icon";
 import LeaveReason from "../models/LeaveReason";
 import PropTypes from "prop-types";
 import React from "react";
+import findDocumentsByTypes from "../utils/findDocumentsByTypes";
 import findKeyByValue from "../utils/findKeyByValue";
 import { useTranslation } from "../locales/i18n";
+import withClaimDocuments from "../hoc/withClaimDocuments";
 
 /**
  * Main header for the top of status cards
@@ -30,9 +34,7 @@ HeaderSection.propTypes = {
  */
 const TitleAndDetailSectionItem = ({ details, title }) => (
   <div className="padding-2 padding-bottom-1 padding-top-0 margin-top-0">
-    <Heading level="4" size="6">
-      {title}
-    </Heading>
+    <p>{title}</p>
     <p className="margin-bottom-05 margin-top-05 text-bold">{details}</p>
   </div>
 );
@@ -47,9 +49,9 @@ TitleAndDetailSectionItem.propTypes = {
  */
 const ButtonSection = ({ buttonText, href, iconComponent = null }) => {
   return (
-    <div className="border-top border-base-lighter padding-2 margin-1 margin-bottom-0">
+    <div className="border-top border-base-lighter padding-y-2 margin-2 margin-bottom-0">
       <ButtonLink
-        className="display-flex flex-align-center flex-justify-center flex-column"
+        className="display-flex flex-align-center flex-justify-center flex-column margin-right-0"
         href={href}
       >
         <div>{buttonText}</div>
@@ -87,8 +89,8 @@ const ManageDocumentSection = ({ claim }) => {
   );
 
   return (
-    <div className="border-top border-base-lighter padding-2 padding-top-0 margin-1 margin-top-0">
-      <Heading className="padding-bottom-3 padding-top-3" level="4">
+    <div className="border-top border-base-lighter margin-2 margin-top-0 padding-bottom-1">
+      <Heading className="padding-y-3" level="3">
         {t("components.applicationCardV2.manageApplicationDocuments")}
       </Heading>
       <ButtonLink
@@ -113,6 +115,113 @@ const ManageDocumentSection = ({ claim }) => {
 ManageDocumentSection.propTypes = {
   claim: PropTypes.instanceOf(BenefitsApplication).isRequired,
 };
+
+/**
+ * Section to view notices and upload documents
+ */
+const LegalNoticeSection = ({ appLogic, claim, documents }) => {
+  const isSubmitted = claim.status === "Submitted";
+  const hasDocuments = Boolean(documents?.length);
+  const { t } = useTranslation();
+
+  /**
+   * If application is not submitted or has
+   * no documents, don't display section
+   */
+  if(!isSubmitted || !hasDocuments) return null;
+
+  const legalNotices = findDocumentsByTypes(documents, [
+    DocumentType.approvalNotice,
+    DocumentType.denialNotice,
+    DocumentType.requestForInfoNotice,
+  ]);
+
+  const legalNoticeList = legalNotices.map((document) => (
+    <li
+      className="grid-row flex-row flex-justify-start flex-align-start margin-bottom-1 text-primary"
+      key={document.fineos_document_id}
+    >
+      <Icon
+        className="margin-right-1"
+        fill="currentColor"
+        name="file_present"
+        size={3}
+      />
+      <div>
+        <DownloadableDocument
+          className="margin-left-2"
+          document={document}
+          onDownloadClick={appLogic.documents.download}
+          showCreatedAt
+        />
+      </div>
+    </li>
+  ));
+
+  return (
+    <div className="margin-2 margin-top-0 padding-bottom-1">
+      <Heading level="3">
+        {t("components.applicationCardV2.notice")}
+      </Heading>
+      <p className="padding-bottom-2 margin-top-05" style={{ maxWidth: 385 }}>
+        {t("components.applicationCardV2.noticeOnClickDetails")}
+      </p>
+      <ul className="add-list-reset">
+        {legalNoticeList}
+      </ul> 
+    </div>
+  );
+};
+
+LegalNoticeSection.defaultProps = {
+  documents: [],
+};
+
+LegalNoticeSection.propTypes = {
+  appLogic: PropTypes.shape({
+    appErrors: PropTypes.object.isRequired,
+    documents: PropTypes.shape({
+      download: PropTypes.func.isRequired,
+    }),
+  }).isRequired,
+  claim: PropTypes.instanceOf(BenefitsApplication).isRequired,
+  documents: PropTypes.arrayOf(PropTypes.instanceOf(Document)),
+};
+
+/**
+* Status card for claim.status != "Completed" (In Progress)
+*/
+const InProgressStatusCard = (props) => {
+  const { claim, number } = props;
+  const { t } = useTranslation();
+  
+  return (
+    <React.Fragment>
+      <HeaderSection
+        title={t("components.applicationCardV2.heading", {
+          number,
+        })}
+      />
+      <TitleAndDetailSectionItem
+        title={t("components.applicationCardV2.employerEIN")}
+        details={claim.employer_fein}
+      />
+      <LegalNoticeSection { ...props } />
+      <ButtonSection
+        buttonText={t("components.applicationCardV2.continueApplication")}
+        href={routeWithParams("applications.checklist", {
+          claim_id: claim.application_id,
+        })}
+      />
+    </React.Fragment>
+  );
+};
+
+InProgressStatusCard.propTypes = {
+  claim: PropTypes.instanceOf(BenefitsApplication).isRequired,
+  /**  The 1-based index of the application card */
+  number: PropTypes.number.isRequired,
+}; 
 
 /**
  * Status card for claim.status = "Completed"
@@ -149,8 +258,8 @@ const CompletedStatusCard = ({ claim }) => {
         buttonText={t(
           "components.applicationCardV2.viewStatusUpdatesAndDetails"
         )}
-        href={routeWithParams("applications.status", {
-          absence_case_id: claim.fineos_absence_id,
+        href={routeWithParams("applications.uploadDocsOptions", {
+          claim_id: claim.application_id,
         })}
         iconComponent={iconComponent}
       />
@@ -161,39 +270,6 @@ const CompletedStatusCard = ({ claim }) => {
 
 CompletedStatusCard.propTypes = {
   claim: PropTypes.instanceOf(BenefitsApplication).isRequired,
-};
-
-/**
- * Status card for claim.status != "Completed" (In Progress)
- */
-const InProgressStatusCard = ({ claim, number }) => {
-  const { t } = useTranslation();
-
-  return (
-    <React.Fragment>
-      <HeaderSection
-        title={t("components.applicationCardV2.heading", {
-          number,
-        })}
-      />
-      <TitleAndDetailSectionItem
-        title={t("components.applicationCardV2.employerEIN")}
-        details={claim.employer_fein}
-      />
-      <ButtonSection
-        buttonText={t("components.applicationCardV2.continueApplication")}
-        href={routeWithParams("applications.checklist", {
-          claim_id: claim.application_id,
-        })}
-      />
-    </React.Fragment>
-  );
-};
-
-InProgressStatusCard.propTypes = {
-  claim: PropTypes.instanceOf(BenefitsApplication).isRequired,
-  /**  The 1-based index of the application card */
-  number: PropTypes.number.isRequired,
 };
 
 /**
@@ -231,4 +307,4 @@ ApplicationCardV2.propTypes = {
   claim: PropTypes.instanceOf(BenefitsApplication).isRequired,
 };
 
-export default ApplicationCardV2;
+export default withClaimDocuments(ApplicationCardV2);
--- portal/storybook/stories/components/ApplicationCardV2.stories.js
+++ portal/storybook/stories/components/ApplicationCardV2.stories.js
@@ -1,7 +1,11 @@
+import Document, { DocumentType } from "src/models/Document";
+
+import AppErrorInfoCollection from "src/models/AppErrorInfoCollection";
 import { ApplicationCardV2 } from "src/components/ApplicationCardV2";
 import BenefitsApplication from "src/models/BenefitsApplication";
 import { MockBenefitsApplicationBuilder } from "tests/test-utils";
 import React from "react";
+import { uniqueId } from 'lodash';
 
 export default {
   title: "Components/ApplicationCardV2",
@@ -11,22 +15,79 @@ export default {
   },
   argTypes: {
     claim: {
-      defaultValue: "Completed",
+      defaultValue: "In Progress + Notices",
       control: {
         type: "radio",
-        options: ["Completed", "In Progress"],
+        options: [
+          "Completed",
+          "In Progress",
+          "In Progress + Notices",
+        ],
       },
     },
   },
 };
-
+ 
 export const Story = ({ claim, ...args }) => {
-  const claimAttrs = {
-    Completed: new MockBenefitsApplicationBuilder().completed().create(),
-    "In Progress": new MockBenefitsApplicationBuilder().employed().create(),
-  }[claim];
 
-  return (
-    <ApplicationCardV2 claim={new BenefitsApplication(claimAttrs)} {...args} />
-  );
+  // Generates claim of type (e.g., completed)
+  const generateClaim = (type) => {
+    return (
+      new BenefitsApplication(
+        new MockBenefitsApplicationBuilder()[type]().create()
+      )
+    );
+  };
+
+  // Generates notice of type (e.g., denialNotice)
+  const generateNotice = (type) => {
+
+    // Creates random number up to limit {number} value
+    const createRandomInteger = (limit) => {
+      const randomNumber = Math.floor(Math.random() * limit) + 1;
+      return `0${randomNumber}`.slice(-2);
+    };
+
+    // Four-digit prior year (e.g., 2020)
+    const lastYear = new Date().getFullYear() - 1;
+
+    // Random month/day for notice date
+    const randomMonth = createRandomInteger(12);
+    const randomDay = createRandomInteger(28);
+    
+    return new Document({
+      created_at: `${lastYear}-${randomMonth}-${randomDay}`,
+      document_type: DocumentType[type],
+      fineos_document_id: uniqueId('notice'),
+    })
+  };
+ 
+  // Fake appLogic for stories
+  const appLogic = {
+    appErrors: new AppErrorInfoCollection([]),
+    documents: {
+      download: () => {},
+    },
+  }
+
+  // Configuration for ApplicationCard props
+  const cardProps = Object.assign({
+    "Completed": {
+      claim: generateClaim('completed'),
+    },
+
+    "In Progress + Notices": {
+      claim: generateClaim('submitted'),
+      documents: [
+        generateNotice('requestForInfoNotice'),
+        generateNotice('denialNotice')
+      ],
+    },
+    
+    "In Progress": {
+      claim: generateClaim('employed'),
+    }
+  }[claim], { appLogic, ...args });
+  
+  return <ApplicationCardV2 { ...cardProps } />;
 };
