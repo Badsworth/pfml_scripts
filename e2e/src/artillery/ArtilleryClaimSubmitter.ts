0c6ea321b09f05db338381ca303cb6643fbc9877
Joseph C.A. Garcia <48695203+kingmoc@users.noreply.github.com>
2021-08-26 16:50:31 -0500

    PFMLPB-1858: Enrich logs being sent to AWS Cloudwatch (winston logger) (#5162)
    Uses winston during LST to enrich logging specifically for viewing/searching in AWS Cloudwatch. Logs will eventually be used in Cloudwatch metrics for analysis.
    
    Co-authored-by: Joseph Garcia <joe@lastcallmedia.com>

--- e2e/src/artillery/ArtilleryClaimSubmitter.ts
+++ e2e/src/artillery/ArtilleryClaimSubmitter.ts
@@ -4,6 +4,7 @@ import PortalSubmitter from "../submission/PortalSubmitter";
 import { ApplicationSubmissionResponse } from "../types";
 import { splitClaimToParts } from "../util/common";
 import { getAuthManager } from "../util/common";
+import winston from "winston";
 
 class ArtilleryClaimSubmitter extends PortalSubmitter {
   constructor(authenticator: AuthenticationManager, apiBaseUrl: string) {
@@ -17,7 +18,8 @@ class ArtilleryClaimSubmitter extends PortalSubmitter {
   async lstSubmit(
     claim: GeneratedClaim,
     credentials: Credentials,
-    employerCredentials?: Credentials
+    employerCredentials?: Credentials,
+    logger?: winston.Logger
   ): Promise<ApplicationSubmissionResponse> {
     const options = await this.getOptions(credentials);
     const application_id = await this.createApplication(options);
@@ -25,30 +27,57 @@ class ArtilleryClaimSubmitter extends PortalSubmitter {
     for (const part of claimParts)
       await this.updateApplication(application_id, part, options);
 
+    logger?.debug("Submitting Part 1 of application");
     const submitResponseData = await this.submitApplication(
       application_id,
       options
     );
-
     const { fineos_absence_id, first_name, last_name } = submitResponseData;
+    logger?.debug("Submitting Part 1 of application complete!", {
+      fineos_absence_id,
+    });
+    logger?.debug("Attempting to upload Documents", {
+      fineos_absence_id,
+    });
     await this.uploadDocuments(application_id, claim.documents, options);
+    logger?.debug("Document upload complete!", {
+      fineos_absence_id,
+    });
+    logger?.debug("Attempting to upload Payment Info", {
+      fineos_absence_id,
+    });
     await this.uploadPaymentPreference(
       application_id,
       claim.paymentPreference,
       options
     );
+    logger?.debug("Attempting to complete Application (Final Submit)", {
+      fineos_absence_id,
+    });
     await this.completeApplication(application_id, options);
+    logger?.debug("Full Application submittal complete!", {
+      fineos_absence_id,
+    });
     if (claim.employerResponse) {
       if (!employerCredentials) {
         throw new Error(
           "Unable to submit employer response without leave admin credentials"
         );
       }
+      logger?.debug("Attempting to complete Employer Response", {
+        fineos_absence_id,
+      });
       await this.submitEmployerResponse(
         employerCredentials,
         fineos_absence_id,
         claim.employerResponse
       );
+      logger?.debug("Employer Response complete!", {
+        fineos_absence_id,
+      });
+      logger?.debug("Full Submittal and ER complete w/o errors!", {
+        fineos_absence_id,
+      });
     }
     return {
       fineos_absence_id: fineos_absence_id,
