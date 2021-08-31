dc163f72cc51e9f90660d22674d9fe31b0159544
Michael Huneke <mhuneke@gmail.com>
2021-08-31 09:27:20 -0400

    Format

--- api/tests/util/aws/test_ses.py
+++ api/tests/util/aws/test_ses.py
@@ -47,9 +47,7 @@ def test_send_email(mock_ses):
         {"fineos_customer_number": 1, "ctr_vendor_customer_code": 1},
         {"fineos_customer_number": 2, "ctr_vendor_customer_code": 2},
     ]
-    attachments = [
-        create_csv_from_list(fineos_vendor_customer_numbers, fieldnames, file_name)
-    ]
+    attachments = [create_csv_from_list(fineos_vendor_customer_numbers, fieldnames, file_name)]
     response = conn.send_email(
         recipient, subject, body_text, sender, bounce_forwarding_email_address_arn, attachments
     )
