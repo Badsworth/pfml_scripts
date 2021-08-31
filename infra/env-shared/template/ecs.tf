2863c420b0601d12a7f569dad4c349e6bb388b2e
paz-focus <76061418+paz-focus@users.noreply.github.com>
2021-08-30 20:21:56 -0400

    INFRA-633: Added cloudwatch log group (#5054)
    * Added cloudwatch log group
    
    * added local
    
    * added env
    
    * added var
    
    * added env
    
    * added default
    
    * fix for env
    
    * loop
    
    * string test
    
    * formatting
    
    * added target and rule
    
    * adding loop
    
    * adding loop
    
    * formatting
    
    * description change
    
    * env change
    
    * formatting
    
    * moved the code to ecs.rf
    
    * formatting
    
    * add cluster arn
    
    * log group change
    
    * fix the comma
    
    * added array
    
    * fix typo
    
    * detail type change
    
    * adding policy
    
    * adding a wild card
    
    * direct reference for iam role
    
    * fix the cluster arn
    
    * adding subscription filter and lambda permission
    
    * add log ingestion lambda
    
    * added comment

--- infra/env-shared/template/ecs.tf
+++ infra/env-shared/template/ecs.tf
@@ -1,3 +1,11 @@
+locals {
+  app_name = "pfml-api"
+  # This ARN describes a 3rd-party lambda installed outside of Terraform thru the AWS Serverless Application Repository.
+  # This lambda ingests CloudWatch logs from several sources, and packages them for transmission to New Relic's servers.
+  # This lambda was modified post-installation to fix an apparent bug in the processing/packaging of its telemetry data.
+  newrelic_log_ingestion_lambda = module.constants.newrelic_log_ingestion_arn
+}
+
 # ECS cluster for running applications in an environment.
 #
 # Applications within this cluster are not actually tied
@@ -16,3 +24,50 @@ resource "aws_ecs_cluster" "cluster" {
     "SMX:Asset" = "v1:${var.environment_name}:${module.constants.smartronix_environment_tags[var.environment_name]}:ECS:PFML:Advanced:None"
   })
 }
+
+###### ECS Task Stopped State Events Monitoring
+
+resource "aws_cloudwatch_log_group" "ecs_tasks_events" {
+  name = "service/${local.app_name}-${var.environment_name}/ecs-tasks/events"
+}
+
+resource "aws_cloudwatch_event_rule" "ecs_tasks_events" {
+  name          = "ecs-tasks-events"
+  description   = "Monitors ECS tasks all events"
+  event_pattern = <<EOF
+  {
+    "source": [
+      "aws.ecs"
+    ],
+    "detail-type": [
+      "ECS Task State Change"
+    ],
+    "detail": {
+      "desiredStatus": ["STOPPED"],
+      "clusterArn": ["${aws_ecs_cluster.cluster.arn}"]
+    }
+  }
+  EOF
+}
+
+resource "aws_cloudwatch_event_target" "ecs_tasks_events_target" {
+  arn  = aws_cloudwatch_log_group.ecs_tasks_events.arn
+  rule = aws_cloudwatch_event_rule.ecs_tasks_events.id
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "ecs_tasks_events_logging" {
+  name            = "ecs_tasks_events_logs"
+  log_group_name  = aws_cloudwatch_log_group.ecs_tasks_events.name
+  destination_arn = local.newrelic_log_ingestion_lambda
+  # matches all log events
+  filter_pattern = ""
+}
+
+resource "aws_lambda_permission" "ecs_permission_tasks_events_logging" {
+  statement_id  = "NRLambdaPermission_ECSTasksEventsLogging_${var.environment_name}"
+  action        = "lambda:InvokeFunction"
+  function_name = local.newrelic_log_ingestion_lambda
+  principal     = "logs.us-east-1.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_log_group.ecs_tasks_events.arn}:*"
+}
+
