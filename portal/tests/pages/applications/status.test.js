3dbd5ceff71ba83f73ddb0a73d4f4f7b9429cc0a
Jamal Pace <jkpace@users.noreply.github.com>
2021-08-31 11:18:06 -0400

    CP-2499: Conditionally show info about bonding and pregnancy leave (#5213)
    Show info about taking both bonding and pregnancy leave.

--- portal/tests/pages/applications/status.test.js
+++ portal/tests/pages/applications/status.test.js
@@ -1,4 +1,5 @@
 import Status, { LeaveDetails } from "../../../src/pages/applications/status";
+import LeaveReason from "../../../src/models/LeaveReason";
 import { renderWithAppLogic } from "../../test-utils";
 import routes from "../../../src/routes";
 
@@ -24,6 +25,56 @@ describe("status page", () => {
       diveLevels: 0,
     });
     expect(wrapper).toMatchSnapshot();
+  });
+
+  describe("info alert", () => {
+    it("displays if claimant has bonding but not pregnancy claims", () => {
+      const claimsWithoutPregnancy = {
+        [LeaveReason.bonding]: [{ reason: LeaveReason.bonding }],
+        [LeaveReason.care]: [{ reason: LeaveReason.care }],
+      };
+
+      const { wrapper } = renderWithAppLogic(Status, {
+        diveLevels: 0,
+        props: { absenceDetails: claimsWithoutPregnancy },
+      });
+
+      const infoAlertComponent = wrapper.find({ "data-test": "info-alert" });
+      expect(infoAlertComponent.exists()).toBe(true);
+      expect(infoAlertComponent.dive()).toMatchSnapshot();
+      expect(infoAlertComponent.find("Trans").dive()).toMatchSnapshot();
+    });
+
+    it("displays if claimant has pregnancy but not bonding claims", () => {
+      const claimsWithoutBonding = {
+        [LeaveReason.pregnancy]: [{ reason: LeaveReason.pregnancy }],
+        [LeaveReason.care]: [{ reason: LeaveReason.care }],
+      };
+
+      const { wrapper } = renderWithAppLogic(Status, {
+        diveLevels: 0,
+        props: { absenceDetails: claimsWithoutBonding },
+      });
+
+      const infoAlertComponent = wrapper.find({ "data-test": "info-alert" });
+      expect(infoAlertComponent.exists()).toBe(true);
+      expect(infoAlertComponent.dive()).toMatchSnapshot();
+      expect(infoAlertComponent.find("Trans").dive()).toMatchSnapshot();
+    });
+
+    it("does not display if claimant has bonding AND pregnancy claims", () => {
+      const claimsWithBondingAndPregnancy = {
+        [LeaveReason.bonding]: [{ reason: LeaveReason.bonding }],
+        [LeaveReason.pregnancy]: [{ reason: LeaveReason.pregnancy }],
+      };
+
+      const { wrapper } = renderWithAppLogic(Status, {
+        diveLevels: 0,
+        props: { absenceDetails: claimsWithBondingAndPregnancy },
+      });
+
+      expect(wrapper.find({ "data-test": "info-alert" }).exists()).toBe(false);
+    });
   });
 
   it("does not render ViewYourNotices if documents not given", () => {
