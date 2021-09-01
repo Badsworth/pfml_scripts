fa682eda8827d67e148b53f1a0a0f5b658a4f849
Michael Huneke <mhuneke@gmail.com>
2021-09-01 18:17:08 -0400

    Fix import

--- api/massgov/pfml/reductions/dia.py
+++ api/massgov/pfml/reductions/dia.py
@@ -20,7 +20,7 @@ from massgov.pfml.delegated_payments.delegated_payments_util import (
     get_now,
     move_file_and_update_ref_file,
 )
-from massgov.pfml.delegated_payments.sftp_s3_transfer import (
+from massgov.pfml.util.sftp_s3_transfer import (
     SftpS3TransferConfig,
     copy_from_sftp_to_s3_and_archive_files,
     copy_to_sftp_and_archive_s3_files,
