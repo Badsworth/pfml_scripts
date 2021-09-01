bfe3b50a1c737afa4fbfa88b775cab501d024241
Michael Huneke <mhuneke@gmail.com>
2021-09-01 18:21:37 -0400

    Format

--- api/massgov/pfml/reductions/dua.py
+++ api/massgov/pfml/reductions/dua.py
@@ -25,15 +25,15 @@ from massgov.pfml.delegated_payments.delegated_payments_util import (
     get_now,
     move_file_and_update_ref_file,
 )
+from massgov.pfml.reductions.common import AgencyLoadResult, get_claimants_for_outbound
+from massgov.pfml.reductions.config import get_moveit_config, get_s3_config
+from massgov.pfml.util.datetime import utcnow
+from massgov.pfml.util.files import create_csv_from_list, upload_to_s3
 from massgov.pfml.util.sftp_s3_transfer import (
     SftpS3TransferConfig,
     copy_from_sftp_to_s3_and_archive_files,
     copy_to_sftp_and_archive_s3_files,
 )
-from massgov.pfml.reductions.common import AgencyLoadResult, get_claimants_for_outbound
-from massgov.pfml.reductions.config import get_moveit_config, get_s3_config
-from massgov.pfml.util.datetime import utcnow
-from massgov.pfml.util.files import create_csv_from_list, upload_to_s3
 
 logger = logging.get_logger(__name__)
 
