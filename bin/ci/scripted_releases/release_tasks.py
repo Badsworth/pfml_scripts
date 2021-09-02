aecdac9b78f341f818f7bb33fb6eb31d5313eb37
apopma <adapopma@navapbc.com>
2021-09-02 11:54:51 -0700

    INFRA-641: Scripted-releases housekeeping (#5234)

--- bin/ci/scripted_releases/release_tasks.py
+++ bin/ci/scripted_releases/release_tasks.py
@@ -1,16 +1,20 @@
 from scripted_releases import git_utils
 import semver
+import logging
 
-def start(args, logger):
+logger = logging.getLogger(__name__)
+
+
+def start(args):
     # increments minor release number
     logger.info(f"Running 'start-release', args: {str(args)}")
 
     # getting the proper tags/branches for the release
     git_utils.fetch_remotes()
 
-    recent_tag = git_utils.most_recent_tag(args.app) # get most recent tag
+    recent_tag = git_utils.most_recent_tag(args.app)  # get most recent tag
 
-    v = git_utils.to_semver(recent_tag) # convert tag to semver object
+    v = git_utils.to_semver(recent_tag)  # convert tag to semver object
     version_name = git_utils.from_semver(v.bump_minor(), args.app)
     branch_name = "release/" + version_name
 
@@ -22,17 +26,17 @@ def start(args, logger):
     git_utils.tag_branch(branch_name, tag_name)
 
 
-def update(args, logger):
+def update(args):
     logger.info(f"Running 'update-release', args: {repr(args)}")
 
 
-def finalize(args, logger):
+def finalize(args):
     logger.info(f"Running 'finalize-release', args: {repr(args)}")
 
 
-def hotfix(args, logger):
+def hotfix(args):
     logger.info(f"Running 'hotfix'; args, {repr(args)}")
 
 
-def major(args, logger):
+def major(args):
     logger.info(f"Running 'major-release'; args, {repr(args)}")
