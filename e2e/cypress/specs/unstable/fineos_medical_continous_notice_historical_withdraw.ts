81fd664ab3c4564abd036eea77747d672ae51e9e
Asa Needle <asa@asaneedle.com>
2021-09-03 14:46:03 -0400

    add withdraw notice test

--- e2e/cypress/specs/unstable/fineos_medical_continous_notice_historical_withdraw.ts
+++ e2e/cypress/specs/unstable/fineos_medical_continous_notice_historical_withdraw.ts
@@ -1,6 +1,7 @@
-import { fineos, fineosPages } from "../../actions";
-import { config } from "../../actions/common";
+import { fineos, fineosPages, portal } from "../../actions";
+
 import { Submission } from "../../../src/types";
+import { config } from "../../actions/common";
 
 describe("Create a new continuous medical leave claim in FINEOS and add Historical Absence case. Then withdraw the Absence Case", () => {
   const credentials: Credentials = {
@@ -8,7 +9,7 @@ describe("Create a new continuous medical leave claim in FINEOS and add Historic
     password: config("PORTAL_PASSWORD"),
   };
 
-  it("Create historical absence case within Absence Case", () => {
+  const submit = it("Create historical absence case within Absence Case", () => {
     cy.task("generateClaim", "HIST_CASE").then((claim) => {
       cy.task("submitClaimToAPI", {
         ...claim,
@@ -43,4 +44,33 @@ describe("Create a new continuous medical leave claim in FINEOS and add Historic
       });
     });
   });
+
+  it(
+    "Should generate a legal notice (Withdrawal) that the claimant can view",
+    { retries: 0 },
+    () => {
+      cy.dependsOnPreviousPass([submit]);
+      portal.before();
+      cy.visit("/");
+      portal.login(credentials);
+      cy.unstash<Submission>("submission").then((submission) => {
+        // Wait for the legal document to arrive.
+        cy.task(
+          "waitForClaimDocuments",
+          {
+            credentials: credentials,
+            application_id: submission.application_id,
+            document_type: "Pending Application Withdrawn",
+          },
+          { timeout: 30000 }
+        );
+
+        cy.visit("/applications");
+        cy.contains("article", submission.fineos_absence_id).within(() => {
+          cy.findByText("Pending Application Withdrawn (PDF)").should("be.visible").click();
+        });
+        portal.downloadLegalNotice(submission.fineos_absence_id);
+      });
+    }
+  );
 });
