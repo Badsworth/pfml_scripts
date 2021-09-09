4ab827526148805eee0a79fcc524777d6453c8cd
Paul Parker <paul.parker@focusconsulting.io>
2021-09-09 12:23:15 -0400

    Prefix s3 bucket with s3://

--- infra/ecs-tasks/template/tasks.tf
+++ infra/ecs-tasks/template/tasks.tf
@@ -364,7 +364,7 @@ locals {
       task_role = aws_iam_role.task_execute_sql_task_role.arn
       env = [
         local.db_access,
-        { name : "S3_BUCKET", value : "massgov-pfml-${var.environment_name}-execute-sql-export" }
+        { name : "S3_BUCKET", value : "s3://massgov-pfml-${var.environment_name}-execute-sql-export" }
       ]
     },
   }
