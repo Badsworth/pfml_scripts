65c4f430657ea2792a286f8af9e5f818737790c6
Melissa Rossi <mrossi113@gmail.com>
2021-09-15 13:24:59 -0400

    adjust the portal submit for documents

--- e2e/cypress/specs/unstable/fineos_appeal_denial_decision.ts
+++ e2e/cypress/specs/unstable/fineos_appeal_denial_decision.ts
@@ -24,7 +24,7 @@ describe("Submit a claim through Portal: Verify it creates an absence case in Fi
             timestamp_from: Date.now(),
           });
         });
-        portal.submitPartsTwoThreeNoLeaveCert(paymentPreference);
+        portal.submitClaimPartsTwoThree(application, paymentPreference);
       });
     });
 
