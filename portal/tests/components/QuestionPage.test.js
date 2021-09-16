1a0e19d267223b1e630803bcde6c014499eb9a0a
umihui <umihui86@gmail.com>
2021-09-16 10:30:24 -0700

    increase test coverage of QuestionPage

--- portal/tests/components/QuestionPage.test.js
+++ portal/tests/components/QuestionPage.test.js
@@ -1,7 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import QuestionPage from "../../src/components/QuestionPage";
 import React from "react";
+import tracker from "../../src/services/tracker";
 import userEvent from "@testing-library/user-event";
+
+jest.mock("../../src/services/tracker");
 
 describe("QuestionPage", () => {
   const sampleRoute = "/";
@@ -31,5 +34,19 @@ describe("QuestionPage", () => {
     userEvent.click(screen.getByRole("button"));
 
     expect(handleSaveMock).toHaveBeenCalled();
+    expect(tracker.trackEvent).not.toHaveBeenCalled();
+  });
+
+  it("tracks the event when onSave is not a Promise", () => {
+    const handleSaveMock = jest.fn();
+    render(
+      <QuestionPage title={sampleTitle} onSave={handleSaveMock}>
+        <div>Some stuff here</div>
+      </QuestionPage>
+    );
+
+    userEvent.click(screen.getByRole("button"));
+    expect(handleSaveMock).toHaveBeenCalled();
+    expect(tracker.trackEvent).toHaveBeenCalled();
   });
 });
