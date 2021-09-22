ff1f916a7e9ed8178aaaa79720ee1106957068dc
jtosbornex <joshosborne@navapbc.com>
2021-09-22 08:54:36 -0700

    keeping track of  sql

---
+++ api/tests/duplicate_users/merge_duplicate_users.sql
@@ -0,0 +1,142 @@
+WITH duplicate_user_emails AS (
+    SELECT
+        "user".email_address,
+        MIN("user".created_at) AS oldest_created_at
+    FROM
+        "user"
+    GROUP BY
+        "user".email_address
+    HAVING
+        count("user".user_id) > 1
+),
+duplicated_users AS (
+    SELECT
+        "user".created_at,
+        "user".updated_at,
+        "user".user_id,
+        "user".sub_id,
+        "user".email_address,
+        "user".consented_to_data_sharing
+    FROM
+        "user"
+    WHERE
+        "user".email_address IN (
+            SELECT
+                email_address
+            FROM
+                duplicate_user_emails
+        )
+),
+latest_updated_users AS (
+    SELECT
+        u.created_at,
+        u.updated_at,
+        u.user_id,
+        u.sub_id,
+        u.email_address,
+        u.consented_to_data_sharing
+    FROM
+        duplicate_user_emails u_dup
+        INNER JOIN "user" u ON u.email_address = u_dup.email_address
+        AND u_dup.oldest_created_at = u.created_at
+),
+duplicated_users_to_remove AS (
+    SELECT
+        luu.user_id AS user_id_to_keep,
+        u.email_address,
+        u.user_id,
+        u.created_at,
+        u.updated_at,
+        u.sub_id,
+        u.consented_to_data_sharing
+    FROM
+        duplicate_user_emails u_dup
+        INNER JOIN "user" u ON u.email_address = u_dup.email_address
+        INNER JOIN latest_updated_users luu ON luu.email_address = u.email_address
+    WHERE
+        u.user_id NOT IN (
+            SELECT
+                user_id
+            FROM
+                latest_updated_users
+        )
+),
+merged_documents AS (
+    UPDATE
+        "document"
+    SET
+        user_id = duu.user_id_to_keep
+    FROM
+        "document" doc
+        INNER JOIN duplicated_users_to_remove duu ON duu.user_id = doc.user_id RETURNING doc.*
+),
+merged_applications AS (
+    UPDATE
+        application
+    SET
+        user_id = duu.user_id_to_keep
+    FROM
+        application a
+        INNER JOIN duplicated_users_to_remove duu ON duu.user_id = a.user_id RETURNING a.*
+),
+merged_user_leave_administrators AS (
+    UPDATE
+        link_user_leave_administrator
+    SET
+        user_id = duu.user_id_to_keep
+    FROM
+        link_user_leave_administrator lula
+        INNER JOIN duplicated_users_to_remove duu ON duu.user_id = lula.user_id RETURNING lula.*
+),
+merged_requirements AS (
+    UPDATE
+        managed_requirement
+    SET
+        respondent_user_id = duu.user_id_to_keep
+    FROM
+        managed_requirement mr
+        INNER JOIN duplicated_users_to_remove duu ON duu.user_id = mr.respondent_user_id RETURNING mr.*
+),
+merged_user_roles AS (
+    INSERT INTO
+        link_user_role (user_id, role_id)
+    SELECT
+        duu.user_id_to_keep,
+        lur.role_id
+    FROM
+        link_user_role lur
+    WHERE
+        lur.user_id IN (
+            SELECT
+                duu.user_id
+            FROM
+                duplicated_users_to_remove duu
+        ) ON CONFLICT (user_id, role_id) DO NOTHING
+),
+deleted_user_roles AS (
+    DELETE FROM
+        link_user_role lur USING duplicated_users_to_remove
+    WHERE
+        lur.user_id IN (
+            SELECT
+                duu.user_id
+            FROM
+                duplicated_users_to_remove duu
+        ) RETURNING lur.*
+),
+deleted_users AS (
+    DELETE FROM
+        "user" uu USING duplicated_users_to_remove
+    WHERE
+        uu.user_id IN (
+            SELECT
+                duu.user_id
+            FROM
+                duplicated_users_to_remove duu
+        ) RETURNING uu.*
+)
+SELECT
+    du.user_id,
+    du.email_address
+FROM
+    deleted_users du
---
+++ api/tests/duplicate_users/test_duplicate_users_sql.py
@@ -0,0 +1,113 @@
+import os
+from typing import List
+import pytest
+from sqlalchemy.orm.session import Session
+from sqlalchemy.sql.expression import select, text
+from sqlalchemy.sql.functions import count, func
+from sqlalchemy.dialects import postgresql
+from massgov.pfml import db
+from massgov.pfml.db.models.applications import Application
+
+from massgov.pfml.db.models.employees import Role, User, UserLeaveAdministrator, UserRole
+from massgov.pfml.db.models.factories import (
+    ApplicationFactory,
+    DocumentFactory,
+    EmployerFactory,
+    ManagedRequirementFactory,
+    UserFactory,
+)
+
+
+@pytest.fixture
+def generate_duplicate_users(initialize_factories_session, test_db_session):
+
+    emails_to_duplicate = [
+        "test@email.com",
+        "test2@email.com",
+        "test3@email.com",
+        "test4@email.com",
+    ]
+
+    for email in emails_to_duplicate:
+        # Add data to a single user
+        user_1_a = UserFactory.create(email_address=email, roles=[Role.USER])
+
+        for _ in range(22):
+            employer = EmployerFactory.create()
+            UserLeaveAdministrator(
+                user_id=user_1_a.user_id, employer_id=employer.employer_id,
+            )
+
+            app = ApplicationFactory.create(user=user_1_a)
+            for _2 in range(6):
+                DocumentFactory.create(user_id=user_1_a.user_id, application_id=app.application_id)
+
+            ManagedRequirementFactory.create(respondent_user=user_1_a)
+
+        # Create duplicate data for that user
+        for _ in range(10):
+            user = UserFactory.create(email_address=email, roles=[Role.USER])
+            employer = EmployerFactory.create()
+
+            UserLeaveAdministrator(
+                user_id=user.user_id, employer_id=employer.employer_id,
+            )
+            for _ in range(6):
+                app = ApplicationFactory.create(user=user)
+                for _2 in range(4):
+                    DocumentFactory.create(user_id=user.user_id, application_id=app.application_id)
+
+                ManagedRequirementFactory.create(respondent_user=user)
+
+
+def open_file(fileName: str):
+    pth = os.path.join(os.path.dirname(__file__), fileName)
+    print(pth)
+    fd = open(pth, "r")
+    sql_file = fd.read()
+    fd.close()
+
+    return sql_file.strip("\n")
+
+
+class UserEmailCount(db.RowProxy):
+    email_address: User.email_address
+    count: int
+
+
+def test_remove_and_merge_duplicates(
+    initialize_factories_session, test_db_session: Session, generate_duplicate_users,
+):
+    all_users = test_db_session.query(User).count()
+    assert all_users == 44
+
+    sql_file = open_file("merge_duplicate_users.sql")
+
+    duplicated_users: List[UserEmailCount] = (
+        test_db_session.query(User.email_address, count(User.user_id).label("count"))
+        .group_by(User.email_address)
+        .having(func.count(User.user_id) > 1)
+        .all()
+    )
+
+    assert len(duplicated_users) == 4
+
+    test_db_session.execute(sql_file)
+
+    duplicated_users: List[UserEmailCount] = (
+        test_db_session.query(User.email_address, count(User.user_id).label("count"))
+        .group_by(User.email_address)
+        .having(func.count(User.user_id) > 1)
+        .all()
+    )
+
+    assert len(duplicated_users) == 4
+
+    assert duplicated_users[0].count == 1
+    assert duplicated_users[1].count == 1
+    assert duplicated_users[2].count == 1
+    assert duplicated_users[4].count == 1
+
+    all_users = test_db_session.query(User).count()
+    assert all_users == 44
+
