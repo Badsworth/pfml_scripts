619b5a05fb72a7a9ba4432309b9dc530e8e22a19
Zanson Watt <91619176+zansonLCM@users.noreply.github.com>
2021-10-01 14:48:56 -0400

    add depends on previous to intermittent failing tests (#5648)
    PFMLPB-2083
    
    Co-authored-by: PhantomBit <zanson8@gmail.com>

--- e2e/cypress/specs/unstable/fineos_medical_continuous_notice_historical_withdraw.ts
+++ e2e/cypress/specs/unstable/fineos_medical_continuous_notice_historical_withdraw.ts
@@ -2,25 +2,27 @@ import { fineos, fineosPages } from "../../actions";
 import { Submission } from "../../../src/types";
 
 describe("Create a new continuous medical leave claim in FINEOS and add Historical Absence case. Then withdraw the Absence Case", () => {
-  it("Create historical absence case within Absence Case", () => {
-    cy.task("generateClaim", "HIST_CASE").then((claim) => {
-      cy.task("submitClaimToAPI", claim).then((res) => {
-        fineos.before();
-        cy.visit("/");
-        cy.stash("claim", claim.claim);
-        cy.stash("submission", {
-          application_id: res.application_id,
-          fineos_absence_id: res.fineos_absence_id,
-          timestamp_from: Date.now(),
+  const submit =
+    it("Create historical absence case within Absence Case", () => {
+      cy.task("generateClaim", "HIST_CASE").then((claim) => {
+        cy.task("submitClaimToAPI", claim).then((res) => {
+          fineos.before();
+          cy.visit("/");
+          cy.stash("claim", claim.claim);
+          cy.stash("submission", {
+            application_id: res.application_id,
+            fineos_absence_id: res.fineos_absence_id,
+            timestamp_from: Date.now(),
+          });
+          fineosPages.ClaimPage.visit(
+            res.fineos_absence_id
+          ).addHistoricalAbsenceCase();
         });
-        fineosPages.ClaimPage.visit(
-          res.fineos_absence_id
-        ).addHistoricalAbsenceCase();
       });
     });
-  });
 
   it('Withdraw a claim in Fineos and check for a "Pending Application Withdrawn" notice', () => {
+    cy.dependsOnPreviousPass([submit]);
     fineos.before();
 
     cy.unstash<Submission>("submission").then((submission) => {
