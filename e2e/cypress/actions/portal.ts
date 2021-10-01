be1d2bce2ed8b631a59d37b57d5bac56176a063b
Armando Roman <armandoroman1016@gmail.com>
2021-10-01 10:55:13 -0700

    fix selector (#5646)

--- e2e/cypress/actions/portal.ts
+++ e2e/cypress/actions/portal.ts
@@ -799,7 +799,7 @@ export function visitActionRequiredERFormPage(fineosAbsenceId: string): void {
   cy.contains("Are you the right person to respond to this application?", {
     timeout: 20000,
   });
-  cy.contains("Yes").click();
+  cy.contains("label", "Yes").click();
   cy.contains("Agree and submit").click();
 }
 
