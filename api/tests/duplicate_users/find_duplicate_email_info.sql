d10f99295725683f6deeef0dda5ec202ba6549d7
jtosbornex <joshosborne@navapbc.com>
2021-10-05 12:05:14 -0700

    Tracking additional query

--- api/tests/duplicate_users/find_duplicate_email_info.sql
+++ api/tests/duplicate_users/find_duplicate_email_info.sql
@@ -10,7 +10,15 @@ FROM
     FULL OUTER JOIN document ON document.user_id = "user".user_id
     FULL OUTER JOIN link_user_leave_administrator ON link_user_leave_administrator.user_id = "user".user_id
     FULL OUTER JOIN managed_requirement ON managed_requirement.respondent_user_id = "user".user_id
+WHERE
+    "user".email_address IN (
+        SELECT
+            "user".email_address AS user_email_address
+        FROM
+            "user"
+        GROUP BY
+            "user".email_address
+        HAVING
+            count("user".user_id) > 1)
 GROUP BY
     "user".email_address
-HAVING
-    count("user".email_address) > 1;
