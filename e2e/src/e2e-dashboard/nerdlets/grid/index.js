cfb044c0536337e92952aa21aeb3c0423ff5a2ea
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-10-06 18:06:25 +0500

    init e2e dashboard package

---
+++ e2e/src/e2e-dashboard/nerdlets/grid/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {} from "nr1";
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class GridNerdlet extends React.Component {
+  render() {
+    return <h1>Hello, grid Nerdlet!</h1>;
+  }
+}
