029c8cffcfaff1027e7c7815c00b9bf23f261a78
Austin Gardner <austintgardner@gmail.com>
2021-10-06 14:31:06 -0700

    API-1900 api key auth (#5708)
    🎫 Ticket
    https://lwd.atlassian.net/browse/API-1888
    Subtask: https://lwd.atlassian.net/browse/API-1900
    
    🛠 Changes
    Adds the following API gateway resources:
    
    An aws_api_gateway_usage_plan called pfmldata_plan
    An aws_api_gateway_api_key called pfmldata_api_key
    An aws_api_gateway_usage_plan_key called pfmldata_usage_plan_key

--- infra/env-shared/template/api_gateway_pfmldata_s3.tf
+++ infra/env-shared/template/api_gateway_pfmldata_s3.tf
@@ -36,3 +36,34 @@ resource "aws_api_gateway_resource" "pfmldata_list_bucket_key" {
   parent_id   = aws_api_gateway_resource.pfmldata_list_bucket.id
   path_part   = "{key+}"
 }
+
+resource "aws_api_gateway_usage_plan" "pfmldata_plan" {
+  name        = "pfmldata plan ${var.environment_name}"
+  description = "Usage Plan for system (PowerAutomate) accessing files from S3"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.pfml.id
+    stage  = aws_api_gateway_deployment.stage.stage_name
+  }
+
+  quota_settings {
+    limit  = 10000
+    offset = 0
+    period = "DAY"
+  }
+
+  throttle_settings {
+    burst_limit = 10
+    rate_limit  = 100
+  }
+}
+
+resource "aws_api_gateway_api_key" "pfmldata_api_key" {
+  name = "pfmldata_api_key_${var.environment_name}"
+}
+
+resource "aws_api_gateway_usage_plan_key" "pfmldata_usage_plan_key" {
+  key_id        = aws_api_gateway_api_key.pfmldata_api_key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.pfmldata_plan.id
+}
