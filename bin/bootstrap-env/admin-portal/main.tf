2d1b4f8c8eba9a58112d7c100bc46a90c64f3b77
Melquisedeque Pereira <melqui1@live.com.pt>
2021-10-07 17:53:43 +0100

    Updated admin portal env template

--- bin/bootstrap-env/admin-portal/main.tf
+++ bin/bootstrap-env/admin-portal/main.tf
@@ -20,8 +20,24 @@ terraform {
   }
 }
 
+locals {
+  environment_name = "$ENV_NAME"
+}
+
+output "cloudfront_distribution_id" {
+  description = "Cloudfront distribution id for portal environment. Used for cache invalidation in github workflow."
+  value       = module.massgov_pfml.cloudfront_distribution_id
+}
+
 module "massgov_pfml" {
+  # Firewall rules
+  # 'true' will set rule to 'BLOCK' (or 'NONE' which is equivalent)
+  # 'false' will set rule to 'COUNT' (counts traffic that meets rule(s) instead of blocking)
+  enforce_cloudfront_rate_limit     = true
+  enforce_cloudfront_fortinet_rules = true
+
   # You probably don't need to change the variables below:
   source                 = "../../template"
-  environment_name       = "$ENV_NAME"
+  environment_name       = local.environment_name
+  cloudfront_origin_path = local.cloudfront_origin_path
 }
