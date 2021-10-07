19f192fdc72a2d623f0bcf682717e1593c70f5d8
sergeikabuldzhanov <sergeikabuldzhanov@gmail.com>
2021-10-07 17:35:45 +0500

    update with status timeline widget source code

--- e2e/src/e2e-dashboard/nerdlets/grid/index.js
+++ e2e/src/e2e-dashboard/nerdlets/grid/index.js
@@ -1,83 +1,26 @@
 import React from "react";
-import {
-  PlatformStateContext,
-  Grid,
-  GridItem,
-  HeadingText,
-  AreaChart,
-  TableChart,
-  BillboardChart,
-  LineChart,
-  NrqlQuery,
-} from "nr1";
-// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+import { PlatformStateContext, AutoSizer } from "nr1";
+import StatusTimelineWidget from "./status-timeline-widget";
 
-export default class GridNerdlet extends React.Component {
-  constructor(props) {
-    super(props);
-    this.accountId = "2837112";
-  }
+export default class StatusTimelineWidgetRoot extends React.Component {
   render() {
-    const avgResTime = `SELECT average(duration) FROM Transaction FACET appName TIMESERIES AUTO `;
-    const trxOverview = `FROM Transaction SELECT count(*) as 'Transactions', apdex(duration) as 'apdex', percentile(duration, 99, 95) FACET appName `;
-    const latestRuns =
-      "SELECT count(*) FROM CypressTestResult WHERE environment LIKE 'test%' AND (tag LIKE 'Morning run%' OR tag LIKE 'Deploy%') FACET runId, file SINCE today";
-
     return (
-      <PlatformStateContext.Consumer>
-        {(PlatformState) => {
-          const since = "SINCE today";
-
-          return (
-            <>
-              <Grid
-                className="primary-grid"
-                spacingType={[Grid.SPACING_TYPE.NONE, Grid.SPACING_TYPE.NONE]}
-              >
-                <GridItem className="primary-content-container" columnSpan={6}>
-                  <main className="primary-content full-height">
-                    <HeadingText
-                      spacingType={[HeadingText.SPACING_TYPE.MEDIUM]}
-                      type={HeadingText.TYPE.HEADING_4}
-                    >
-                      Transaction Overview
-                    </HeadingText>
-                    <TableChart
-                      fullWidth
-                      accountId={this.accountId}
-                      query={trxOverview + since}
-                    />
-                  </main>
-                </GridItem>
-                <GridItem className="primary-content-container" columnSpan={6}>
-                  <main className="primary-content full-height">
-                    <HeadingText
-                      spacingType={[HeadingText.SPACING_TYPE.MEDIUM]}
-                      type={HeadingText.TYPE.HEADING_4}
-                    >
-                      Average Response Time
-                    </HeadingText>
-                    <AreaChart
-                      fullWidth
-                      accountId={this.accountId}
-                      query={avgResTime + since}
-                    />
-                  </main>
-                </GridItem>
-                <GridItem columnSpan={12}></GridItem>
-              </Grid>
-              <NrqlQuery accountId={this.accountId} query={latestRuns}>
-                {(res) => {
-                  console.log(res);
-                  return (
-                    <pre>{JSON.stringify(res.data?.[0]?.data, null, 2)}</pre>
-                  );
-                }}
-              </NrqlQuery>
-            </>
-          );
-        }}
-      </PlatformStateContext.Consumer>
+      <AutoSizer>
+        {({ width, height }) => (
+          <PlatformStateContext.Consumer>
+            {(platformState) => {
+              return (
+                <StatusTimelineWidget
+                  timeRange={platformState.timeRange}
+                  width={width}
+                  height={height}
+                  {...this.props}
+                />
+              );
+            }}
+          </PlatformStateContext.Consumer>
+        )}
+      </AutoSizer>
     );
   }
 }
