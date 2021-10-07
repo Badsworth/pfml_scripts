84867ed77502a090fd45d9f722912351fe94bf59
Melquisedeque Pereira <melqui1@live.com.pt>
2021-10-07 17:52:52 +0100

    Removed cloudfront handler

--- infra/admin-portal/template/cloudfront-handler.tf
+++
@@ -1,9 +0,0 @@
-#
-# Lambda@Edge configuration for modifying Cloudfront response headers
-#
-
-data "archive_file" "cloudfront_handler" {
-  type        = "zip"
-  source_file = "${path.module}/cloudfront-handler.js"
-  output_path = "${path.module}/.zip/cloudfront-handler.zip"
-}
