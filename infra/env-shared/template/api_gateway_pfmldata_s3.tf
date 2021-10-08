9eb9d05f4a042bc53b3af0fc1ff45ef1ff704dba
Paul Parker <paul.parker@focusconsulting.io>
2021-10-08 13:02:08 -0400

    API-1903 Create s3 get proxy endpoint (#5746)
    * Create s3 get proxy endpoint
    
    * Add s3 get integration to list of gateway integrations

--- infra/env-shared/template/api_gateway_pfmldata_s3.tf
+++ infra/env-shared/template/api_gateway_pfmldata_s3.tf
@@ -67,3 +67,70 @@ resource "aws_api_gateway_usage_plan_key" "pfmldata_usage_plan_key" {
   key_type      = "API_KEY"
   usage_plan_id = aws_api_gateway_usage_plan.pfmldata_plan.id
 }
+
+# ----------------------------------------------------------------------------------------------------------------------
+# GET /{bucket}/{key} endpoint to proxy S3 GetObject
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+resource "aws_api_gateway_method" "pfmldata_get_object_method" {
+  rest_api_id      = aws_api_gateway_rest_api.pfml.id
+  resource_id      = aws_api_gateway_resource.pfmldata_bucket_key.id
+  http_method      = "GET"
+  authorization    = "NONE"
+  api_key_required = true
+
+  request_parameters = {
+    "method.request.path.bucket" = true
+    "method.request.path.key"    = true
+  }
+}
+
+resource "aws_api_gateway_method_response" "pfmldata_get_object_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.pfmldata_bucket_key.id
+  http_method = aws_api_gateway_method.pfmldata_get_object_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_method_response" "pfmldata_get_object_response_403" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.pfmldata_bucket_key.id
+  http_method = aws_api_gateway_method.pfmldata_get_object_method.http_method
+  status_code = "403"
+}
+
+
+resource "aws_api_gateway_integration" "pfmldata_get_object_s3_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.pfml.id
+  resource_id             = aws_api_gateway_resource.pfmldata_bucket_key.id
+  http_method             = aws_api_gateway_method.pfmldata_get_object_method.http_method
+
+  integration_http_method = "GET"
+  type                    = "AWS"
+
+  uri                     = "arn:aws:apigateway:us-east-1:s3:path/{bucket}/{key}" 
+  credentials             = aws_iam_role.pfmldata_executor_role.arn
+  request_parameters = {
+    "integration.request.path.bucket": "method.request.path.bucket"
+    "integration.request.path.key": "method.request.path.key"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "pfmldata_get_object_s3_integration_response_200" {
+  depends_on        = [aws_api_gateway_integration.pfmldata_get_object_s3_integration]
+  rest_api_id       = aws_api_gateway_rest_api.pfml.id
+  resource_id       = aws_api_gateway_resource.pfmldata_bucket_key.id
+  http_method       = aws_api_gateway_method.pfmldata_get_object_method.http_method
+  status_code       = aws_api_gateway_method_response.pfmldata_get_object_response_200.status_code
+  selection_pattern = aws_api_gateway_method_response.pfmldata_get_object_response_200.status_code
+}
+
+resource "aws_api_gateway_integration_response" "pfmldata_get_object_s3_integration_response_403" {
+  depends_on        = [aws_api_gateway_integration.pfmldata_get_object_s3_integration]
+  rest_api_id       = aws_api_gateway_rest_api.pfml.id
+  resource_id       = aws_api_gateway_resource.pfmldata_bucket_key.id
+  http_method       = aws_api_gateway_method.pfmldata_get_object_method.http_method
+  status_code       = aws_api_gateway_method_response.pfmldata_get_object_response_403.status_code
+  selection_pattern = aws_api_gateway_method_response.pfmldata_get_object_response_403.status_code
+}
