f37c518ac2f01c7c93ba022a1869ded046be6c89
Josh Osborne <josh@qcmmedia.com>
2021-10-08 09:28:38 -0700

    Merging multiple migration heads (#5752)

---
+++ api/massgov/pfml/db/migrations/versions/2021_10_08_15_59_30_fa3b00015aba_merge_multiple_heads.py
@@ -0,0 +1,21 @@
+"""Merge multiple heads
+
+Revision ID: fa3b00015aba
+Revises: 528f8c65980f, 5721fd5becbd
+Create Date: 2021-10-08 15:59:30.592158
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "fa3b00015aba"
+down_revision = ("528f8c65980f", "5721fd5becbd")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
