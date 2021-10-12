ef377792e26e2a79cd330d98537262a16344697d
Ali <7331495+aligg@users.noreply.github.com>
2021-10-12 12:25:56 -0700

    Portal 811: Upgrade to webpack 5 (#5748)
    * progress
    
    * compiling
    
    * remove webpack line
    
    * miau
    
    * add back image line
    
    * add back image.d.ts file

--- portal/next.config.js
+++ portal/next.config.js
@@ -29,8 +29,9 @@ const config = {
     // Mayflowers requires us to expose its includePaths so its imports work
     includePaths: mayflowerAssets.includePaths,
   },
-  // TODO (PORTAL-811)
-  webpack5: false,
+  images: {
+    disableStaticImages: true,
+  },
   trailingSlash: true,
   webpack: function (webpackConfig) {
     // Include our polyfills before all other code
