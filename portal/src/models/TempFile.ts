ce56a4beda4fb67b56939108bbfee0ef791df3df
Sawyer Hollenshead <git@sawyerh.com>
2021-10-12 16:55:08 -0400

    PORTAL-795: Add typing to TempFile data model (#5767)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/PORTAL-795

--- portal/src/models/TempFile.ts
+++ portal/src/models/TempFile.ts
@@ -1,17 +1,14 @@
-import BaseModel from "./BaseModel";
 import { uniqueId } from "lodash";
 
 /**
- * Model for managing temporary Files before they are successfully
- * uploaded
+ * Model for managing temporary Files before they are successfully uploaded
  */
-class TempFile extends BaseModel {
-  // @ts-expect-error ts-migrate(2416) FIXME: Property 'defaults' in type 'TempFile' is not assi... Remove this comment to see the full error message
-  get defaults() {
-    return {
-      id: uniqueId("TempFile"),
-      file: null,
-    };
+class TempFile {
+  id?: string = uniqueId("TempFile");
+  file: Blob;
+
+  constructor(attrs: TempFile) {
+    Object.assign(this, attrs);
   }
 }
 
