28567b39bcfd2179c23fd281982c6df549aebe04
Ali <7331495+aligg@users.noreply.github.com>
2021-10-13 11:20:56 -0700

    Portal-793: payment preference Typescript class declaration (#5775)
    * payment preference
    
    * new typing way
    
    * number => string

--- portal/src/models/PaymentPreference.ts
+++ portal/src/models/PaymentPreference.ts
@@ -1,19 +1,5 @@
 /* eslint sort-keys: ["error", "asc"] */
 
-import BaseModel from "./BaseModel";
-
-class PaymentPreference extends BaseModel {
-  // @ts-expect-error ts-migrate(2416) FIXME: Property 'defaults' in type 'PaymentPreference' is... Remove this comment to see the full error message
-  get defaults() {
-    return {
-      account_number: null,
-      bank_account_type: null, // BankAccountType
-      payment_method: null, // PaymentPreferenceMethod
-      routing_number: null,
-    };
-  }
-}
-
 /**
  * Enums for the Application's `payment_preference.bank_account_type` field
  * @enum {string}
@@ -31,5 +17,20 @@ export const PaymentPreferenceMethod = {
   ach: "Elec Funds Transfer",
   check: "Check",
 } as const;
+
+type BankAccountTypeType = typeof BankAccountType[keyof typeof BankAccountType];
+type PaymentPreferenceMethodType =
+  typeof PaymentPreferenceMethod[keyof typeof PaymentPreferenceMethod];
+
+class PaymentPreference {
+  account_number: string | null = null;
+  bank_account_type: BankAccountTypeType | null = null;
+  payment_method: PaymentPreferenceMethodType | null = null;
+  routing_number: string | null = null;
+
+  constructor(attrs: PaymentPreference) {
+    Object.assign(this, attrs);
+  }
+}
 
 export default PaymentPreference;
