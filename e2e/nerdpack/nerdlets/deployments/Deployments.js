5f6b92be0c8e890fefacd4bea91e74991ffcaede
Rob Bayliss <rob@lastcallmedia.com>
2021-10-18 13:00:24 -0400

    PFMLPB-2032: Environment dashboard (#5836)
    * PFMLPB-1999: Add Nerdpack for visualizing environment health in NR
    
    * Revamped UX.
    
    Co-authored-by: PhantomBit <zanson8@gmail.com>

---
+++ e2e/nerdpack/nerdlets/deployments/Deployments.js
@@ -0,0 +1,43 @@
+import { EmptyState, NrqlQuery } from "nr1";
+import { Timeline } from "@newrelic/nr1-community";
+import React from "react";
+import {labelComponent} from "../common";
+
+export default function DeploymentsWidget({
+  environment,
+  component,
+  accountId,
+}) {
+  const where = [];
+  if (environment) {
+    where.push(`environment = '${environment}'`);
+  }
+  if (component) {
+    where.push(`component = '${component}'`);
+  }
+  const query = `SELECT version, component, timestamp FROM CustomDeploymentMarker ${
+    where.length ? `WHERE ${where.join(" AND ")}` : ""
+  } SINCE 3 months ago LIMIT 20`;
+  return (
+    <NrqlQuery query={query} accountId={accountId}>
+      {({ data }) => {
+        const rows = (data?.[0]?.data ?? []).map((row) => {
+          const component = labelComponent(row.component);
+          return {
+            ...row,
+            label: `${component} Deployment: ${row.version}`,
+          };
+        });
+        if (rows.length) {
+          return <Timeline data={rows} labelField={"label"} />;
+        }
+        return (
+          <EmptyState
+            title={"No Deployments Found"}
+            description={"No deployments were recorded during this time range."}
+          />
+        );
+      }}
+    </NrqlQuery>
+  );
+}
