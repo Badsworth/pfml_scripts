a7cc5b42529638d3ae88fabd326460df97b73181
aorticweb <towabrice@gmail.com>
2021-10-19 11:53:21 -0400

    format

--- api/massgov/pfml/db/migrations/versions/2021_10_19_15_48_41_209b024b1ea6_merge_multiple_heads.py
+++ api/massgov/pfml/db/migrations/versions/2021_10_19_15_48_41_209b024b1ea6_merge_multiple_heads.py
@@ -5,9 +5,8 @@ Revises: 12017bbb31e1, 3e14d30da6ff
 Create Date: 2021-10-19 15:48:41.458186
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "209b024b1ea6"
