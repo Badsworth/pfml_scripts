c18b248d7487a65952f2699562ec3356ac3adf77
aorticweb <towabrice@gmail.com>
2021-10-20 19:57:35 -0400

    format

--- api/bin/create-erds.py
+++ api/bin/create-erds.py
@@ -2,33 +2,35 @@
 # Generate database schema diagram
 
 import codecs
+import subprocess
+
 import sadisplay
 
-import subprocess
 from massgov.pfml.db.models import applications, employees, payments, verifications
-
 
 schema_modules = [
     (applications, "applications"),
     (employees, "employees"),
     (payments, "payments"),
-    (verifications, "verifications")
+    (verifications, "verifications"),
 ]
 
 for module, file_name in schema_modules:
     description = sadisplay.describe(
-    [getattr(module, attr) for attr in dir(module)],
-    show_methods=True,
-    show_properties=True,
-    show_indexes=True,
+        [getattr(module, attr) for attr in dir(module)],
+        show_methods=True,
+        show_properties=True,
+        show_indexes=True,
     )
 
-    with codecs.open(f'{file_name}.dot', 'w', encoding='utf8') as f:
+    with codecs.open(f"{file_name}.dot", "w", encoding="utf8") as f:
         dot_file = f.write(sadisplay.dot(description))
         print(dot_file)
-    
+
     # TODO:
     # use python library
     # https://graphviz.org/resources/#python
-    process = subprocess.Popen(f"dot -Tpng {file_name}.dot > {file_name}.png", shell=True, stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        f"dot -Tpng {file_name}.dot > {file_name}.png", shell=True, stdout=subprocess.PIPE
+    )
     process.wait()
