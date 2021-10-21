83089a3d8dbafe49ce4f5ac3239d71459277a1d4
aorticweb <towabrice@gmail.com>
2021-10-21 19:39:45 -0400

    migration_fix

--- api/massgov/pfml/db/migrations/versions/2021_10_21_16_48_11_458af2d103eb_nullable_content_type_table.py
+++ api/massgov/pfml/db/migrations/versions/2021_10_21_16_48_11_458af2d103eb_nullable_content_type_table.py
@@ -1,7 +1,7 @@
 """nullable_content_type_table
 
 Revision ID: 458af2d103eb
-Revises: 175e38fc50d0
+Revises: 3765c13fe7e6
 Create Date: 2021-10-21 16:48:11.138836
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "458af2d103eb"
-down_revision = "175e38fc50d0"
+down_revision = "3765c13fe7e6"
 branch_labels = None
 depends_on = None
 
