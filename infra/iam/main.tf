61f5255010714da45dab7204df47bc994ab7641b
Jamie Taschetti <jtaschetti@smartronix.com>
2021-10-22 13:34:07 -0400

    revover failers in test from infra-616

--- infra/iam/main.tf
+++ infra/iam/main.tf
@@ -2,7 +2,7 @@
 
 provider "aws" {
   region = "us-east-1"
-}
+  }
 
 data "aws_caller_identity" "current" {
 }
@@ -12,11 +12,11 @@ data "aws_region" "current" {
 
 terraform {
 
-  backend "s3" {
-    bucket         = "massgov-pfml-test-env-mgmt"
+    backend "s3" {
+    bucket         = "massgov-pfml-prod-env-mgmt"
     key            = "terraform/iam.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_locks"
     kms_key_id     = "arn:aws:kms:us-east-1:498823821309:key/641eba51-98e5-4776-98b6-98ed06866ec8"
-  }
+    }
 }
