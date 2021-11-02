0196d5597e344468e42c39a533247592f517189c
JoelPelaezP <joelpelaez@JOELPELAEZ2.localdomain>
2021-11-01 23:39:52 -0400

    implemented env var for new pdf api and feature flags for generating and merging pdfs

--- api/massgov/pfml/delegated_payments/irs_1099/generate_documents.py
+++ api/massgov/pfml/delegated_payments/irs_1099/generate_documents.py
@@ -1,9 +1,14 @@
-import massgov.pfml.util.logging
-from massgov.pfml.delegated_payments.step import Step
-from massgov.pfml.db.models.payments import Pfml1099
+import os
+
 import requests
 
+import massgov.pfml.api.app as app
+import massgov.pfml.util.logging
+from massgov.pfml.db.models.payments import Pfml1099
+from massgov.pfml.delegated_payments.step import Step
+
 logger = massgov.pfml.util.logging.get_logger(__name__)
+PDF_API_HOST = os.environ.get("PDF_API_HOST")
 
 
 class Generate1099DocumentsStep(Step):
@@ -12,16 +17,21 @@ class Generate1099DocumentsStep(Step):
 
     def _generate_1099_documents(self) -> None:
         logger.info("1099 Documents - Generate 1099 Documents Step")
-        records = self.get_payment_records()
 
-        for record in records:
-            self.generate_document(record)
+        if app.get_config().enable_generate_1099_pdf:
+            logger.info("Generate 1099 Pdf flag is enabled")
+            records = self.get_payment_records()
+
+            for record in records:
+                self.generate_document(record)
+        else:
+            logger.info("Generate 1099 Pdf flag is not enabled")
 
     def get_payment_records(self):
         return self.db_session.query(Pfml1099).all()
 
     def generate_document(self, record) -> None:
-        pdfApiUrl = "http://mass-pfml-pdf-api:5001/api/pdf/generate"
+        pdfApiUrl = f"{PDF_API_HOST}/api/pdf/generate"
 
         try:
             documentDto = {
@@ -33,24 +43,21 @@ class Generate1099DocumentsStep(Step):
                 "federalTaxesWithheld": str(record.federal_tax_withholdings),
                 "stateTaxesWithheld": str(record.state_tax_withholdings),
                 "repayments": str(record.overpayment_repayments),
-                "name": f"{record.first_name} {record.last_name}" ,
+                "name": f"{record.first_name} {record.last_name}",
                 "address": record.address_line_1,
                 "city": record.city,
                 "state": record.state,
                 "zipCode": record.zip,
-                "accountNumber": None
+                "accountNumber": None,
             }
 
             response = requests.post(
-                pdfApiUrl, 
-                json=documentDto, 
-                verify=False, 
-                headers = {
-                    'Content-type': 'application/json',
-                    'Accept': 'application/json'
-                }
+                pdfApiUrl,
+                json=documentDto,
+                verify=False,
+                headers={"Content-type": "application/json", "Accept": "application/json"},
             )
 
+            logger.info(response)
         except requests.exceptions.RequestException as error:
-            logger.error(error)    
-        
+            logger.error(error)
--- api/massgov/pfml/delegated_payments/irs_1099/merge_documents.py
+++ api/massgov/pfml/delegated_payments/irs_1099/merge_documents.py
@@ -1,9 +1,14 @@
-import massgov.pfml.util.logging
-from massgov.pfml.delegated_payments.step import Step
-from massgov.pfml.db.models.payments import Pfml1099Batch
+import os
+
 import requests
 
+import massgov.pfml.api.app as app
+import massgov.pfml.util.logging
+from massgov.pfml.db.models.payments import Pfml1099Batch
+from massgov.pfml.delegated_payments.step import Step
+
 logger = massgov.pfml.util.logging.get_logger(__name__)
+PDF_API_HOST = os.environ.get("PDF_API_HOST")
 
 
 class Merge1099Step(Step):
@@ -12,31 +17,30 @@ class Merge1099Step(Step):
 
     def _merge_1099_documents(self) -> None:
         logger.info("1099 Documents - Merge 1099 Documents Step")
-        records = self.get_batches_records()
 
-        for record in records:
-            self.merge_documents(record)
+        if app.get_config().enable_generate_1099_pdf:
+            logger.info("Merge 1099 Pdf flag is enabled")
+            records = self.get_batches_records()
+
+            for record in records:
+                self.merge_documents(record)
+        else:
+            logger.info("Merge 1099 Pdf flag is not enabled")
 
     def get_batches_records(self):
         return self.db_session.query(Pfml1099Batch).all()
 
     def merge_documents(self, record) -> None:
-        pdfApiUrl = "http://mass-pfml-pdf-api:5001/api/pdf/merge"
+        pdfApiUrl = f"{PDF_API_HOST}/api/pdf/merge"
 
         try:
-            mergeDto = {
-                "batchId": str(record.pfml_1099_batch_id),
-                "numOfRecords" : 250
-            }
+            mergeDto = {"batchId": str(record.pfml_1099_batch_id), "numOfRecords": 250}
 
             response = requests.post(
-                pdfApiUrl, 
-                json=mergeDto, 
-                verify=False, 
-                headers = {
-                    'Content-type': 'application/json',
-                    'Accept': 'application/json'
-                }
+                pdfApiUrl,
+                json=mergeDto,
+                verify=False,
+                headers={"Content-type": "application/json", "Accept": "application/json"},
             )
 
             logger.info(response)
