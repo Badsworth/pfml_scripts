6b112ddea63a6921423ac48aa24013ab5c8c0f6a
Paul Parker <paul.parker@focusconsulting.io>
2021-11-18 12:34:23 -0500

    test range in past scenario

--- api/tests/api/test_payments.py
+++ api/tests/api/test_payments.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 from urllib.parse import urlencode
 
 import pytest
@@ -152,10 +152,66 @@ def test_get_payments_200_pending_validation_scenario_no_prenote_sent_at(
 
     assert payment_response == {
         "payment_id": str(payment.payment_id),
+        "fineos_c_value": str(payment.fineos_pei_c_value),
+        "fineos_i_value": str(payment.fineos_pei_i_value),
+        "period_start_date": str(payment.period_start_date),
+        "period_end_date": str(payment.period_end_date),
         "amount": None,
         "sent_to_bank_date": None,
+        "payment_method": payment.disb_method.payment_method_description,
         "expected_send_date_start": expected_start_date,
         "expected_send_date_end": expected_end_date,
+        "status": FEPaymentStatus.DELAYED,
+    }
+
+
+def test_get_payments_200_range_before_today(client, auth_token, user, test_db_session):
+    absence_id = "NTN-12345-ABS-01"
+
+    payment_factory = DelegatedPaymentFactory(
+        test_db_session,
+        fineos_absence_id=absence_id,
+        payment_method=PaymentMethod.ACH,
+        amount=750.67,
+    )
+    claim = payment_factory.get_or_create_claim()
+    ApplicationFactory.create(claim=claim, user=user)
+
+    transaction_status = FineosWritebackTransactionStatus.DIA_ADDITIONAL_INCOME
+    writeback_status = payment_factory.get_or_create_payment_with_writeback(transaction_status)
+
+    # The expected_end_date of the range (2-4 days from writeback created_at)
+    # will be one day prior to todays date. In this scenario, we don't return values for
+    # the expected start/end date fields
+    writeback_status.created_at = datetime.today() - timedelta(days=5)
+
+    test_db_session.commit()
+
+    querystring = urlencode({"absence_case_id": absence_id})
+    response = client.get(
+        "/v1/payments?{}".format(querystring), headers={"Authorization": f"Bearer {auth_token}"},
+    )
+
+    assert response.status_code == 200
+
+    response_body = response.get_json().get("data")
+    assert response_body["absence_case_id"] == absence_id
+
+    payment_response = response_body["payments"][0]
+
+    payment = payment_factory.payment
+
+    assert payment_response == {
+        "payment_id": str(payment.payment_id),
+        "fineos_c_value": str(payment.fineos_pei_c_value),
+        "fineos_i_value": str(payment.fineos_pei_i_value),
+        "period_start_date": str(payment.period_start_date),
+        "period_end_date": str(payment.period_end_date),
+        "amount": None,
+        "sent_to_bank_date": None,
+        "payment_method": payment.disb_method.payment_method_description,
+        "expected_send_date_start": None,
+        "expected_send_date_end": None,
         "status": FEPaymentStatus.DELAYED,
     }
 
