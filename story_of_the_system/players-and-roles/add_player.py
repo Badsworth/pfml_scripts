3fe5c3362844bc3f9bdb30d0e8126f2ccc6f005a
ChrisAshton <ChrisAshton@users.noreply.github.com>
2021-11-22 11:57:43 -0700

    SOTS-51: Create README template for tools (#6219)

--- story_of_the_system/players-and-roles/add_player.py
+++ story_of_the_system/players-and-roles/add_player.py
@@ -2,7 +2,7 @@ import re
 from sys import argv
 from pathlib import Path
 
-full_player_name = ' '.join(argv[1:])
+full_player_name = " ".join(argv[1:])
 has_abbreviation = False
 abbreviation = ""
 separator = "---\n"
@@ -12,38 +12,38 @@ if re.search(r"\([A-Z]+\)", full_player_name):
     has_abbreviation = True
 
 player_name = re.sub("(^[A-Za-z0-9 ']+) \([A-Z]+\)", r"\1", full_player_name)
-directory_name = '-'.join(player_name.replace("'", "").lower().split(' '))
+directory_name = "-".join(player_name.replace("'", "").lower().split(" "))
 readme_heading = f"# {full_player_name}\n\n"
 if has_abbreviation:
     hyperlink = f"### [{abbreviation} – {player_name}]({directory_name}/README.md)"
 else:
     hyperlink = f"### [{player_name}]({directory_name}/README.md)"
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 #                     Add player to list of players
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 with open("README.md", "r") as file:
     lines = file.readlines()
 
 # Insert new player and re-sort list
 lines.insert(2, f"{hyperlink}\n")
-player_links = list(filter(lambda a: a != "\n", lines[1:lines.index(separator)]))
+player_links = list(filter(lambda a: a != "\n", lines[1 : lines.index(separator)]))
 player_links.sort()
 
 # Build new README
 heading = "# Players and Roles\n\n"
 player_links = "".join(player_links)
-notes = "".join(lines[lines.index(separator):])
+notes = "".join(lines[lines.index(separator) :])
 new_readme = f"{heading}{player_links}\n{notes}"
 
 
 with open("README.md", "w") as file:
     file.write(new_readme)
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 #                Create directory and README for new player
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 new_dir = Path(directory_name)
 
