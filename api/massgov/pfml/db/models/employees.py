168db725d5ed0e225f1990130a8c738ff223a9aa
rrajegowda <rgowda@TRI-MJ0DY1YD.localdomain>
2021-11-23 10:55:16 -0500

    PAY90:lint changes

--- api/massgov/pfml/db/models/employees.py
+++ api/massgov/pfml/db/models/employees.py
@@ -2726,7 +2726,6 @@ class ReferenceFileType(LookupTable):
     DIA_CONSOLIDATED_REDUCTION_REPORT_ERRORS = LkReferenceFileType(
         30, "Consolidated DIA payments for DFML reduction report", 1
     )
-   
     FINEOS_PAYMENT_RECONCILIATION_EXTRACT = LkReferenceFileType(
         31, "Payment reconciliation extract", 3
     )
@@ -2735,10 +2734,11 @@ class ReferenceFileType(LookupTable):
 
     DUA_DEMOGRAPHICS_REQUEST_FILE = LkReferenceFileType(33, "DUA demographics request", 1)
 
+    IRS_1099_ORIG = LkReferenceFileType(34, "IRS 1099 org file", 1)
+
     CLAIMANT_ADDRESS_VALIDATION_REPORT = LkReferenceFileType(
         35, "Claimant Address validation Report", 1
     )
-    IRS_1099_ORIG = LkReferenceFileType(34, "IRS 1099 org file", 1)
 
 
 class Title(LookupTable):
