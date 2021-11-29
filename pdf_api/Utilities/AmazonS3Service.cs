496ed8e2c068491ab6bde862d99425c9a5fcb721
Joel Pelaez <joel.pelaezpariona@mass.gov>
2021-11-29 14:09:53 -0500

    PAY-102: Support sub folders to save up to 250 pdfs (#6287)
    * PAY-102: Updated code to point to real S3 bucket
    
    * Removed structure env var folder in S3 bucket
    
    * updated logic to name generated pdf
    
    * Included logs to detect what is causing error PDF header not found
    
    * Added more error info to the console
    
    * filtered only .pdf files returned from bucket
    
    * removed limit of 5 records
    
    * rollback init file
    
    * Updated code to support sub folders and save up to 250 pdfs
    
    Co-authored-by: JoelPelaezP <joelpelaez@JOELPELAEZ2.localdomain>

--- pdf_api/Utilities/AmazonS3Service.cs
+++ pdf_api/Utilities/AmazonS3Service.cs
@@ -42,6 +42,13 @@ namespace PfmlPdfApi.Utilities
         /// <param name="folderName">The folder name</param>
         /// <returns></returns>
         Task<IList<Stream>> GetFilesAsync(string folderName);
+
+        /// <summary>
+        /// Gets subfolders by folder
+        /// </summary>
+        /// <param name="folderName">The folder name</param>
+        /// <returns>A list of folders</returns>
+        Task<IList<string>> GetFoldersAsync(string folderName);
     }
 
     public class AmazonS3Service : IAmazonS3Service
@@ -64,7 +71,7 @@ namespace PfmlPdfApi.Utilities
 
             await CreateObjectAsync(request);
 
-            Console.WriteLine($"Amazon S3 Service - {folderName} was successfully created.");
+            Console.WriteLine($"Amazon S3 Service: Prefix {folderName} was successfully created.");
 
             return true;
         }
@@ -84,7 +91,7 @@ namespace PfmlPdfApi.Utilities
 
             await CreateObjectAsync(request);
 
-            Console.WriteLine($"Amazon S3 Service - {fileName} was successfully created.");
+            Console.WriteLine($"Amazon S3 Service: Key {fileName} was successfully created.");
 
             return true;
         }
@@ -122,20 +129,20 @@ namespace PfmlPdfApi.Utilities
                     var request = new ListObjectsV2Request
                     {
                         BucketName = _amazonS3Setting.BucketName,
-                        Prefix = $"{_amazonS3Setting.Key}/{folderName}"
+                        Prefix = folderName
                     };
 
                     var objects = await awsClient.ListObjectsV2Async(request, new System.Threading.CancellationToken());
-                    Console.WriteLine($"Amazon S3 Service - {objects.S3Objects.Count} items were returned from S3 bucket");
+                    Console.WriteLine($"Amazon S3 Service: Prefix {folderName} returned {objects.S3Objects.Count} keys.");
 
                     foreach (var s3Object in objects.S3Objects)
                     {
                         var file = await awsClient.GetObjectAsync(new GetObjectRequest { BucketName = s3Object.BucketName, Key = s3Object.Key }, new System.Threading.CancellationToken());
-                        
+
                         if (s3Object.Key.EndsWith(".pdf"))
                         {
                             files.Add(file.ResponseStream);
-                            Console.WriteLine($"Amazon S3 Service - getting key: {s3Object.Key}");
+                            Console.WriteLine($"Amazon S3 Service: Prefix {folderName} - key {s3Object.Key}.");
                         }
                     }
                 }
@@ -149,6 +156,41 @@ namespace PfmlPdfApi.Utilities
             }
         }
 
+        public async Task<IList<string>> GetFoldersAsync(string folderName)
+        {
+            using (IAmazonS3 awsClient = await GetAWSClient())
+            {
+                var folders = new List<string>();
+
+                try
+                {
+                    var request = new ListObjectsV2Request
+                    {
+                        BucketName = _amazonS3Setting.BucketName,
+                        Prefix = $"{_amazonS3Setting.Key}/{folderName}/",
+                        Delimiter = "/"
+                    };
+
+                    var objects = await awsClient.ListObjectsV2Async(request, new System.Threading.CancellationToken());
+
+                    Console.WriteLine($"Amazon S3 Service: {objects.CommonPrefixes.Count} prefixes returned.");
+
+                    foreach (var prefix in objects.CommonPrefixes)
+                    {
+                        folders.Add(prefix);
+                        Console.WriteLine($"Amazon S3 Service: Prefix: {prefix}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception Detected! - {GetType().Name}/{MethodBase.GetCurrentMethod().Name}{Environment.NewLine}{ex.Message}");
+                    throw;
+                }
+
+                return folders;
+            }
+        }
+
         private async Task CreateObjectAsync(PutObjectRequest request)
         {
             using (IAmazonS3 awsClient = await GetAWSClient())
@@ -159,7 +201,7 @@ namespace PfmlPdfApi.Utilities
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine($"Amazon S3 Service Exception Detected! - {GetType().Name}/{MethodBase.GetCurrentMethod().Name}{Environment.NewLine}{ex.Message}");
+                    Console.WriteLine($"Exception Detected! - {GetType().Name}/{MethodBase.GetCurrentMethod().Name}{Environment.NewLine}{ex.Message}");
                     throw;
                 }
             }
@@ -168,11 +210,12 @@ namespace PfmlPdfApi.Utilities
         private async Task<IAmazonS3> GetAWSClient()
         {
             var awsClient = new AmazonS3Client();
-
             bool bucketExists = await AmazonS3Util.DoesS3BucketExistV2Async(awsClient, _amazonS3Setting.BucketName);
 
-            if (!bucketExists)
-                throw new Exception($"Amazon S3 bucket with name '{_amazonS3Setting.BucketName}' does not exists");
+            if (bucketExists)
+                Console.WriteLine($"Amazon S3 Service: Succesfully connected to S3 Bucket {_amazonS3Setting.BucketName}");
+            else
+                throw new Exception($"Amazon S3 Service: Amazon S3 bucket with name '{_amazonS3Setting.BucketName}' does not exist.");
 
             return awsClient;
         }
