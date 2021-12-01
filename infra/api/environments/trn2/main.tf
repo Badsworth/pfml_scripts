7fc643d56937f92811e3a284e15f190d012bb93e
Paz Tursun <paz@Ps-MBP-2.fios-router.home>
2021-12-01 11:50:31 -0500

    add cognito info

--- infra/api/environments/trn2/main.tf
+++ infra/api/environments/trn2/main.tf
@@ -67,10 +67,10 @@ module "api" {
   release_version                = var.release_version
 
   # TODO: Fill this in after the portal is deployed.
-  cognito_user_pool_arn       = null
-  cognito_user_pool_id        = ""
-  cognito_user_pool_client_id = ""
-  cognito_user_pool_keys_url  = ""
+  cognito_user_pool_arn       = "arn:aws:cognito-idp:us-east-1:498823821309:userpool/us-east-1_oxOGrdAe8"
+  cognito_user_pool_id        = "us-east-1_oxOGrdAe8"
+  cognito_user_pool_client_id = "3dgp7vtcdt7bo0utlp2tqit1ee"
+  cognito_user_pool_keys_url  = "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_oxOGrdAe8/.well-known/jwks.json"
 
   # TODO: Connect to an RMV endpoint if desired. All nonprod environments are connected to the staging API
   #       in either a fully-mocked or partially-mocked setting.
