0ef302a6f0a4fcfc1fb2cdcd0e6ed7dad7bbe395
Jamie Taschetti <jtaschetti@smartronix.com>
2021-12-02 15:55:47 -0500

    Add missing parameter for pdf compression in the long environment

--- infra/api/environments/long/main.tf
+++ infra/api/environments/long/main.tf
@@ -65,6 +65,7 @@ module "api" {
   ]
   enable_application_fraud_check = "0"
   release_version                = var.release_version
+  enable_pdf_document_compression = "0"
 
   # TODO: Fill this in after the portal is deployed.
   cognito_user_pool_arn       = null
