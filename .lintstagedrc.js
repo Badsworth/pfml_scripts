b23c68a395ef5506cff26ee44f2a4cd7e73dea79
Josh Osborne <josh@qcmmedia.com>
2021-12-02 13:12:19 -0800

    API-2144: Filtering lint-staged parameters before sending to pre-commit (#6296)
    https://lwd.atlassian.net/browse/API-2144
    
    Filtering lint-staged parameters before sending to pre-commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,17 +8,31 @@ module.exports = {
     const cwd = process.cwd();
     const RUN_CMD_OPT = (process.env && process.env.RUN_CMD_OPT) || "";
     const isDocker = RUN_CMD_OPT.indexOf("NATIVE") == -1;
-    const fileNames = absolutePaths
-      .map((filename) => {
-        const relativePath = path.relative(cwd, filename);
-        if (isDocker) {
-          return relativePath.replace("api/", "");
-        }
-        return filename;
-      })
+    const unfilteredFileNames = absolutePaths.map((filename) => {
+      const relativePath = path.relative(cwd, filename);
+      if (isDocker) {
+        return relativePath.replace("api/", "");
+      }
+      return filename;
+    });
+    const prefix = isDocker ? '^' : '/api/'
+    const arguments = {
+      args: unfilteredFileNames,
+      flake_args: unfilteredFileNames.filter((fn) =>
+        fn.match(RegExp(`${prefix}(massgov|tests|bin)/`))
+      ),
+      mypy_args: unfilteredFileNames.filter((fn) =>
+        fn.match(RegExp(`${prefix}(massgov|bin)/`))
+      ),
+    };
+    const args = Object.keys(arguments)
+      .map((key) =>
+        !(arguments[key] && arguments[key].length) ? undefined : `${key}="${arguments[key].join(" ")}"`
+      )
+      .filter(Boolean)
       .join(" ");
 
-    return [`cd ./api && make pre-commit args="${fileNames}"`];
+    return [`cd ./api && make pre-commit ${args} `];
   },
   "api/massgov/pfml/db/**/*": ["npm run-script db-parity-check"],
   "api/openapi.yaml": ["npm run-script spectral"],
