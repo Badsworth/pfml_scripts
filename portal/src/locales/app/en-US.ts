8dd7e92035de59a8803e27398560a9e79d0f5576
C Cheng <10414576+ccheng26@users.noreply.github.com>
2021-12-03 16:30:44 -0500

    PORTAL-1166: implement updates to content for phase 1 (#6355)
    https://lwd.atlassian.net/browse/PORTAL-1166 
    
    Implements content changes as of 12/3/21; adds logic adjustments and to update payload response in storybook to show ONLY statuses for delayed and cancelled payments.
    
    Demo: view via storybook and open page for UAT

--- portal/src/locales/app/en-US.ts
+++ portal/src/locales/app/en-US.ts
@@ -2124,7 +2124,7 @@ const pages = {
       "$t(shared.documentCategory.identification)",
     viewNoticesHeading: "View your notices",
     viewPaymentTimeline:
-      "<p>Once your application is approved, you can expect weekly payments to begin 2-4 weeks after your leave begins. If your leave has already begun, you can expect your first payment to arrive 2 weeks after it is approved. After that you will receive your payments every week.</p><p>You can see your expected payment timeline on your <payments-page-link>payments page</payments-page-link>.</p>",
+      "<p>See your payments page to find information about when to expect payments.</p><p><payments-page-link>Track your payments</payments-page-link></p>",
     whatHappensNext: "What happens next",
     whatHappensNextButton_adoption: "Upload proof of adoption",
     whatHappensNextButton_fosterCare: "Upload proof of placement",
@@ -2578,17 +2578,20 @@ const pages = {
   },
   payments: {
     changesToPaymentsAmountAnswer:
-      "<p>See your approval notice for your maximum weekly benefit. We may reduce your benefit amount based on <using-other-leave-link>other leave, income and benefits</using-other-leave-link> you reported to us. You will receive another notice in this application’s details if we reduce or offset your benefit amount.</p><p>Other scenarios that may change your benefit amount are:</p><ul> <li>You reach the $850 benefit total across multiple employers</li><li>If you transition from medical pregnancy to leave to bond with a child</li><li>Extending your leave</li></ul> <p>Check your <view-notices-link>application’s notices</view-notices-link> to see your approval notice and any updates to your approved benefit amount.</p>",
-    changesToPaymentsAmountQuestion: "What may change your benefit amount",
+      "<p>To see your maximum weekly benefit amount, view your approval notice in your <view-notices-link>application’s notices</view-notices-link>.</p><p>The sent payment amount you see on this page may be less than the maximum benefit amount due to <using-other-leave-link>other leave, income and benefits</using-other-leave-link> you reported. </p><p>You will receive another notice in the mail if we reduce your benefit amount.</p><p><strong>Other scenarios that may change the payment amount you receive are:</strong></p><ul><li>You or your employer report additional other leave, income or benefits</li><li>You reach the maximum total amount that a person can receive in PFML benefits across multiple applications. Read more about <benefit-amount-details-link>benefit amount details</benefit-amount-details-link>.</li><li>You receive a payment for a pay period less than a full week</li></ul>",
+    changesToPaymentsAmountQuestion:
+      "Why your sent payment amount may be less than your approved benefit amount",
     changesToPaymentsScheduleAnswer:
-      "<ul> <li>State and federal holidays</li><li>Your bank’s processes</li><li>Pay periods that end on a weekend</li><li>Payments may be delayed if we need to adjust your benefit amount based on extensions, other income or benefits you’ve reported and other process delays</li></ul>",
+      "<p>Once your payments have started, expect to be paid weekly for the duration of your leave.</p><p><strong>What may cause a delayed payment</strong></p><p>The exact day you receive payment can be delayed because of:</p><ul><li>State and federal holidays</li><li>Leave periods that end on weekends</li><li>Your bank’s processes</li><li>The process to adjust your benefit amount due to additional income or benefits you or your employer reported</li><li>Changes to your leave’s start and end dates</li><li>Other processing issues such as issues with bank account information, or your address</li></ul><p><strong>What may cause a cancelled payment</strong></p><p>Payments can be canceled due to:</p><ul><li>Changes to when you end your leave</li><li>Paid time off taken on days you also receive PFML benefits.</li><li>The process to adjust your benefit amount due to additional income or benefits you or your employer reported may temporarily create a canceled payment</li></ul>",
     changesToPaymentsScheduleQuestion: "What may change your payment schedule",
     changesToPaymentsYourPreferencesAnswer:
-      "<p>To make changes to your application, call the Contact Center at <contact-center-phone-link>$t(shared.contactCenterPhoneNumberNoBreak)</contact-center-phone-link>. If you request a change to your start and end dates, we may need to review your application again.</p>",
+      "<p>To make changes to your payment method or banking information, call the Contact Center at <contact-center-phone-link>$t(shared.contactCenterPhoneNumberNoBreak)</contact-center-phone-link>.</p>",
     changesToPaymentsYourPreferencesQuestion: "Change your payment preference",
     feedbackDetails:
       "<p>We’d like to hear more about your experience on this site. Please take a few minutes to share your feedback.</p><p><feedback-link>Take the survey</feedback-link></p>",
     feedbackHeader: "Help us improve payment information by giving feedback",
+    paymentsIntro:
+      "<p>Payments are scheduled weekly. Check back once your leave week has ended to see when you can expect payment for that week.</p><p><strong>What does estimated scheduled date mean?</strong></p><p>This is the date range we expect to process your direct deposit payment or send your check in the mail. Depending on your bank’s processes, expect to receive direct deposit payments in your account 2-3 days after this date.  Checks may take 7-11 days to arrive in the mail. Once your payment is processed or your check is sent, that date will be shown in the “date sent” column.</p>",
     paymentsTable: {
       amountSent: "{{amount, currency}}",
       amountSentHeader: "Amount sent",
@@ -2597,11 +2600,11 @@ const pages = {
       leaveDatesHeader: "Leave dates",
       paymentMethodHeader: "Payment Method",
       paymentMethod_Check: "Check",
-      "paymentMethod_Elec Funds Transfer": "EFT",
+      "paymentMethod_Elec Funds Transfer": "Direct Deposit",
       paymentStatus_Cancelled: "Cancelled",
       paymentStatus_Delayed: "Delayed",
       paymentStatus_Pending: "Pending",
-      "paymentStatus_Sent to bank": "Sent to bank",
+      "paymentStatus_Sent to bank": "Sent",
       waitingWeekText:
         "This week is unpaid. Learn more about the <waiting-week-link>7-day waiting period</waiting-week-link>.",
     },
