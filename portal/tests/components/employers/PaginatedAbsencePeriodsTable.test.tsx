2e597e24b5594b9a3141cb3bde9bf3e88eeb4158
Umi Hui <umihui86@gmail.com>
2021-12-08 13:49:09 -0800

    PORTAL-1214: Implement pagination function into `PaginatedAbsencePeriodsTable` (#6392)
    * add pagination parts
    * absencePeriodsWithPageOffsetMap
    * update tests

--- portal/tests/components/employers/PaginatedAbsencePeriodsTable.test.tsx
+++ portal/tests/components/employers/PaginatedAbsencePeriodsTable.test.tsx
@@ -2,11 +2,12 @@ import {
   AbsencePeriod,
   AbsencePeriodRequestDecision,
 } from "../../../src/models/AbsencePeriod";
+import { render, screen } from "@testing-library/react";
 import PaginationAbsencePeriodsTable from "../../../src/components/employers/PaginatedAbsencePeriodsTable";
 import React from "react";
 import { StatusTagMap } from "../../../src/pages/applications/status";
 import { createAbsencePeriod } from "tests/test-utils";
-import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 const periodTypes: Array<AbsencePeriod["period_type"]> = [
   "Continuous",
@@ -25,11 +26,47 @@ const absencePeriodsList = (
   })
 );
 
+const longAbsencePeriodsList = absencePeriodsList.concat(absencePeriodsList);
+
 describe("PaginatedAbsencePeriodsTable", () => {
-  it("renders the component with absence period list", () => {
+  it("renders the component", () => {
     const { container } = render(
       <PaginationAbsencePeriodsTable absencePeriods={absencePeriodsList} />
     );
     expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("renders pagination parts when absence period list is longer than 10", () => {
+    render(
+      <PaginationAbsencePeriodsTable absencePeriods={absencePeriodsList} />
+    );
+
+    expect(screen.queryByText(/Viewing 1 to 10 of/)).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("navigation", { name: "Pagination Navigation" })
+    ).not.toBeInTheDocument();
+
+    render(
+      <PaginationAbsencePeriodsTable absencePeriods={longAbsencePeriodsList} />
+    );
+
+    expect(screen.getByText(/Viewing 1 to 10 of/)).toBeInTheDocument();
+    expect(
+      screen.getByRole("navigation", { name: "Pagination Navigation" })
+    ).toBeInTheDocument();
+  });
+
+  it("changes table contents when clicking the nav button", () => {
+    render(
+      <PaginationAbsencePeriodsTable absencePeriods={longAbsencePeriodsList} />
+    );
+
+    expect(screen.getAllByTestId(/1-/).length).toBe(10);
+
+    userEvent.click(screen.getByRole("button", { name: "Go to Next page" }));
+    expect(screen.queryAllByTestId(/1-/).length).toBe(0);
+    expect(screen.getAllByTestId(/2-/).length).toBe(
+      longAbsencePeriodsList.length - 10
+    );
   });
 });
