02f5a22299f7a5ff4cdf2741966b536511eba4b5
Paz Tursun <paz@Ps-MBP-2.fios-router.home>
2021-12-08 20:12:24 -0500

    formatting

--- portal/config/infra-test.js
+++ portal/config/infra-test.js
@@ -3,16 +3,16 @@
  * WARNING: No secrets!
  * Only store non-secrets here. Everything in this file can be included in build artifacts.
  */
- module.exports = {
-    envName: "infra-test",
-    // API Trn2 environment (https://lwd.atlassian.net/wiki/spaces/DD/pages/246612440/Environments)
-    apiUrl: "https://paidleave-api-infra-test.dfml.eol.mass.gov/api/v1",
-    awsConfig: {
-      // Cognito Infra-test environment (must match whatever API environment we're targeting)
-      cognitoUserPoolId: "us-east-1_8OEJk2XeD",
-      cognitoUserPoolWebClientId: "5pf01ur8rsdoumtu3ta8jvqbsj",
-    },
-    domain: "paidleave-infra-test.dfml.eol.mass.gov",
-    gtmConfig: {},
-    newRelicAppId: "847219405",
-  };
+module.exports = {
+  envName: "infra-test",
+  // API Trn2 environment (https://lwd.atlassian.net/wiki/spaces/DD/pages/246612440/Environments)
+  apiUrl: "https://paidleave-api-infra-test.dfml.eol.mass.gov/api/v1",
+  awsConfig: {
+    // Cognito Infra-test environment (must match whatever API environment we're targeting)
+    cognitoUserPoolId: "us-east-1_8OEJk2XeD",
+    cognitoUserPoolWebClientId: "5pf01ur8rsdoumtu3ta8jvqbsj",
+  },
+  domain: "paidleave-infra-test.dfml.eol.mass.gov",
+  gtmConfig: {},
+  newRelicAppId: "847219405",
+};
--- portal/config/long.js
+++ portal/config/long.js
@@ -3,17 +3,16 @@
  * WARNING: No secrets!
  * Only store non-secrets here. Everything in this file can be included in build artifacts.
  */
- module.exports = {
-    envName: "long",
-    // API Long environment (https://lwd.atlassian.net/wiki/spaces/DD/pages/246612440/Environments)
-    apiUrl: "https://paidleave-api-long.dfml.eol.mass.gov/api/v1",
-    awsConfig: {
-      // Cognito Long environment (must match whatever API environment we're targeting)
-      cognitoUserPoolId: "us-east-1_FADLpF6um",
-      cognitoUserPoolWebClientId: "1ufb6r40s1ad0evjffvdtl3113",
-    },
-    domain: "paidleave-long.dfml.eol.mass.gov",
-    gtmConfig: {},
-    newRelicAppId: "1531932432",
-  };
-  
+module.exports = {
+  envName: "long",
+  // API Long environment (https://lwd.atlassian.net/wiki/spaces/DD/pages/246612440/Environments)
+  apiUrl: "https://paidleave-api-long.dfml.eol.mass.gov/api/v1",
+  awsConfig: {
+    // Cognito Long environment (must match whatever API environment we're targeting)
+    cognitoUserPoolId: "us-east-1_FADLpF6um",
+    cognitoUserPoolWebClientId: "1ufb6r40s1ad0evjffvdtl3113",
+  },
+  domain: "paidleave-long.dfml.eol.mass.gov",
+  gtmConfig: {},
+  newRelicAppId: "1531932432",
+};
