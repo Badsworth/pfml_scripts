ce5f9539a3a81dad0ddebe3ea7ea71290a3dbe71
Paz Tursun <paz@Ps-MBP-2.fios-router.home>
2021-12-10 11:45:27 -0500

    add iam role arn

--- infra/api/environments/long/main.tf
+++ infra/api/environments/long/main.tf
@@ -87,8 +87,8 @@ module "api" {
   fineos_client_wscomposer_user_id                    = ""
   fineos_client_oauth2_url                            = "https://dt4-api.masspfml.fineos.com/oauth2/token"
   fineos_import_employee_updates_input_directory_path = "s3://fin-somdev-data-import/DT4/absence-eligibility/upload/"
-  fineos_aws_iam_role_arn                             = null
-  fineos_aws_iam_role_external_id                     = null
+  fineos_aws_iam_role_arn                             = "arn:aws:iam::666444232783:role/somdev-IAMRoles-CustomerAccountAccessRole-BF05IBJSG74B"
+  fineos_aws_iam_role_external_id                     = "12345"
 
   # TODO: This value is provided by FINEOS over Interchange.
   fineos_client_oauth2_client_id = "5744bq9dpveko6jie4ma1e4g93"
