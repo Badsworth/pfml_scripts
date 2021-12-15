81b39292bb23cb7f408793780b10fedb9c893abf
Matt McCarty <3298030+mattmccarty@users.noreply.github.com>
2021-12-15 14:37:29 -0500

    INFRA-829: Send SMS logs from CloudWatch to NewRelic (#6436)
    * Push SMS CloudWatch Logs to NewRelic
    
    The log groups DirectPublishToPhoneNumber && DirectPublishToPhoneNumber/Failure are created automatically by SNS. With 2FA/MFA being enabled through Cognito using SNS/SMS, we need to start capturing those logs in NewRelic. This requires adding the NewRelic lambda log filter to the log groups
    
    * Fixing Jamie's commit override
    
    Jamie accidentally overwrote some things in my initial commit
    
    Co-authored-by: Matt McCarty <steven.m.mccarty@mass.gov>

--- infra/pfml-aws/cloudwatch-logs.tf
+++ infra/pfml-aws/cloudwatch-logs.tf
@@ -5,3 +5,22 @@ resource "aws_cloudwatch_log_subscription_filter" "rds_lambdafunction_logfilter"
   // Hard coded bc this function is managed by AWS SAM for now
   destination_arn = module.constants.newrelic_log_ingestion_arn
 }
+
+# The DirectPublishToPhoneNumber is created automatically by SNS
+# https://aws.amazon.com/premiumsupport/knowledge-center/monitor-sns-texts-cloudwatch/
+resource "aws_cloudwatch_log_subscription_filter" "nr_lambda_sns_sms_success" {
+  name            = "nr_lambda_sns_sms_success"
+  log_group_name  = "sns/${data.aws_region.current.name}/${data.aws_caller_identity.current.account_id}/DirectPublishToPhoneNumber"
+  filter_pattern  = ""
+  destination_arn = module.constants.newrelic_log_ingestion_arn
+}
+
+# The DirectPublishToPhoneNumber/Failure is created automatically by SNS
+# https://aws.amazon.com/premiumsupport/knowledge-center/monitor-sns-texts-cloudwatch/
+resource "aws_cloudwatch_log_subscription_filter" "nr_lambda_sns_sms_failure" {
+  name            = "nr_lambda_sns_sms_failure"
+  log_group_name  = "sns/${data.aws_region.current.name}/${data.aws_caller_identity.current.account_id}/DirectPublishToPhoneNumber/Failure"
+  filter_pattern  = ""
+  destination_arn = module.constants.newrelic_log_ingestion_arn
+}
+
