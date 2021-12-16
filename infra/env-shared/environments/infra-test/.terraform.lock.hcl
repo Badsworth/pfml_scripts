Badsworth/pfml_scripts

commit 231634e57ea3faa62efbecf2e56b58fb63934746

INFRA-791 Create Resources for PowerAutomate (#6258)
🎫 Ticket
https://lwd.atlassian.net/browse/INFRA-791
Subtask: https://lwd.atlassian.net/browse/INFRA-794
Subtask: https://lwd.atlassian.net/browse/INFRA-793

🛠 Changes
add buckets to the level of dfml-responses
add qlik-reporting api
ℹ️ Context for reviewers
The design of the PUB payments powerautomate flow was intended to be somewhat generic so that it could be used more broadly within PFML than just the PUB payments processing. We are now ready to expand the usage and start leveraging this infrastructure to support data integration between SharePoint and S3 for Qlik reporting.

infra/env-shared/template/api_gateway_qlik_reports.tf
@@ -1,0 +1,228 @@
+# INFRA-794
+# TODO: rename as qlik reporting
+
+resource "aws_api_gateway_resource" "business_intellegence" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  parent_id   = aws_api_gateway_rest_api.pfml.root_resource_id
+  path_part   = "business_intelligence"
+
+}
+
+resource "aws_api_gateway_resource" "business_intelligence_file" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  parent_id   = aws_api_gateway_resource.business_intellegence.id
+  path_part   = "{file+}"
+}
+
+resource "aws_api_gateway_usage_plan" "business_intelligence_usage_plan" {
+  name        = "qlik plan"
+  description = "Usage Plan for system (PowerAutomate) accessing Qlik Reporting API"
+
+  api_stages {
+    api_id = aws_api_gateway_rest_api.pfml.id
+    stage  = aws_api_gateway_deployment.stage.stage_name
+  }
+
+  quota_settings {
+    limit  = 10000
+    offset = 0
+    period = "DAY"
+  }
+
+  throttle_settings {
+    burst_limit = 10
+    rate_limit  = 100
+  }
+}
+
+resource "aws_api_gateway_api_key" "business_intellegence_automate_key" {
+  name = "bi_key"
+}
+
+resource "aws_api_gateway_usage_plan_key" "business_intellegence_usage_plan_key" {
+  key_id        = aws_api_gateway_api_key.business_intellegence_automate_key.id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.business_intelligence_usage_plan.id
+
+}
+
+resource "aws_api_gateway_method" "business_intelligence_get" {
+  rest_api_id      = aws_api_gateway_rest_api.pfml.id
+  resource_id      = "something"
+  http_method      = "GET"
+  authorization    = "NONE"
+  api_key_required = true
+
+  request_parameters = {
+    "method.request.path.file" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "s3_integration_get_business_intelligence" {
+  depends_on  = [aws_api_gateway_method.business_intelligence_get]
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.business_intelligence_file.id
+  http_method = aws_api_gateway_method.business_intelligence_get.http_method
+
+  # Included because of this issue: https://github.com/hashicorp/terraform/issues/10501
+  integration_http_method = "GET"
+
+  type = "AWS"
+
+  # See uri description: https://docs.aws.amazon.com/apigateway/api-reference/resource/integration/
+  uri         = "arn:aws:apigateway:us-east-1:s3:path/${aws_s3_bucket.pfml_reports.bucket}/dfml-qlikdownloads/{file}"
+  credentials = aws_iam_role.executor.arn
+
+  request_parameters = {
+    "integration.request.path.file" = "method.request.path.file"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "business_intelligence_get_response" {
+  depends_on        = [aws_api_gateway_integration.s3_integration_get_business_intelligence]
+  rest_api_id       = aws_api_gateway_rest_api.pfml.id
+  resource_id       = aws_api_gateway_resource.business_intelligence_file.id
+  http_method       = aws_api_gateway_method.business_intelligence_get.http_method
+  status_code       = aws_api_gateway_method_response.business_intelligence_get_response.status_code
+  selection_pattern = aws_api_gateway_method_response.business_intelligence_get_response.status_code
+
+  response_parameters = {
+    "method.response.header.Content-Type" = "'text/csv'" # AWS console wants single quotes
+  }
+}
+
+resource "aws_api_gateway_integration_response" "business_intelligence_get_response_403" {
+  depends_on        = [aws_api_gateway_integration.s3_integration_get_business_intelligence]
+  rest_api_id       = aws_api_gateway_rest_api.pfml.id
+  resource_id       = aws_api_gateway_resource.business_intelligence_file.id
+  http_method       = aws_api_gateway_method.business_intelligence_get.http_method
+  status_code       = aws_api_gateway_method_response.business_intelligence_get_response_403.status_code
+  selection_pattern = aws_api_gateway_method_response.business_intelligence_get_response_403.status_code
+}
+
+resource "aws_api_gateway_method_response" "business_intelligence_get_response" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.business_intelligence_file.id
+  http_method = aws_api_gateway_method.business_intelligence_get.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Content-Type" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_method_response" "business_intelligence_get_response_403" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.business_intelligence_file.id
+  http_method = aws_api_gateway_method.business_intelligence_get.http_method
+  status_code = "403"
+}
+
+# ----------------------------------------------------------------------------------
+resource "aws_api_gateway_method" "business_intelligence_put" {
+  rest_api_id      = aws_api_gateway_rest_api.pfml.id
+  resource_id      = aws_api_gateway_resource.business_intelligence_file.id
+  http_method      = "PUT"
+  authorization    = "NONE"
+  api_key_required = true
+
+  request_parameters = {
+    "method.request.path.file" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "s3_integration_put_business_intelligence" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.business_intelligence_file.id
+  http_method = aws_api_gateway_method.business_intelligence_put.http_method
+
+  # Included because of this issue: https://github.com/hashicorp/terraform/issues/10501
+  integration_http_method = "PUT"
+
+  type = "AWS"
+
+  # See uri description: https://docs.aws.amazon.com/apigateway/api-reference/resource/integration/
+  uri         = "arn:aws:apigateway:us-east-1:s3:path/${aws_s3_bucket.pfml_reports.bucket}/dfml-qlikuploads/{file}"
+  credentials = aws_iam_role.executor.arn
+
+  request_parameters = {
+    "integration.request.path.file" = "method.request.path.file"
+  }
+}
+
+# Defines any adjustments made to the response from the api_gateway_integration above: status codes, Content-Types, etc.
+resource "aws_api_gateway_integration_response" "business_intelligence_put_response" {
+  depends_on        = [aws_api_gateway_integration.s3_integration_put_business_intelligence]
+  rest_api_id       = aws_api_gateway_rest_api.pfml.id
+  resource_id       = aws_api_gateway_resource.business_intelligence_file.id
+  http_method       = aws_api_gateway_method.business_intelligence_put.http_method
+  status_code       = aws_api_gateway_method_response.business_intelligence_put_response.status_code
+  selection_pattern = aws_api_gateway_method_response.business_intelligence_put_response.status_code
+}
+
+# Defines which segments of the api_gateway_integration_response to return to the original caller of this API endpoint.
+resource "aws_api_gateway_method_response" "business_intelligence_put_response" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.business_intelligence_file.id
+  http_method = aws_api_gateway_method.business_intelligence_put.http_method
+  status_code = "200"
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Defines the presence of a raw HTTP method without routing or response logic. The resources below handle all of that.
+resource "aws_api_gateway_method" "business_intelligence_delete" {
+  rest_api_id      = aws_api_gateway_rest_api.pfml.id
+  resource_id      = aws_api_gateway_resource.business_intelligence_file.id
+  http_method      = "DELETE"
+  authorization    = "NONE"
+  api_key_required = true
+
+  request_parameters = {
+    "method.request.path.file" = true
+  }
+}
+
+# Defines the AWS service, or external HTTP endpoint, that the api_gateway_method above routes inbound requests towards.
+# There's a lot of magic here. An integration with S3 translates roughly to "do RESTful actions on data stored in S3".
+resource "aws_api_gateway_integration" "s3_integration_delete_business_intelligence" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.business_intelligence_file.id
+  http_method = aws_api_gateway_method.business_intelligence_delete.http_method
+
+  # Included because of this issue: https://github.com/hashicorp/terraform/issues/10501
+  integration_http_method = "DELETE"
+
+  type = "AWS"
+
+  # See uri description: https://docs.aws.amazon.com/apigateway/api-reference/resource/integration/
+  uri         = "arn:aws:apigateway:us-east-1:s3:path/${aws_s3_bucket.pfml_reports.bucket}/dfml-qlikdownloads/{file}"
+  credentials = aws_iam_role.executor.arn
+
+  request_parameters = {
+    "integration.request.path.file" = "method.request.path.file"
+  }
+}
+
+# Defines any adjustments made to the response from the api_gateway_integration above: status codes, Content-Types, etc.
+resource "aws_api_gateway_integration_response" "business_intelligence_delete_response" {
+  depends_on = [aws_api_gateway_integration.s3_integration_delete_business_intelligence]
+
+  rest_api_id       = aws_api_gateway_rest_api.pfml.id
+  resource_id       = aws_api_gateway_resource.business_intelligence_file.id
+  http_method       = aws_api_gateway_method.business_intelligence_delete.http_method
+  status_code       = aws_api_gateway_method_response.business_intelligence_delete_response.status_code
+  selection_pattern = aws_api_gateway_method_response.business_intelligence_delete_response.status_code
+}
+
+# Defines which segments of the api_gateway_integration_response to return to the original caller of this API endpoint.
+resource "aws_api_gateway_method_response" "business_intelligence_delete_response" {
+  rest_api_id = aws_api_gateway_rest_api.pfml.id
+  resource_id = aws_api_gateway_resource.business_intelligence_file.id
+  http_method = aws_api_gateway_method.business_intelligence_delete.http_method
+  status_code = "204"
+}

infra/env-shared/template/iam.tf
@@ -122,3 +122,41 @@ resource "aws_iam_role_policy" "pfmldata_executor_policy" {
   role   = aws_iam_role.pfmldata_executor_role.id
   policy = data.aws_iam_policy_document.pfmldata_executor_policy_document.json
 }
+
+
+data "aws_iam_policy_document" "qlikdata_executor_policy_document" {
+  # Allow API gateway to read and delete files from dfml-qlikdownloads.
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.pfml_reports.arn}/dfml-qlikdownloads",
+      "${aws_s3_bucket.pfml_reports.arn}/dfml-qlikdownloads/*"
+    ]
+  }
+
+  # Allow API gateway to add files to dfml-qlikuploads.
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.pfml_reports.arn}/dfml-qlikuploads",
+      "${aws_s3_bucket.pfml_reports.arn}/dfml-qlikuploads/*"
+    ]
+  }
+}
+resource "aws_iam_role" "qlikdata_executor_role" {
+  name               = "massgov-pfml-${var.environment_name}-data-qlik-gateway-executor"
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy" "qlikdata_executor_policy" {
+  name   = "massgov-pfml-${var.environment_name}-data-executor-role-policy"
+  role   = aws_iam_role.qlikdata_executor_role.id
+  policy = data.aws_iam_policy_document.qlikdata_executor_policy_document.json
+}

commit a06b94a9e9fc266f68c9094fc62375af643b434c

INFRA-791 Create Resources for Powerautomate bug (#6521)
🎫 Ticket
https://lwd.atlassian.net/browse/INFRA-791

🛠 Changes
typo in api gateway methods resource id
add missing integrations to gateway.tf
ℹ️ Context for reviewers
Bug was introduced in #6258 due to improper configuration

infra/env-shared/template/api_gateway_qlik_reports.tf
@@ -48,7 +48,7 @@ resource "aws_api_gateway_usage_plan_key" "business_intellegence_usage_plan_key"
 
 resource "aws_api_gateway_method" "business_intelligence_get" {
   rest_api_id      = aws_api_gateway_rest_api.pfml.id
-  resource_id      = "something"
+  resource_id      = aws_api_gateway_resource.business_intelligence_file.id
   http_method      = "GET"
   authorization    = "NONE"
   api_key_required = true

infra/env-shared/template/gateway.tf
@@ -49,6 +49,9 @@ resource "aws_api_gateway_deployment" "stage" {
     aws_api_gateway_integration.pfmldata_delete_object_s3_integration,
     aws_api_gateway_integration.pfmldata_list_objects_s3_integration,
     aws_api_gateway_integration.pfmldata_copy_object_s3_integration,
+    aws_api_gateway_integration.s3_integration_get_business_intelligence,
+    aws_api_gateway_integration.s3_integration_put_business_intelligence,
+    aws_api_gateway_integration.s3_integration_delete_business_intelligence,
   ]
 }
 

infra/env-shared/environments/infra-test/.terraform.lock.hcl
@@ -1,0 +1,38 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/archive" {
+  version = "2.2.0"
+  hashes = [
+    "h1:2K5LQkuWRS2YN1/YoNaHn9MAzjuTX8Gaqy6i8Mbfv8Y=",
+    "zh:06bd875932288f235c16e2237142b493c2c2b6aba0e82e8c85068332a8d2a29e",
+    "zh:0c681b481372afcaefddacc7ccdf1d3bb3a0c0d4678a526bc8b02d0c331479bc",
+    "zh:100fc5b3fc01ea463533d7bbfb01cb7113947a969a4ec12e27f5b2be49884d6c",
+    "zh:55c0d7ddddbd0a46d57c51fcfa9b91f14eed081a45101dbfc7fd9d2278aa1403",
+    "zh:73a5dd68379119167934c48afa1101b09abad2deb436cd5c446733e705869d6b",
+    "zh:841fc4ac6dc3479981330974d44ad2341deada8a5ff9e3b1b4510702dfbdbed9",
+    "zh:91be62c9b41edb137f7f835491183628d484e9d6efa82fcb75cfa538c92791c5",
+    "zh:acd5f442bd88d67eb948b18dc2ed421c6c3faee62d3a12200e442bfff0aa7d8b",
+    "zh:ad5720da5524641ad718a565694821be5f61f68f1c3c5d2cfa24426b8e774bef",
+    "zh:e63f12ea938520b3f83634fc29da28d92eed5cfbc5cc8ca08281a6a9c36cca65",
+    "zh:f6542918faa115df46474a36aabb4c3899650bea036b5f8a5e296be6f8f25767",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version = "3.69.0"
+  hashes = [
+    "h1:PBvQ5w86YSJqL5x7lAwKYxdd7NRyNCTlXz3S61VQWZk=",
+    "zh:0cedd84ba908ba7190052b16cd7f70c41b0e2c2e914e54eecf2e3dae193f47fa",
+    "zh:14b89bac6412e20d415fe67d5f2eaa1414d9bbf75a5bd8fc963f6ab8e3b8b1a0",
+    "zh:159131129edab7ea118dee7d6daf1bfe4615f200a2d5120deb8369cbd2c4b598",
+    "zh:32a3a35964c9becb167180df905f34eb0cae7c30e00452527a0e600ee95c033f",
+    "zh:5330374066ca27d9a00ca667c81183c1dbfa0fcfdd5c1797a6185b76bc7c13bc",
+    "zh:78b75c45b7c660efaf89428ca988a77a4f55eba359f95ed7a54efe87fad1ab8b",
+    "zh:81f723c3f33dc0761ed12b025c1f411fe22f2c6a97e22f4adeb10f7668a5df8f",
+    "zh:98053adb091233fea8c1a82768dfce994e8407e2cb948d28ff88865d2d9a4dcd",
+    "zh:a52866826b51c0b4cb6b970cb3328542846e108c8f4d24c090d7ca0ffa341e44",
+    "zh:a9923cbdf30e9b66f889fef22e1f4b657d9ac1a48812f476ef841405a3c11525",
+    "zh:c079f98be9b8456e6eae6c07c5dcb84ecbcbb70b2f361f1c6f9c3ba90366d905",
+  ]
+}