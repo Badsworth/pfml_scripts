cefaafeb128d2425b09faf83737b6b7505c62bcf
Briany <52139616+tbriany@users.noreply.github.com>
2021-12-30 19:02:36 -0500

    PORTAL-980: Add business logic for persisting audit trail of MFA preferences (#6586)

--- api/massgov/pfml/api/services/users.py
+++ api/massgov/pfml/api/services/users.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 
 import massgov.pfml.api.app as app
@@ -5,7 +6,11 @@ import massgov.pfml.db as db
 import massgov.pfml.db.lookups as db_lookups
 from massgov.pfml.api.models.users.requests import UserUpdateRequest
 from massgov.pfml.api.util.phone import convert_to_E164
-from massgov.pfml.db.models.employees import LkMFADeliveryPreference, User
+from massgov.pfml.db.models.employees import (
+    LkMFADeliveryPreference,
+    LkMFADeliveryPreferenceUpdatedBy,
+    User,
+)
 
 
 def update_user(user: User, update_request: UserUpdateRequest) -> User:
@@ -14,7 +19,14 @@ def update_user(user: User, update_request: UserUpdateRequest) -> User:
             value = getattr(update_request, key)
 
             if key == "mfa_delivery_preference":
-                _add_or_update_mfa_delivery_preference(db_session, user, key, value)
+                delivery_preference = (
+                    user.mfa_delivery_preference.mfa_delivery_preference_description
+                    if user.mfa_delivery_preference is not None
+                    else None
+                )
+                if delivery_preference != value:
+                    _add_or_update_mfa_delivery_preference(db_session, user, key, value)
+                    _update_mfa_preference_audit_trail(db_session, user, key, value)
                 continue
 
             if key == "mfa_phone_number":
@@ -35,3 +47,22 @@ def _add_or_update_mfa_delivery_preference(
 
     mfa_delivery_preference = db_lookups.by_value(db_session, LkMFADeliveryPreference, value)
     setattr(user, key, mfa_delivery_preference)
+
+
+def _update_mfa_preference_audit_trail(
+    db_session: db.Session, user: User, key: str, value: Optional[str]
+) -> None:
+    if value is None:
+        setattr(user, key, None)
+
+    # when a user changes their security preferences
+    # we want to record an audit trail of who made the change, and when
+    now = datetime.utcnow()
+    mfa_delivery_preference_updated_at = "mfa_delivery_preference_updated_at"
+    setattr(user, mfa_delivery_preference_updated_at, now)
+
+    mfa_delivery_preference_updated_by_user = db_lookups.by_value(
+        db_session, LkMFADeliveryPreferenceUpdatedBy, "User"
+    )
+    mfa_delivery_preference_updated_by = "mfa_delivery_preference_updated_by"
+    setattr(user, mfa_delivery_preference_updated_by, mfa_delivery_preference_updated_by_user)
