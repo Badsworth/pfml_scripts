01df48f1ae99957373de6c3dd0abfd95ec900085
Zanson Watt <91619176+zansonLCM@users.noreply.github.com>
2022-01-04 13:59:32 -0500

    PFMLPB-2798: Update NR Fineos API dashboards with new endpoints (#6704)

--- e2e/src/newrelic/queries.ts
+++ e2e/src/newrelic/queries.ts
@@ -29,7 +29,7 @@ export async function buildFineosAPIEndpoints(
   limiters: string
 ): Promise<FineosAPIResult[]> {
   const requestIds = await client.nrql<{ request_id: string }>(
-    `SELECT request_id FROM Log WHERE aws.logGroup LIKE 'API-Gateway-Execution-Logs_%/prod' AND status_code = '504' ${limiters} LIMIT MAX`
+    `SELECT request_id FROM Log WHERE aws.logGroup LIKE 'API-Gateway-Execution-Logs_%/${environment}' AND status_code = '504' ${limiters} LIMIT MAX`
   );
   const requestIdString = requestIds.map((i) => `'${i.request_id}'`).join(",");
   const results = await client.nrql<FineosAPIResult>(
