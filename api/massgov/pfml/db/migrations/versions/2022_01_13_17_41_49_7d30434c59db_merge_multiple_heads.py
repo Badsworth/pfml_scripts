bd6af9915aad51a543dbbd52b4552ce3337bc1b2
Mike L <myclues@gmail.com>
2022-01-13 12:57:59 -0500

    merge multiple db heads (#6841)

---
+++ api/massgov/pfml/db/migrations/versions/2022_01_13_17_41_49_7d30434c59db_merge_multiple_heads.py
@@ -0,0 +1,21 @@
+"""Merge multiple heads
+
+Revision ID: 7d30434c59db
+Revises: f21e9e1db760, f89a0e06fb92
+Create Date: 2022-01-13 17:41:49.727952
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "7d30434c59db"
+down_revision = ("f21e9e1db760", "f89a0e06fb92")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
