e522908f2986964080b8e0ad31cb193c650f2b35
Sawyer Hollenshead <git@sawyerh.com>
2022-01-13 13:45:06 -0500

    PORTAL-1504: Fix Employer.has_verification_data not accounting for previously exempt employers (#6837)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/PORTAL-1504
    
    ## 🛠 Changes
    
    - Update `Employer.has_verification_data` to use the same constraints as the `employer_get_most_recent_withholding_dates` queries. 
    
    ## ℹ️ Context for reviewers
    
    In the Portal, we first check `has_verification_data` in order to determine whether to route an employer to their verification page. On the verification page, we then retrieve the data that the employer should verify.  The logic we use for both is dependent on being consistent, so we should just use the same logic.

--- api/massgov/pfml/api/employers.py
+++ api/massgov/pfml/api/employers.py
@@ -1,8 +1,5 @@
-from datetime import date
-
 import connexion
 import flask
-from sqlalchemy import desc
 from sqlalchemy.exc import IntegrityError
 from werkzeug.exceptions import Conflict, NotFound, Unauthorized
 
@@ -22,11 +19,7 @@ from massgov.pfml.api.validation.exceptions import (
     ValidationErrorDetail,
     ValidationException,
 )
-from massgov.pfml.db.models.employees import (
-    Employer,
-    EmployerQuarterlyContribution,
-    UserLeaveAdministrator,
-)
+from massgov.pfml.db.models.employees import Employer, UserLeaveAdministrator
 
 logger = massgov.pfml.util.logging.get_logger(__name__)
 
@@ -34,10 +27,6 @@ logger = massgov.pfml.util.logging.get_logger(__name__)
 @requires(READ, "EMPLOYER_API")
 def employer_get_most_recent_withholding_dates(employer_id: str) -> flask.Response:
     with app.db_session() as db_session:
-
-        current_date = date.today()
-        last_years_date = date(current_date.year - 1, current_date.month, current_date.day)
-
         employer = (
             db_session.query(Employer).filter(Employer.employer_id == employer_id).one_or_none()
         )
@@ -45,36 +34,12 @@ def employer_get_most_recent_withholding_dates(employer_id: str) -> flask.Respon
         if employer is None:
             raise NotFound(description="Employer not found")
 
-        # Check the last four quarters. Does not include the current quarter, which would be a future filing period.
-        non_zero_contribution = (
-            db_session.query(EmployerQuarterlyContribution)
-            .filter(EmployerQuarterlyContribution.employer_id == employer_id)
-            .filter(EmployerQuarterlyContribution.employer_total_pfml_contribution > 0)
-            .filter(
-                EmployerQuarterlyContribution.filing_period.between(last_years_date, current_date)
-            )
-            .order_by(desc(EmployerQuarterlyContribution.filing_period))
-            .first()
-        )
+        contribution = employer.verification_data
 
-        if non_zero_contribution is None:
-            # If this is a new or previously-exempt Employer to the program, we may not
-            # have any non-zero contributions within the past year. We still want to support
-            # verification for them, so for them we check future filing periods, which includes
-            # the current quarter:
-            non_zero_contribution = (
-                db_session.query(EmployerQuarterlyContribution)
-                .filter(EmployerQuarterlyContribution.employer_id == employer_id)
-                .filter(EmployerQuarterlyContribution.employer_total_pfml_contribution > 0)
-                .filter(EmployerQuarterlyContribution.filing_period > current_date)
-                .order_by(desc(EmployerQuarterlyContribution.filing_period))
-                .first()
-            )
-
-        if non_zero_contribution is None:
+        if contribution is None:
             raise PaymentRequired(description="No valid contributions found")
 
-        response = {"filing_period": non_zero_contribution.filing_period}
+        response = {"filing_period": contribution.filing_period}
 
         return response_util.success_response(
             message="Successfully retrieved quarterly contribution", data=response, status_code=200
