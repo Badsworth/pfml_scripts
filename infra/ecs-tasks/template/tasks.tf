55aa21074a83c1c1f6d71c2d3ccd942dbc651bc2
Melquisedeque Pereira <hire@melquip.com>
2022-01-21 19:03:42 +0000

    INFRA-999: Shorten task name to prevent long string issues (#6932)
    * Update broken tests to match new extract structure (#6930)
    
    🎫 Ticket
    https://lwd.atlassian.net/browse/api-2204
    
    🛠 Changes
    Updated tests to match the new extract structure
    
    * Shorten task name to prevent long string issues
    
    Co-authored-by: Austin Gardner <austintgardner@gmail.com>

--- infra/ecs-tasks/template/tasks.tf
+++ infra/ecs-tasks/template/tasks.tf
@@ -177,8 +177,8 @@ locals {
       ]
     },
 
-    "fineos-import-leave-admin-org-units" = {
-      command   = ["fineos-import-leave-admin-org-units"]
+    "fineos-import-la-units" = {
+      command   = ["fineos-import-la-units"]
       task_role = aws_iam_role.fineos_import_la_org_units_task_role.arn
       cpu       = 2048
       memory    = 9216
