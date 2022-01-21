77141a520a8cedad2f8e5dd1ff9555d62831e43c
James Bursa <james@navapbc.com>
2022-01-21 16:37:09 -0500

    API-2328: add back Application start/updated_time columns and merge migration heads (#6942)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/API-2328
    
    ## 🛠 Changes
    
    - Cherry-pick 54fda85: `git cherry-pick 54fda85`
    - Merge migration heads: `make db-migrate-merge-heads`
    
    ## ℹ️ Context for reviewers
    
    The deployment of https://github.com/EOLWD/pfml/releases/tag/api%2Fv2.49.0 resulted in the migration head in `test` being branched from that in the other environments. This merge brings them back together.
    
    ## 👀 Demo
    
    Testing alembic behaviour from each possible head:
    
    ```
    pfml=> SELECT * FROM alembic_version;
     version_num  
    --------------
     5eed6252f2a3
    
    $ make db-upgrade
    ...
    18:50:41.015  alembic.runtime.migration            run_migrations               INFO     Running upgrade 4f94ce2880fd -> c76d8f0aa8c6, Add back and deprecate Application.start/update_time 
    18:50:41.027  alembic.runtime.migration            run_migrations               INFO     Running upgrade c76d8f0aa8c6, 5eed6252f2a3 -> 50c0a1e47747, Merge multiple heads 
    ```
    
    ```
    pfml=> SELECT * FROM alembic_version;
     version_num  
    --------------
     c76d8f0aa8c6
    
    $ make db-upgrade
    ...
    18:57:30.362  alembic.runtime.migration            run_migrations               INFO     Running upgrade 4f94ce2880fd -> 5eed6252f2a3, API-2323: Add indexes for slowest queries 
    18:57:30.373  alembic.runtime.migration            run_migrations               INFO     Running upgrade c76d8f0aa8c6, 5eed6252f2a3 -> 50c0a1e47747, Merge multiple heads 
    ```
    
    ## 📜 Link to relevant documentation
    
    N/A
    
    Reviewers: @myclues

---
+++ api/massgov/pfml/db/migrations/versions/2022_01_21_13_49_47_50c0a1e47747_merge_multiple_heads.py
@@ -0,0 +1,23 @@
+"""Merge multiple heads
+
+Revision ID: 50c0a1e47747
+Revises: c76d8f0aa8c6, 5eed6252f2a3
+Create Date: 2022-01-21 13:49:47.372318
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "50c0a1e47747"
+down_revision = ("c76d8f0aa8c6", "5eed6252f2a3")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
