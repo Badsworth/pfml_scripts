0e72d455dec85dc19672c53a366c3a4ba9aecbea
Tyler Stephens <roberttstephens@gmail.com>
2022-02-07 16:42:34 -0500

    PFMLPB-1711: Tweak tests

--- api/tests/api/test_admin.py
+++ api/tests/api/test_admin.py
@@ -6,6 +6,7 @@ from mock import patch
 from massgov.pfml.api.admin import SERVICE_UNAVAILABLE_MESSAGE
 from massgov.pfml.db.models.employees import AzureGroup, AzureGroupPermission, AzurePermission
 from massgov.pfml.db.models.factories import UserFactory
+from massgov.pfml.db.models.flags import FeatureFlag, FeatureFlagValue, UserAzureFeatureFlagLog
 
 FAKE_AUTH_URI_RESPONSE = {
     "auth_uri": "test",
@@ -33,6 +34,176 @@ def test_admin_authorize_success(mock_build, client, mock_azure):
     assert response.status_code == 200
     json = response.get_json()
     assert json.get("data").get("auth_uri") == "test"
+
+
+def test_admin_flag_get_logs_by_name_success(
+    app, client, test_db_session, auth_claims_unit, azure_auth_private_key
+):
+    azure_token = auth_claims_unit.copy()
+    azure_token["unique_name"] = "johndoe@example.com"
+    azure_token["given_name"] = "john"
+    azure_token["family_name"] = "doe"
+    azure_token["groups"] = [
+        AzureGroup.NON_PROD.azure_group_guid,
+        AzureGroup.NON_PROD_DEV.azure_group_guid,
+    ]
+    test_db_session.add(
+        AzureGroupPermission(
+            azure_group_id=AzureGroup.NON_PROD_DEV.azure_group_id,
+            azure_permission_id=AzurePermission.MAINTENANCE_EDIT.azure_permission_id,
+        )
+    )
+    encoded = jwt.encode(
+        azure_token,
+        azure_auth_private_key,
+        algorithm=ALGORITHMS.RS256,
+        headers={"kid": azure_auth_private_key.get("kid")},
+    )
+    flag = FeatureFlag.get_instance(test_db_session, description="maintenance")
+    feature_flag_value = FeatureFlagValue()
+    feature_flag_value.feature_flag = flag
+    feature_flag_value.enabled = True
+    test_db_session.add(feature_flag_value)
+    test_db_session.flush()
+    log = UserAzureFeatureFlagLog(
+        azure_feature_flag_value_id=feature_flag_value.feature_flag_value_id,
+        email_address=azure_token["unique_name"],
+        sub_id=azure_token["sub"],
+        family_name=azure_token["given_name"],
+        given_name=azure_token["family_name"],
+        action="INSERT",
+    )
+    test_db_session.add(log)
+    test_db_session.commit()
+    response = client.get(
+        "/v1/admin/flags/logs/maintenance", headers={"Authorization": f"Bearer {encoded}"}
+    )
+    assert response.status_code == 200
+    response_data = response.get_json().get("data")
+    assert len(response_data) == 1
+    flag_response = response_data[0]
+    assert flag_response.get("enabled") is True
+
+
+def test_admin_flag_get_logs_by_name_unauthorized(
+    app, client, test_db_session, auth_claims_unit, azure_auth_private_key
+):
+    azure_token = auth_claims_unit.copy()
+    azure_token["groups"] = [
+        AzureGroup.NON_PROD.azure_group_guid,
+        AzureGroup.NON_PROD_DEV.azure_group_guid,
+    ]
+    encoded = jwt.encode(
+        azure_token,
+        azure_auth_private_key,
+        algorithm=ALGORITHMS.RS256,
+        headers={"kid": azure_auth_private_key.get("kid")},
+    )
+    flag = FeatureFlag.get_instance(test_db_session, description="maintenance")
+    feature_flag_value = FeatureFlagValue()
+    feature_flag_value.feature_flag = flag
+    feature_flag_value.enabled = True
+    test_db_session.add(feature_flag_value)
+    test_db_session.flush()
+    log = UserAzureFeatureFlagLog(
+        azure_feature_flag_value_id=feature_flag_value.feature_flag_value_id,
+        email_address="johndoe@example.com",
+        sub_id=azure_token["sub"],
+        family_name="john",
+        given_name="doe",
+        action="INSERT",
+    )
+    test_db_session.add(log)
+    test_db_session.commit()
+    response = client.get(
+        "/v1/admin/flags/logs/maintenance", headers={"Authorization": f"Bearer {encoded}"}
+    )
+    assert response.status_code == 401
+
+
+def test_admin_flags_post_success(
+    app, client, test_db_session, auth_claims_unit, azure_auth_private_key
+):
+    azure_token = auth_claims_unit.copy()
+    azure_token["unique_name"] = "johndoe@example.com"
+    azure_token["given_name"] = "john"
+    azure_token["family_name"] = "doe"
+    azure_token["groups"] = [
+        AzureGroup.NON_PROD.azure_group_guid,
+        AzureGroup.NON_PROD_DEV.azure_group_guid,
+    ]
+    test_db_session.add(
+        AzureGroupPermission(
+            azure_group_id=AzureGroup.NON_PROD_DEV.azure_group_id,
+            azure_permission_id=AzurePermission.MAINTENANCE_EDIT.azure_permission_id,
+        )
+    )
+    encoded = jwt.encode(
+        azure_token,
+        azure_auth_private_key,
+        algorithm=ALGORITHMS.RS256,
+        headers={"kid": azure_auth_private_key.get("kid")},
+    )
+    post_body = {
+        "enabled": True,
+        "start": "2022-01-14T00:00:00-05:00",
+        "end": "2022-01-15T00:00:00-05:00",
+        "options": {
+            "name": "applications and custom",
+            "page_routes": ["/applications/*", "/custom/*"],
+        },
+    }
+    with app.app.test_request_context("/v1/admin/flags/maintenance"):
+        response = client.post(
+            "/v1/admin/flags/maintenance",
+            headers={"Authorization": f"Bearer {encoded}"},
+            json=post_body,
+        )
+        assert response.status_code == 201
+        assert g.azure_user.sub_id == "foo"
+        assert g.azure_user.permissions == [
+            AzurePermission.MAINTENANCE_EDIT.azure_permission_id,
+        ]
+        response_json = response.get_json()
+        data = response_json.get("data")
+        assert data.get("enabled") is True
+        assert data.get("start") == "2022-01-14T00:00:00-05:00"
+        assert data.get("end") == "2022-01-15T00:00:00-05:00"
+        assert data.get("options").get("name") == "applications and custom"
+        assert len(data.get("options").get("page_routes")) == 2
+        assert data.get("options").get("page_routes") == ["/applications/*", "/custom/*"]
+
+
+def test_admin_flags_post_no_permissions(
+    client, app, mock_azure, auth_claims_unit, azure_auth_private_key
+):
+    azure_token = auth_claims_unit.copy()
+    azure_token["groups"] = [
+        AzureGroup.NON_PROD.azure_group_guid,
+        AzureGroup.NON_PROD_DEV.azure_group_guid,
+    ]
+    encoded = jwt.encode(
+        azure_token,
+        azure_auth_private_key,
+        algorithm=ALGORITHMS.RS256,
+        headers={"kid": azure_auth_private_key.get("kid")},
+    )
+    post_body = {
+        "enabled": True,
+        "start": "2022-01-14T00:00:00-05:00",
+        "end": "2022-01-15T00:00:00-05:00",
+        "options": {
+            "name": "applications and custom",
+            "page_routes": ["/applications/*", "/custom/*"],
+        },
+    }
+    with app.app.test_request_context("/v1/admin/flags/maintenance"):
+        response = client.post(
+            "/v1/admin/flags/maintenance",
+            headers={"Authorization": f"Bearer {encoded}"},
+            json=post_body,
+        )
+        assert response.status_code == 401
 
 
 def test_admin_login_success(
@@ -196,7 +367,9 @@ def test_admin_token_value_error(mock_build, client):
     assert response.status_code == 400
 
 
-def test_admin_no_permissions(client, app, mock_azure, auth_claims_unit, azure_auth_private_key):
+def test_admin_users_no_permissions(
+    client, app, mock_azure, auth_claims_unit, azure_auth_private_key
+):
     azure_token = auth_claims_unit.copy()
     azure_token["groups"] = [
         AzureGroup.NON_PROD.azure_group_guid,
--- api/tests/api/test_flags.py
+++ api/tests/api/test_flags.py
@@ -1,14 +1,28 @@
-from flask import g
-from jose import jwt
-from jose.constants import ALGORITHMS
-
-from massgov.pfml.db.models.employees import AzureGroup, AzureGroupPermission, AzurePermission
 from massgov.pfml.db.models.flags import FeatureFlag, FeatureFlagValue
 
 # There are also feature flag related tests in tests/massgov/pfml/util/feature_gate/features_cache.py
 
 
 def test_flags_get_enabled_feature_flag_with_start_end_options(client, test_db_session):
+    flag_name = "maintenance"
+    flag = FeatureFlag.get_instance(test_db_session, description=flag_name)
+    feature_flag_value = FeatureFlagValue()
+    feature_flag_value.feature_flag = flag
+    feature_flag_value.enabled = True
+    feature_flag_value.end = "2021-06-26 18:00:00-04"
+    feature_flag_value.start = "2021-06-26 17:00:00-04"
+    feature_flag_value.options = dict(page_routes=["/applications/*", "/employers/*"])
+    test_db_session.add(feature_flag_value)
+    response = client.get(f"/v1/flags/{flag_name}")
+    assert response.status_code == 200
+    flag_response = response.get_json().get("data")
+    assert flag_response.get("enabled") is True
+    assert flag_response.get("start") == "2021-06-26T17:00:00-04:00"
+    assert flag_response.get("end") == "2021-06-26T18:00:00-04:00"
+    assert flag_response.get("options") == {"page_routes": ["/applications/*", "/employers/*"]}
+
+
+def test_flags_get_enabled_feature_flags_with_start_end_options(client, test_db_session):
     flag = FeatureFlag.get_instance(test_db_session, description="maintenance")
     feature_flag_value = FeatureFlagValue()
     feature_flag_value.feature_flag = flag
@@ -39,52 +53,3 @@ def test_flags_get_empty(client):
     assert flag.get("start") is None
     assert flag.get("end") is None
     assert flag.get("options") is None
-
-
-def test_flags_post_success(app, client, test_db_session, auth_claims_unit, azure_auth_private_key):
-    azure_token = auth_claims_unit.copy()
-    azure_token["unique_name"] = "johndo@example.com"
-    azure_token["given_name"] = "john"
-    azure_token["family_name"] = "doe"
-    azure_token["groups"] = [
-        AzureGroup.NON_PROD.azure_group_guid,
-        AzureGroup.NON_PROD_DEV.azure_group_guid,
-    ]
-    test_db_session.add(
-        AzureGroupPermission(
-            azure_group_id=AzureGroup.NON_PROD_DEV.azure_group_id,
-            azure_permission_id=AzurePermission.MAINTENANCE_EDIT.azure_permission_id,
-        )
-    )
-    encoded = jwt.encode(
-        azure_token,
-        azure_auth_private_key,
-        algorithm=ALGORITHMS.RS256,
-        headers={"kid": azure_auth_private_key.get("kid")},
-    )
-    post_body = {
-        "enabled": True,
-        "start": "2022-01-14T00:00:00-05:00",
-        "end": "2022-01-15T00:00:00-05:00",
-        "options": {
-            "name": "applications and custom",
-            "page_routes": ["/applications/*", "/custom/*"],
-        },
-    }
-    with app.app.test_request_context("/v1/admin/users"):
-        response = client.post(
-            "/v1/flags/maintenance", headers={"Authorization": f"Bearer {encoded}"}, json=post_body
-        )
-        assert response.status_code == 201
-        assert g.azure_user.sub_id == "foo"
-        assert g.azure_user.permissions == [
-            AzurePermission.MAINTENANCE_EDIT.azure_permission_id,
-        ]
-        response_json = response.get_json()
-        data = response_json.get("data")
-        assert data.get("enabled") is True
-        assert data.get("start") == "2022-01-14T00:00:00-05:00"
-        assert data.get("end") == "2022-01-15T00:00:00-05:00"
-        assert data.get("options").get("name") == "applications and custom"
-        assert len(data.get("options").get("page_routes")) == 2
-        assert data.get("options").get("page_routes") == ["/applications/*", "/custom/*"]
