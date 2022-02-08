5113a8bca95452445f711a429bb8bb5f3d1ca921
Ellery Royston <73132536+ellery-nava@users.noreply.github.com>
2022-02-08 13:48:06 -0500

    PORTAL-1624: Logging updates for MfaLockoutResolver (#7202)
    * PORTAL-1624: Logging updates for MfaLockoutResolver
    
    * update test
    
    * bad import
    
    * Update test_mfa_lockout_resolver.py

--- api/tests/mfa/test_mfa_lockout_resolver.py
+++ api/tests/mfa/test_mfa_lockout_resolver.py
@@ -18,13 +18,29 @@ def create_lockout_resolver(dry_run=True):
     )
 
 
-class TestMFALockoutResolverWithDryRunEnabled:
+class TestMfaLockoutResolver:
     mock_logger = get_mock_logger()
-    mfa_lockout_resolver = create_lockout_resolver()
+    mfa_lockout_resolver = create_lockout_resolver(dry_run=False)
 
-    @patch("massgov.pfml.mfa.mfa_lockout_resolver.logger", mock_logger)
-    def test_mfa_lockout_resolution_init(self):
-        assert self.mfa_lockout_resolver.user_email == "test@email.com"
+    @patch("massgov.pfml.mfa.mfa_lockout_resolver.get_user_by_email")
+    @patch(
+        "massgov.pfml.mfa.mfa_lockout_resolver.check_phone_number_opt_out", return_value=True,
+    )
+    @patch("massgov.pfml.mfa.mfa_lockout_resolver.opt_in_phone_number")
+    @patch("massgov.pfml.mfa.mfa_lockout_resolver.update_user")
+    @patch("massgov.pfml.mfa.mfa_lockout_resolver.disable_user_mfa")
+    def test_mfa_lockout_resolution_dry_run(
+        self,
+        mock_disable_mfa,
+        mock_update_user,
+        mock_opt_in_phone_number,
+        mock_opt_out,
+        mock_get_user,
+        user_with_mfa,
+    ):
+        mock_get_user.return_value = user_with_mfa
+        mfa_lockout_resolver = create_lockout_resolver()
+        assert mfa_lockout_resolver.user_email == "test@email.com"
         log_attrs = {
             "psd_ticket_number": "PSD-1234",
             "reason_for_disabling": "lost phone",
@@ -32,60 +48,17 @@ class TestMFALockoutResolverWithDryRunEnabled:
             "identity_verification_method": "seemed truthful",
             "dry_run": True,
         }
-        assert self.mfa_lockout_resolver.log_attr == log_attrs
+        assert mfa_lockout_resolver.log_attr == log_attrs
         # dry_run is flipped when passed in
-        assert self.mfa_lockout_resolver.should_commit_changes is False
+        assert mfa_lockout_resolver.should_commit_changes is False
 
-    @patch("massgov.pfml.mfa.mfa_lockout_resolver.logger", mock_logger)
-    @patch("massgov.pfml.mfa.mfa_lockout_resolver.disable_user_mfa")
-    def test_disable_mfa_cognito(self, mock_disable_user_mfa):
-        self.mfa_lockout_resolver._disable_mfa_cognito()
+        mfa_lockout_resolver.run()
 
-        mock_disable_user_mfa.assert_not_called()
-        self.mock_logger.info.assert_any_call(
-            "Disabling user MFA in Cognito", extra=self.mfa_lockout_resolver.log_attr
-        )
-        self.mock_logger.info.assert_any_call(
-            "(DRY RUN: Skipping API call to disable user MFA)",
-            extra=self.mfa_lockout_resolver.log_attr,
-        )
-
-    @patch("massgov.pfml.mfa.mfa_lockout_resolver.logger", mock_logger)
-    @patch(
-        "massgov.pfml.mfa.mfa_lockout_resolver.check_phone_number_opt_out", return_value=True,
-    )
-    @patch("massgov.pfml.mfa.mfa_lockout_resolver.opt_in_phone_number")
-    def test_sns_opt_out_phone_opted_out(self, mock_opt_in, mock_phone_opt_out, user_with_mfa):
-        self.mfa_lockout_resolver._set_sns_opt_in(user_with_mfa)
-
-        mock_phone_opt_out.assert_called_once_with(user_with_mfa.mfa_phone_number)
-        mock_opt_in.assert_not_called()
-        self.mock_logger.info.assert_any_call(
-            "Setting user opt-in in SNS", extra=self.mfa_lockout_resolver.log_attr
-        )
-        self.mock_logger.info.assert_any_call(
-            "(DRY RUN: Skipping API call to opt user into SNS)",
-            extra=self.mfa_lockout_resolver.log_attr,
-        )
-
-    @patch("massgov.pfml.mfa.mfa_lockout_resolver.logger", mock_logger)
-    @patch("massgov.pfml.mfa.mfa_lockout_resolver.update_user")
-    def test_disable_mfa_pfml_dry_run(self, mock_update_user, user):
-        self.mfa_lockout_resolver._disable_mfa_pfml(user)
-
+        # check that none of the methods that make external calls are called
+        mock_disable_mfa.assert_not_called()
         mock_update_user.assert_not_called()
-        self.mock_logger.info.assert_any_call(
-            "Disabling user MFA in PFML db", extra=self.mfa_lockout_resolver.log_attr
-        )
-        self.mock_logger.info.assert_any_call(
-            "(DRY RUN: Skipping API call to disable MFA in PFML DB)",
-            extra=self.mfa_lockout_resolver.log_attr,
-        )
-
-
-class TestMFALockoutResolverWithDryRunDisabled:
-    mock_logger = get_mock_logger()
-    mfa_lockout_resolver = create_lockout_resolver(dry_run=False)
+        mock_opt_out.assert_called_once_with(user_with_mfa.mfa_phone_number)
+        mock_opt_in_phone_number.assert_not_called()
 
     @patch("massgov.pfml.mfa.mfa_lockout_resolver.logger", mock_logger)
     def test_mfa_lockout_resolution_init(self):
@@ -116,16 +89,17 @@ class TestMFALockoutResolverWithDryRunDisabled:
         )
 
     @patch("massgov.pfml.mfa.mfa_lockout_resolver.logger", mock_logger)
-    @patch(
-        "massgov.pfml.mfa.mfa_lockout_resolver.get_user_by_email", return_value="user",
-    )
-    def test_get_user_found(self, mock_get_user_by_email):
+    @patch("massgov.pfml.mfa.mfa_lockout_resolver.get_user_by_email")
+    def test_get_user_found(self, mock_get_user_by_email, user_with_mfa):
+        mock_get_user_by_email.return_value = user_with_mfa
         self.mfa_lockout_resolver._get_user()
 
         mock_get_user_by_email.assert_called_once()
         self.mock_logger.info.assert_any_call(
             "Getting user from PFML API", extra=self.mfa_lockout_resolver.log_attr
         )
+        # user id added to list of attrs we're logging
+        assert self.mfa_lockout_resolver.log_attr["user_id"] == user_with_mfa.user_id
 
     @patch("massgov.pfml.mfa.mfa_lockout_resolver.logger", mock_logger)
     @patch(
