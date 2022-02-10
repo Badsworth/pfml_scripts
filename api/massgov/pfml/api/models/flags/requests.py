22e7e5fd1bb6252cfb50f153726f951680e04d8c
Robert Stephens <roberttstephens@gmail.com>
2022-02-10 18:15:34 -0500

    PFMLPB-1711: Stub API endpoints to read/write flags from table (#7223)
    This adds stub API endpoints and the corresponding request and response models to enable reading maintenance from and setting it in a database table.

--- api/massgov/pfml/api/models/flags/requests.py
+++ api/massgov/pfml/api/models/flags/requests.py
@@ -4,15 +4,8 @@ from typing import Optional
 from massgov.pfml.util.pydantic import PydanticBaseModel
 
 
-class FlagResponse(PydanticBaseModel):
-    name: str
+class FlagRequest(PydanticBaseModel):
     start: Optional[datetime]
     end: Optional[datetime]
     options: Optional[dict]
     enabled: bool
-
-
-class FlagLogResponse(FlagResponse):
-    first_name: str
-    last_name: str
-    updated_at: datetime
