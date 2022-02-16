e29adb78f5be46b99d866268cbaf743b89fa8c9c
Ali <7331495+aligg@users.noreply.github.com>
2022-02-16 15:59:18 -0800

    Portal-1703: Add testing for new dashboard filters in combination (#7363)
    Ticket:
    https://lwd.atlassian.net/browse/PORTAL-1703
    
    Changes:
    Added testing for is_reviewable and request_decision filter used in combination
    Nested in TestClaimsMultipleParams and renamed existing with Old tag so ready for deletion once we deprecate absence status etc

--- api/tests/api/test_claims.py
+++ api/tests/api/test_claims.py
@@ -4591,7 +4591,8 @@ class TestGetClaimsEndpoint:
 
     # Test the combination of claims feature
     # ordering, filtering and search
-    class TestClaimsMultipleParams:
+    # TODO: (PORTAL-1561) - delete this test class
+    class TestClaimsMultipleParamsOld:
         @pytest.fixture
         def employee(self):
             return EmployeeFactory.create()
@@ -4663,6 +4664,7 @@ class TestGetClaimsEndpoint:
                     claim=claim,
                     managed_requirement_status_id=ManagedRequirementStatus.OPEN.managed_requirement_status_id,
                 )
+
             self.claims_count = 20
             test_db_session.commit()
 
@@ -4728,6 +4730,136 @@ class TestGetClaimsEndpoint:
             response_body = resp.get_json()
             data = response_body.get("data", [])
             assert len(data) == self.claims_count / 4
+
+    # Test the combination of claims filters and search
+    class TestClaimsMultipleParams:
+        @pytest.fixture
+        def employee(self):
+            return EmployeeFactory.create()
+
+        @pytest.fixture
+        def other_employee(self):
+            return EmployeeFactory.create()
+
+        @pytest.fixture(autouse=True)
+        def load_test_db(
+            self, employer_user, test_verification, test_db_session, employee, other_employee
+        ):
+            employer = EmployerFactory.create()
+
+            link = UserLeaveAdministrator(
+                user_id=employer_user.user_id,
+                employer_id=employer.employer_id,
+                fineos_web_id="fake-fineos-web-id",
+                verification=test_verification,
+            )
+            test_db_session.add(link)
+
+            other_employer = EmployerFactory.create()
+
+            other_link = UserLeaveAdministrator(
+                user_id=employer_user.user_id,
+                employer_id=other_employer.employer_id,
+                fineos_web_id="fake-fineos-web-id",
+                verification=test_verification,
+            )
+            test_db_session.add(other_link)
+
+            # create 10 claims
+            # employee claims are Open, other employee are not Open
+            # varied absence periods for employee claims
+            # always withdrawn for other employee claims
+            for i in range(5):
+                employee_claim = ClaimFactory.create(
+                    employer=employer, employee=employee, claim_type_id=1,
+                )
+                other_employee_claim = ClaimFactory.create(
+                    employer=other_employer,
+                    employee=other_employee,
+                    claim_type_id=1,
+                    created_at=factory.Faker(
+                        "date_between_dates",
+                        date_start=date(2021, 1, 1),
+                        date_end=date(2021, 1, 15),
+                    ),
+                )
+                ManagedRequirementFactory.create(
+                    claim=employee_claim,
+                    managed_requirement_status_id=ManagedRequirementStatus.OPEN.managed_requirement_status_id,
+                )
+                ManagedRequirementFactory.create(
+                    claim=other_employee_claim,
+                    managed_requirement_status_id=ManagedRequirementStatus.SUPPRESSED.managed_requirement_status_id,
+                )
+                AbsencePeriodFactory.create(
+                    claim=employee_claim,
+                    absence_period_start_date=date.today() + timedelta(days=5),
+                    absence_period_end_date=date.today() + timedelta(days=20),
+                    leave_request_decision_id=i + 1,
+                )
+                AbsencePeriodFactory.create(
+                    claim=other_employee_claim,
+                    absence_period_start_date=date.today() + timedelta(days=5),
+                    absence_period_end_date=date.today() + timedelta(days=20),
+                    leave_request_decision_id=LeaveRequestDecision.WITHDRAWN.leave_request_decision_id,
+                )
+
+            self.claims_count = 10
+            test_db_session.commit()
+
+        def _perform_api_call(self, request, client, employer_auth_token):
+            query_string = "&".join([f"{key}={value}" for key, value in request.items()])
+            return client.get(
+                f"/v1/claims?{query_string}",
+                headers={"Authorization": f"Bearer {employer_auth_token}"},
+            )
+
+        @pytest.mark.parametrize(
+            "is_reviewable, request_decision, expected_output_len",
+            [
+                ["yes", "approved", 1],
+                ["no", "approved", 0],
+                ["no", "pending", 0],
+                ["yes", "pending", 2],
+            ],
+        )
+        def test_get_claims_is_reviewable_and_request_decision_filter(
+            self, is_reviewable, request_decision, expected_output_len, client, employer_auth_token
+        ):
+            params = {"is_reviewable": is_reviewable, "request_decision": request_decision}
+            resp = self._perform_api_call(params, client, employer_auth_token)
+            assert resp.status_code == 200
+            response_body = resp.get_json()
+            data = response_body.get("data", [])
+            assert len(data) == expected_output_len
+
+        def test_get_claims_filter_by_request_decision_with_search(
+            self, client, employer_auth_token, other_employee
+        ):
+            params = {
+                "request_decision": "withdrawn",
+                "search": other_employee.first_name,
+                "order_by": "employee",
+            }
+            resp = self._perform_api_call(params, client, employer_auth_token)
+            assert resp.status_code == 200
+            response_body = resp.get_json()
+            data = response_body.get("data", [])
+            assert len(data) == 5
+
+        def test_get_claims_filter_by_request_decision_with_search_no_result(
+            self, client, employer_auth_token, other_employee
+        ):
+            params = {
+                "request_decision": "approved",
+                "search": other_employee.first_name,
+                "order_by": "employee",
+            }
+            resp = self._perform_api_call(params, client, employer_auth_token)
+            assert resp.status_code == 200
+            response_body = resp.get_json()
+            data = response_body.get("data", [])
+            assert len(data) == 0
 
     # Test validation of claims endpoint query param validation
     class TestClaimsAPIInputValidation:
