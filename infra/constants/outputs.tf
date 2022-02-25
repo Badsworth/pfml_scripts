f1b003a0091ffc507fca634628dc1db4c6d1b613
bdlake <75392311+bdlake@users.noreply.github.com>
2022-02-25 10:00:57 -0500

    Adding additional IPs to WAF Whitelist (#7477)

--- infra/constants/outputs.tf
+++ infra/constants/outputs.tf
@@ -237,6 +237,8 @@ output "admin_portal_waf_whitelist" {
     "68.204.24.240/32",  # Last Call Media WFH IP - Tyler
     "68.84.12.117/32",   # Last Call Media WFH IP - Niki Ramlogan
     "71.227.169.195/32", # Last Call Media WFH IP - Jessi Murray
+    "73.39.112.119/32",  # Last Call Media WFH IP - Jim Ruggiero
+    "73.47.218.158/32",  # Chris Griffith WFH
     "47.200.176.201/32", # Ben WFH
     "47.199.161.99/32",  # Jamie WFH
     "76.202.246.67/32",  # Mark WFH
