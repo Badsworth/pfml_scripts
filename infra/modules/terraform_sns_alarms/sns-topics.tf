16e25af94aa42f41bbf4f9f6f19ca4ea884f74f0
terraform <jamietaschetti@gmail.com>
2022-03-02 15:11:33 -0500

    apply tf fmt

--- infra/modules/terraform_sns_alarms/sns-topics.tf
+++ infra/modules/terraform_sns_alarms/sns-topics.tf
@@ -1,24 +1,24 @@
 resource "aws_sns_topic" "sms_monthly_spend_limit" {
-  name = "sms-monthly-spend-limit"
+  name              = "sms-monthly-spend-limit"
   kms_master_key_id = var.kms_key_id
 }
 
 resource "aws_sns_topic" "sms_messages_success_rate" {
-  name = "sms-messages-success-rate"
+  name              = "sms-messages-success-rate"
   kms_master_key_id = var.kms_key_id
 }
 
 resource "aws_sns_topic" "sms_phone_carrier_unavailable" {
-  name = "sms-phone-carrier-unavailable"
+  name              = "sms-phone-carrier-unavailable"
   kms_master_key_id = var.kms_key_id
 }
 
 resource "aws_sns_topic" "sms_blocked_as_spam" {
-  name = "sms-blocked-as-spam"
+  name              = "sms-blocked-as-spam"
   kms_master_key_id = var.kms_key_id
 }
 
 resource "aws_sns_topic" "sns_sms_rate_exceeded" {
-  name = "sns-sms-rate-exceeded"
+  name              = "sns-sms-rate-exceeded"
   kms_master_key_id = var.kms_key_id
 }
