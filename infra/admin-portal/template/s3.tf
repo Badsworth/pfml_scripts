dac6f67651764b077fcf59ad8d272904c152b5a3
terraform <jamietaschetti@gmail.com>
2022-03-02 14:46:14 -0500

    Revert "INFRA-1076 - Enable CloudFront logging - bdlake (#7218)"
    This reverts commit bb37e3aaae85f212aa41519721228669d0d2d9cb.

--- infra/admin-portal/template/s3.tf
+++ infra/admin-portal/template/s3.tf
@@ -40,9 +40,3 @@ resource "aws_s3_bucket_policy" "admin_portal_web_policy" {
   bucket = aws_s3_bucket.admin_portal_web.id
   policy = data.aws_iam_policy_document.admin_portal_web.json
 }
-
-# One bucket will be used to log all Admin Portal Cloudfront requests broken up by env prefixes
-# Bucket is defined in infra/pfml-aws/s3.tf
-data "aws_s3_bucket" "cloudfront_access_logging" {
-  bucket = "massgov-pfml-admin-portal-cloudfront-logging"
-}
