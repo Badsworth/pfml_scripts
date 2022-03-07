1837d28159d62c3d7b1378cd031b99fa1bddebad
jtosbornex <joshosborne@navapbc.com>
2022-03-07 14:03:45 -0800

    Created usage plan for files endpoint unsing config

--- infra/env-shared/template/api_gateway_files_s3.tf
+++ infra/env-shared/template/api_gateway_files_s3.tf
@@ -1,7 +1,8 @@
 ################################################################
 # Utilizes the "endpoints" config to generate API gateway resources
 # authorized for S3 operations under the /files endpoint. 
-#
+################################################################
+
 # ----------------------------------------------------------------------------------------------------------------------
 # API Resources
 # ----------------------------------------------------------------------------------------------------------------------
@@ -43,4 +44,67 @@ resource "aws_api_gateway_resource" "files_list_key" {
   rest_api_id = aws_api_gateway_rest_api.pfml.id
   parent_id   = aws_api_gateway_resource.files_list[each.key].id
   path_part   = "{key+}"
+}
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Usage Plan
+# ----------------------------------------------------------------------------------------------------------------------
+
+# Each use case has a quota that restricts use of other api resources
+resource "aws_api_gateway_usage_plan" "files_usage_plan" {
+  for_each    = local.endpoints
+  name        = "massgov-pfml-${var.environment_name}-${each.key}-usage-plan"
+  description = "Usage plan set for ${each.key} to access files from S3"
+  api_stages {
+    api_id = aws_api_gateway_rest_api.pfml.id
+    stage  = aws_api_gateway_deployment.stage.stage_name
+
+    # By default access is restricted on all endpoints
+    throttle {
+      burst_limit = 0
+      path        = "*/*"
+      rate_limit  = 0
+    }
+    throttle {
+      burst_limit = 10
+      path        = "/files/list/${each.value.resource_name}/{key+}/GET"
+      rate_limit  = 100
+    }
+    throttle {
+      burst_limit = 10
+      path        = "/files/${each.value.resource_name}/{key+}/DELETE"
+      rate_limit  = 100
+    }
+    throttle {
+      burst_limit = 10
+      path        = "/files/${each.value.resource_name}/{key+}/GET"
+      rate_limit  = 100
+    }
+    throttle {
+      burst_limit = 10
+      path        = "/files/${each.value.resource_name}/{key+}/PUT"
+      rate_limit  = 100
+    }
+  }
+
+  quota_settings {
+    limit  = 10000
+    offset = 0
+    period = "DAY"
+  }
+}
+
+# API key specific to each use case
+resource "aws_api_gateway_api_key" "files_api_key" {
+  for_each = local.endpoints
+  name     = "massgov-pfml-${var.environment_name}-${each.key}-api-key"
+}
+
+# Associate the api key with the usage plan
+resource "aws_api_gateway_usage_plan_key" "files_usage_plan_key" {
+  for_each      = local.endpoints
+  key_id        = aws_api_gateway_api_key.files_api_key[each.key].id
+  key_type      = "API_KEY"
+  usage_plan_id = aws_api_gateway_usage_plan.files_usage_plan[each.key].id
 }
--- infra/env-shared/template/main.tf
+++ infra/env-shared/template/main.tf
@@ -5,6 +5,12 @@ provider "aws" {
 
 terraform {
   required_version = "0.14.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.64.0"
+    }
+  }
 }
 
 module "constants" {
