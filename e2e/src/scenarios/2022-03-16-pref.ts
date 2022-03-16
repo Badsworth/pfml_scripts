a8ed13444213a59fb1b0c7cb70706b8802d3a03a
Tariq Mostahid <tariqmostahid@gmail.com>
2022-03-16 23:27:50 +0600

    PFMLPB-3788: Updateed scenarios and claim generation script.

--- e2e/src/scenarios/2022-03-16-pref.ts
+++ e2e/src/scenarios/2022-03-16-pref.ts
@@ -1,53 +1,77 @@
 import { ScenarioSpecification } from "../generation/Scenario";
 import { parseISO } from "date-fns";
 
-export const PERF_APRIL_A: ScenarioSpecification = {
-    employee: { mass_id: true, wages: "eligible" },
-    claim: {
-      label: "PERF_APRIL_A",
-      work_pattern_spec: "standard",
-      has_continuous_leave_periods: true,
-      reason: "Serious Health Condition - Employee",
-      leave_dates: [parseISO("2022-02-06"), parseISO("2022-03-13")],
-      is_withholding_tax: true,
-      metadata: {
-        postSubmit: "APPROVE",
-        amount: 20,
-      },
-    },
-  };
+// export const PERF_APRIL_A: ScenarioSpecification = {
+//     employee: { mass_id: true, wages: "eligible" },
+//     claim: {
+//       label: "PERF_APRIL_A",
+//       work_pattern_spec: "standard",
+//       has_continuous_leave_periods: true,
+//       reason: "Serious Health Condition - Employee",
+//       leave_dates: [parseISO("2022-02-06"), parseISO("2022-03-13")],
+//       is_withholding_tax: true,
+//       docs: {
+//         MASSID: {},
+//         HCP: {}
+//       },
+//       employerResponse: {
+//         hours_worked_per_week: 40,
+//         employer_decision: "Approve",
+//       },
+//       metadata: {
+//         postSubmit: "APPROVE",
+//         amount: 20,
+//       },
+//     },
+//   };
 
-  export const PERF_APRIL_B: ScenarioSpecification = {
-    employee: { mass_id: true, wages: "eligible" },
-    claim: {
-      label: "PERF_APRIL_B",
-      work_pattern_spec: "standard",
-      has_continuous_leave_periods: true,
-      reason: "Serious Health Condition - Employee",
-      leave_dates: [parseISO("2022-03-06"), parseISO("2022-05-17")],
-      is_withholding_tax: true,
-      metadata: {
-        postSubmit: "APPROVE",
-        amount: 20,
-      },
-    },
-  };
+//   export const PERF_APRIL_B: ScenarioSpecification = {
+//     employee: { mass_id: true, wages: "eligible" },
+//     claim: {
+//       label: "PERF_APRIL_B",
+//       work_pattern_spec: "standard",
+//       has_continuous_leave_periods: true,
+//       reason: "Serious Health Condition - Employee",
+//       leave_dates: [parseISO("2022-03-06"), parseISO("2022-05-17")],
+//       is_withholding_tax: true,
+//       docs: {
+//         MASSID: {},
+//         HCP: {}
+//       },
+//       employerResponse: {
+//         hours_worked_per_week: 40,
+//         employer_decision: "Approve",
+//       },
+//       metadata: {
+//         postSubmit: "APPROVE",
+//         amount: 20,
+//       },
+//     },
+//   };
 
-  export const PERF_APRIL_C: ScenarioSpecification = {
-    employee: { mass_id: true, wages: "eligible" },
-    claim: {
-      label: "PERF_APRIL_C",
-      work_pattern_spec: "standard",
-      has_continuous_leave_periods: true,
-      reason: "Serious Health Condition - Employee",
-      leave_dates: [parseISO("2022-03-20"), parseISO("2022-05-02")],
-      is_withholding_tax: true,
-      metadata: {
-        postSubmit: "APPROVE",
-        amount: 5,
-      },
-    },
-  };
+//   export const PERF_APRIL_C: ScenarioSpecification = {
+//     employee: { mass_id: true, wages: "eligible" },
+//     claim: {
+//       label: "PERF_APRIL_C",
+//       work_pattern_spec: "standard",
+//       has_continuous_leave_periods: true,
+//       reason: "Serious Health Condition - Employee",
+//       leave_dates: [parseISO("2022-03-20"), parseISO("2022-05-02")],
+//       is_withholding_tax: true,
+//       docs: {
+//         MASSID: {},
+//         HCP: {}
+//       },
+//       employerResponse: {
+//         hours_worked_per_week: 40,
+//         employer_decision: "Approve",
+//       },
+//       metadata: {
+//         postSubmit: "APPROVE",
+//         amount: 5,
+//       },
+//     },
+//   };
 
   export const PERF_APRIL_D: ScenarioSpecification = {
     employee: { mass_id: true, wages: "eligible" },
@@ -63,6 +87,14 @@ export const PERF_APRIL_A: ScenarioSpecification = {
       bondingDate: "past",
       leave_dates: [parseISO("2022-03-06"), parseISO("2022-05-15")],
       is_withholding_tax: false,
+      docs: {
+        MASSID: {},
+        BIRTHCERTIFICATE: {},
+      },
+      employerResponse: {
+        hours_worked_per_week: 40,
+        employer_decision: "Approve",
+      },
       metadata: {
         postSubmit: "APPROVE",
         amount: 10,
@@ -80,6 +112,14 @@ export const PERF_APRIL_A: ScenarioSpecification = {
       reason: "Pregnancy/Maternity",
       leave_dates: [parseISO("2022-02-27"), parseISO("2022-05-15")],
       is_withholding_tax: true,
+      docs: {
+        MASSID: {},
+        PREGNANCY_MATERNITY_FORM: {},
+      },
+      employerResponse: {
+        hours_worked_per_week: 40,
+        employer_decision: "Approve",
+      },
       metadata: {
         postSubmit: "APPROVE",
         amount: 12,
@@ -97,6 +137,14 @@ export const PERF_APRIL_A: ScenarioSpecification = {
       reason_qualifier: "Foster Care",
       leave_dates: [parseISO("2022-03-20"), parseISO("2022-05-17")],
       is_withholding_tax: true,
+      docs: {
+        MASSID: {},
+        FOSTERPLACEMENT: {},
+      },
+      employerResponse: {
+        hours_worked_per_week: 40,
+        employer_decision: "Approve",
+      },
       metadata: {
         postSubmit: "APPROVE",
         amount: 5,
@@ -113,6 +161,14 @@ export const PERF_APRIL_A: ScenarioSpecification = {
       reason: "Pregnancy/Maternity",
       leave_dates: [parseISO("2022-03-27"), parseISO("2022-05-15")],
       is_withholding_tax: true,
+      docs: {
+        MASSID: {},
+        PREGNANCY_MATERNITY_FORM: {},
+      },
+      employerResponse: {
+        hours_worked_per_week: 40,
+        employer_decision: "Approve",
+      },
       metadata: {
         postSubmit: "APPROVE",
         amount: 5,
@@ -120,17 +176,25 @@ export const PERF_APRIL_A: ScenarioSpecification = {
     },
   };
 
-  export const PERF_APRIL_H: ScenarioSpecification = {
-    employee: { mass_id: true, wages: "eligible" },
-    claim: {
-      label: "PERF_APRIL_H",
-      work_pattern_spec: "standard",
-      has_continuous_leave_periods: true,
-      reason: "Serious Health Condition - Employee",
-      leave_dates: [parseISO("2022-03-06"), parseISO("2022-04-24")],
-      is_withholding_tax: false,
-      metadata: {
-        amount: 10,
-      },
-    },
-  };
+  // export const PERF_APRIL_H: ScenarioSpecification = {
+  //   employee: { mass_id: true, wages: "eligible" },
+  //   claim: {
+  //     label: "PERF_APRIL_H",
+  //     work_pattern_spec: "standard",
+  //     has_continuous_leave_periods: true,
+  //     reason: "Serious Health Condition - Employee",
+  //     leave_dates: [parseISO("2022-03-06"), parseISO("2022-04-24")],
+  //     is_withholding_tax: false,
+  //     docs: {
+  //       MASSID: {},
+  //       HCP: {}
+  //     },
+  //     employerResponse: {
+  //       hours_worked_per_week: 40,
+  //       employer_decision: "Approve",
+  //     },
+  //     metadata: {
+  //       amount: 10,
+  //     },
+  //   },
+  // };
---
+++ e2e/src/scripts/2022-03-16-PFMLPB-3788-perf-claim-gen.ts
@@ -0,0 +1,42 @@
+import * as scenarios from "../scenarios/2022-03-16-pref";
+import dataDirectory from "../generation/DataDirectory";
+import ClaimPool from "../generation/Claim";
+import EmployeePool from "../generation/Employee";
+import { collect, filter } from "streaming-iterables";
+import path from "path";
+
+(async () => {
+  const dataDir = dataDirectory("2022-03-10-PFMLPB-3720");
+  const employees = await EmployeePool.load(dataDir.employees);
+  const claims = await ClaimPool.load(
+    path.join(dataDir.dir, "claims-1.ndjson"),
+    dataDir.documents
+  );
+
+  const usedSSNS = new Set(
+    ...(await collect(claims)).map((employee) => employee.claim.tax_identifier)
+  );
+  const filteredEmployeePool = new EmployeePool(
+    await collect(
+      await filter((e) => {
+        return !usedSSNS.has(e.ssn);
+      }, employees)
+    )
+  );
+
+  await ClaimPool.load(dataDir.claims, dataDir.documents).orGenerateAndSave(
+    () => {
+      return ClaimPool.merge(
+        ...Object.values(scenarios).map((scenario) => {
+          if (!scenario.claim.metadata?.amount) throw Error("Missing amount");
+          return ClaimPool.generate(
+            filteredEmployeePool,
+            scenario.employee,
+            scenario.claim,
+            scenario.claim.metadata.amount as number
+          );
+        })
+      );
+    }
+  );
+})();
