383807629b7401629e5d2873a57625faa7b84a21
Brice <43508170+aorticweb@users.noreply.github.com>
2022-03-24 06:47:34 -0400

    PORTAL-1310: remove application nickname schema chnages (#7657)
    * add schema changes for application nickname removal
    
    * ERD generation auto-commit

--- api/massgov/pfml/db/models/applications.py
+++ api/massgov/pfml/db/models/applications.py
@@ -28,7 +28,7 @@ from massgov.pfml.rmv.models import RmvAcknowledgement
 from massgov.pfml.util.decimals import round_nearest_hundredth
 
 from ..lookup import LookupTable
-from .base import Base, TimestampMixin, deprecated_column, uuid_gen
+from .base import Base, TimestampMixin, uuid_gen
 from .common import PostgreSQLUUID, StrEnum
 
 # (typed_hybrid_property) https://github.com/dropbox/sqlalchemy-stubs/issues/98
@@ -277,7 +277,6 @@ class Application(Base, TimestampMixin):
     tax_identifier_id = Column(
         PostgreSQLUUID, ForeignKey("tax_identifier.tax_identifier_id"), index=True
     )
-    nickname = deprecated_column(Text)
     requestor = Column(Integer)
     claim_id = Column(PostgreSQLUUID, ForeignKey("claim.claim_id"), nullable=True, unique=True)
     has_mailing_address = Column(Boolean)
