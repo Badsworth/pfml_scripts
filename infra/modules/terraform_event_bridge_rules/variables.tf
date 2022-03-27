2071c645780cd8894e6885c32bcd6dae52da3f15
Paz Tursun <paz@Ps-MBP-2.fios-router.home>
2022-03-27 11:31:59 -0400

    add sns var for SH

--- infra/modules/terraform_event_bridge_rules/variables.tf
+++ infra/modules/terraform_event_bridge_rules/variables.tf
@@ -3,3 +3,9 @@ variable "sns_topic_arn" {
   type        = string
   default     = "arn:aws:sns:us-region-1:123456789012:topic_name"
 }
+
+variable "security_hub_finding_notification_arn" {
+  description = "aws security hub finding sns topic arn"
+  type        = string
+  default     = "arn:aws:sns:us-region-1:123456789012:topic_name"
+}
