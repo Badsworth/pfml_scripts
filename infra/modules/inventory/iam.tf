aa47a0e88a3e1b987a76d60e99869636c76e64b1
jacobitegbojemass <Jacob.Osigbemhe-Itegboje@mass.gov>
2022-03-29 00:46:48 -0500

    format

--- infra/modules/inventory/iam.tf
+++ infra/modules/inventory/iam.tf
@@ -3,27 +3,27 @@ data "aws_iam_policy_document" "auditor_iam_trust_policy" {
     actions = ["sts:AssumeRole"]
     effect  = "Allow"
     principals {
-      type       = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
   }
 }
 
 data "aws_iam_policy_document" "auditor_inventory_policy" {
-    statement {
-      actions = ["dynamodb:PutItem"]
-      effect  = "Allow"
-      resources = ["arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${aws_dynamodb_table.inventory.name}"]
-    }
+  statement {
+    actions   = ["dynamodb:PutItem"]
+    effect    = "Allow"
+    resources = ["arn:aws:dynamodb:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:table/${aws_dynamodb_table.inventory.name}"]
+  }
 }
 
 data "aws_iam_policy_document" "auditor_policy" {
-    for_each = local.auditors
-    statement {
-      actions = each.value["actions"]
-      effect  = "Allow"
-      resources = ["*"]
-    }
+  for_each = local.auditors
+  statement {
+    actions   = each.value["actions"]
+    effect    = "Allow"
+    resources = ["*"]
+  }
 }
 
 resource "aws_iam_role" "audit" {
@@ -33,7 +33,7 @@ resource "aws_iam_role" "audit" {
 }
 
 resource "aws_iam_role_policy_attachment" "audit" {
-    for_each = local.auditors
+  for_each   = local.auditors
   role       = aws_iam_role.audit[each.key].name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
