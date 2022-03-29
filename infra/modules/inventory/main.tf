2e4a6221f12270e4431a9d21524bb1f57a086a65
jacobitegbojemass <Jacob.Osigbemhe-Itegboje@mass.gov>
2022-03-29 14:15:20 -0500

    abstract prefix

--- infra/modules/inventory/main.tf
+++ infra/modules/inventory/main.tf
@@ -5,15 +5,29 @@ data "aws_caller_identity" "current" {}
 
 module "constants" {
   source = "../../constants"
+  prefix = "massgov_pfml_audit_"
 }
 
 locals {
   auditors = jsondecode(file("auditors.json"))
 }
 
+resource "aws_cloudwatch_event_rule" "audit" {
+  for_each = auditors
+  name          = "${local.prefix}${each.key}"
+  description   = "Notify when an EC2 instance changes state"
+  event_pattern = <<EOF
+{
+  "detail-type": [
+    "EC2 Instance State-change Notification"
+  ]
+}
+EOF
+}
+
 resource "aws_dynamodb_table" "inventory" {
   for_each     = local.auditors
-  name         = "massgov_pfml_audit_${each.key}"
+  name         = "${local.prefix}${each.key}"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "ResourceName"
 
@@ -22,7 +36,7 @@ resource "aws_dynamodb_table" "inventory" {
     type = "S"
   }
 
-  tags = merge({ "Name" = "massgov_pfml_audit_${each.key}" }, module.constants.common_tags)
+  tags = merge({ "Name" = "${local.prefix}${each.key}" }, module.constants.common_tags)
 }
 
 data "archive_file" "audit" {
@@ -38,8 +52,8 @@ resource "aws_lambda_function" "audit" {
   description      = each.key
   filename         = data.archive_file.audit[each.key].output_path
   source_code_hash = data.archive_file.audit[each.key].output_base64sha256
-  function_name    = "massgov_pfml_audit_${each.key}"
-  handler          = "massgov_pfml_audit_${each.key}.handler"
+  function_name    = "${local.prefix}${each.key}"
+  handler          = "${local.prefix}${each.key}.handler"
   role             = aws_iam_role.audit[each.key].arn
   runtime          = "python3.9"
 
