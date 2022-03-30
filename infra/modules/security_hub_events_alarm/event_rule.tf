4461e046c2ec66f1ddbca31271d55b6df032a136
Paz Tursun <paz@Ps-MBP-2.fios-router.home>
2022-03-30 10:42:38 -0400

    formatting

--- infra/modules/security_hub_events_alarm/event_rule.tf
+++ infra/modules/security_hub_events_alarm/event_rule.tf
@@ -2,9 +2,9 @@
 ### Security Hub Findings ###
 
 resource "aws_cloudwatch_event_rule" "security_hub_finding" {
-  name = "alert-on-securityhub-finding"
-  description = "A CloudWatch Event Rule that triggers on AWS Security Hub findings. The Event Rule triggers an email notifications"
-  is_enabled = true
+  name          = "alert-on-securityhub-finding"
+  description   = "A CloudWatch Event Rule that triggers on AWS Security Hub findings. The Event Rule triggers an email notifications"
+  is_enabled    = true
   event_pattern = <<PATTERN
     {
       "source": ["aws.securityhub"],
@@ -29,7 +29,7 @@ resource "aws_cloudwatch_event_rule" "security_hub_finding" {
 }
 
 resource "aws_cloudwatch_event_target" "security_hub_finding_sns" {
-  rule = aws_cloudwatch_event_rule.security_hub_finding.name
+  rule      = aws_cloudwatch_event_rule.security_hub_finding.name
   target_id = "notify_sns_on_security_hub_finding"
-  arn = var.security_hub_finding_notification_arn
+  arn       = var.security_hub_finding_notification_arn
 }
