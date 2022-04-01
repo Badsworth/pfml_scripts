cea68403bdfcb6ff2fc789debca48b927293428b
Libbey White <libbeywhite@navapbc.com>
2022-04-01 13:05:32 -0700

    Fix prerender error in build process

--- portal/src/pages/employers/applications/new-application.tsx
+++ portal/src/pages/employers/applications/new-application.tsx
@@ -1,17 +1,13 @@
 import { AppLogic } from "../../../hooks/useAppLogic";
+import { useEffect } from "react";
 
-interface PageProps {
-  appLogic: AppLogic;
-}
-
-export const NewApplication = (props: PageProps) => {
+export default function NewApplication({ appLogic }: { appLogic: AppLogic }) {
   /**
    * This page is deprecated, but was previously linked to in email notifications to leave admins,
    * so we want to preserve the URL and redirect to the new destination.
    */
-  props.appLogic.portalFlow.goToPageFor("REDIRECT", {}, {}, { redirect: true });
-
+  useEffect(() => {
+    appLogic.portalFlow.goToPageFor("REDIRECT", {}, {}, { redirect: true });
+  });
   return null;
-};
-
-export default NewApplication;
+}
