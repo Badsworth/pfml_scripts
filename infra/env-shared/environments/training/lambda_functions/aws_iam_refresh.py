36b7e9d865dbf4a013c27ca6f1ed027d4adb9ffe
wilfred okoro <wilfred.okoro@mass.gov>
2022-04-07 15:33:39 -0700

    add repository dispatch

--- infra/env-shared/environments/training/lambda_functions/aws_iam_refresh.py
+++ infra/env-shared/environments/training/lambda_functions/aws_iam_refresh.py
@@ -1,39 +1,27 @@
 import boto3
 import github
-import os
-import requests
 
-# SSM = boto3.client('ssm', region_name="us-east-1")
-# RDS_IAM_REFRESH = os.environ["RDS_IAM_REFRESH"]
+SSM = boto3.client('ssm', region_name="us-east-1")
 
-# headers = {'Authorization': "Bearer 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"}
-# requests.get('https://api.github.com/', headers=headers)
-# response = requests
-# print(response)
-
-def get_parameter():
+def get_github_token():
     return SSM.get_parameter(
-        Name='RDS_IAM_REFRESH',
-        WithDecryption=True | False
+        Name='/service/pfml-api/rds-iam-sync',
+        WithDecryption=True
+    )['Parameter']['Value']
+
+def trigger_rds_iam_sync():
+    github_client = github.Github(
+        login_or_token=get_github_token()
     )
-
-def github_token():
-    return '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'
-
-def trigger_github_action(environment='training'):
-    github_client = github.Github(login_or_token=github_token())
     eolwd = github_client.get_repo('EOLWD/pfml')
     result = eolwd.create_repository_dispatch(
-        event_type='rds_iam_refresh',
-        # client_payload={
-        #     'environment': environment
-        # },
+        event_type='rds_iam_sync',
+
     )
     print(result)
 
 def lambda_handler(event, context=None):
-    trigger_github_action()
-
+    trigger_rds_iam_sync()
 
 if __name__ == "__main__":
-    lambda_handler({'environment': 'infra-test'})
+    lambda_handler(None)
