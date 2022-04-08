78b0bbb189557489998bcdbdb66e9224163ab39d
Sheldon Bachstein <sheldon@navapbc.com>
2022-04-08 09:33:58 -0400

    PORTAL-2018: Flesh out modification success page (#7994)
    ## 🎫 Ticket
    
    https://lwd.atlassian.net/browse/PORTAL-2018
    
    ## 🛠 Changes
    
    * Updates the content of /applications/modify/success to match the figma design
    
    ## ℹ️ Context for reviewers
    
    N/A
    
    ## 👀 Demo
    
    ![localhost_3000_applications_modify_success__claim_id=89d8cd57-a3e5-416f-903f-38279f78e801](https://user-images.githubusercontent.com/9039672/162263484-bc3f430e-36d3-42bd-8c30-9fbc49474493.png)
    
    ## 📜 Link to relevant documentation
    
    N/A

--- portal/src/pages/applications/modify/success.tsx
+++ portal/src/pages/applications/modify/success.tsx
@@ -2,31 +2,30 @@ import withBenefitsApplication, {
   WithBenefitsApplicationProps,
 } from "../../../hoc/withBenefitsApplication";
 import BackButton from "../../../components/BackButton";
+import Heading from "../../../components/core/Heading";
+import { IconCalendar } from "@massds/mayflower-react/dist/Icon";
 import PageNotFound from "../../../components/PageNotFound";
 import React from "react";
 import Title from "../../../components/core/Title";
+import { Trans } from "react-i18next";
 import { isFeatureEnabled } from "../../../services/featureFlags";
+import routeWithParams from "../../../utils/routeWithParams";
 import routes from "../../../routes";
 import { useTranslation } from "../../../locales/i18n";
 
-type SuccessProps = WithBenefitsApplicationProps & {
-  query: {
-    claim_id: string;
-  };
-};
-
-export const Success = (props: SuccessProps) => {
-  const claimId = props.query.claim_id;
+export const Success = (props: WithBenefitsApplicationProps) => {
+  const { claim } = props;
   const { t } = useTranslation();
-
-  /* eslint-disable require-await */
-  const handleSubmit = async (event: React.FormEvent) => {
-    event.preventDefault();
-    // Do nothing for now
-  };
 
   // TODO(PORTAL-2064): Remove claimantShowModifications feature flag
   if (!isFeatureEnabled("claimantShowModifications")) return <PageNotFound />;
+
+  const iconProps = {
+    className: "margin-right-2 text-secondary text-middle",
+    height: 30,
+    width: 30,
+    fill: "currentColor",
+  };
 
   return (
     <React.Fragment>
@@ -34,10 +33,29 @@ export const Success = (props: SuccessProps) => {
         label={t("pages.claimsModifySuccess.backButtonLabel")}
         href={routes.applications.index}
       />
-      <form onSubmit={handleSubmit} className="usa-form">
-        <Title>{t("pages.claimsModifySuccess.title")}</Title>
-        <p>{claimId}</p>
-      </form>
+      <Title>{t("pages.claimsModifySuccess.title")}</Title>
+      <Heading level="2">
+        <IconCalendar {...iconProps} />
+        {t("pages.claimsModifySuccess.adjudicationProcessHeading")}
+      </Heading>
+
+      <Trans
+        i18nKey="pages.claimsModifySuccess.adjudicationProcess"
+        components={{
+          "contact-center-phone-link": (
+            <a href={`tel:${t("shared.contactCenterPhoneNumber")}`} />
+          ),
+          "track-status-link": (
+            <a
+              href={routeWithParams("applications.status.claim", {
+                absence_id: claim.fineos_absence_id,
+              })}
+            />
+          ),
+          ul: <ul className="usa-list" />,
+          li: <li />,
+        }}
+      />
     </React.Fragment>
   );
 };
