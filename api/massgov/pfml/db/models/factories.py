8f7283cc14c9c3f0024fb3487c7aade6d0460b87
Kevin Boyer <kevinboyer@navapbc.com>
2022-04-11 14:54:08 -0400

    API-2664: Remove FINEOS C & I values from payments API response (#8015)

--- api/massgov/pfml/db/models/factories.py
+++ api/massgov/pfml/db/models/factories.py
@@ -561,7 +561,7 @@ class PaymentDetailsFactory(BaseFactory):
         "date_between_dates", date_start=date(2021, 1, 16), date_end=date(2021, 1, 28)
     )
 
-    payment_details_c_value = factory.Sequence(lambda n: "%d" % n)
+    payment_details_c_value = "7806"
     payment_details_i_value = factory.Sequence(lambda n: "%d" % n)
 
     amount = Generators.Money
