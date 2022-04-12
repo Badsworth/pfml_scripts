65514e08e270f87f6adbc54b4c9ed27bc59e2ebd
Kevin Boyer <kevinboyer@navapbc.com>
2022-04-12 16:01:08 -0400

    refactor tests into test class and add additional coverage

--- api/tests/api/services/test_payments.py
+++ api/tests/api/services/test_payments.py
@@ -513,76 +513,98 @@ def test_consolidate_successors_cancelled_and_regular(test_db_session):
     assert original_payment2.payment_id == consolidated_payment_containers[0].payment.payment_id
 
 
-def test_get_payments_with_details_and_lines(test_db_session):
-    payment_factory_1 = DelegatedPaymentFactory(
-        test_db_session,
-        amount=100,
-        period_start_date=date(2021, 11, 1),
-        period_end_date=date(2021, 11, 7),
-        payment_method=PaymentMethod.ACH,
-    )
-    claim_1 = payment_factory_1.get_or_create_claim()
-    payment_factory_1.get_or_create_payment()
-    payment_1_details_1 = payment_factory_1.create_payment_detail()
-    payment_1_details_1_line_1 = payment_factory_1.create_payment_line(
-        payment_detail=payment_1_details_1
-    )
-    payment_1_details_2 = payment_factory_1.create_payment_detail()
+class TestGetPaymentDetailsAndLines:
+    @pytest.fixture
+    def claim_with_multiple_details(self, test_db_session):
+        payment_factory = DelegatedPaymentFactory(
+            test_db_session,
+            amount=100,
+            period_start_date=date(2021, 11, 1),
+            period_end_date=date(2021, 11, 7),
+            payment_method=PaymentMethod.ACH,
+        )
+        claim = payment_factory.get_or_create_claim()
+        payment_factory.get_or_create_payment()
 
-    payment_factory_2 = DelegatedPaymentFactory(
-        test_db_session,
-        amount=200,
-        period_start_date=date(2020, 11, 1),
-        period_end_date=date(2020, 11, 7),
-        payment_method=PaymentMethod.ACH,
-    )
-    payment_factory_2.get_or_create_payment()
-    claim_2 = payment_factory_2.get_or_create_claim()
-    payment_2_details = payment_factory_2.create_payment_detail()
-    payment_2_details_line_1 = payment_factory_2.create_payment_line(
-        payment_detail=payment_2_details
-    )
-    payment_2_details_line_2 = payment_factory_2.create_payment_line(
-        payment_detail=payment_2_details
-    )
+        payment_details_1 = payment_factory.create_payment_detail(
+            amount=25, period_start_date=date(2021, 3, 1)
+        )
+        payment_factory.create_payment_line(payment_detail=payment_details_1, amount=25)
+        payment_factory.create_payment_detail(amount=75, period_start_date=date(2021, 1, 1))
 
-    claim_1_response = payment_services.get_payments_with_status(test_db_session, claim_1)
-    assert len(claim_1_response["payments"]) == 1
-    assert len(claim_1_response["payments"][0]["payment_details"]) == 2
-    assert len(claim_1_response["payments"][0]["payment_details"][0]["payment_lines"]) == 1
-    assert len(claim_1_response["payments"][0]["payment_details"][1]["payment_lines"]) == 0
+        return claim
 
-    assert (
-        claim_1_response["payments"][0]["payment_details"][0]["payment_details_id"]
-        == payment_1_details_1.payment_details_id
-    )
-    assert (
-        claim_1_response["payments"][0]["payment_details"][1]["payment_details_id"]
-        == payment_1_details_2.payment_details_id
-    )
+    @pytest.fixture
+    def claim_with_multiple_lines(self, test_db_session):
+        payment_factory = DelegatedPaymentFactory(
+            test_db_session,
+            amount=200,
+            period_start_date=date(2020, 11, 1),
+            period_end_date=date(2020, 11, 7),
+            payment_method=PaymentMethod.ACH,
+        )
+        payment_factory.get_or_create_payment()
+        claim = payment_factory.get_or_create_claim()
 
-    assert (
-        claim_1_response["payments"][0]["payment_details"][0]["payment_lines"][0]["payment_line_id"]
-        == payment_1_details_1_line_1.payment_line_id
-    )
+        payment_details = payment_factory.create_payment_detail(amount=100)
+        payment_factory.create_payment_line(
+            payment_detail=payment_details, amount=50, line_type="Positive Adjustment"
+        )
+        payment_factory.create_payment_line(
+            payment_detail=payment_details, amount=-15, line_type="State Income Tax"
+        )
+        payment_factory.create_payment_line(
+            payment_detail=payment_details, amount=-25, line_type="FIT Amount"
+        )
+        payment_factory.create_payment_line(
+            payment_detail=payment_details, amount=-35, line_type="Maximum Threshold Adjustment"
+        )
+        payment_factory.create_payment_line(
+            payment_detail=payment_details, amount=125, line_type="Auto Gross Entitlement"
+        )
 
-    claim_2_response = payment_services.get_payments_with_status(test_db_session, claim_2)
-    assert len(claim_2_response["payments"]) == 1
-    assert len(claim_2_response["payments"][0]["payment_details"]) == 1
-    assert len(claim_2_response["payments"][0]["payment_details"][0]["payment_lines"]) == 2
+        return claim
 
-    assert (
-        claim_2_response["payments"][0]["payment_details"][0]["payment_details_id"]
-        == payment_2_details.payment_details_id
-    )
-    assert (
-        claim_2_response["payments"][0]["payment_details"][0]["payment_lines"][0]["payment_line_id"]
-        == payment_2_details_line_1.payment_line_id
-    )
-    assert (
-        claim_2_response["payments"][0]["payment_details"][0]["payment_lines"][1]["payment_line_id"]
-        == payment_2_details_line_2.payment_line_id
-    )
+    def test_multiple_details(self, test_db_session, claim_with_multiple_details):
+        claim_response = payment_services.get_payments_with_status(
+            test_db_session, claim_with_multiple_details
+        )
+
+        # Confirm that the expected number of entities is returned
+        assert len(claim_response["payments"]) == 1
+        assert len(claim_response["payments"][0]["payment_details"]) == 2
+        assert len(claim_response["payments"][0]["payment_details"][0]["payment_lines"]) == 0
+        assert len(claim_response["payments"][0]["payment_details"][1]["payment_lines"]) == 1
+
+        # Confirm the PaymentDetails are in the expected order
+        assert claim_response["payments"][0]["payment_details"][0]["amount"] == 75
+        assert claim_response["payments"][0]["payment_details"][1]["amount"] == 25
+
+        # Confirm the PaymentLine is associated with the correct PaymentDetails
+        assert (
+            claim_response["payments"][0]["payment_details"][1]["payment_lines"][0]["amount"] == 25
+        )
+
+    def test_multiple_lines(self, test_db_session, claim_with_multiple_lines):
+        claim_response = payment_services.get_payments_with_status(
+            test_db_session, claim_with_multiple_lines
+        )
+
+        # Confirm that the expected number of entities is returned
+        assert len(claim_response["payments"]) == 1
+        assert len(claim_response["payments"][0]["payment_details"]) == 1
+        assert len(claim_response["payments"][0]["payment_details"][0]["payment_lines"]) == 4
+
+        payment_lines = claim_response["payments"][0]["payment_details"][0]["payment_lines"]
+
+        # Confirm that Auto Gross Entitlement and Max Threshold Adjustment are combined
+        assert payment_lines[0]["line_type"] == "Auto Gross Entitlement"
+        assert payment_lines[0]["amount"] == 125 + -35
+
+        # Confirm that payment lines are in the expected order
+        assert payment_lines[1]["amount"] == 50
+        assert payment_lines[2]["amount"] == -25
+        assert payment_lines[3]["amount"] == -15
 
 
 def test_get_payments_with_status(test_db_session, caplog):
