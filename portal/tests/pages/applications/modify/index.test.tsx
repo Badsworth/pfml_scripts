4b0834ea90606aa495d39573c18cc9ca27f0d368
Andrew Lomax <andrewlomaxr@gmail.com>
2022-04-13 09:18:54 -0400

    update snap

--- portal/tests/pages/applications/modify/index.test.tsx
+++ portal/tests/pages/applications/modify/index.test.tsx
@@ -1,16 +1,11 @@
-import {
-  MockBenefitsApplicationBuilder,
-  renderPage,
-} from "../../../test-utils";
-import ApiResourceCollection from "src/models/ApiResourceCollection";
-import BenefitsApplication from "src/models/BenefitsApplication";
-import Index from "src/pages/applications/modify/index";
+import Index from "../../../../src/pages/applications/modify/index";
+import { renderPage } from "../../../test-utils";
 import { screen } from "@testing-library/react";
 
 jest.mock("../../../../src/hooks/useAppLogic");
 
 const props = {
-  query: { claim_id: "7180eae0-0ad8-46a9-b140-5076863330d2" },
+  query: { absence_id: "7180eae0-0ad8-46a9-b140-5076863330d2" },
 };
 
 beforeEach(() => {
@@ -22,7 +17,7 @@ beforeEach(() => {
 describe("Claim Modification Index", () => {
   it("renders page content", () => {
     const { container } = renderPage(Index, {}, props);
-    expect(container.firstChild).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 
   it("renders PageNotFound if the claimantShowModifications feature flag is not set", () => {
