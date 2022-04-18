308cf47e6526baa0fb5a8fedc3404d948419ef13
wilfred okoro <wilfred.okoro@mass.gov>
2022-04-17 17:04:39 -0700

    change source_dir

--- infra/tooling/trigger_rds_iam_sync/main.tf
+++ infra/tooling/trigger_rds_iam_sync/main.tf
@@ -3,9 +3,11 @@ locals {
 }
 data "archive_file" "trigger_rds_iam_sync" {
   type        = "zip"
-  output_path = "../lambda_functions/${local.name}/${local.name}.zip"
-  source_dir  = "../lambda_functions/${local.name}"
-  excludes    = ["../lambda_functions/${local.name}/${local.name}.zip"]
+  output_path = "${path.module}/lambda_functions/${local.name}/${local.name}.zip"
+  source {
+    filename = "${local.name}.py"
+    content  = file("${path.module}/${local.name}.py")
+  }
 }
 resource "aws_lambda_function" "trigger_rds_iam_sync" {
   description      = "Trigger RDS IAM Sync GitHub Action"
