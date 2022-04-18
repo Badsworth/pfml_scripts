8375b98eba141a583124b9923cf30eb0a9c4cb56
wilfred okoro <wilfred.okoro@mass.gov>
2022-04-18 09:05:16 -0700

    tf fmt recursive

--- infra/tooling/trigger_rds_iam_sync/main.tf
+++ infra/tooling/trigger_rds_iam_sync/main.tf
@@ -5,7 +5,7 @@ data "archive_file" "trigger_rds_iam_sync" {
   type        = "zip"
   output_path = "lambda_functions/${local.name}/${local.name}.zip"
   source_file = "lambda_functions/${local.name}/${local.name}.py"
-  }
+}
 resource "aws_lambda_function" "trigger_rds_iam_sync" {
   description      = "Trigger RDS IAM Sync GitHub Action"
   filename         = data.archive_file.trigger_rds_iam_sync.output_path
